b3a141753f9331306704818c1b7b806ae8836834
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-22 06:58:39 +0100

    New exercise: python nth-prime

---
+++ nth-prime/example.py
@@ -0,0 +1,31 @@
+from itertools import count
+from math import sqrt
+
+
+def nth_prime(n):
+    known = []
+    candidates = prime_candidates()
+
+    def is_prime(m):
+        sqrt_m = sqrt(m)
+        for k in known:
+            if k > sqrt_m:
+                return True
+            elif m % k == 0:
+                return False
+        return True
+
+    while len(known) < n:
+        x = next(candidates)
+        if is_prime(x):
+            known.append(x)
+
+    return known[n - 1]
+
+
+def prime_candidates():
+    yield 2
+    yield 3
+    for n in count(6, 6):
+        yield n - 1
+        yield n + 1
