55126db9e0d434873b766a63e0a01a1845a2776c
Hermano Cabral <hcabral00@gmail.com>
2014-03-28 22:19:26 -0300

    New exercise: minesweeper

---
+++ minesweeper/minesweeper_test.py
@@ -0,0 +1,91 @@
+try:
+    from minesweeper import board
+except ImportError:
+    raise SystemExit('Could not find minesweeper.py. Does it exist?')
+
+import unittest
+
+class MinesweeperTest(unittest.TestCase):
+    def test_board1(self):
+        inp = ["+------+", "| *  * |", "|  *   |", "|    * |", "|   * *|",
+               "| *  * |", "|      |", "+------+"]
+        out = ["+------+", "|1*22*1|", "|12*322|", "| 123*2|", "|112*4*|",
+               "|1*22*2|", "|111111|", "+------+"]
+        self.assertEqual(out, board(inp))
+
+    @unittest.skip("Not implemented yet")
+    def test_board2(self):
+        inp = ["+-----+", "| * * |", "|     |", "|   * |", "|  * *|",
+               "| * * |", "+-----+"]
+        out = ["+-----+", "|1*2*1|", "|11322|", "| 12*2|", "|12*4*|",
+               "|1*3*2|", "+-----+"]
+        self.assertEqual(out, board(inp))
+
+    @unittest.skip("Not implemented yet")
+    def test_board3(self):
+        inp = ["+-----+", "| * * |", "+-----+"]
+        out = ["+-----+", "|1*2*1|", "+-----+"]
+        self.assertEqual(out, board(inp))
+
+    @unittest.skip("Not implemented yet")
+    def test_board4(self):
+        inp = ["+-+", "|*|", "| |", "|*|", "| |", "| |", "+-+"]
+        out = ["+-+", "|*|", "|2|", "|*|", "|1|", "| |", "+-+"]
+        self.assertEqual(out, board(inp))
+
+    @unittest.skip("Not implemented yet")
+    def test_board5(self):
+        inp = ["+-+", "|*|", "+-+"]
+        out = ["+-+", "|*|", "+-+"]
+        self.assertEqual(out, board(inp))
+
+    @unittest.skip("Not implemented yet")
+    def test_board6(self):
+        inp = ["+--+", "|**|", "|**|", "+--+"]
+        out = ["+--+", "|**|", "|**|", "+--+"]
+        self.assertEqual(out, board(inp))
+
+    @unittest.skip("Not implemented yet")
+    def test_board7(self):
+        inp = ["+--+", "|**|", "|**|", "+--+"]
+        out = ["+--+", "|**|", "|**|", "+--+"]
+        self.assertEqual(out, board(inp))
+
+    @unittest.skip("Not implemented yet")
+    def test_board8(self):
+        inp = ["+---+", "|***|", "|* *|", "|***|", "+---+"]
+        out = ["+---+", "|***|", "|*8*|", "|***|", "+---+"]
+        self.assertEqual(out, board(inp))
+
+    @unittest.skip("Not implemented yet")
+    def test_board9(self):
+        inp = ["+-----+", "|     |", "|   * |", "|     |", "|     |",
+               "| *   |", "+-----+"]
+        out = ["+-----+", "|  111|", "|  1*1|", "|  111|", "|111  |",
+               "|1*1  |", "+-----+"]
+        self.assertEqual(out, board(inp))
+
+    @unittest.skip("Not implemented yet")
+    def test_different_len(self):
+        inp = ["+-+", "| |", "|*  |", "|  |", "+-+"]
+        with self.assertRaises(ValueError) as context:
+            board(inp)
+        self.assertEqual(context.exception.message, 'Invalid board')
+
+    @unittest.skip("Not implemented yet")
+    def test_faulty_border(self):
+        inp = ["+-----+", "*   * |", "+-- --+"]
+        with self.assertRaises(ValueError) as context:
+            board(inp)
+        self.assertEqual(context.exception.message, 'Invalid board')
+
+    @unittest.skip("Not implemented yet")
+    def test_invalid_char(self):
+        inp = ["+-----+", "|X  * |", "+-----+"]
+        with self.assertRaises(ValueError) as context:
+            board(inp)
+        self.assertEqual(context.exception.message, 'Invalid board')
+
+
+if __name__ == '__main__':
+    unittest.main()
