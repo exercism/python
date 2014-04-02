5632ee66d70c25fbf30a7b25de5f578d333a96a9
Hermano Cabral <hcabral00@gmail.com>
2014-04-02 07:29:54 -0300

    New exercise: saddle-points

---
+++ saddle-points/example.py
@@ -0,0 +1,10 @@
+def saddle_points(m):
+    if not m:
+        return []
+    if any(len(r)!=len(m[0]) for r in m):
+        raise ValueError('Matrix with uneven rows')
+    mmax = [max(r) for r in m]
+    mmin = [min(c) for c in zip(*m)]
+    points = [(i,j) for i in range(len(m)) for j in range(len(m[0])) if mmax[i] == mmin[j]]
+    
+    return set(points)
