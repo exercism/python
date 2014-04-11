26dec9503797d96084286646c8fbbbf6b7b99b6e
Hermano Cabral <hcabral00@gmail.com>
2014-04-11 00:37:19 -0300

    New exercise: house

---
+++ house/house_test.py
@@ -0,0 +1,92 @@
+# Rhyme found in http://www.pitt.edu/~dash/type2035.html
+
+from house import rhyme
+
+import unittest
+
+
+class VerseTest(unittest.TestCase):
+    def test_rhyme(self):
+        expected = 'This is the house that Jack built.\n\n'\
+                   'This is the malt\n'\
+                   'that lay in the house that Jack built.\n\n'\
+                   'This is the rat\n'\
+                   'that ate the malt\n'\
+                   'that lay in the house that Jack built.\n\n'\
+                   'This is the cat\n'\
+                   'that killed the rat\n'\
+                   'that ate the malt\n'\
+                   'that lay in the house that Jack built.\n\n'\
+                   'This is the dog\n'\
+                   'that worried the cat\n'\
+                   'that killed the rat\n'\
+                   'that ate the malt\n'\
+                   'that lay in the house that Jack built.\n\n'\
+                   'This is the cow with the crumpled horn\n'\
+                   'that tossed the dog\n'\
+                   'that worried the cat\n'\
+                   'that killed the rat\n'\
+                   'that ate the malt\n'\
+                   'that lay in the house that Jack built.\n\n'\
+                   'This is the maiden all forlorn\n'\
+                   'that milked the cow with the crumpled horn\n'\
+                   'that tossed the dog\n'\
+                   'that worried the cat\n'\
+                   'that killed the rat\n'\
+                   'that ate the malt\n'\
+                   'that lay in the house that Jack built.\n\n'\
+                   'This is the man all tattered and torn\n'\
+                   'that kissed the maiden all forlorn\n'\
+                   'that milked the cow with the crumpled horn\n'\
+                   'that tossed the dog\n'\
+                   'that worried the cat\n'\
+                   'that killed the rat\n'\
+                   'that ate the malt\n'\
+                   'that lay in the house that Jack built.\n\n'\
+                   'This is the priest all shaven and shorn\n'\
+                   'that married the man all tattered and torn\n'\
+                   'that kissed the maiden all forlorn\n'\
+                   'that milked the cow with the crumpled horn\n'\
+                   'that tossed the dog\n'\
+                   'that worried the cat\n'\
+                   'that killed the rat\n'\
+                   'that ate the malt\n'\
+                   'that lay in the house that Jack built.\n\n'\
+                   'This is the rooster that crowed in the morn\n'\
+                   'that woke the priest all shaven and shorn\n'\
+                   'that married the man all tattered and torn\n'\
+                   'that kissed the maiden all forlorn\n'\
+                   'that milked the cow with the crumpled horn\n'\
+                   'that tossed the dog\n'\
+                   'that worried the cat\n'\
+                   'that killed the rat\n'\
+                   'that ate the malt\n'\
+                   'that lay in the house that Jack built.\n\n'\
+                   'This is the farmer sowing his corn\n'\
+                   'that kept the rooster that crowed in the morn\n'\
+                   'that woke the priest all shaven and shorn\n'\
+                   'that married the man all tattered and torn\n'\
+                   'that kissed the maiden all forlorn\n'\
+                   'that milked the cow with the crumpled horn\n'\
+                   'that tossed the dog\n'\
+                   'that worried the cat\n'\
+                   'that killed the rat\n'\
+                   'that ate the malt\n'\
+                   'that lay in the house that Jack built.\n\n'\
+                   'This is the horse and the hound and the horn\n'\
+                   'that belonged to the farmer sowing his corn\n'\
+                   'that kept the rooster that crowed in the morn\n'\
+                   'that woke the priest all shaven and shorn\n'\
+                   'that married the man all tattered and torn\n'\
+                   'that kissed the maiden all forlorn\n'\
+                   'that milked the cow with the crumpled horn\n'\
+                   'that tossed the dog\n'\
+                   'that worried the cat\n'\
+                   'that killed the rat\n'\
+                   'that ate the malt\n'\
+                   'that lay in the house that Jack built.'
+        self.assertEqual(expected, rhyme())
+
+
+if __name__ == '__main__':
+    unittest.main()
