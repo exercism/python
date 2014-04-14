22516d4519fdcc2a7434dc17be587ec87e1f7047
Simon Jakobi <simon.jakobi@gmail.com>
2014-04-15 00:09:01 +0200

    Add zebra-puzzle exercise

---
+++ zebra-puzzle/example.py
@@ -0,0 +1,44 @@
+"""This solution for the Zebra Puzzle is adapted from a solution
+by Peter Norvig for the course "Design of Computer Programs" on Udacity.
+https://www.udacity.com/course/cs212
+"""
+
+from itertools import permutations
+
+
+def just_right_of(x, y):
+    return x - y == 1
+
+
+def next_to(x, y):
+    return abs(x - y) == 1
+
+
+def solution():
+    houses = first, _, middle, _, _ = range(5)
+    orderings = list(permutations(houses))
+    result = next([{Englishman: "Englishman", Spaniard: "Spaniard",
+                    Ukranian: "Ukranian", Japanese: "Japanese",
+                    Norwegian: "Norwegian"}[x]
+                   for x in (water, zebra)]
+                  for (red, green, ivory, yellow, blue) in orderings
+                  if just_right_of(green, ivory)
+                  for (Englishman, Spaniard, Ukranian, Japanese, Norwegian) in orderings
+                  if Englishman is red
+                  if Norwegian is first
+                  if next_to(Norwegian, blue)
+                  for (coffee, tea, milk, oj, water) in orderings
+                  if coffee is green
+                  if Ukranian is tea
+                  if milk is middle
+                  for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings
+                  if Kools is yellow
+                  if LuckyStrike is oj
+                  if Japanese is Parliaments
+                  for (dog, snails, fox, horse, zebra) in orderings
+                  if Spaniard is dog
+                  if OldGold is snails
+                  if next_to(Chesterfields, fox)
+                  if next_to(Kools, horse))
+    return ("It is the {} who drinks the water.\n"
+            "The {} keeps the zebra.").format(*result)
---
+++ zebra-puzzle/zebra_puzzle_test.py
@@ -0,0 +1,13 @@
+import unittest
+from zebra_puzzle import solution
+
+
+class ZebraPuzzleTest(unittest.TestCase):
+    def test_solution(self):
+        self.assertEqual(("It is the Norwegian who drinks the water.\n"
+                          "The Japanese keeps the zebra."),
+                         solution())
+
+
+if __name__ == '__main__':
+    unittest.main()
