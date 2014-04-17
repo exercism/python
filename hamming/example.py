6c9ffe9ed4c2e30f3415b5c250ee54d89a44f8e6
Simon Jakobi <simon.jakobi@gmail.com>
2014-04-17 18:29:24 +0200

    Make hamming exercise compatible

--- hamming/example.py
+++ hamming/example.py
@@ -1,2 +1,10 @@
-def hamming(s1,s2):
-    return sum(1 for a,b in map(None,s1,s2) if a != b)
+import sys
+
+if sys.version_info[0] == 2:
+    from itertools import izip_longest as zip_longest
+else:
+    from itertools import zip_longest
+
+
+def hamming(s1, s2):
+    return sum(1 for a, b in zip_longest(s1, s2) if a != b)
