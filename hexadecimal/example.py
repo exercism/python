0bc09f206d99a191abb606e64d34a74a92576cd7
Simon Jakobi <simon.jakobi@gmail.com>
2014-04-18 11:21:41 +0200

    Make hexadecimal exercise compatible with Python3

--- hexadecimal/example.py
+++ hexadecimal/example.py
@@ -1,12 +1,10 @@
+from functools import reduce
+
+
 def hexa(s):
     s = s.lower()
     if set(s) - set('0123456789abcdef'):
         raise ValueError('Invalid hexadecimal string')
     l = [ord(c) - ord('a') + 10 if c in 'abcdef' else ord(c) - ord('0')
-            for c in s]
-    return reduce(lambda x,y:x*16 + y, l, 0)
-
-if __name__ == '__main__':
-    print hexa('19ACE')
-    print hexa('100')
-    print hexa('dead')
+         for c in s]
+    return reduce(lambda x, y: x * 16 + y, l, 0)
