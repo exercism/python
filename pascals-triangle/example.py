6a6d4e8348742f7a4240e9192e59ed52f00f84e9
ikkebr <ikkibr@gmail.com>
2014-04-20 14:45:29 -0300

    Updated example.py
    Changed xrange to range and added a conversion to int in line 11

--- pascals-triangle/example.py
+++ pascals-triangle/example.py
@@ -1,5 +1,5 @@
 def triangle(nth):
-    return [row(i) for i in xrange(nth+1)]
+    return [row(i) for i in range(nth+1)]
 
 def is_triangle(t):
     new_t = triangle(len(t)-1)
@@ -7,6 +7,6 @@ def is_triangle(t):
 
 def row(nth):
     r = [1]
-    for i in xrange(1,nth+1):
-        r.append(r[-1]*(nth-i+1)/i)
+    for i in range(1,nth+1):
+        r.append(int(r[-1]*(nth-i+1)/i))
     return " ".join([str(i) for i in r])
