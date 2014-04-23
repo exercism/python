ea4d2509873f897a52b67b1b06d9ea1baf42df5c
Ben Lewis <bennlewis@gmail.com>
2014-04-22 21:58:09 -0600

    Add missing leap tests

--- leap/leap_test.py
+++ leap/leap_test.py
@@ -10,11 +10,18 @@ class YearTest(unittest.TestCase):
     def test_leap_year(self):
         self.assertTrue(Year(1996).is_leap_year())
 
-    def test_any_old_year(self):
+    def test_non_leap_year(self):
         self.assertFalse(Year(1997).is_leap_year())
+
+    def test_non_leap_even_year(self):
+        self.assertFalse(Year(1998).is_leap_year())
+
+    def test_century(self):
+        self.assertFalse(Year(1900).is_leap_year())
 
     def test_exceptional_century(self):
         self.assertTrue(Year(2400).is_leap_year())
 
 if __name__ == '__main__':
     unittest.main()
+
