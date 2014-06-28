db9da8b35639cd0b6df0a51b62e3c142448d10c2
Simon Jakobi <simon.jakobi@gmail.com>
2014-06-28 14:46:01 +0200

    series: Adopt functional style
    As discussed in #79.

--- series/example.py
+++ series/example.py
@@ -1,11 +1,7 @@
-class Series(object):
-    def __init__(self, digits):
-        self.digits = digits
-        self.numbers = [int(d) for d in digits]
-
-    def slices(self, length):
-        if not 1 <= length <= len(self.numbers):
-            raise ValueError("Invalid slice length for this series: " +
-                             str(length))
-        return [self.numbers[i:i + length]
-                for i in range(len(self.numbers) - length + 1)]
+def slices(series, length):
+    numbers = [int(digit) for digit in series]
+    if not 1 <= length <= len(numbers):
+        raise ValueError("Invalid slice length for this series: "
+                         + str(length))
+    return [numbers[i:i + length]
+            for i in range(len(numbers) - length + 1)]
--- series/series_test.py
+++ series/series_test.py
@@ -1,42 +1,44 @@
 """Tests for the series exercise
 
 Implementation note:
-The Series.slices method should raise a ValueError with a meaningful error
-message if its argument isn't a valid slice length.
+The slices function should raise a ValueError with a meaningful error
+message if its length argument doesn't fit the series.
 """
 import unittest
 
-from series import Series
+from series import slices
 
 
 class SeriesTest(unittest.TestCase):
     def test_slices_of_one(self):
         self.assertEqual([[0], [1], [2], [3], [4]],
-                         Series("01234").slices(1))
+                         slices("01234", 1))
 
     def test_slices_of_two(self):
         self.assertEqual([[9, 7], [7, 8], [8, 6], [6, 7],
                           [7, 5], [5, 6], [6, 4]],
-                         Series("97867564").slices(2))
+                         slices("97867564", 2))
 
     def test_slices_of_three(self):
         self.assertEqual([[9, 7, 8], [7, 8, 6], [8, 6, 7],
                           [6, 7, 5], [7, 5, 6], [5, 6, 4]],
-                         Series("97867564").slices(3))
+                         slices("97867564", 3))
 
     def test_slices_of_four(self):
         self.assertEqual([[0, 1, 2, 3], [1, 2, 3, 4]],
-                         Series("01234").slices(4))
+                         slices("01234", 4))
 
     def test_slices_of_five(self):
         self.assertEqual([[0, 1, 2, 3, 4]],
-                         Series("01234").slices(5))
+                         slices("01234", 5))
 
     def test_overly_long_slice(self):
-        self.assertRaises(ValueError, Series("012").slices, 4)
+        with self.assertRaises(ValueError):
+            slices("012", 4)
 
     def test_overly_short_slice(self):
-        self.assertRaises(ValueError, Series("01234").slices, 0)
+        with self.assertRaises(ValueError):
+            slices("01234", 0)
 
 
 if __name__ == '__main__':
