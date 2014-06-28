c4d001912af1ed77352d4aedf6e0483d820c3b9f
Simon Jakobi <simon.jakobi@gmail.com>
2014-06-28 03:52:14 +0200

    beer-song: Adopt functional style
    As discussed in #79.
    
    The original "Beer.sing" method is replaced by a "song" function -
    "song" being on the same level of abstraction as "verse".

--- beer-song/beer_song_test.py
+++ beer-song/beer_song_test.py
@@ -1,44 +1,40 @@
 import unittest
 
-from beer import Beer
+from beer import song, verse
 
 
 class BeerTest(unittest.TestCase):
-    def setUp(self):
-        self.beer = Beer()
-
     def test_a_verse(self):
         self.assertEqual(
-            self.beer.verse(8),
+            verse(8),
             "8 bottles of beer on the wall, 8 bottles of beer.\n"
             "Take one down and pass it around, 7 bottles of beer on the wall.\n"
         )
 
     def test_verse_1(self):
         self.assertEqual(
-            self.beer.verse(1),
+            verse(1),
             "1 bottle of beer on the wall, 1 bottle of beer.\n"
             "Take it down and pass it around, no more bottles of beer on the wall.\n"
         )
 
     def test_verse_2(self):
         self.assertEqual(
-            self.beer.verse(2),
+            verse(2),
             "2 bottles of beer on the wall, 2 bottles of beer.\n"
             "Take one down and pass it around, 1 bottle of beer on the wall.\n"
         )
 
     def test_verse_0(self):
         self.assertEqual(
-            self.beer.verse(0),
+            verse(0),
             "No more bottles of beer on the wall, no more bottles of beer.\n"
             "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
         )
 
-    def test_singing_several_verses(self):
+    def test_songing_several_verses(self):
         self.assertEqual(
-            self.beer.sing(8, 6),
-
+            song(8, 6),
             "8 bottles of beer on the wall, 8 bottles of beer.\n"
             "Take one down and pass it around, 7 bottles of beer on the wall.\n"
             "\n"
@@ -50,10 +46,9 @@ class BeerTest(unittest.TestCase):
             "\n"
         )
 
-    def test_sing_all_the_rest_of_the_verses(self):
+    def test_song_all_the_rest_of_the_verses(self):
         self.assertEqual(
-            self.beer.sing(3),
-
+            song(3),
             "3 bottles of beer on the wall, 3 bottles of beer.\n"
             "Take one down and pass it around, 2 bottles of beer on the wall.\n"
             "\n"
