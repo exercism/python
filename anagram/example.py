4ced228c03d74c1430514ee3ab6a7a91b27b3ab4
Simon Jakobi <simon.jakobi@gmail.com>
2014-06-28 03:40:14 +0200

    anagram: Adopt functional style
    As discussed in #79.
    
    This also adds a test case to make sure that differently cased
    versions of a word aren't recognized as anagrams. E.g. "the"
    and "The" aren't anagrams.

--- anagram/example.py
+++ anagram/example.py
@@ -1,21 +1,9 @@
-class Word(object):
-    def __init__(self, word):
-        self.word = word
-        self.canonical = self._canonicalize(word)
-
-    def _canonicalize(self, word):
-        return sorted(word.lower())
-
-    def is_anagram_of(self, other):
-        return self.word != other and self.canonical == self._canonicalize(other)
+def detect_anagrams(word, candidates):
+    return [candidate
+            for candidate in candidates
+            if _letters(candidate) == _letters(word)
+            if candidate.lower() != word.lower()]
 
 
-class Anagram(object):
-    def __init__(self, target):
-        self.target = Word(target)
-
-    def match(self, candidates):
-        return [
-            candidate for candidate in candidates
-            if self.target.is_anagram_of(candidate)
-        ]
+def _letters(word):
+    return sorted(word.lower())
