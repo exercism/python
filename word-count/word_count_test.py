54f5fae64b67028676e5c62bcabe3b78c29d1d8e
Simon Jakobi <simon.jakobi@gmail.com>
2014-06-28 03:14:08 +0200

    word-count: Adopt functional style
    As discussed in #79.

--- word-count/word_count_test.py
+++ word-count/word_count_test.py
@@ -1,43 +1,43 @@
 import unittest
 
-from wordcount import Phrase
+from wordcount import word_count
 
 
 class WordCountTests(unittest.TestCase):
     def test_count_one_word(self):
         self.assertEqual(
             {'word': 1},
-            Phrase('word').word_count()
+            word_count('word')
         )
 
     def test_count_one_of_each(self):
         self.assertEqual(
             {'one': 1, 'of': 1, 'each': 1},
-            Phrase('one of each').word_count()
+            word_count('one of each')
         )
 
     def test_count_multiple_occurences(self):
         self.assertEqual(
             {'one': 1, 'fish': 4, 'two': 1, 'red': 1, 'blue': 1},
-            Phrase('one fish two fish red fish blue fish').word_count()
+            word_count('one fish two fish red fish blue fish')
         )
 
     def test_ignore_punctuation(self):
         self.assertEqual(
             {'car': 1, 'carpet': 1, 'as': 1, 'java': 1, 'javascript': 1},
-            Phrase('car : carpet as java : javascript!!&@$%^&').word_count()
+            word_count('car : carpet as java : javascript!!&@$%^&')
         )
 
     def test_include_numbers(self):
         self.assertEqual(
             {'testing': 2, '1': 1, '2': 1},
-            Phrase('testing, 1, 2 testing').word_count()
+            word_count('testing, 1, 2 testing')
         )
 
     def test_normalize_case(self):
         self.assertEqual(
             {'go': 3},
-            Phrase('go Go GO').word_count()
+            word_count('go Go GO')
         )
 
 if __name__ == '__main__':
