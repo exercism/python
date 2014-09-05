210f9eb5626431ebe69771103df61096402cbc69
Wiktor Matuszewski <pywkm@wukaem.pl>
2014-09-05 18:36:44 +0200

    Slightly different input in test_irregular_matrix. Added few whitespaces in compliance with PEP8.

--- saddle-points/saddle_points_test.py
+++ saddle-points/saddle_points_test.py
@@ -12,22 +12,23 @@ from saddle_points import saddle_points
 
 class SaddlePointTest(unittest.TestCase):
     def test_one_saddle(self):
-        inp = [[9,8,7],[5,3,2],[6,6,7]]
-        self.assertEqual(set([(1,0)]), saddle_points(inp))
+        inp = [[9, 8, 7], [5, 3, 2], [6, 6, 7]]
+        self.assertEqual(set([(1, 0)]), saddle_points(inp))
 
     def test_no_saddle(self):
-        self.assertEqual(set(), saddle_points([[2,1],[1,2]]))
+        self.assertEqual(set(), saddle_points([[2, 1], [1, 2]]))
 
     def test_mult_saddle(self):
-        inp = [[5,3,5,4],[6,4,7,3],[5,1,5,3]]
-        ans = set([(0,0),(0,2),(2,0),(2,2)])
+        inp = [[5, 3, 5, 4], [6, 4, 7, 3], [5, 1, 5, 3]]
+        ans = set([(0, 0), (0, 2), (2, 0), (2, 2)])
         self.assertEqual(ans, saddle_points(inp))
 
     def test_empty_matrix(self):
         self.assertEqual(set(), saddle_points([]))
 
     def test_irregular_matrix(self):
-        self.assertRaises(ValueError, saddle_points, [[1,2,3],[2,3],[3,2,1]])
+        inp = [[3, 2, 1], [0, 1], [2, 1, 0]]
+        self.assertRaises(ValueError, saddle_points, inp)
 
 
 if __name__ == '__main__':
