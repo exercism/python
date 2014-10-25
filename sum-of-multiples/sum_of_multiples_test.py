f37251ff1ef1da0d591f12c47b605eb2abf5ce23
Maurice Makaay <mauricem@crunchbang>
2014-10-25 23:28:24 +0200

    sum-of-multiples: add extra unit tests

--- sum-of-multiples/sum_of_multiples_test.py
+++ sum-of-multiples/sum_of_multiples_test.py
@@ -7,7 +7,7 @@ class SumOfMultiplesTest(unittest.TestCase):
     def test_sum_to_1(self):
         self.assertEqual(0, SumOfMultiples().to(1))
 
-    def test_sum_to_3(self):
+    def test_sum_to_4(self):
         self.assertEqual(3, SumOfMultiples().to(4))
 
     def test_sum_to_10(self):
@@ -21,6 +21,15 @@ class SumOfMultiplesTest(unittest.TestCase):
 
     def test_configurable_43_47_to_10000(self):
         self.assertEqual(2203160, SumOfMultiples(43, 47).to(10000))
+
+    def test_configurable_20_24_25_to_800(self):
+        self.assertEqual(36144, SumOfMultiples(20, 24, 25).to(800))
+
+    def test_configurable_0_to_10(self):
+        self.assertEqual(0, SumOfMultiples(0).to(10))
+
+    def test_configurable_0_1_to_10(self):
+        self.assertEqual(45, SumOfMultiples(0, 1).to(10))
 
 
 if __name__ == '__main__':
