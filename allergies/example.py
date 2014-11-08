e02b8b2e8a05b64ad708f47f447590a451b9da6d
Dog <Dog@users.noreply.github.com>
2014-11-08 00:32:50 -0800

    Suggesting my allergies solution
    Here is another solution that I feel is cleaner and takes fewer steps to achieve the same result.

--- allergies/example.py
+++ allergies/example.py
@@ -1,22 +1,23 @@
-def powers_of_2(n):
-    """Return a list of the powers of 2 whose sum is the input number."""
-    return [2 ** exponent
-            for exponent, bit in enumerate(bin(n)[:1:-1])
-            if bit == '1']
+class Allergies:
 
-
-class Allergies(object):
-
-    __allergens = {2 ** exponent: allergen
-                   for exponent, allergen in enumerate(
-                       ("eggs peanuts shellfish strawberries tomatoes "
-                        "chocolate pollen cats").split())}
+    _allergies = [
+        "eggs",
+        "peanuts",
+        "shellfish",
+        "strawberries",
+        "tomatoes",
+        "chocolate",
+        "pollen",
+        "cats"
+    ]
 
     def __init__(self, score):
         self.score = score
-        self.list = [self.__allergens[p]
-                     for p in powers_of_2(score)
-                     if p in self.__allergens]
 
-    def is_allergic_to(self, allergen):
-        return allergen in self.list
+    def is_allergic_to(self, allergy):
+        return self.score & 1 << self._allergies.index(allergy)
+
+    @property
+    def list(self):
+        return [allergy for allergy in self._allergies
+                if self.is_allergic_to(allergy)]
