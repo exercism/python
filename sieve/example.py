ce97db5d4ce4e1e087dae5f85bac1d3337915d6a
I can not only fetch JSON, but parse it too. <Dog@users.noreply.github.com>
2015-01-07 11:42:53 -0800

    Added Python 3 support
    Attempt to add support for Python 3.
    
    Python 3 no longer has range return a list.

--- sieve/example.py
+++ sieve/example.py
@@ -1,6 +1,6 @@
 def sieve(limit):
     primes = [2]
-    pRange = range(3, limit+1, 2)
+    pRange = list(range(3, limit+1, 2))
     while pRange:
         p = pRange.pop(0)
         pRange = filter(lambda x: x % p, pRange)
