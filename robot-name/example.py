56b9675cb5c8f7647b8fb65978cef5a28c78b6e3
tstirrat <tstirrat@gmail.com>
2015-02-28 13:26:00 -0600

    changed robot-name test to reliably trigger collision detection and updated example code to pass test
    in test_spread_sublist: multiples of 3 is a sublist of multiples of 15; changed from 'UNEQUAL' to 'SUBLIST'
    
    rolled back sublist changes
    
    modified robot-name example to pass updated robot-name test

--- robot-name/example.py
+++ robot-name/example.py
@@ -6,6 +6,7 @@ class Robot(object):
 
     def __init__(self):
         self._name = None
+        self._past_names = set()
 
     def prefix(self):
         return ''.join([
@@ -21,7 +22,13 @@ class Robot(object):
 
     def get_name(self):
         if not self._name:
-            self._name = self.prefix() + self.suffix()
+
+            # Collision detection
+            while True:
+                self._name = self.prefix() + self.suffix()
+                if self._name not in self._past_names:
+                    self._past_names.add(self._name)
+                    break
 
         return self._name
 
