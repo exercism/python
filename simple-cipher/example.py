8511a9e81b3af9b06525d4e55c97cfc711e0294c
Miguel Sánchez de León Peque <msdeleonpeque@gmail.com>
2015-09-02 16:22:17 +0200

    simple-cipher: Avoid assigning lambda expressions (satisfy PEP-8)

--- simple-cipher/example.py
+++ simple-cipher/example.py
@@ -13,32 +13,19 @@ class Cipher:
             raise ValueError('Wrong key parameter!')
         self.key = key
 
-    def base_encode(self, s, shift):
-        xkey = self.key * (len(s) // len(self.key) + 1)
-        return ''.join(shift(c, k) for c, k in zip(s, xkey))
+    def encode(self, text):
+        text = ''.join(c for c in text if c.isalpha()).lower()
+        key = self.key * (len(text) // len(self.key) + 1)
+        return ''.join(chr((ord(c) - 194 + ord(k)) % 26 + 97)
+                       for c, k in zip(text, key))
 
-    def encode(self, s):
-        s = _normalize(s)
-        shift = lambda c, k: chr(((ord(c) + ord(k) - 2 * ord('a'))
-                                  % len(ascii_lowercase)) + ord('a'))
-        return self.base_encode(s, shift)
-
-    def decode(self, s):
-        shift = lambda c, k: chr(((ord(c) - ord(k) + len(ascii_lowercase))
-                                  % len(ascii_lowercase)) + ord('a'))
-        return self.base_encode(s, shift)
+    def decode(self, text):
+        key = self.key * (len(text) // len(self.key) + 1)
+        return ''.join(chr((ord(c) - ord(k) + 26) % 26 + 97)
+                       for c, k in zip(text, key))
 
 
 class Caesar(Cipher):
 
     def __init__(self):
         Cipher.__init__(self, 'd')
-
-
-def _normalize(s):
-    return ''.join([c for c in s if c.isalpha()]).lower()
-
-
-if __name__ == '__main__':
-    print(Caesar().encode('venividivici'))
-    print(Caesar().encode('\'Twas the night before Christmas'))
