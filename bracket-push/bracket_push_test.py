591e39d36dce2bd9f3a5da1bddd55fc1927c69c0
Tammo Behrends <behr.tam@gmail.com>
2015-11-12 12:02:06 +0100

    bracket-push: Removed excessive tests
    Removed all tests related to validate how to handle characters
    that are not brackets and improved the naming of some test functions.
    Related discussion: https://github.com/exercism/x-common/issues/127

--- bracket-push/bracket_push_test.py
+++ bracket-push/bracket_push_test.py
@@ -4,41 +4,33 @@ from bracket_push import check_brackets
 
 
 class BracketPushTests(unittest.TestCase):
+
     def test_input_empty(self):
         self.assertEqual(check_brackets(""), True)
 
-    def test_simple_curly(self):
+    def test_single(self):
         self.assertEqual(check_brackets("{}"), True)
 
-    def test_simple_square(self):
-        self.assertEqual(check_brackets("[]"), True)
-
-    def test_curly_only_open(self):
+    def test_unclosed(self):
         self.assertEqual(check_brackets("{{"), False)
 
-    def test_parenthesis_switched(self):
-        self.assertEqual(check_brackets(")("), False)
-
-    def test_simple_square_with_numbers(self):
-        self.assertEqual(check_brackets("[42]"), True)
+    def test_wrong_order(self):
+        self.assertEqual(check_brackets("}{"), False)
 
     def test_mixed_not_nested(self):
         self.assertEqual(check_brackets("{}[]"), True)
 
-    def test_nested(self):
+    def test_mixed_nested(self):
         self.assertEqual(check_brackets("{[]}"), True)
 
-    def test_crossed(self):
+    def test_improperly_nested(self):
         self.assertEqual(check_brackets("{[}]"), False)
 
-    def test_round_single(self):
-        self.assertEqual(check_brackets("{[])[]}"), False)
+    def test_not_opened_nested(self):
+        self.assertEqual(check_brackets("{[)][]}"), False)
 
     def test_nested_ensemble(self):
         self.assertEqual(check_brackets("{[]([()])}"), True)
-
-    def test_with_different_chars(self):
-        self.assertEqual(check_brackets(" {[<](x [(42)]) }"), True)
 
 
 if __name__ == '__main__':
