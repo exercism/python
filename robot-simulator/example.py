b554c95e8d427dcbb8018197d05ce22bc25abe0c
Tammo Behrends <behr.tam@gmail.com>
2015-11-16 17:36:54 +0100

    robot-simulator: Update based on review notes

--- robot-simulator/example.py
+++ robot-simulator/example.py
@@ -1,42 +1,40 @@
-# Support for enumerations is new in version 3.4.
-try:
-    from enum import Enum
-except ImportError:
-    Enum = object
+NORTH, EAST, SOUTH, WEST = range(4)
 
 
-class Bearing(Enum):
-    NORTH, EAST, SOUTH, WEST = range(4)
+class Compass(object):
+    compass = [NORTH, EAST, SOUTH, WEST]
+
+    def __init__(self, bearing=NORTH):
+        self.bearing = bearing
+
+    def left(self):
+        self.bearing = self.compass[self.bearing - 1]
+
+    def right(self):
+        self.bearing = self.compass[(self.bearing + 1) % 4]
 
 
 class Robot(object):
-    def __init__(self, bearing=Bearing.NORTH, coordiantes=[0, 0]):
-        self.coordiantes = coordiantes
-        self.bearing = bearing
+    def __init__(self, bearing=NORTH, x=0, y=0):
+        self.compass = Compass(bearing)
+        self.x = x
+        self.y = y
 
     def advance(self):
-        if self.bearing == Bearing.NORTH:
-            self.coordiantes[1] += 1
-        elif self.bearing == Bearing.SOUTH:
-            self.coordiantes[1] -= 1
-        elif self.bearing == Bearing.EAST:
-            self.coordiantes[0] += 1
-        elif self.bearing == Bearing.WEST:
-            self.coordiantes[0] -= 1
+        if self.bearing == NORTH:
+            self.y += 1
+        elif self.bearing == SOUTH:
+            self.y -= 1
+        elif self.bearing == EAST:
+            self.x += 1
+        elif self.bearing == WEST:
+            self.x -= 1
 
     def turn_left(self):
-        left = {Bearing.NORTH: Bearing.WEST,
-                Bearing.WEST: Bearing.SOUTH,
-                Bearing.SOUTH: Bearing.EAST,
-                Bearing.EAST: Bearing.NORTH}
-        self.bearing = left[self.bearing]
+        self.compass.left()
 
     def turn_right(self):
-        right = {Bearing.NORTH: Bearing.EAST,
-                 Bearing.EAST: Bearing.SOUTH,
-                 Bearing.SOUTH: Bearing.WEST,
-                 Bearing.WEST: Bearing.NORTH}
-        self.bearing = right[self.bearing]
+        self.compass.right()
 
     def simulate(self, commands):
         instructions = {'A': self.advance,
@@ -45,3 +43,11 @@ class Robot(object):
         for cmd in commands:
             if cmd in instructions:
                 instructions[cmd]()
+
+    @property
+    def bearing(self):
+        return self.compass.bearing
+
+    @property
+    def coordinates(self):
+        return (self.x, self.y)
