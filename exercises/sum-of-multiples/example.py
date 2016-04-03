e048a3e1882d8e7714a0a8b73069701c4e388caf
Victor Goff <keeperotphones@gmail.com>
2016-04-03 03:33:33 -0400

    sum-of-multiples: Remove default values
    references #319

--- exercises/sum-of-multiples/example.py
+++ exercises/sum-of-multiples/example.py
@@ -1,7 +1,5 @@
 def sum_of_multiples(limit, multiples=None):
-    if multiples is None:
-        multiples = [3, 5]
-    elif multiples[0] == 0:
+    if multiples[0] == 0:
         # multiples of 0 don't change the sum
         multiples = multiples[1:]
     return sum(value for value in range(limit)
--- exercises/sum-of-multiples/sum_of_multiples_test.py
+++ exercises/sum-of-multiples/sum_of_multiples_test.py
@@ -3,9 +3,8 @@ You can make the following assumptions about the inputs to the
 'sum_of_multiples' function:
     * All input numbers are non-negative 'int's, i.e. natural numbers including
       zero.
-    * If a list of factors is given, its elements are uniqe and sorted in
+    * A list of factors must be given, and its elements are uniqe and sorted in
       ascending order.
-    * If the 'factors' argument is missing, use the list [3, 5] instead.
 """
 
 import unittest
@@ -15,19 +14,19 @@ from sum_of_multiples import sum_of_multiples
 
 class SumOfMultiplesTest(unittest.TestCase):
     def test_sum_to_1(self):
-        self.assertEqual(0, sum_of_multiples(1))
+        self.assertEqual(0, sum_of_multiples(1, [3, 5]))
 
     def test_sum_to_3(self):
-        self.assertEqual(3, sum_of_multiples(4))
+        self.assertEqual(3, sum_of_multiples(4, [3, 5]))
 
     def test_sum_to_10(self):
-        self.assertEqual(23, sum_of_multiples(10))
+        self.assertEqual(23, sum_of_multiples(10, [3, 5]))
 
     def test_sum_to_100(self):
-        self.assertEqual(2318, sum_of_multiples(100))
+        self.assertEqual(2318, sum_of_multiples(100, [3, 5]))
 
     def test_sum_to_1000(self):
-        self.assertEqual(233168, sum_of_multiples(1000))
+        self.assertEqual(233168, sum_of_multiples(1000, [3, 5]))
 
     def test_configurable_7_13_17_to_20(self):
         self.assertEqual(51, sum_of_multiples(20, [7, 13, 17]))
