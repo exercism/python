bd37944db1d7a9fc6dc01a34453e1d8e2da18fd6
michaelkunc <mjkunc@gmail.com>
2016-06-27 17:06:35 -0700

    started on binary search

--- exercises/binary-search/binary_search_test.py
+++ exercises/binary-search/binary_search_test.py
@@ -1,12 +1,18 @@
 import unittest
 
-from example import binary_search
+from example import binary_search, list_sorted
 
 
 class BinarySearchTests(unittest.TestCase):
 
-    def test_is_this_thing_on(self):
-        self.assertEqual('is this thing on', binary_search())
+    def test_list_sorted(self):
+        self.assertEqual(False, list_sorted([3,1,4,5,6]))
+
+    def test_raises_value_error_if_not_sorted(self):
+        self.assertEqual('The list must be sorted', binary_search([44,5,2,1,4], 6))
+
+    def test_list_item_not_found(self):
+        self.assertEqual("Item not found", binary_search([1,2,3,4,5,6], 23))
 
 
 if __name__ == '__main__':
--- exercises/binary-search/example.py
+++ exercises/binary-search/example.py
@@ -1,4 +1,18 @@
 
+def binary_search(search_list, value):
+    raise ValueError('The list must be sorted') if not list_sorted(search_list):
+    low = 0
+    high = len(search_list) - 1
+    while low <= high:
+        middle = (low + high ) // 2
+        if search_list[middle] > value:
+            high = middle - 1
+        elif search_list[middle] < value:
+            low = middle + 1
+        else:
+            return middle
+    return "Item not found"
 
-def binary_search():
-    return 'this thing is on'
+
+def list_sorted(search_list):
+    return all(search_list[i] <= search_list[i+1] for i in xrange(len(search_list) - 1))
