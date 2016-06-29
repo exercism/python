5d9a29331ee094fcd4d5dfc618c8b94008e07838
michaelkunc <mjkunc@gmail.com>
2016-06-28 17:21:26 -0700

    added another two test cases

--- exercises/binary-search/binary_search_test.py
+++ exercises/binary-search/binary_search_test.py
@@ -5,14 +5,18 @@ from example import binary_search, list_sorted
 
 class BinarySearchTests(unittest.TestCase):
 
-    def test_list_sorted(self):
-        self.assertEqual(False, list_sorted([3,1,4,5,6]))
+    def test_raises_error_if_list_is_not_sorted(self):
+        self.assertRaises(ValueError, binary_search, [44, 5, 2, 1, 4], 6)
 
-    def test_raises_value_error_if_not_sorted(self):
-        self.assertRaises(ValueError, binary_search,[44,5,2,1,4], 6)
+    def test_raises_error_if_list_is_empty(self):
+        self.assertRaises(ValueError, binary_search, [],1)
 
     def test_list_item_not_found(self):
-        self.assertEqual("Item not found", binary_search([1,2,3,4,5,6], 23))
+        self.assertEqual("Item not found",
+                         binary_search([1, 2, 3, 4, 5, 6], 23))
+
+    def test_list_item_found(self):
+        self.assertEqual(3, binary_search([1, 2, 3, 4, 5, 6, 7, 8], 4))
 
 
 if __name__ == '__main__':
--- exercises/binary-search/example.py
+++ exercises/binary-search/example.py
@@ -1,20 +1,24 @@
 
 def binary_search(search_list, value):
-    if not list_sorted(search_list):
-        raise ValueError("The list must be sorted")
-    else:
-        low = 0
-        high = len(search_list) - 1
-        while low <= high:
-            middle = (low + high ) // 2
-            if search_list[middle] > value:
-                high = middle - 1
-            elif search_list[middle] < value:
-                low = middle + 1
-            else:
-                return middle
-        return "Item not found"
+    list_empty(search_list)
+    list_sorted(search_list)
+    low = 0
+    high = len(search_list) - 1
+    while low <= high:
+        middle = (low + high ) // 2
+        if search_list[middle] > value:
+            high = middle - 1
+        elif search_list[middle] < value:
+            low = middle + 1
+        else:
+            return middle
+    return "Item not found"
 
 
 def list_sorted(search_list):
-    return all(search_list[i] <= search_list[i+1] for i in xrange(len(search_list) - 1))
+    if not all(search_list[i] <= search_list[i+1] for i in xrange(len(search_list) - 1)):
+        raise ValueError("This list must be sorted")
+
+def list_empty(search_list):
+    if len(search_list) == 0:
+        raise ValueError('This list has no items.')
