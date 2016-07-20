a1323ad50bd54a1f5b5b9fbd59b74ab5c431b936
Patrick Jackson <PatrickSJackson@gmail.com>
2016-07-20 13:03:07 -0700

    pangram: Update pangram test suite
    Updates the pangram test suite to match the new json file in x-common.
    
    Resolves #337

--- exercises/pangram/pangram_test.py
+++ exercises/pangram/pangram_test.py
@@ -14,13 +14,25 @@ class PangramTests(unittest.TestCase):
         self.assertTrue(
             is_pangram('the quick brown fox jumps over the lazy dog'))
 
-    def test_invalid_pangram(self):
-        self.assertFalse(
-            is_pangram('the quick brown fish jumps over the lazy dog'))
-
     def test_missing_x(self):
         self.assertFalse(is_pangram('a quick movement of the enemy will '
                                     'jeopardize five gunboats'))
+
+    def test_another_missing_character(self):
+        self.assertFalse(
+            is_pangram('the quick brown fish jumps over the lazy dog'))
+
+    def test_pangram_with_underscores(self):
+        self.assertTrue(
+            is_pangram("the_quick_brown_fox_jumps_over_the_lazy_dog"))
+
+    def test_pangram_with_numbers(self):
+        self.assertTrue(
+            is_pangram("the 1 quick brown fox jumps over the 2 lazy dogs"))
+
+    def test_missing_letters_replaced_by_numbers(self):
+        self.assertFalse(
+            is_pangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"))
 
     def test_mixedcase_and_punctuation(self):
         self.assertTrue(is_pangram('"Five quacking Zephyrs jolt my wax bed."'))
