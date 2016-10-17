f6fdc39b885bd0abf59b74e654e740519f7868cf
Trey Hunner <trey@treyhunner.com>
2016-10-16 20:54:14 -0700

    Fix example on Python 2

--- exercises/flatten-array/example.py
+++ exercises/flatten-array/example.py
@@ -1,11 +1,17 @@
-from collections.abc import Iterable
+def is_iterable(thing):
+    try:
+        iter(thing)
+    except TypeError:
+        return False
+    else:
+        return True
 
 
 def flatten(iterable):
     """Flatten a list of lists."""
     flattened = []
     for item in iterable:
-        if isinstance(item, Iterable) and not isinstance(item, (str, bytes)):
+        if is_iterable(item) and not isinstance(item, (str, bytes)):
             flattened += flatten(item)
         elif item is not None:
             flattened.append(item)
