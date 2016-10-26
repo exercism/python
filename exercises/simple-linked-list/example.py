14a219c105d41a4cd58a65f92cc6f36db46151d3
laurauzcategui <laura.uzcateguij@gmail.com>
2016-10-27 00:23:37 +0100

    Adding data structures pop peak empty methods and test class

---
+++ exercises/simple-linked-list/example.py
@@ -0,0 +1,29 @@
+class Element(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+
+    def push(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+
+
+    def empty(self):
+        return self.size == 0
+
+    def peek(self):
+        pass
+        if empty:
+            return None
+        else:
+            #iterate
+    end
---
+++ exercises/simple-linked-list/simple-linked-list_test.py
@@ -0,0 +1,6 @@
+import unittest
+
+from simple-linked-list import LinkedList
+
+
+class LinkedListTests(unittest.TestCase):
