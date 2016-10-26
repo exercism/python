9ca4ff25cf0edc3e68b8b5d4cfca60b0fd88499d
laurauzcategui <laura.uzcateguij@gmail.com>
2016-10-27 00:23:37 +0100

    Adding data structures pop peak empty methods and test class

--- exercises/simple-linked-list/example.py
+++ exercises/simple-linked-list/example.py
@@ -1,3 +1,4 @@
+<<<<<<< e2f6b831658936e2dcb9623291bc9f56bf5449d6
 class Element(object):
     def __init__(self, value):
         self.value = value
@@ -24,3 +25,35 @@ class LinkedList(object):
         pass
         if self.empty:
             return None
+||||||| merged common ancestors
+=======
+class Element(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+
+    def push(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+
+
+    def empty(self):
+        return self.size == 0
+
+    def peek(self):
+        pass
+        if empty:
+            return None
+        else:
+            #iterate
+    end
+>>>>>>> Adding data structures pop peak empty methods and test class
--- exercises/simple-linked-list/simple-linked-list_test.py
+++ exercises/simple-linked-list/simple-linked-list_test.py
@@ -1,3 +1,4 @@
+<<<<<<< e2f6b831658936e2dcb9623291bc9f56bf5449d6
 import unittest
 
 # from simple_linked_list import LinkedList
@@ -5,3 +6,12 @@ import unittest
 
 class LinkedListTests(unittest.TestCase):
     pass
+||||||| merged common ancestors
+=======
+import unittest
+
+from simple-linked-list import LinkedList
+
+
+class LinkedListTests(unittest.TestCase):
+>>>>>>> Adding data structures pop peak empty methods and test class
