71687b776e290a384fb03b8b5932d13bf9afa955
laurauzcategui <laura.uzcateguij@gmail.com>
2016-10-31 14:24:43 +0000

    including test cases for simple-linked-list

--- exercises/simple-linked-list/example.py
+++ exercises/simple-linked-list/example.py
@@ -32,28 +32,81 @@ class Element(object):
         self.value = value
         self.next = None
 
+
 class LinkedList(object):
     def __init__(self, head=None):
         self.head = head
+        self.peek = head
+        self.size = 0
 
     def push(self, new_element):
         current = self.head
         if self.head:
             while current.next:
                 current = current.next
-            current.next = new_element
+            current.next = Element(new_element)
+            self.peek = current.next
         else:
-            self.head = new_element
+            self.head = Element(new_element)
+            self.peek = self.head
+        self.size += 1
 
+    def pop(self):
+        if self.empty():
+            return None
+        else:
+            current = self.head
+            prev = None
+            while current.next:
+                prev = current
+                current = current.next
+            if prev:
+                e, prev.next = current.value, None
+            else:
+                e, prev = current.value, None
+            self.peek = prev
+            self.size -= 1
+            return e
 
     def empty(self):
         return self.size == 0
 
-    def peek(self):
-        pass
-        if empty:
+    def get_peek(self):
+        if self.empty():
             return None
         else:
+<<<<<<< 9ca4ff25cf0edc3e68b8b5d4cfca60b0fd88499d
             #iterate
     end
 >>>>>>> Adding data structures pop peak empty methods and test class
+||||||| merged common ancestors
+            #iterate
+    end
+=======
+            return self.peek.value
+
+    def reverse(self):
+        new_list = LinkedList()
+        while self.size > 0:
+            new_list.push(self.pop())
+        return new_list
+
+    def to_array(self):
+        arr = []
+        if self.empty():
+            return []
+        current = self.head
+        while current.next:
+            arr.append(current.value)
+            current = current.next
+        arr.append(current.value)
+        return arr
+
+    def from_array(self, arr=[]):
+        if len(arr) == 0:
+            return self
+        else:
+            for i in arr:
+                self.push(i)
+        return self
+>>>>>>> including test cases for simple-linked-list
--- exercises/simple-linked-list/simple-linked-list_test.py
+++ exercises/simple-linked-list/simple-linked-list_test.py
@@ -10,8 +10,71 @@ class LinkedListTests(unittest.TestCase):
 =======
 import unittest
 
-from simple-linked-list import LinkedList
+from simple_linked_list import LinkedList
 
 
 class LinkedListTests(unittest.TestCase):
+<<<<<<< 9ca4ff25cf0edc3e68b8b5d4cfca60b0fd88499d
 >>>>>>> Adding data structures pop peak empty methods and test class
+||||||| merged common ancestors
+=======
+    def setUp(self):
+        self.slist = LinkedList()
+
+    def test_size(self):
+        self.slist.push(0)
+        self.assertEqual(1, self.slist.size)
+        # notice if size is increased
+        self.slist.push(1)
+        self.assertEqual(2, self.slist.size)
+        self.slist.push(2)
+        self.assertEqual(3, self.slist.size)
+
+    def test_pop(self):
+        # with cero element
+        self.assertIsNone(self.slist.pop())
+        # with multiple elements
+        self.slist.push(1)
+        self.slist.push(2)
+        self.slist.push(3)
+        self.assertEqual(3, self.slist.pop())
+        self.assertEqual(2, self.slist.pop())
+        self.assertEqual(1, self.slist.pop())
+        self.assertIsNone(self.slist.pop())
+
+    def test_reverse(self):
+        # empty reversed LinkedList
+        empty_reversed = self.slist.reverse()
+        self.assertEqual(0, empty_reversed.size)
+        self.assertIsNone(empty_reversed.head)
+        # push_elements
+        self.slist.push(1)
+        self.slist.push(2)
+        self.slist.push(3)
+        self.slist.push(4)
+        reversed_list = self.slist.reverse()
+        self.assertEqual(4, reversed_list.head.value)
+        self.assertEqual(1, reversed_list.get_peek())
+
+    def test_to_array(self):
+        # in : empty list out : empty array
+        self.assertListEqual([], self.slist.to_array())
+        # push_elements
+        self.slist.push(1)
+        self.slist.push(2)
+        self.slist.push(3)
+        self.slist.push(4)
+        self.assertListEqual([1, 2, 3, 4], self.slist.to_array())
+
+    def test_from_array(self):
+        # in : empty array out : empty list
+        self.assertIsNone(self.slist.from_array([]).head)
+        # push_elements
+        new_arr = [1, 2, 3, 4]
+        self.assertEqual(1, self.slist.from_array(new_arr).head.value)
+        self.assertEqual(4, self.slist.from_array(new_arr).get_peek())
+
+
+if __name__ == '__main__':
+    unittest.main()
+>>>>>>> including test cases for simple-linked-list
