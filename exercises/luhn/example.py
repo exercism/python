b5ee75cfa615f0599f4c10b0a3ee51e2cbd113f2
Rootul Patel <rootulp@palantir.com>
2017-01-29 18:16:46 +0000

    luhn: Update example to satisfy new tests

--- exercises/luhn/example.py
+++ exercises/luhn/example.py
@@ -1,11 +1,11 @@
 class Luhn(object):
-    def __init__(self, number):
-        self.number = number
+    def __init__(self, string):
+        self.string = string.replace(" ", "")
 
     def addends(self):
         def luhn_transform(n):
             return (2 * n - 9) if (n > 4) else (2 * n)
-        old_digits = [int(d) for d in str(self.number)]
+        old_digits = [int(d) for d in str(self.string)]
         return [(luhn_transform(n) if (i % 2 == 0) else n)
                 for i, n in enumerate(old_digits, start=len(old_digits) % 2)]
 
@@ -13,9 +13,6 @@ class Luhn(object):
         return sum(self.addends())
 
     def is_valid(self):
+        if len(self.string) <= 1 or not self.string.isdigit():
+            return False
         return self.checksum() % 10 == 0
-
-    @staticmethod
-    def create(n):
-        diff = (10 - Luhn(n * 10).checksum()) % 10
-        return 10 * n + diff
