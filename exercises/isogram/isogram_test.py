3142d895b719ebbf99b04408b638cd5d1c603f47
Tammo Behrends <behr.tam@gmail.com>
2017-03-07 14:13:15 +0100

    Add missing test cases
    The test cases are based on the x-common canonical test data.

--- exercises/isogram/isogram_test.py
+++ exercises/isogram/isogram_test.py
@@ -1,20 +1,33 @@
 import unittest
-from example import isogram
+from isogram import is_isogram
 
 
 class TestIsogram(unittest.TestCase):
 
-    def test_isogram(self):
-        tests = ['lumberjacks', 'background', 'downstream']
-        for test in tests:
-            string = isogram(test)
-            self.assertTrue(string, True)
+    def test_empty_string(self):
+        self.assertTrue(is_isogram(""))
 
-    def test_isogram_false(self):
-        tests = ['isograms', 'aba', '  ']
-        for test in tests:
-            string = isogram(test)
-            self.assertFalse(string, False)
+    def test_isogram_with_only_lower_case_characters(self):
+        self.assertTrue(is_isogram("isogram"))
+
+    def test_word_with_one_duplicated_character(self):
+        self.assertFalse(is_isogram("eleven"))
+
+    def test_longest_reported_english_isogram(self):
+        self.assertTrue(is_isogram("subdermatoglyphic"))
+
+    def test_word_with_duplicated_character_in_mixed_case(self):
+        self.assertFalse(is_isogram("Alphabet"))
+
+    def test_hypothetical_isogrammic_word_with_hyphen(self):
+        self.assertTrue(is_isogram("thumbscrew-japingly"))
+
+    def test_isogram_with_duplicated_non_letter_character(self):
+        self.assertTrue(is_isogram("Hjelmqvist-Gryb-Zock-Pfund-Wax"))
+
+    def test_made_up_name_that_is_an_isogram(self):
+        self.assertTrue(is_isogram("Emily Jung Schwartzkopf"))
+
 
 if __name__ == '__main__':
     unittest.main()
