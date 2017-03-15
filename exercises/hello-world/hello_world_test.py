fc668004c8cc1d2bbe76a139d58ed7bde6637472
Tammo Behrends <behr.tam@gmail.com>
2017-03-15 12:11:30 +0100

    hello-world: Simplify to unconditional "Hello, World!"
    As discussed in https://github.com/exercism/x-common/issues/520
    
    closes #430

--- exercises/hello-world/hello_world_test.py
+++ exercises/hello-world/hello_world_test.py
@@ -1,48 +1,14 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import unicode_literals
 import unittest
 
 import hello_world
 
 
+# test cases adapted from `x-common//canonical-data.json` @ version: 1.0.0
+
 class HelloWorldTests(unittest.TestCase):
 
-    def test_hello_without_name(self):
-        self.assertEqual(
-            'Hello, World!',
-            hello_world.hello()
-        )
-
-    def test_hello_with_sample_name(self):
-        self.assertEqual(
-            'Hello, Alice!',
-            hello_world.hello('Alice')
-        )
-
-    def test_hello_with_other_sample_name(self):
-        self.assertEqual(
-            'Hello, Bob!',
-            hello_world.hello('Bob')
-        )
-
-    def test_hello_with_umlaut_name(self):
-        self.assertEqual(
-            'Hello, Jürgen!',
-            hello_world.hello('Jürgen')
-        )
-
-    def test_hello_with_blank_name(self):
-        self.assertEqual(
-            'Hello, World!',
-            hello_world.hello('')
-        )
-
-    def test_hello_with_none_name(self):
-        self.assertEqual(
-            'Hello, World!',
-            hello_world.hello(None)
-        )
+    def test_hello(self):
+        self.assertEqual('Hello, World!', hello_world.hello())
 
 
 if __name__ == '__main__':
