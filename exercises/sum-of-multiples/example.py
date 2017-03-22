3bd9bbba6868fbe3f98f3cc84381c67a9646ae62
Tammo Behrends <behr.tam@gmail.com>
2017-03-22 22:39:46 +0100

    Fix the example to fit the new tests

--- exercises/sum-of-multiples/example.py
+++ exercises/sum-of-multiples/example.py
@@ -1,7 +1,4 @@
-def sum_of_multiples(limit, multiples=None):
-    if multiples[0] == 0:
-        # multiples of 0 don't change the sum
-        multiples = multiples[1:]
+def sum_of_multiples(limit, multiples):
     return sum(value for value in range(limit)
                if any(value % multiple == 0
                       for multiple in multiples))
