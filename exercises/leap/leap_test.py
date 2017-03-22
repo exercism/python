b128171513f9fbed19370516d06b37834cea286f
Tammo Behrends <behr.tam@gmail.com>
2017-03-22 13:26:18 +0100

    leap: Update test cases
    Adds the changes made to the the canonical test data in
    https://github.com/exercism/x-common/pull/463
    and stores the test data version.

--- exercises/leap/leap_test.py
+++ exercises/leap/leap_test.py
@@ -3,21 +3,20 @@ import unittest
 from leap import is_leap_year
 
 
+# test cases adapted from `x-common//canonical-data.json` @ version: 1.0.0
+
 class YearTest(unittest.TestCase):
-    def test_leap_year(self):
-        self.assertIs(is_leap_year(1996), True)
+    def test_year_not_divisible_by_4(self):
+        self.assertFalse(is_leap_year(2015))
 
-    def test_non_leap_year(self):
-        self.assertIs(is_leap_year(1997), False)
+    def test_year_divisible_by_4_not_divisible_by_100(self):
+        self.assertTrue(is_leap_year(2016))
 
-    def test_non_leap_even_year(self):
-        self.assertIs(is_leap_year(1998), False)
+    def test_year_divisible_by_100_not_divisible_by_400(self):
+        self.assertFalse(is_leap_year(2100))
 
-    def test_century(self):
-        self.assertIs(is_leap_year(1900), False)
-
-    def test_exceptional_century(self):
-        self.assertIs(is_leap_year(2400), True)
+    def test_year_divisible_by_400(self):
+        self.assertTrue(is_leap_year(2000))
 
 
 if __name__ == '__main__':
