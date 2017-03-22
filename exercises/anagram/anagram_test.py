2ca83467d64df982769fc6b2d556a70b64da35d2
Tammo Behrends <behr.tam@gmail.com>
2017-03-22 16:12:56 +0100

    anagram: Update test cases
    Updates the test cases according to the canonical test data
    and stores the test data version.

--- exercises/anagram/anagram_test.py
+++ exercises/anagram/anagram_test.py
@@ -3,69 +3,73 @@ import unittest
 from anagram import detect_anagrams
 
 
+# test cases adapted from `x-common//canonical-data.json` @ version: 1.0.1
+
 class AnagramTests(unittest.TestCase):
     def test_no_matches(self):
-        self.assertEqual(
-            [],
-            detect_anagrams('diaper', 'hello world zombies pants'.split())
-        )
+        candidates = ["hello", "world", "zombies", "pants"]
+        self.assertEqual(detect_anagrams("diaper", candidates), [])
 
-    def test_detect_simple_anagram(self):
-        self.assertEqual(
-            ['tan'],
-            detect_anagrams('ant', 'tan stand at'.split())
-        )
+    def test_detects_simple_anagram(self):
+        candidates = ["tan", "stand", "at"]
+        self.assertEqual(detect_anagrams("ant", candidates), ["tan"])
 
-    def test_detect_multiple_anagrams(self):
-        self.assertEqual(
-            ['stream', 'maters'],
-            detect_anagrams('master', 'stream pigeon maters'.split())
-        )
+    def test_does_not_detect_false_positives(self):
+        self.assertEqual(detect_anagrams("galea", ["eagle"]), [])
 
-    def test_does_not_confuse_different_duplicates(self):
+    def test_detects_two_anagrams(self):
+        candidates = ["stream", "pigeon", "maters"]
         self.assertEqual(
-            [],
-            detect_anagrams('galea', ['eagle'])
-        )
+            detect_anagrams("master", candidates), ["stream", "maters"])
 
-    def test_eliminate_anagram_subsets(self):
-        self.assertEqual(
-            [],
-            detect_anagrams('good', 'dog goody'.split())
-        )
+    def test_does_not_detect_anagram_subsets(self):
+        self.assertEqual(detect_anagrams("good", ["dog", "goody"]), [])
 
-    def test_detect_anagram(self):
-        self.assertEqual(
-            ['inlets'],
-            detect_anagrams('listen', 'enlists google inlets banana'.split())
-        )
+    def test_detects_anagram(self):
+        candidates = ["enlists", "google", "inlets", "banana"]
+        self.assertEqual(detect_anagrams("listen", candidates), ["inlets"])
 
-    def test_multiple_anagrams(self):
+    def test_detects_three_anagrams(self):
+        candidates = [
+            "gallery", "ballerina", "regally", "clergy", "largely", "leading"
+        ]
         self.assertEqual(
-            'gallery regally largely'.split(),
-            detect_anagrams(
-                'allergy',
-                'gallery ballerina regally clergy largely leading'.split()
-            )
-        )
+            detect_anagrams("allergy", candidates),
+            ["gallery", "regally", "largely"])
 
-    def test_anagrams_are_case_insensitive(self):
-        self.assertEqual(
-            ['Carthorse'],
-            detect_anagrams('Orchestra',
-                            'cashregister Carthorse radishes'.split())
-        )
+    def test_does_not_detect_identical_words(self):
+        candidates = ["corn", "dark", "Corn", "rank", "CORN", "cron", "park"]
+        self.assertEqual(detect_anagrams("corn", candidates), ["cron"])
 
-    def test_same_word_isnt_anagram(self):
-        self.assertEqual(
-            [],
-            detect_anagrams('banana', ['banana'])
-        )
+    def test_does_not_detect_non_anagrams_with_identical_checksum(self):
+        self.assertEqual(detect_anagrams("mass", ["last"]), [])
 
+    def test_detects_anagrams_case_insensitively(self):
+        candidates = ["cashregister", "Carthorse", "radishes"]
         self.assertEqual(
-            [],
-            detect_anagrams('go', 'go Go GO'.split())
-        )
+            detect_anagrams("Orchestra", candidates), ["Carthorse"])
+
+    def test_detects_anagrams_using_case_insensitive_subjec(self):
+        candidates = ["cashregister", "carthorse", "radishes"]
+        self.assertEqual(
+            detect_anagrams("Orchestra", candidates), ["carthorse"])
+
+    def test_detects_anagrams_using_case_insensitive_possible_matches(self):
+        candidates = ["cashregister", "Carthorse", "radishes"]
+        self.assertEqual(
+            detect_anagrams("orchestra", candidates), ["Carthorse"])
+
+    def test_does_not_detect_a_word_as_its_own_anagram(self):
+        self.assertEqual(detect_anagrams("banana", ["Banana"]), [])
+
+    def test_does_not_detect_a_anagram_if_the_original_word_is_repeated(self):
+        self.assertEqual(detect_anagrams("go", ["go Go GO"]), [])
+
+    def test_anagrams_must_use_all_letters_exactly_once(self):
+        self.assertEqual(detect_anagrams("tapper", ["patter"]), [])
+
+    def test_capital_word_is_not_own_anagram(self):
+        self.assertEqual(detect_anagrams("BANANA", ["Banana"]), [])
 
 
 if __name__ == '__main__':
