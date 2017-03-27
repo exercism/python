2647aae88f8f9014ca674d36c057798d9439bc45
Tammo Behrends <behr.tam@gmail.com>
2017-03-27 12:33:15 +0200

    gigasecond: Refactor test names
    Renames the tests according to the canonical test data
    and stores the test version.

--- exercises/gigasecond/gigasecond_test.py
+++ exercises/gigasecond/gigasecond_test.py
@@ -1,31 +1,34 @@
-from datetime import datetime
 import unittest
+
+from datetime import datetime
 
 from gigasecond import add_gigasecond
 
 
+# test cases adapted from `x-common//canonical-data.json` @ version: 1.0.0
+
 class GigasecondTest(unittest.TestCase):
-    def test_1(self):
+    def test_date_only_specification_of_time(self):
         self.assertEqual(
             add_gigasecond(datetime(2011, 4, 25)),
             datetime(2043, 1, 1, 1, 46, 40))
 
-    def test_2(self):
+    def test_another_date_only_specification_of_time(self):
         self.assertEqual(
             add_gigasecond(datetime(1977, 6, 13)),
             datetime(2009, 2, 19, 1, 46, 40))
 
-    def test_3(self):
+    def test_one_more_date_only_specification_of_time(self):
         self.assertEqual(
             add_gigasecond(datetime(1959, 7, 19)),
             datetime(1991, 3, 27, 1, 46, 40))
 
-    def test_4(self):
+    def test_full_time_specified(self):
         self.assertEqual(
             add_gigasecond(datetime(2015, 1, 24, 22, 0, 0)),
             datetime(2046, 10, 2, 23, 46, 40))
 
-    def test_5(self):
+    def test_full_time_with_day_roll_over(self):
         self.assertEqual(
             add_gigasecond(datetime(2015, 1, 24, 23, 59, 59)),
             datetime(2046, 10, 3, 1, 46, 39))
