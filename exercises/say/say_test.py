e3b319876c2acddc2e6db1889dcefa0f7b6a7692
Tammo Behrends <behr.tam@gmail.com>
2017-03-29 19:55:40 +0200

    say: Reorder test cases
    Reorders the test cases to match the order in the canonical test data,
    marks three track specific test cases and stores the test data version.

--- exercises/say/say_test.py
+++ exercises/say/say_test.py
@@ -3,7 +3,12 @@ import unittest
 from say import say
 
 
+# test cases adapted from `x-common//canonical-data.json` @ version: 1.0.0
+
 class SayTest(unittest.TestCase):
+    def test_zero(self):
+        self.assertEqual(say(0), "zero")
+
     def test_one(self):
         self.assertEqual(say(1), "one")
 
@@ -19,6 +24,7 @@ class SayTest(unittest.TestCase):
     def test_one_hundred(self):
         self.assertEqual(say(100), "one hundred")
 
+    # additional track specific test
     def test_one_hundred_twenty(self):
         self.assertEqual(say(120), "one hundred and twenty")
 
@@ -31,12 +37,14 @@ class SayTest(unittest.TestCase):
     def test_one_thousand_two_hundred_thirty_four(self):
         self.assertEqual(say(1234), "one thousand two hundred and thirty-four")
 
+    # additional track specific test
     def test_eight_hundred_and_ten_thousand(self):
         self.assertEqual(say(810000), "eight hundred and ten thousand")
 
     def test_one_million(self):
         self.assertEqual(say(1e6), "one million")
 
+    # additional track specific test
     def test_one_million_two(self):
         self.assertEqual(say(1000002), "one million and two")
 
@@ -48,23 +56,20 @@ class SayTest(unittest.TestCase):
     def test_one_billion(self):
         self.assertEqual(say(1e9), "one billion")
 
-    def test_number_to_large(self):
-        with self.assertRaises(AttributeError):
-            say(1e12)
-
-    def test_number_negative(self):
-        with self.assertRaises(AttributeError):
-            say(-42)
-
-    def test_zero(self):
-        self.assertEqual(say(0), "zero")
-
     def test_987654321123(self):
         self.assertEqual(
             say(987654321123), ("nine hundred and eighty-seven billion "
                                 "six hundred and fifty-four million "
                                 "three hundred and twenty-one thousand "
                                 "one hundred and twenty-three"))
+
+    def test_number_to_large(self):
+        with self.assertRaises(AttributeError):
+            say(1e12)
+
+    def test_number_negative(self):
+        with self.assertRaises(AttributeError):
+            say(-1)
 
 
 if __name__ == '__main__':
