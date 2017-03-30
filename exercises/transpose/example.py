308e5f27eb0bbae89c35d027064f6c589534803b
nithin-vijayan <nithinkvijayan@icloud.com>
2017-03-30 13:49:35 +0530

    transpose : Added test files and example

---
+++ exercises/transpose/example.py
@@ -0,0 +1,9 @@
+
+def transpose(input_lines):
+    lines = input_lines.split("\n")
+    zipped = map(list, [line.ljust(len(max(lines, key=len))) for line in lines])
+    return "\n".join("".join(line) for line in zip(*zipped)).strip()
+
+
+if __name__ == '__main__':
+    transpose()
---
+++ exercises/transpose/transpose.py
@@ -0,0 +1,3 @@
+
+def transpose():
+    pass
