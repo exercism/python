d1b2144559d37f95cf0f55e1d3d5fba5c8d7e926
krapes <kerri.rapes@gmail.com>
2017-03-30 15:25:35 -0300

    rotationa_cipher: Completed test and example

---
+++ exercises/rotational-cipher/rotational_cipher.py
@@ -0,0 +1,14 @@
+def rotational_cipher(message,key):
+    import string
+    alpha_lower = list(string.ascii_lowercase)
+    alpha_upper = list(string.ascii_uppercase)
+    coded_message = ""
+    for char in list(message):
+        if char in alpha_lower:
+            char = alpha_lower[(alpha_lower.index(char) + key)%26]
+        elif char in alpha_upper:
+            char = alpha_upper[(alpha_upper.index(char) + key)%26]
+        coded_message = coded_message + char
+    return coded_message
+
+
---
+++ exercises/rotational-cipher/rotational_cipher_test.py
@@ -0,0 +1,57 @@
+import unittest
+
+import rotational_cipher
+
+
+class RotationalCipher(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(rotational_cipher.rotational_cipher('hello',0),
+                         'hello')
+
+    def test_space(self):
+        self.assertEqual(
+            rotational_cipher.rotational_cipher(
+                                                'from the other side',
+                                                0),
+            'from the other side')
+
+    def test_ciphered(self):
+        self.assertEqual(
+            rotational_cipher.rotational_cipher(
+                                                'i must have called',
+                                                13),
+            'v zhfg unir pnyyrq')
+
+    def test_mixed(self):
+        self.assertEqual(
+            rotational_cipher.rotational_cipher(
+                                                '... a 1000 TIMES',
+                                                13),
+            '... n 1000 GVZRF')
+
+    def test_emptystring(self):
+        self.assertEqual(rotational_cipher.rotational_cipher('',13),'')
+
+    def test_numbers(self):
+        self.assertEqual(rotational_cipher.rotational_cipher('07041776!!%$$',13),
+                         '07041776!!%$$')
+
+    def test_mixed(self):
+        self.assertEqual(
+            rotational_cipher.rotational_cipher(
+                                                "Doesn't tear you apart;",
+                                                27),
+            "Epfto'u ufbs zpv bqbsu;")
+
+    def test_mixed(self):
+        self.assertEqual(
+            rotational_cipher.rotational_cipher(
+                                                "Anymoreeee :'(",
+                                                -26),
+            "Anymoreeee :'(")
+        
+
+
+
+if __name__ == '__main__':
+    unittest.main()
