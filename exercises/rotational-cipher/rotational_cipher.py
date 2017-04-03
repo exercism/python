a0e8c02e021e25e637383a01b2617a53b194a2fc
krapes <kerri.rapes@gmail.com>
2017-04-02 23:16:55 -0300

    rotational_cipher: Removed doc string

--- exercises/rotational-cipher/rotational_cipher.py
+++ exercises/rotational-cipher/rotational_cipher.py
@@ -1,14 +1,4 @@
 
 
-def rotational_cipher(message, key):
-    """Preform a rotation cipher on a message.
-
-    Arguments:
-    message -- Message to be incoded in the form of a string
-    key -- The number of letters the charactors are to be shifted
-
-    Outputs:
-    coded_message -- A string containing the transposed message
-    """
-    coded_message = ""
-    return coded_message
+def rotate():
+    pass
