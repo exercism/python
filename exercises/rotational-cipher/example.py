9b5407ec39c73adefb5a3db7e22955f04d62aa59
krapes <kerri.rapes@gmail.com>
2017-04-02 23:15:36 -0300

    rotational_cipher: Streamlined example.py processes

--- exercises/rotational-cipher/example.py
+++ exercises/rotational-cipher/example.py
@@ -1,12 +1,12 @@
-def rotational_cipher(message, key):
-    import string
-    alpha_lower = list(string.ascii_lowercase)
-    alpha_upper = list(string.ascii_uppercase)
+def rotate(message, key):
+    from string import ascii_lowercase as alpha_lower
+    from string import ascii_uppercase as alpha_upper
+    ALPHA_LEN = len(alpha_lower)
     coded_message = ""
-    for char in list(message):
+    for char in message:
         if char in alpha_lower:
-            char = alpha_lower[(alpha_lower.index(char) + key) % 26]
+            char = alpha_lower[(alpha_lower.index(char) + key) % ALPHA_LEN]
         elif char in alpha_upper:
-            char = alpha_upper[(alpha_upper.index(char) + key) % 26]
-        coded_message = coded_message + char
+            char = alpha_upper[(alpha_upper.index(char) + key) % ALPHA_LEN]
+        coded_message += char
     return coded_message
