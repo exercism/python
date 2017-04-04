4c2dfe40cdaa75dade909c364c001811f3595501
Tammo Behrends <behr.tam@gmail.com>
2017-04-04 22:11:55 +0200

    rail-fence-cipher: Rewrite test names
    Rewrites the test names to match the canonical test data and
    stores the test data version.

--- exercises/rail-fence-cipher/rail_fence_cipher_test.py
+++ exercises/rail-fence-cipher/rail_fence_cipher_test.py
@@ -3,28 +3,30 @@ import unittest
 from rail_fence_cipher import encode, decode
 
 
+# test cases adapted from `x-common//canonical-data.json` @ version: 1.0.0
+
 class RailFenceTests(unittest.TestCase):
-    def test_encode_with_two_rails(self):
+    def test_to_encode_with_two_rails(self):
         self.assertMultiLineEqual(
             encode('XOXOXOXOXOXOXOXOXO', 2), 'XXXXXXXXXOOOOOOOOO')
 
-    def test_encode_with_three_rails(self):
+    def test_to_encode_with_three_rails(self):
         self.assertMultiLineEqual(
             encode('WEAREDISCOVEREDFLEEATONCE', 3),
             'WECRLTEERDSOEEFEAOCAIVDEN')
 
-    def test_encode_with_middle_stop(self):
+    def test_to_encode_with_ending_in_the_middle(self):
         self.assertMultiLineEqual(encode('EXERCISES', 4), 'ESXIEECSR')
 
-    def test_decode_with_three_rails(self):
+    def test_to_decode_with_three_rails(self):
         self.assertMultiLineEqual(
             decode('TEITELHDVLSNHDTISEIIEA', 3), 'THEDEVILISINTHEDETAILS')
 
-    def test_decode_with_five_rails(self):
+    def test_to_decode_with_five_rails(self):
         self.assertMultiLineEqual(
             decode('EIEXMSMESAORIWSCE', 5), 'EXERCISMISAWESOME')
 
-    def test_decode_with_six_rails(self):
+    def test_to_decode_with_six_rails(self):
         self.assertMultiLineEqual(
             decode(
                 '133714114238148966225439541018335470986172518171757571896261',
