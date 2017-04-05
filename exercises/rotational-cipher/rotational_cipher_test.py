2cf4d4387419e2ed688b7370a7e90ca968cbfdff
Tammo Behrends <behr.tam@gmail.com>
2017-04-05 11:36:23 +0200

    rotational-cipher: Add test data version

--- exercises/rotational-cipher/rotational_cipher_test.py
+++ exercises/rotational-cipher/rotational_cipher_test.py
@@ -3,6 +3,8 @@ import unittest
 import rotational_cipher
 
 
+# test cases adapted from `x-common//canonical-data.json` @ version: 1.0.0
+
 class RotationalCipher(unittest.TestCase):
     def test_rotate_a_by_1(self):
         self.assertEqual(rotational_cipher.rotate('a', 1), 'b')
@@ -23,29 +25,22 @@ class RotationalCipher(unittest.TestCase):
         self.assertEqual(rotational_cipher.rotate('OMG', 5), 'TRL')
 
     def test_rotate_spaces(self):
-        self.assertEqual(rotational_cipher.rotate('O M G', 5),
-                         'T R L')
+        self.assertEqual(rotational_cipher.rotate('O M G', 5), 'T R L')
 
     def test_rotate_numbers(self):
         self.assertEqual(
-            rotational_cipher.rotate(
-                                     'Testing 1 2 3 testing',
-                                     4),
+            rotational_cipher.rotate('Testing 1 2 3 testing', 4),
             'Xiwxmrk 1 2 3 xiwxmrk')
 
     def test_rotate_punctuation(self):
         self.assertEqual(
-            rotational_cipher.rotate(
-                                     "Let's eat, Grandma!",
-                                     21),
+            rotational_cipher.rotate("Let's eat, Grandma!", 21),
             "Gzo'n zvo, Bmviyhv!")
 
     def test_rotate_all_letters(self):
         self.assertEqual(
-            rotational_cipher.rotate(
-                                     "The quick brown fox jumps"
-                                     " over the lazy dog.",
-                                     13),
+            rotational_cipher.rotate("The quick brown fox jumps"
+                                     " over the lazy dog.", 13),
             "Gur dhvpx oebja sbk whzcf bire gur ynml qbt.")
 
 
