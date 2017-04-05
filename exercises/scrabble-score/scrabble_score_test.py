84dbfd4e097bee32aad34f54ff1f38951ad9b116
Tammo Behrends <behr.tam@gmail.com>
2017-04-05 10:57:31 +0200

    scrabble-score: Update test cases
    Updates the test cases according to the canonical test data
    and stores the test data version.

--- exercises/scrabble-score/scrabble_score_test.py
+++ exercises/scrabble-score/scrabble_score_test.py
@@ -3,27 +3,41 @@ import unittest
 from scrabble_score import score
 
 
+# test cases adapted from `x-common//canonical-data.json` @ version: 1.0.0
+
 class WordTest(unittest.TestCase):
-    def test_invalid_word_scores_zero(self):
-        self.assertEqual(score(''), 0)
-        self.assertEqual(score(' \t\n'), 0)
-        self.assertEqual(score('hous3'), 0)
-        self.assertEqual(score('wo rd'), 0)
+    def test_lowercase_letter(self):
+        self.assertEqual(score("a"), 1)
 
-    def test_scores_very_short_word(self):
-        self.assertEqual(score('a'), 1)
+    def test_uppercase_letter(self):
+        self.assertEqual(score("A"), 1)
 
-    def test_scores_other_very_short_word(self):
-        self.assertEqual(score('f'), 4)
+    def test_valuable_letter(self):
+        self.assertEqual(score("f"), 4)
 
-    def test_simple_word_scores_the_number_of_letters(self):
+    def test_short_word(self):
+        self.assertEqual(score("at"), 2)
+
+    def test_short_valuable_word(self):
+        self.assertEqual(score("zoo"), 12)
+
+    def test_medium_word(self):
         self.assertEqual(score("street"), 6)
 
-    def test_complicated_word_scores_more(self):
+    def test_medium_valuable_word(self):
         self.assertEqual(score("quirky"), 22)
 
-    def test_scores_are_case_insensitive(self):
+    def test_long_mixed_case_word(self):
         self.assertEqual(score("OxyphenButazone"), 41)
+
+    def test_english_like_word(self):
+        self.assertEqual(score("pinata"), 8)
+
+    def test_empty_input(self):
+        self.assertEqual(score(""), 0)
+
+    def test_entire_alphabet_available(self):
+        self.assertEqual(score("abcdefghijklmnopqrstuvwxyz"), 87)
 
 
 if __name__ == '__main__':
