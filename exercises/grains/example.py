b92960da6e83b8926dc0984de2a511ccff24bc38
C <eternallygeek@gmx.com>
2017-04-11 06:05:52 -0400

    grains: Add missing exceptions (#453)
    From Update grains exercise (tests and example) issue #450,
    it was noted that 3 tests for exceptions were missing
    - adds the missing test to the grains_test.py
    - adds the appropriate exception handling and definitions to example.py
    - makes the input parameter on the on_square function consistent naming
    
    close #450

--- exercises/grains/example.py
+++ exercises/grains/example.py
@@ -1,9 +1,20 @@
-def on_square(number):
-    return 2 ** (number - 1)
+def on_square(square):
+    check_square_input(square)
+    return 2 ** (square - 1)
 
 
 def total_after(square):
+    check_square_input(square)
     return sum(
         on_square(n + 1) for n
         in range(square)
     )
+
+
+def check_square_input(square):
+    if square == 0:
+        raise ValueError("Square input of zero is invalid.")
+    elif square < 0:
+        raise ValueError("Negative square input is invalid.")
+    elif square > 64:
+        raise ValueError("Square input greater than 64 is invalid.")
--- exercises/grains/grains_test.py
+++ exercises/grains/grains_test.py
@@ -1,6 +1,9 @@
 import unittest
 
-from grains import on_square, total_after
+from grains import (
+    on_square,
+    total_after,
+)
 
 
 class GrainsTest(unittest.TestCase):
@@ -31,6 +34,24 @@ class GrainsTest(unittest.TestCase):
     def test_square_64(self):
         self.assertEqual(on_square(64), 9223372036854775808)
         self.assertEqual(total_after(64), 18446744073709551615)
+
+    def test_square_0_raises_exception(self):
+        with self.assertRaises(ValueError):
+            on_square(0)
+        with self.assertRaises(ValueError):
+            total_after(0)
+
+    def test_square_negative_raises_exception(self):
+        with self.assertRaises(ValueError):
+            on_square(-1)
+        with self.assertRaises(ValueError):
+            total_after(-1)
+
+    def test_square_gt_64_raises_exception(self):
+        with self.assertRaises(ValueError):
+            on_square(65)
+        with self.assertRaises(ValueError):
+            total_after(65)
 
 
 if __name__ == '__main__':
