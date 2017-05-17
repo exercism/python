4c3fd880d1032a2247e7c18d46252ad665061e4b
Tammo Behrends <behr.tam@gmail.com>
2017-05-17 17:39:54 +0200

    difference-of-squares: Update tests
    Incorporates the updated canonical test data which added tests for 1,
    removed the tests for 10 and `difference 0`.
    
    https://github.com/exercism/x-common/pull/791

--- exercises/difference-of-squares/difference_of_squares_test.py
+++ exercises/difference-of-squares/difference_of_squares_test.py
@@ -6,32 +6,29 @@ from difference_of_squares import difference, square_of_sum, sum_of_squares
 # test cases adapted from `x-common//canonical-data.json` @ version: 1.0.0
 
 class DifferenceOfSquaresTest(unittest.TestCase):
+    def test_square_of_sum_1(self):
+        self.assertEqual(square_of_sum(1), 1)
+
     def test_square_of_sum_5(self):
         self.assertEqual(square_of_sum(5), 225)
-
-    def test_square_of_sum_10(self):
-        self.assertEqual(square_of_sum(10), 3025)
 
     def test_square_of_sum_100(self):
         self.assertEqual(square_of_sum(100), 25502500)
 
+    def test_sum_of_squares_1(self):
+        self.assertEqual(sum_of_squares(1), 1)
+
     def test_sum_of_squares_5(self):
         self.assertEqual(sum_of_squares(5), 55)
-
-    def test_sum_of_squares_10(self):
-        self.assertEqual(sum_of_squares(10), 385)
 
     def test_sum_of_squares_100(self):
         self.assertEqual(sum_of_squares(100), 338350)
 
-    def test_difference_of_squares_0(self):
-        self.assertEqual(difference(0), 0)
+    def test_difference_of_squares_1(self):
+        self.assertEqual(difference(1), 0)
 
     def test_difference_of_squares_5(self):
         self.assertEqual(difference(5), 170)
-
-    def test_difference_of_squares_10(self):
-        self.assertEqual(difference(10), 2640)
 
     def test_difference_of_squares_100(self):
         self.assertEqual(difference(100), 25164150)
