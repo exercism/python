a0c5e7049d6f3db6d7ab4e5b209c7e59e7279b3f
Carl Jackson <carl.jackson@gmail.com>
2017-05-30 11:54:47 -0400

    Bring up to Flake8 standard

--- exercises/protein-translation/example.py
+++ exercises/protein-translation/example.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python
 class InvalidCodonError(Exception):
-    def __init__(self,codon):
+    def __init__(self, codon):
         self.codon = codon
         Exception.__init__(self, 'Invalid Codon exception: %s is not a valid \
                            codon' % codon)
+
 
 class Translation(object):
 
     # Lookup table
     codonList = {'AUG': "Methionine", 'UUU': "Phenylalanine",
-                'UUC': "Phenylalanine", 'UUA': "Leucine", 'UUG': "Leucine",
-                'UCU': "Serine", 'UCC': "Serine", 'UCA': "Serine",
-                'UCG': "Serine", 'UAU': "Tyrosine", 'UAC': "Tyrosine",
-                'UGU': "Cysteine", 'UGC': "Cysteine", 'UGG': "Tryptophan",
-                'UAA': "STOP", 'UAG': "STOP", 'UGA': "STOP"}
+                 'UUC': "Phenylalanine", 'UUA': "Leucine", 'UUG': "Leucine",
+                 'UCU': "Serine", 'UCC': "Serine", 'UCA': "Serine",
+                 'UCG': "Serine", 'UAU': "Tyrosine", 'UAC': "Tyrosine",
+                 'UGU': "Cysteine", 'UGC': "Cysteine", 'UGG': "Tryptophan",
+                 'UAA': "STOP", 'UAG': "STOP", 'UGA': "STOP"}
 
     def __init__(self):
         self.codons = self.codonList
 
-    def ofCodon(self,codon):
+    def ofCodon(self, codon):
         try:
             return self.codons[codon]
         except KeyError:
             raise(InvalidCodonError(codon))
 
-
-    def ofRNA(self,strand):
+    def ofRNA(self, strand):
         output = []
-        for i in range(0,len(strand),3):
+        for i in range(0, len(strand), 3):
             codon = strand[i:i+3]
             try:
                 if self.codons[codon] == 'STOP':
--- exercises/protein-translation/protein_translation_test.py
+++ exercises/protein-translation/protein_translation_test.py
@@ -9,66 +9,55 @@ class ProteinTranslationTests(unittest.TestCase):
     def setUp(self):
         self.transl = Translation()
 
-
     def test_AUG_translates_to_methionine(self):
         self.assertEqual('Methionine', self.transl.ofCodon('AUG'))
 
-
     def test_identifies_Phenylalanine_codons(self):
-        for codon in ['UUU','UUC']:
+        for codon in ['UUU', 'UUC']:
             self.assertEqual('Phenylalanine', self.transl.ofCodon(codon))
 
-
     def test_identifies_Leucine_codons(self):
-        for codon in ['UUA','UUG']:
+        for codon in ['UUA', 'UUG']:
             self.assertEqual('Leucine', self.transl.ofCodon(codon))
 
-
     def test_identifies_Serine_codons(self):
-        for codon in ['UCU','UCC','UCA','UCG']:
+        for codon in ['UCU', 'UCC', 'UCA', 'UCG']:
             self.assertEqual('Serine', self.transl.ofCodon(codon))
 
-
     def test_identifies_Tyrosine_codons(self):
-        for codon in ['UAU','UAC']:
+        for codon in ['UAU', 'UAC']:
             self.assertEqual('Tyrosine', self.transl.ofCodon(codon))
 
-
     def test_identifies_Cysteine_codons(self):
-        for codon in ['UGU','UGC']:
+        for codon in ['UGU', 'UGC']:
             self.assertEqual('Cysteine', self.transl.ofCodon(codon))
-
 
     def test_identifies_Tryptophan_codons(self):
         self.assertEqual('Tryptophan', self.transl.ofCodon('UGG'))
 
-
     def test_identifies_stop_codons(self):
         for codon in ['UAA', 'UAG', 'UGA']:
             self.assertEqual('STOP', self.transl.ofCodon(codon))
-
 
     def test_translates_rna_strand_into_correct_protein(self):
         strand = 'AUGUUUUGG'
         expected = ['Methionine', 'Phenylalanine', 'Tryptophan']
         self.assertEqual(expected, self.transl.ofRNA(strand))
 
-
     def test_stops_translation_if_stop_codon_present(self):
         strand = 'AUGUUUUAA'
         expected = ['Methionine', 'Phenylalanine']
         self.assertEqual(expected, self.transl.ofRNA(strand))
-
 
     def test_stops_translation_of_longer_strand(self):
         strand = 'UGGUGUUAUUAAUGGUUU'
         expected = ['Tryptophan', 'Cysteine', 'Tyrosine']
         self.assertEqual(expected, self.transl.ofRNA(strand))
 
-
     def test_invalid_codons(self):
         strand = 'CARROT'
         self.assertRaises(InvalidCodonError, self.transl.ofRNA, strand)
+
 
 if __name__ == '__main__':
     unittest.main()
