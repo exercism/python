35ec8892cba71f4b987d9c0d8094fc40d850376a
justani98 <justani98@gmail.com>
2017-07-03 16:50:16 +0530

    Add test file

---
+++ exercises/change/change_test.py
@@ -0,0 +1,43 @@
+import unittest
+
+from change import find_minimum_coins
+
+
+class ChangeTest(unittest.TestCase):
+    def test_single_coin_change(self):
+        self.assertEqual(find_minimum_coins(25, [1, 5, 10, 25, 100]), [25])
+
+    def test_multiple_coin_change(self):
+        self.assertEqual(find_minimum_coins(15, [1, 5, 10, 25, 100]), [5, 10])
+
+    def test_change_with_Lilliputian_Coins(self):
+        self.assertEqual(find_minimum_coins(23, [1, 4, 15, 20, 50]),
+                         [4, 4, 15])
+
+    def test_change_with_Lower_Elbonia_Coins(self):
+        self.assertEqual(find_minimum_coins(63, [1, 5, 10, 21, 25]),
+                         [21, 21, 21])
+
+    def test_large_target_values(self):
+        self.assertEqual(find_minimum_coins(21, [2, 5, 10, 20, 50]),
+                         [2, 2, 2, 5, 10])
+
+    def test_possible_change_without_unit_coins_available(self):
+        self.assertEqual(find_minimum_coins(21, [2, 5, 10, 20, 50]),
+                         [2, 2, 2, 5, 10])
+
+    def test_no_coins_make_0_change(self):
+        self.assertEqual(find_minimum_coins(0, [1, 5, 10, 21, 25]), [])
+
+    def test_error_testing_for_change_smaller_than_smallest_coin(self):
+        self.assertEqual(find_minimum_coins(3, [5, 10]), -1)
+
+    def test_error_if_no_combination_can_add_up_to_target(self):
+        self.assertEqual(find_minimum_coins(94, [5, 10]), -1)
+
+    def test_cannot_find_negative_change_values(self):
+        self.assertEqual(find_minimum_coins(-5, [1, 2, 5]), -1)
+
+
+if __name__ == "__main__":
+    unittest.main()
