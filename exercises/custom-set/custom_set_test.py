01a8af6ce648e6973139d7179ed14d76b21a74f0
Javed Nissar <javednissar@gmail.com>
2017-07-22 09:43:53 +0200

    added all tests

--- exercises/custom-set/custom_set_test.py
+++ exercises/custom-set/custom_set_test.py
@@ -8,7 +8,7 @@ class CustomSetTest(unittest.TestCase):
         self.assertTrue(empty.empty())
     
     def test_not_empty(self):
-        not_empty = CustomSet([1])
+        not_empty = CustomSet(1)
         self.assertFalse(not_empty.empty())
     
     def test_empty_contains_nothing(self):
@@ -16,13 +16,13 @@ class CustomSetTest(unittest.TestCase):
         self.assertFalse(1 in empty)
     
     def test_simple_contains(self):
-        simple = CustomSet([1,2,3])
+        simple = CustomSet(1,2,3)
         self.assertTrue(1 in simple)
         self.assertTrue(2 in simple)
         self.assertTrue(3 in simple)
     
     def test_simple_not_contains(self):
-        simple = CustomSet([1,2,3])
+        simple = CustomSet(1,2,3)
         self.assertFalse(4 in simple)
     
     def test_empty_subset_of_empty(self):
@@ -31,26 +31,26 @@ class CustomSetTest(unittest.TestCase):
     
     def test_empty_subset_of_not_empty(self):
         empty = CustomSet()
-        not_empty = CustomSet([1])
+        not_empty = CustomSet(1)
         self.assertTrue(not_empty.subset(empty))
     
     def test_non_empty_not_subset_of_empty(self):
         empty = CustomSet()
-        not_empty = CustomSet([1])
+        not_empty = CustomSet(1)
         self.assertFalse(empty.subset(not_empty))
     
     def test_set_is_subset_of_itself(self):
-        test_set = CustomSet([1,2,3])
-        self.assertTrue(test_set.subset(CustomSet([1,2,3])))
+        test_set = CustomSet(1,2,3)
+        self.assertTrue(test_set.subset(CustomSet(1,2,3)))
     
     def test_set_is_subset_of_larger(self):
-        test_set = CustomSet([1,2,3])
-        larger = CustomSet([1,2,3,4])
+        test_set = CustomSet(1,2,3)
+        larger = CustomSet(1,2,3,4)
         self.assertTrue(larger.subset(test_set))
     
     def test_set_is_not_subset_of_larger(self):
-        test_set = CustomSet([1,2,3])
-        larger = CustomSet([1,4,5,6])
+        test_set = CustomSet(1,2,3)
+        larger = CustomSet(1,4,5,6)
         self.assertFalse(larger.subset(test_set))
 
     def test_empty_disjoint_with_empty(self):
@@ -59,23 +59,23 @@ class CustomSetTest(unittest.TestCase):
     
     def test_empty_disjoint_with_not_empty(self):
         empty = CustomSet()
-        not_empty = CustomSet([1,2,3])
+        not_empty = CustomSet(1,2,3)
         self.assertTrue(empty.disjoint(not_empty))
     
     def test_not_empty_disjoint_with_empty(self):
         empty = CustomSet()
-        not_empty = CustomSet([1,2,3])
+        not_empty = CustomSet(1,2,3)
         self.assertTrue(not_empty.disjoint(empty))
     
     def test_sets_that_share_element_not_disjoint(self):
-        seq = CustomSet([1,2,3])
-        odds = CustomSet([1,3,5])
+        seq = CustomSet(1,2,3)
+        odds = CustomSet(1,3,5)
         self.assertFalse(odds.disjoint(seq))
         self.assertFalse(seq.disjoint(odds))
     
     def test_sets_that_share_none_disjoint(self):
-        seq = CustomSet([1,2,3])
-        after = CustomSet([4,5,6])
+        seq = CustomSet(1,2,3)
+        after = CustomSet(4,5,6)
 
         self.assertTrue(seq.disjoint(after))
     
@@ -91,17 +91,20 @@ class CustomSetTest(unittest.TestCase):
         self.assertNotEqual(seq, after)
     
     def test_add_to_empty(self):
-        empty = CustomSet().add(1)
-        expected = CustomSet([1])
+        empty = CustomSet()
+        empty.add(1)
+        expected = CustomSet(1)
         self.assertEqual(empty, expected)
     
     def test_add_to_not_empty(self):
-        not_empty = CustomSet(1,2,3).add(4)
+        not_empty = CustomSet(1,2,3)
+        not_empty.add(4)
         expected = CustomSet(1,2,3,4)
         self.assertEqual(not_empty, expected)
     
     def test_add_existing_elem(self):
-        test_set = CustomSet(1,2,3).add(1)
+        test_set = CustomSet(1,2,3)
+        test_set.add(1)
         expected =  CustomSet(1,2,3)
         self.assertEqual(test_set, expected)
     
@@ -128,12 +131,12 @@ class CustomSetTest(unittest.TestCase):
     
     def test_empty_diff(self):
         empty = CustomSet()
-        self.assertEqual(empty.difference(empty), CustomSet())
+        self.assertEqual(empty - empty, CustomSet())
     
     def test_empty_non_empty_diff(self):
         empty = CustomSet()
         non_empty = CustomSet(1,2,3)
-        self.assertEqual(empty - non_empty, non_empty)
+        self.assertEqual(empty - non_empty, empty)
         self.assertEqual(non_empty - empty, non_empty)
     
     def test_non_empty_diff(self):
