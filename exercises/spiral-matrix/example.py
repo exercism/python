d072f34f62dd49e62221218d2004e5327c1a5530
Average-user <lucaspolymeris@gmail.com>
2017-09-21 02:09:59 -0300

    spiral-matrix: implement.

---
+++ exercises/spiral-matrix/example.py
@@ -0,0 +1,11 @@
+def spiral(n):
+    m = [[0] * n for i in range(n)]
+    dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+    x, y, c = 0, -1, 1
+    for i in range(n + n - 1):
+        for j in range((n + n - i) // 2):
+            x += dx[i % 4]
+            y += dy[i % 4]
+            m[x][y] = c
+            c += 1
+    return m
---
+++ exercises/spiral-matrix/spiral_matrix_test.py
@@ -0,0 +1,38 @@
+import unittest
+
+from spiral_matrix import spiral
+
+
+class SpiralMatrixTests(unittest.TestCase):
+
+    def spiral_matrix_of_0(self):
+        self.assertEqual(spiral(0), [])
+
+    def spiral_matrix_of_1(self):
+        self.assertEqual(spiral(1), [[1]])
+
+    def spiral_matrix_of_2(self):
+        self.assertEqual(spiral(2), [[1, 2],
+                                     [4, 3]])
+
+    def spiral_matrix_of_3(self):
+        self.assertEqual(spiral(3), [[1, 2, 3],
+                                     [8, 9, 4],
+                                     [7, 6, 5]])
+
+    def spiral_matrix_of_4(self):
+        self.assertEqual(spiral(4), [[1,  2,  3,  4],
+                                     [12, 13, 14, 5],
+                                     [11, 16, 15, 6],
+                                     [10, 9,  8,  7]])
+
+    def spiral_matrix_of_5(self):
+        self.assertEqual(spiral(5), [[1,  2,  3,  4,  5],
+                                     [16, 17, 18, 19, 6],
+                                     [15, 24, 25, 20, 7],
+                                     [14, 23, 22, 21, 8],
+                                     [13, 12, 11, 10, 9]])
+
+
+if __name__ == '__main__':
+    unittest.main()
