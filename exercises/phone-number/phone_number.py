b71d1517e49be2037690d485e567b38efa905f87
Sharno <sharnoby3@gmail.com>
2017-10-03 03:01:17 -0400

    Added more parameters till rectangles + fixed linting issues

--- exercises/phone-number/phone_number.py
+++ exercises/phone-number/phone_number.py
@@ -1,8 +1,9 @@
 class Phone(object):
     def __init__(self, number):
         pass
+
     def pretty(self):
         pass
+
     def area_code(self):
         pass
-    
--- exercises/queen-attack/queen_attack.py
+++ exercises/queen-attack/queen_attack.py
@@ -1,6 +1,6 @@
-def board():
+def board(pos1, pos2):
     pass
 
 
-def can_attack():
+def can_attack(pos1, pos2):
     pass
