6d95b1b75b5ac3689f81dc04900b16f8d3a0e586
Sharno <sharnoby3@gmail.com>
2017-10-03 03:37:57 -0400

    added parameter to init of Cipher and the methods that are being tested

--- exercises/simple-cipher/simple_cipher.py
+++ exercises/simple-cipher/simple_cipher.py
@@ -1,7 +1,12 @@
 class Cipher(object):
-    def __init__(self):
+    def __init__(self, key):
         pass
 
+    def encode(self, text):
+        pass
+
+    def decode(self, text):
+        pass
 
 class Caesar(object):
     def __init__(self):
