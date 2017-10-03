adf06cde19dd941a32d94b158b61455ec304803a
Saurabh Chalke <saurabhchalke@gmail.com>
2017-10-03 17:35:33 +0530

    perfect-number: add parameters to exercise placeholder function

--- exercises/perfect-numbers/perfect_numbers.py
+++ exercises/perfect-numbers/perfect_numbers.py
@@ -1,6 +1,7 @@
-def divisor_generator():
+def divisor_generator(number):
     pass
 
 
-def is_perfect():
+def is_perfect(number):
+''' A perfect number equals the sum of its positive divisors. '''
     pass
