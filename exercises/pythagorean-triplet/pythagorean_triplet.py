f6b9ed584979ac58096d12c042a35cd9868d6072
Parin Porecha <parinporecha@gmail.com>
2017-10-04 15:01:17 +0530

    pythagorean-triplet: add parameters to exercise placeholder

--- exercises/pythagorean-triplet/pythagorean_triplet.py
+++ exercises/pythagorean-triplet/pythagorean_triplet.py
@@ -1,10 +1,10 @@
-def primitive_triplets():
+def primitive_triplets(nbr):
     pass
 
 
-def triplets_in_range():
+def triplets_in_range(range_start, range_end):
     pass
 
 
-def is_triplet():
+def is_triplet(triplet):
     pass
