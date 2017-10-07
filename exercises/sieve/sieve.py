abfeca82654df657fcc92a199b71b5273509469b
teohxuen <32366507+teohxuen@users.noreply.github.com>
2017-10-08 03:59:31 +0800

    Update sieve.py

--- exercises/sieve/sieve.py
+++ exercises/sieve/sieve.py
@@ -1,2 +1,9 @@
 def sieve(limit):
-    pass
+    isprimes = [True] * (limit+1)
+    primes = [2]
+    for n in range(3,limit+1,2):
+        if isprimes[n] is True:
+            primes.append(n)
+            for i in range(n*n,limit+1,n):
+                isprimes[i] = False
+    return primes
