a5350dcb16d4bed7de54fc4c3fee017cde0e6087
Rodrigo <rodrigo.redcode@gmail.com>
2017-10-08 19:06:44 -0300

    Add more testes, examle file and a readme

--- exercises/binary-search-tree/binary_search_tree.py
+++ exercises/binary-search-tree/binary_search_tree.py
@@ -1,66 +1,7 @@
 class TreeNode(object):
     def __init__(self, value):
-        self.value = value
-        self.left_node = None
-        self.right_node = None
-
-    def __str__(self):
-        return str(self.value)
+        pass
 
 class BinarySearchTree(object):
     def __init__(self):
-        self.root = None
-
-    def add(self, value):
-        if(self.root is None):
-            self.root = TreeNode(value)
-        else:
-            inserted = False
-            cur_node = self.root
-
-            while not inserted:
-                if(value < cur_node.value):
-                    if(cur_node.left_node):
-                        cur_node = cur_node.left_node
-                    else:
-                        cur_node.left_node = TreeNode(value)
-                        inserted = True
-                        return inserted
-                elif(value >  cur_node.value):
-                    if(cur_node.right_node):
-                        cur_node = cur_node.right_node
-                    else:
-                        cur_node.right_node = TreeNode(value)
-                        inserted = True
-                        return inserted            
-
-    def search(self, searched_number):
-        cur_node = self.root
-        found = False
-        while not found:
-            if(searched_number < cur_node.value):
-                cur_node = cur_node.left_node
-            elif(searched_number > cur_node.value):
-                cur_node = cur_node.right_node
-            elif(searched_number == cur_node.value):
-                return cur_node
-            else:
-                raise ValueError("Element not found")
-                    
-    def print_inorder(self, node):
-        if(node is not None):
-            self.print_inorder(node.left_node)
-            print(node.value)
-            self.print_inorder(node.right_node)
-
-    def print_preorder(self, node):
-        if(node is not None):
-            print(node.value)
-            self.print_preorder(node.left_node)
-            self.print_preorder(node.right_node)
-
-    def print_postorder(self, node):
-        if(node is not None):
-            self.print_preorder(node.left_node)
-            self.print_preorder(node.right_node)
-            print(node.value)
+        pass
--- exercises/binary-search-tree/binary_search_tree_test.py
+++ exercises/binary-search-tree/binary_search_tree_test.py
@@ -5,18 +5,30 @@ from binary_search_tree import BinarySearchTree
 class BinarySearchTreeTests(unittest.TestCase):
     bst = BinarySearchTree()
 
-    def test_add_integer_number(self):
+    def test_add_integer_number1(self):
         self.assertTrue(self.bst.add(7))
+    
+    def test_add_integer_number2(self):
+        self.assertTrue(self.bst.add(1))
+
+    def test_add_integer_number3(self):
+        self.assertTrue(self.bst.add(6))
+
+    def test_add_integer_number4(self):
+        self.assertTrue(self.bst.add(5))
 
     def test_add_float_number(self):
         self.assertTrue(self.bst.add(7.5))
 
-    def test_search_valid_number(self):
-        self.assertEqual(self.bst.search(7), 7)
-    
-    def test_search_invalid_number(self):
-        self.assertFalse(self.bst.search(8), 8)
+    def test_search_valid_number1(self):
+        self.assertEqual(self.bst.search(7).value, 7)
 
+    def test_search_valid_number2(self):
+        self.assertEqual(self.bst.search(7.5).value, 7.5)
+
+    def test_search_valid_number3(self):
+        self.assertEqual(self.bst.search(1).value, 1)
+    
 
 if __name__ == '__main__':
     unittest.main()
