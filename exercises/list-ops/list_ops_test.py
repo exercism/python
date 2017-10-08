1bee015283c0c2d66151a92d2f09d8d70de455ab
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-10-08 23:16:36 +0100

    Fix typo in test version comment

--- exercises/list-ops/list_ops_test.py
+++ exercises/list-ops/list_ops_test.py
@@ -4,7 +4,7 @@ import operator
 import list_ops
 
 
-# Tests adapted from problem-specification//canonical-data.json @ version 2.0.0
+# Tests adapted from problem-specifications//canonical-data.json @ v2.0.0
 
 class ListOpsTest(unittest.TestCase):
 
