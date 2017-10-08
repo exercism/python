6c3d95d0fba630a41db17cb798dc11ad524fc49d
Nathan Parsons <nathan.parsons@warwick.ac.uk>
2017-10-08 20:56:24 +0100

    Reorder functions to match test and canonical-data.json order

--- exercises/list-ops/example.py
+++ exercises/list-ops/example.py
@@ -1,25 +1,28 @@
-def map_clone(function, xs):
-    return [function(elem) for elem in xs]
+def append(xs, y):
+    xs[len(xs):] = [y]
+    return xs
 
 
-def length(xs):
-    return sum(1 for _ in xs)
+def concat(xs, ys):
+    if not ys:
+        return xs
+    else:
+        for item in ys:
+            xs.append(item)
+        return xs
 
 
 def filter_clone(function, xs):
     return [x for x in xs if function(x)]
 
 
-def reverse(xs):
-    if not xs:
-        return []
-    else:
-        return xs[::-1]
+def length(xs):
+    return sum(1 for _ in xs)
 
 
-def append(xs, y):
-    xs[len(xs):] = [y]
-    return xs
+def map_clone(function, xs):
+    return [function(elem) for elem in xs]
+
 
 
 def foldl(function, xs, acc):
@@ -35,21 +38,8 @@ def foldr(function, xs, acc):
     else:
         return function(xs[0], foldr(function, xs[1:], acc))
 
-
-def flat(xs):
-    out = []
-    for item in xs:
-        if isinstance(item, list):
-            out.extend(flat(item))
-        else:
-            out.append(item)
-    return out
-
-
-def concat(xs, ys):
-    if not ys:
-        return xs
+def reverse(xs):
+    if not xs:
+        return []
     else:
-        for item in ys:
-            xs.append(item)
-        return xs
+        return xs[::-1]
