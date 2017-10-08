249cdaa516c833e8583569e13e50e4326959191d
Yunchih Chen <yunchih.cat@gmail.com>
2017-10-08 11:14:03 +0800

    go-counting: Implement the exercise "go-counting"
    See #748

---
+++ exercises/go-counting/example.py
@@ -0,0 +1,57 @@
+
+class GoCounting:
+
+    none = ""
+    white = "W"
+    black = "B"
+    stones = [black, white]
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    def __init__(self, board):
+        self.board = board.splitlines()
+        self.width = len(self.board[0])
+        self.height = len(self.board)
+
+    def valid(self, x, y):
+        return x >= 0 and x < self.width and y >= 0 and y < self.height
+
+    def walk_board(self, x, y, visited_territory=[], visited_coords=[], visited_stones=[]):
+        if not (x, y) in visited_coords and self.valid(x, y):
+            s = self.board[y][x]
+            if s in self.stones:
+                if s not in visited_stones:
+                    return (visited_territory, visited_stones + [s])
+            else: # s is empty
+                for d in self.directions:
+                    visited_territory, visited_stones = self.walk_board(x + d[0], y + d[1],
+                                                                        visited_territory + [(x, y)],
+                                                                        visited_coords + [(x, y)],
+                                                                        visited_stones)
+
+        return (visited_territory, visited_stones)
+
+    def territoryFor(self, coord):
+        assert(len(coord) == 2)
+        x, y = coord[0], coord[1]
+        if not self.valid(x, y) or self.board[y][x] in self.stones:
+            return (self.none, set())
+
+        visited_territory, visited_stones = self.walk_board(x, y)
+        result = set(visited_territory)
+
+        if len(visited_stones) == 1:
+            return (visited_stones[0], result)
+        return (self.none, result)
+
+    def territories(self):
+        owners = self.stones + [self.none]
+        result = dict([(owner, set()) for owner in owners])
+        visited = set()
+        for y in range(self.height):
+            for x in range(self.width):
+                if not (x, y) in visited:
+                    owner, owned_territories = self.territoryFor((x, y))
+                    result[owner].update(owned_territories)
+                    visited.update(owned_territories)
+
+        return result
---
+++ exercises/go-counting/gocountingtest.py
@@ -0,0 +1,87 @@
+import unittest
+import gocounting
+
+board5x5 = "\n".join([
+    "  B  ",
+    " B B ",
+    "B W B",
+    " W W ",
+    "  W  "
+])
+
+board9x9 = "\n".join([
+    "  B   B  ",
+    "B   B   B",
+    "WBBBWBBBW",
+    "W W W W W",
+    "         ",
+    " W W W W ",
+    "B B   B B",
+    " W BBB W ",
+    "   B B   "
+])
+
+class GoCountingTest(unittest.TestCase):
+    def test_5x5_for_black(self):
+        board = gocounting.GoCounting(board5x5)
+        stone, territory = board.territoryFor((0, 1))
+        self.assertEqual(stone, board.black)
+        self.assertEqual(territory, set([(0, 0), (0, 1), (1, 0)]))
+
+    def test_5x5_for_white(self):
+        board = gocounting.GoCounting(board5x5)
+        stone, territory = board.territoryFor((2, 3))
+        self.assertEqual(stone, board.white)
+        self.assertEqual(territory, set([(2, 3)]))
+
+    def test_5x5_for_open_territory(self):
+        board = gocounting.GoCounting(board5x5)
+        stone, territory = board.territoryFor((1, 4))
+        self.assertEqual(stone, board.none)
+        self.assertEqual(territory, set([(0, 3), (0, 4), (1, 4)]))
+
+    def test_5x5_for_non_territory(self):
+        board = gocounting.GoCounting(board5x5)
+        stone, territory = board.territoryFor((1, 1))
+        self.assertEqual(stone, board.none)
+        self.assertEqual(territory, set())
+
+    def test_5x5_for_valid_coordinate(self):
+        board = gocounting.GoCounting(board5x5)
+        stone, territory = board.territoryFor((-1, 1))
+        self.assertEqual(stone, board.none)
+        self.assertEqual(territory, set())
+
+    def test_5x5_for_valid_coordinate2(self):
+        board = gocounting.GoCounting(board5x5)
+        stone, territory = board.territoryFor((1, 5))
+        self.assertEqual(stone, board.none)
+        self.assertEqual(territory, set())
+
+    def test_one_territory_whole_board(self):
+        board = gocounting.GoCounting(" ")
+        territories = board.territories()
+        self.assertEqual(territories[board.black], set())
+        self.assertEqual(territories[board.white], set())
+        self.assertEqual(territories[board.none], set([(0, 0)]))
+
+    def test_two_territories_rectangular_board(self):
+        input_board = "\n".join([
+            " BW ",
+            " BW "
+        ])
+        board = gocounting.GoCounting(input_board)
+        territories = board.territories()
+        self.assertEqual(territories[board.black], set([(0, 0), (0, 1)]))
+        self.assertEqual(territories[board.white], set([(3, 0), (3, 1)]))
+        self.assertEqual(territories[board.none], set())
+
+    def test_9x9_for_open_territory(self):
+        board = gocounting.GoCounting(board9x9)
+        stone, territory = board.territoryFor((0, 8))
+        self.assertEqual(stone, board.none)
+        self.assertEqual(territory, set([(2, 7), (2, 8), (1, 8), (0, 8), (0, 7)]))
+
+
+if __name__ == '__main__':
+    unittest.main()
