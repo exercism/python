deb6fea29ff59319d0c0e382d24affed26fbfcb8
Corey McCandless <crm1994@gmail.com>
2017-10-09 13:06:17 -0500

    zipper: add template solution

---
+++ exercises/zipper/zipper.py
@@ -0,0 +1,22 @@
+class Zipper(object):
+    @staticmethod
+    def from_tree(tree):
+        pass
+
+    def left(self):
+        pass
+
+    def set_left(self):
+        pass
+
+    def right(self):
+        pass
+
+    def set_right(self):
+        pass
+
+    def up(self):
+        pass
+
+    def to_tree(self):
+        pass
