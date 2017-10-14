f7ad3fcd02ab03e19b1e5e5f3c5819f246f22f03
Pavel Ilyin <ipaul64321@gmail.com>
2017-10-14 21:13:38 +0300

    markdown: Add exercise to resolve #729

---
+++ exercises/markdown/markdown.py
@@ -0,0 +1,71 @@
+import re
+
+
+def parse_markdown(markdown):
+    lines = markdown.split('\n')
+    res = ''
+    in_list = False
+    for i in lines:
+        if re.match('###### (.*)', i) is not None:
+            i = '<h6>' + i[7:] + '</h6>'
+        elif re.match('## (.*)', i) is not None:
+            i = '<h2>' + i[3:] + '</h2>'
+        elif re.match('# (.*)', i) is not None:
+            i = '<h1>' + i[2:] + '</h1>'
+        m = re.match(r'\* (.*)', i)
+        if m:
+            if not in_list:
+                in_list = True
+                is_bold = False
+                is_italic = False
+                curr = m.group(1)
+                m1 = re.match('(.*)__(.*)__(.*)', curr)
+                if m1:
+                    curr = m1.group(1) + '<strong>' + \
+                        m1.group(2) + '</strong>' + m1.group(3)
+                    is_bold = True
+                m1 = re.match('(.*)_(.*)_(.*)', curr)
+                if m1:
+                    curr = m1.group(1) + '<em>' + m1.group(2) + \
+                        '</em>' + m1.group(3)
+                    is_italic = True
+                if is_italic or is_bold:
+                    i = '<ul><li>' + curr + '</li>'
+                else:
+                    i = '<ul><li><p>' + curr + '</p></li>'
+            else:
+                is_bold = False
+                is_italic = False
+                curr = m.group(1)
+                m1 = re.match('(.*)__(.*)__(.*)', curr)
+                if m1:
+                    curr = m1.group(1) + '<strong>' + \
+                        m1.group(2) + '</strong>' + m1.group(3)
+                    is_bold = True
+                m1 = re.match('(.*)_(.*)_(.*)', curr)
+                if m1:
+                    curr = m1.group(1) + '<em>' + m1.group(2) + \
+                        '</em>' + m1.group(3)
+                    is_italic = True
+                if is_italic or is_bold:
+                    i = '<li>' + curr + '</li>'
+                else:
+                    i = '<li><p>' + curr + '</p></li>'
+        else:
+            if in_list:
+                i = '</ul>+i'
+                in_list = False
+
+        m = re.match('<h|<ul|<p|<li', i)
+        if not m:
+            i = '<p>' + i + '</p>'
+        m = re.match('(.*)__(.*)__(.*)', i)
+        if m:
+            i = m.group(1) + '<strong>' + m.group(2) + '</strong>' + m.group(3)
+        m = re.match('(.*)_(.*)_(.*)', i)
+        if m:
+            i = m.group(1) + '<em>' + m.group(2) + '</em>' + m.group(3)
+        res += i
+    if in_list:
+        res += '</ul>'
+    return res
