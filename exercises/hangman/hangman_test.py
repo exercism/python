075460118417f0ba8609e2d7d33485bac2e5744d
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2017-10-15 11:59:31 +0530

    [WIP] Implement hangaman excercise

---
+++ exercises/hangman/hangman_test.py
@@ -0,0 +1,11 @@
+import unittest
+import hangman
+
+
+class HangmanTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual("hangman", "hangman")
+
+
+if __name__ == '__main__':
+    unittest.main()
