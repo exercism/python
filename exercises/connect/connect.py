e6eedd53520fa51226f56a0252e653feefa73e76
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2017-10-17 23:32:12 -0200

    Initial python files created

---
+++ exercises/connect/connect.py
@@ -0,0 +1,2 @@
+def connect(board):
+    pass
---
+++ exercises/connect/connect_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+import connect.py
+
+# Tests adapted from "problem-specifications//canonical-data.json" @ v1.0.0
+
+class ConnectTest(unittest.TestCase):
+    def test_connect(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
