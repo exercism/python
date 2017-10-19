7bcf67181f3814922fba596ea599bf287eafce14
Hernan Olarte <hernan940730@gmail.com>
2017-10-19 01:50:37 -0500

    binary-search-tree: Implement exercise binary-search-tree

--- exercises/binary-search-tree/binary_search_tree_test.py
+++ exercises/binary-search-tree/binary_search_tree_test.py
@@ -2,14 +2,14 @@ import unittest
 
 from binary_search_tree import BinarySearchTree
 
-class BinarySearchTreeTests(unittest.TestCase):
-    
+
+class BinarySearchTreeTests(unittest.TestCase):    
     def test_add_one_value_in_tree(self):
         tree = BinarySearchTree()
         self.assertEqual(tree.add(3), True)
         self.assertEqual(tree.size(), 1)
         self.assertEqual(tree.contains(3), True)
-    
+
     def test_add_multiple_values_in_tree(self):
         tree = BinarySearchTree()
         self.assertEqual(tree.add(-1), True)
@@ -21,7 +21,7 @@ class BinarySearchTreeTests(unittest.TestCase):
         self.assertEqual(tree.contains(50), True)
         self.assertEqual(tree.contains(-73), True)
         self.assertEqual(tree.contains(20), True)
-        
+
     def test_add_repeated_values_in_tree(self):
         tree = BinarySearchTree()
         self.assertEqual(tree.add(2), True)
@@ -41,13 +41,13 @@ class BinarySearchTreeTests(unittest.TestCase):
         self.assertEqual(tree.contains(3), True)
         self.assertEqual(tree.contains(4), True)
         self.assertEqual(tree.contains(5), True)
-        
+
     def test_remove_one_value_in_tree_with_no_elements(self):
         tree = BinarySearchTree()
         self.assertEqual(tree.remove(70), False)
         self.assertEqual(tree.size(), 0)
         self.assertEqual(tree.contains(70), False)
-        
+
     def test_remove_multiple_values_in_tree_with_no_elements(self):
         tree = BinarySearchTree()
         self.assertEqual(tree.remove(70), False)
@@ -62,7 +62,7 @@ class BinarySearchTreeTests(unittest.TestCase):
         self.assertEqual(tree.remove(0), True)
         self.assertEqual(tree.size(), 0)
         self.assertEqual(tree.contains(0), False)
-        
+
     def test_remove_multiple_values_in_tree(self):
         tree = BinarySearchTree()
         tree.add(3)
@@ -83,12 +83,12 @@ class BinarySearchTreeTests(unittest.TestCase):
         self.assertEqual(tree.contains(5), False)
         self.assertEqual(tree.contains(6), False)
         self.assertEqual(tree.contains(7), False)
-        
+
     def test_contains_value_in_tree_with_no_elements(self):
         tree = BinarySearchTree()
         self.assertEqual(tree.contains(-123), False)
         self.assertEqual(tree.contains(-0), False)
-        
+
     def test_contains_value_in_tree_with_multiple_operations(self):
         tree = BinarySearchTree()
         tree.remove(1)
@@ -117,11 +117,11 @@ class BinarySearchTreeTests(unittest.TestCase):
         self.assertEqual(tree.contains(3), True)
         self.assertEqual(tree.contains(4), True)
         self.assertEqual(tree.contains(5), True)
-        
+
     def test_size_in_tree_with_no_elements(self):
         tree = BinarySearchTree()
         self.assertEqual(tree.size(), 0)
-    
+
     def test_size_in_tree_with_multiple_operations(self):
         tree = BinarySearchTree()
         self.assertEqual(tree.size(), 0)
--- exercises/binary-search-tree/example.py
+++ exercises/binary-search-tree/example.py
@@ -2,12 +2,13 @@ class Node:
     def __init__(self):
         self.children = [None, None]
         self.num = None
-    
+
+
 class BinarySearchTree:
     def __init__(self):
         self.__size = 0
         self.__root = Node()
-    
+
     def __base_function(self, num):
         node = self.__root
         while (not node.num == None):
@@ -22,32 +23,30 @@ class BinarySearchTree:
             else:
                 break
         return node
-    
+
     def __replace_node(self, node):
         node.num = None
-        
         pos = -1
-        
+
         for i in range(len(node.children)):
             if not node.children[i] == None and not node.children[i].num == None:
                 pos = i
-        
+
         if not pos == -1:
             child_node = node.children[pos]
-            
             n_pos = (pos + 1) % 2
             while (not child_node.children[n_pos] == None and not child_node.children[n_pos].num == None):
                 child_node = child_node.children[n_pos]
             node.num = child_node.num
             self.__replace_node(child_node)
-    
+
     def contains(self, num):
         node = self.__base_function(num)
         if node.num == None:
             return False
         else:
             return True
-    
+
     def add(self, num):
         node = self.__base_function(num)
         if node.num == None:
@@ -55,7 +54,7 @@ class BinarySearchTree:
             self.__size += 1
             return True
         return False
-    
+
     def remove(self, num):
         node = self.__base_function(num)
         if not node.num == None:
@@ -63,6 +62,6 @@ class BinarySearchTree:
             self.__size -= 1
             return True
         return False
-    
+
     def size(self):
         return self.__size
