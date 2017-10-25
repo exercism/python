033ca16dbc987ba9da444b5ab37bc4d6a816c871
Abhijit Parida <abhijitparida.me@gmail.com>
2017-10-25 18:31:03 +0530

    space-age: Ref problem-specification in test case

--- exercises/space-age/space_age_test.py
+++ exercises/space-age/space_age_test.py
@@ -3,7 +3,7 @@ import unittest
 from space_age import SpaceAge
 
 
-# test cases adapted from `x-common//canonical-data.json` @ version: 1.0.0
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.0.0
 
 class SpaceAgeTest(unittest.TestCase):
 
