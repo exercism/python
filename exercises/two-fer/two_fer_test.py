3e08ec7340728dcc9751bbbc1c43aee2d7d627a8
Denis Looby <denis@loobyonline.com>
2017-10-26 00:55:54 +0100

    flake8 fixes

--- exercises/two-fer/two_fer_test.py
+++ exercises/two-fer/two_fer_test.py
@@ -2,14 +2,16 @@ import unittest
 
 import two_fer
 
-#Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
+
 
 class Two_Fer_test(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(two_fer.two_fer(), 'One for you, one for me.')
 
     def test_alice(self):
-        self.assertEqual(two_fer.two_fer("Alice"), "One for Alice, one for me.")
+        self.assertEqual(two_fer.two_fer("Alice"),
+                         "One for Alice, one for me.")
 
     def test_bob(self):
         self.assertEqual(two_fer.two_fer("Bob"), "One for Bob, one for me.")
