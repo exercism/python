ee98110c1115bb6ddd27727cc87aa12ed0119ab0
Élysson MR <elyssonmr@gmail.com>
2017-10-26 21:48:26 -0200

    saddle-points: Update the space before class and the assert order

--- exercises/saddle-points/saddle_points_test.py
+++ exercises/saddle-points/saddle_points_test.py
@@ -9,28 +9,29 @@ import unittest
 
 from saddle_points import saddle_points
 
+
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.0.0
 class SaddlePointTest(unittest.TestCase):
     def test_one_saddle(self):
         inp = [[9, 8, 7], [5, 3, 2], [6, 6, 7]]
-        self.assertEqual(set([(1, 0)]), saddle_points(inp))
+        self.assertEqual(saddle_points(inp), set([(1, 0)]))
 
     def test_empty_matrix(self):
-        self.assertEqual(set(), saddle_points([]))
+        self.assertEqual(saddle_points([]), set())
 
     def test_no_saddle(self):
         inp = [[1, 2, 3], [3, 1, 2], [2, 3, 1]]
-        self.assertEqual(set(), saddle_points(inp))
+        self.assertEqual(saddle_points(inp), set())
 
     def test_mult_saddle(self):
         inp = [[4, 5, 4], [3, 5, 5], [1, 5, 4]]
         ans = set([(0, 1), (1, 1), (2, 1)])
-        self.assertEqual(ans, saddle_points(inp))
+        self.assertEqual(saddle_points(inp), ans)
 
     def test_indentify_saddle_bottom_right_corner(self):
         inp = [[8, 7, 9], [6, 7, 6], [3, 2, 5]]
         ans = set([(2, 2)])
-        self.assertEqual(ans, saddle_points(inp))
+        self.assertEqual(saddle_points(inp), ans)
 
     # Additional tests for this track
 
