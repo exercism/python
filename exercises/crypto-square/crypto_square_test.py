3c95514abfc0d64f897f56413b472a54800937c0
Élysson MR <elyssonmr@gmail.com>
2017-10-26 21:31:25 -0200

    crypto-square: Update the assert order and fix the space before class

--- exercises/crypto-square/crypto_square_test.py
+++ exercises/crypto-square/crypto_square_test.py
@@ -2,37 +2,38 @@ import unittest
 
 from crypto_square import encode
 
+
 # Tests adapted from `problem-specifications//canonical-data.json` @ v3.1.0
 class CryptoSquareTest(unittest.TestCase):
     def test_empty_string(self):
-        self.assertEqual('', encode(''))
+        self.assertEqual(encode(''), '')
 
     def test_lowercase(self):
-        self.assertEqual('a', encode('A'))
+        self.assertEqual(encode('A'), 'a')
 
     def test_remove_spaces(self):
-        self.assertEqual('b', encode('  b '))
+        self.assertEqual(encode('  b '), 'b')
 
     def test_remove_punctuation(self):
-        self.assertEqual('1', encode('@1,%!'))
+        self.assertEqual(encode('@1,%!'), '1')
 
     def test_9chars_results_3chunks(self):
-        self.assertEqual('tsf hiu isn', encode('This is fun!'))
+        self.assertEqual(encode('This is fun!'), 'tsf hiu isn')
 
     def test_8chars_results_3chunks_ending_space(self):
-        self.assertEqual('clu hlt io ', encode('Chill out.'))
+        self.assertEqual(encode('Chill out.'), 'clu hlt io ')
 
     def test_54chars_results_7chunks_2ending_space(self):
         self.assertEqual(
-            'imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau ',
             encode('If man was meant to stay on the ground, '
-                   'god would have given us roots.')
+                   'god would have given us roots.'),
+            'imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau '
         )
 
     # Additional tests for this track
 
     def test_perfect_square(self):
-        self.assertEqual('ac bd', encode('ABCD'))
+        self.assertEqual(encode('ABCD'), 'ac bd')
 
     def test_small_imperfect_square(self):
         self.assertEqual(encode('This is easy!'), 'tis hsy ie sa')
@@ -46,7 +47,7 @@ class CryptoSquareTest(unittest.TestCase):
         msg = ('If man was meant to stay on the ground, god would have given '
                'us roots.')
         ciph = 'imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau'
-        self.assertEqual(ciph, encode(msg))
+        self.assertEqual(encode(msg), ciph)
 
 
 if __name__ == '__main__':
