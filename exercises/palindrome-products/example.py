584947d3a73a71bf5d07228c6adf29e6ef314f39
Ejaaz Merali <ejaaz.merali@gmail.com>
2017-10-27 19:58:14 -0400

    Update palindrome products to reflect changes to canonical data

--- exercises/palindrome-products/example.py
+++ exercises/palindrome-products/example.py
@@ -1,16 +1,38 @@
 def largest_palindrome(max_factor, min_factor=0):
-    return max(palindromes(max_factor, min_factor), key=lambda tup: tup[0])
+    if max_factor < min_factor:
+        raise ValueError("invalid input: min is {min} and max is {max}"
+                         .format(min=min_factor, max=max_factor))
+
+    palindromes_found = palindromes(max_factor, min_factor)
+
+    if len(palindromes_found) == 0:
+        raise ValueError("no palindrome with factors in the "
+                         "range {min} to {max}"
+                         .format(min=min_factor, max=max_factor))
+
+    return max(palindromes_found, key=lambda tup: tup[0])
 
 
 def smallest_palindrome(max_factor, min_factor):
-    return min(palindromes(max_factor, min_factor), key=lambda tup: tup[0])
+    if max_factor < min_factor:
+        raise ValueError("invalid input: min is {min} and max is {max}"
+                         .format(min=min_factor, max=max_factor))
+
+    palindromes_found = palindromes(max_factor, min_factor)
+
+    if len(palindromes_found) == 0:
+        raise ValueError("no palindrome with factors in the "
+                         "range {min} to {max}"
+                         .format(min=min_factor, max=max_factor))
+
+    return min(palindromes_found, key=lambda tup: tup[0])
 
 
 def palindromes(max_factor, min_factor):
-    return ((a * b, (a, b))
+    return [(a * b, (a, b))
             for a in range(min_factor, max_factor + 1)
             for b in range(min_factor, a + 1)
-            if is_palindrome(a * b))
+            if is_palindrome(a * b)]
 
 
 def is_palindrome(n):
