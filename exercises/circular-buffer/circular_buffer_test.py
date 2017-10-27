1616bbf842470800b5dafc699cbf552678a45348
Élysson MR <elyssonmr@gmail.com>
2017-10-26 22:56:04 -0200

    circular-buffer: Apply the suggested changes

--- exercises/circular-buffer/circular_buffer_test.py
+++ exercises/circular-buffer/circular_buffer_test.py
@@ -8,6 +8,7 @@ from circular_buffer import (
 
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.0.1
+
 class CircularBufferTest(unittest.TestCase):
     def test_read_empty_buffer(self):
         buf = CircularBuffer(1)
@@ -17,12 +18,12 @@ class CircularBufferTest(unittest.TestCase):
     def test_read_just_written_item(self):
         buf = CircularBuffer(1)
         buf.write('1')
-        self.assertEqual('1', buf.read())
+        self.assertEqual(buf.read(), '1')
 
     def test_write_and_read_back_one_item(self):
         buf = CircularBuffer(1)
         buf.write('1')
-        self.assertEqual('1', buf.read())
+        self.assertEqual(buf.read(), '1')
         with self.assertRaises(BufferEmptyException):
             buf.read()
 
@@ -30,8 +31,8 @@ class CircularBufferTest(unittest.TestCase):
         buf = CircularBuffer(2)
         buf.write('1')
         buf.write('2')
-        self.assertEqual('1', buf.read())
-        self.assertEqual('2', buf.read())
+        self.assertEqual(buf.read(), '1')
+        self.assertEqual(buf.read(), '2')
 
     def test_full_buffer_cant_written(self):
         buf = CircularBuffer(1)
@@ -40,7 +41,7 @@ class CircularBufferTest(unittest.TestCase):
             buf.write('2')
 
     def test_alternate_write_and_read(self):
-        buf = CircularBuffer(2)
+        buf = CircularBuffer(1)
         buf.write('1')
         self.assertEqual(buf.read(), '1')
         buf.write('2')
@@ -50,10 +51,10 @@ class CircularBufferTest(unittest.TestCase):
         buf = CircularBuffer(3)
         buf.write('1')
         buf.write('2')
-        self.assertEqual('1', buf.read())
+        self.assertEqual(buf.read(), '1')
         buf.write('3')
-        self.assertEqual('2', buf.read())
-        self.assertEqual('3', buf.read())
+        self.assertEqual(buf.read(), '2')
+        self.assertEqual(buf.read(), '3')
 
     def test_clearing_buffer(self):
         buf = CircularBuffer(1)
@@ -67,28 +68,28 @@ class CircularBufferTest(unittest.TestCase):
         buf.write('1')
         buf.clear()
         buf.write('2')
-        self.assertEqual('2', buf.read())
+        self.assertEqual(buf.read(), '2')
 
     def test_clear_does_nothin_empty_buffer(self):
         buf = CircularBuffer(1)
         buf.clear()
         buf.write('1')
-        self.assertEqual('1', buf.read())
+        self.assertEqual(buf.read(), '1')
 
     def test_overwrite_non_full_buffer(self):
         buf = CircularBuffer(2)
         buf.write('1')
         buf.overwrite('2')
-        self.assertEqual('1', buf.read())
-        self.assertEqual('2', buf.read())
+        self.assertEqual(buf.read(), '1')
+        self.assertEqual(buf.read(), '2')
 
     def test_overwrite_replaces_oldest_item(self):
         buf = CircularBuffer(2)
         buf.write('1')
         buf.write('2')
         buf.overwrite('3')
-        self.assertEqual('2', buf.read())
-        self.assertEqual('3', buf.read())
+        self.assertEqual(buf.read(), '2')
+        self.assertEqual(buf.read(), '3')
 
     def test_write_full_buffer(self):
         buf = CircularBuffer(2)
@@ -102,12 +103,12 @@ class CircularBufferTest(unittest.TestCase):
         buf.write('1')
         buf.write('2')
         buf.write('3')
-        self.assertEqual('1', buf.read())
+        self.assertEqual(buf.read(), '1')
         buf.write('4')
         buf.overwrite('5')
-        self.assertEqual('3', buf.read())
-        self.assertEqual('4', buf.read())
-        self.assertEqual('5', buf.read())
+        self.assertEqual(buf.read(), '3')
+        self.assertEqual(buf.read(), '4')
+        self.assertEqual(buf.read(), '5')
 
 
 if __name__ == '__main__':
