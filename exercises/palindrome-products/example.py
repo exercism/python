c0b2fcf827ab7ec80b825c2f2f6813fdc0e8009e
Ejaaz Merali <ejaaz.merali@gmail.com>
2017-10-27 22:22:45 -0400

    Refactor example to reduce code duplication

--- exercises/palindrome-products/example.py
+++ exercises/palindrome-products/example.py
@@ -1,38 +1,30 @@
 def largest_palindrome(max_factor, min_factor=0):
-    if max_factor < min_factor:
-        raise ValueError("invalid input: min is {min} and max is {max}"
-                         .format(min=min_factor, max=max_factor))
-
-    palindromes_found = palindromes(max_factor, min_factor)
-
-    if len(palindromes_found) == 0:
-        raise ValueError("no palindrome with factors in the "
-                         "range {min} to {max}"
-                         .format(min=min_factor, max=max_factor))
-
-    return max(palindromes_found, key=lambda tup: tup[0])
+    return max(palindromes(max_factor, min_factor), key=lambda tup: tup[0])
 
 
 def smallest_palindrome(max_factor, min_factor):
-    if max_factor < min_factor:
-        raise ValueError("invalid input: min is {min} and max is {max}"
-                         .format(min=min_factor, max=max_factor))
-
-    palindromes_found = palindromes(max_factor, min_factor)
-
-    if len(palindromes_found) == 0:
-        raise ValueError("no palindrome with factors in the "
-                         "range {min} to {max}"
-                         .format(min=min_factor, max=max_factor))
-
-    return min(palindromes_found, key=lambda tup: tup[0])
+    return min(palindromes(max_factor, min_factor), key=lambda tup: tup[0])
 
 
 def palindromes(max_factor, min_factor):
-    return [(a * b, (a, b))
-            for a in range(min_factor, max_factor + 1)
-            for b in range(min_factor, a + 1)
-            if is_palindrome(a * b)]
+    if max_factor < min_factor:
+        raise ValueError("invalid input: min is {min_factor}"
+                         "and max is {max_factor}"
+                         .format(min_factor=min_factor,
+                                 max_factor=max_factor))
+
+    palindromes_found = [(a * b, (a, b))
+                         for a in range(min_factor, max_factor + 1)
+                         for b in range(min_factor, a + 1)
+                         if is_palindrome(a * b)]
+
+    if len(palindromes_found) == 0:
+        raise ValueError("no palindrome with factors in the "
+                         "range {min_factor} to {max_factor}"
+                         .format(min_factor=min_factor,
+                                 max_factor=max_factor))
+
+    return palindromes_found
 
 
 def is_palindrome(n):
