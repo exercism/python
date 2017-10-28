c2c817c6371fa0d892d2bcaa32d1ecb121d93129
Denis Looby <denis@loobyonline.com>
2017-10-28 16:35:16 +0100

    Restored exception test cases for total_after

--- exercises/grains/grains_test.py
+++ exercises/grains/grains_test.py
@@ -33,14 +33,20 @@ class GrainsTest(unittest.TestCase):
     def test_square_0_raises_exception(self):
         with self.assertRaises(ValueError):
             on_square(0)
+        with self.assertRaises(ValueError):
+            total_after(0)
 
     def test_square_negative_raises_exception(self):
         with self.assertRaises(ValueError):
             on_square(-1)
+        with self.assertRaises(ValueError):
+            total_after(-1)
 
     def test_square_gt_64_raises_exception(self):
         with self.assertRaises(ValueError):
             on_square(65)
+        with self.assertRaises(ValueError):
+            total_after(65)
 
     def test_total(self):
         self.assertEqual(total_after(64), 18446744073709551615)
