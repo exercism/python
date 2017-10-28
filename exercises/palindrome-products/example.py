b54bc9ee658873b75c53b6fe628aee5bbc260d6c
Ejaaz Merali <ejaaz.merali@gmail.com>
2017-10-28 16:25:57 -0400

    Fix code style errors

--- exercises/palindrome-products/example.py
+++ exercises/palindrome-products/example.py
@@ -9,21 +9,23 @@ def smallest_palindrome(max_factor, min_factor):
 
 
 def get_extreme_palindrome_with_factors(max_factor, min_factor, extreme):
-    palindromes_found = palindromes(max_factor, min_factor, reverse = (extreme == "largest"))
+    palindromes_found = palindromes(max_factor, min_factor,
+                                    reverse=(extreme == "largest"))
     factor_pairs = None
     for palin in palindromes_found:
         factor_pairs = ((fact, palin // fact)
                         for fact in range(min_factor, max_factor + 1)
                         if palin % fact == 0)
-        factor_pairs = list(pair for pair in factor_pairs if min_factor <= pair[1] <= max_factor)
+        factor_pairs = list(pair for pair in factor_pairs
+                            if min_factor <= pair[1] <= max_factor)
         if len(factor_pairs) > 0:
             break
 
     if factor_pairs is None or len(factor_pairs) == 0:
         raise ValueError("no palindrome with factors in the "
-                 "range {min_factor} to {max_factor}"
-                 .format(min_factor=min_factor,
-                         max_factor=max_factor))
+                         "range {min_factor} to {max_factor}"
+                         .format(min_factor=min_factor,
+                                 max_factor=max_factor))
 
     return (palin, set(map(frozenset, factor_pairs)))
 
@@ -37,10 +39,10 @@ def palindromes(max_factor, min_factor, reverse=False):
 
     if reverse:
         return (p for p in range(max_factor ** 2, (min_factor ** 2) - 1, -1)
-                  if is_palindrome(p))
+                if is_palindrome(p))
     else:
         return (p for p in range(min_factor ** 2, (max_factor ** 2) + 1)
-                  if is_palindrome(p))
+                if is_palindrome(p))
 
 
 def is_palindrome(n):
