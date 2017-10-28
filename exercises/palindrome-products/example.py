540b65ff8268cf8992608800cd4226e1c3552832
Ejaaz Merali <ejaaz.merali@gmail.com>
2017-10-28 16:22:13 -0400

    Modify example code and tests to follow problem instructions better (ie. compute ALL factor pairs for the desired palindromes)

--- exercises/palindrome-products/example.py
+++ exercises/palindrome-products/example.py
@@ -1,30 +1,46 @@
-def largest_palindrome(max_factor, min_factor=0):
-    return max(palindromes(max_factor, min_factor), key=lambda tup: tup[0])
+def largest_palindrome(max_factor, min_factor):
+    return get_extreme_palindrome_with_factors(max_factor, min_factor,
+                                               "largest")
 
 
 def smallest_palindrome(max_factor, min_factor):
-    return min(palindromes(max_factor, min_factor), key=lambda tup: tup[0])
+    return get_extreme_palindrome_with_factors(max_factor, min_factor,
+                                               "smallest")
 
 
-def palindromes(max_factor, min_factor):
+def get_extreme_palindrome_with_factors(max_factor, min_factor, extreme):
+    palindromes_found = palindromes(max_factor, min_factor, reverse = (extreme == "largest"))
+    factor_pairs = None
+    for palin in palindromes_found:
+        factor_pairs = ((fact, palin // fact)
+                        for fact in range(min_factor, max_factor + 1)
+                        if palin % fact == 0)
+        factor_pairs = list(pair for pair in factor_pairs if min_factor <= pair[1] <= max_factor)
+        if len(factor_pairs) > 0:
+            break
+
+    if factor_pairs is None or len(factor_pairs) == 0:
+        raise ValueError("no palindrome with factors in the "
+                 "range {min_factor} to {max_factor}"
+                 .format(min_factor=min_factor,
+                         max_factor=max_factor))
+
+    return (palin, set(map(frozenset, factor_pairs)))
+
+
+def palindromes(max_factor, min_factor, reverse=False):
     if max_factor < min_factor:
         raise ValueError("invalid input: min is {min_factor} "
                          "and max is {max_factor}"
                          .format(min_factor=min_factor,
                                  max_factor=max_factor))
 
-    palindromes_found = [(a * b, (a, b))
-                         for a in range(min_factor, max_factor + 1)
-                         for b in range(min_factor, a + 1)
-                         if is_palindrome(a * b)]
-
-    if len(palindromes_found) == 0:
-        raise ValueError("no palindrome with factors in the "
-                         "range {min_factor} to {max_factor}"
-                         .format(min_factor=min_factor,
-                                 max_factor=max_factor))
-
-    return palindromes_found
+    if reverse:
+        return (p for p in range(max_factor ** 2, (min_factor ** 2) - 1, -1)
+                  if is_palindrome(p))
+    else:
+        return (p for p in range(min_factor ** 2, (max_factor ** 2) + 1)
+                  if is_palindrome(p))
 
 
 def is_palindrome(n):
--- exercises/palindrome-products/palindrome_products_test.py
+++ exercises/palindrome-products/palindrome_products_test.py
@@ -22,42 +22,42 @@ class PalindromesTests(unittest.TestCase):
     def test_smallest_palindrome_from_single_digit_factors(self):
         value, factors = smallest_palindrome(min_factor=1, max_factor=9)
         self.assertEqual(value, 1)
-        self.assertEqual(set(factors), {1, 1})
+        self.assertEqual(factors, {frozenset((1, 1))})
 
     def test_largest_palindrome_from_single_digit_factors(self):
         value, factors = largest_palindrome(min_factor=1, max_factor=9)
         self.assertEqual(value, 9)
-        self.assertIn(set(factors), [{1, 9}, {3, 3}])
+        self.assertEqual(factors, {frozenset((1, 9)), frozenset((3, 3))})
 
     def test_smallest_palindrome_from_double_digit_factors(self):
         value, factors = smallest_palindrome(min_factor=10, max_factor=99)
         self.assertEqual(value, 121)
-        self.assertEqual(set(factors), {11, 11})
+        self.assertEqual(factors, {frozenset((11, 11))})
 
     def test_largest_palindrome_from_double_digit_factors(self):
         value, factors = largest_palindrome(min_factor=10, max_factor=99)
         self.assertEqual(value, 9009)
-        self.assertEqual(set(factors), {91, 99})
+        self.assertEqual(factors, {frozenset((91, 99))})
 
     def test_smallest_palindrome_from_triple_digit_factors(self):
         value, factors = smallest_palindrome(min_factor=100, max_factor=999)
         self.assertEqual(value, 10201)
-        self.assertEqual(set(factors), {101, 101})
+        self.assertEqual(factors, {frozenset((101, 101))})
 
     def test_largest_palindrome_from_triple_digit_factors(self):
         value, factors = largest_palindrome(min_factor=100, max_factor=999)
         self.assertEqual(value, 906609)
-        self.assertEqual(set(factors), {913, 993})
+        self.assertEqual(factors, {frozenset((913, 993))})
 
     def test_smallest_palindrome_from_four_digit_factors(self):
         value, factors = smallest_palindrome(min_factor=1000, max_factor=9999)
         self.assertEqual(value, 1002001)
-        self.assertEqual(set(factors), {1001, 1001})
+        self.assertEqual(factors, {frozenset((1001, 1001))})
 
     def test_largest_palindrome_from_four_digit_factors(self):
         value, factors = largest_palindrome(min_factor=1000, max_factor=9999)
         self.assertEqual(value, 99000099)
-        self.assertEqual(set(factors), {9901, 9999})
+        self.assertEqual(factors, {frozenset((9901, 9999))})
 
     def test_empty_for_smallest_palindrome_if_none_in_range(self):
         with self.assertRaisesRegexp(
