10627f265ca1234d2d8f7b1d7e9c60bac41d3e89
Rodrigo <rodrigo.redcode@gmail.com>
2017-11-02 22:54:30 -0200

    Add skeletons for add and search methods

--- exercises/binary-search-tree/binary_search_tree.py
+++ exercises/binary-search-tree/binary_search_tree.py
@@ -1,8 +1,14 @@
 class TreeNode(object):
     def __init__(self, value):
-        pass
+        self.value = value
 
 
 class BinarySearchTree(object):
     def __init__(self):
+        pass
+
+    def add(self, value):
+        pass
+
+    def search(self, search_number):
         pass
