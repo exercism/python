f302c8b981f29ef121e7ae8223363b0771818703
Nikolay Mirin <nikolay.a.mirin@gmail.com>
2017-11-23 16:49:43 -0700

    Update alphametics_test.py
    Added a longest test case

--- exercises/alphametics/alphametics_test.py
+++ exercises/alphametics/alphametics_test.py
@@ -66,6 +66,39 @@ class TestAlphametics(unittest.TestCase):
              "S": 6,
              "T": 9})
 
+    def test_puzzle_with_9999over_letters(self):
+        self.assertEqual(
+            solve(("THIS+A+FIRE+ THEREFORE+FOR+ALL+HISTORIES+I+TELL+A+TALE+"
+                   "THAT+FALSIFIES+ITS+TITLE+ TIS+A+LIE+ THE+TALE+OF+THE+"
+                   "LAST+FIRE+HORSES+ LATE+AFTER+THE+FIRST+FATHERS+FORESEE+"
+                   "THE+HORRORS+ THE+LAST+FREE+TROLL+TERRIFIES+THE+HORSES+OF+"
+                   "FIRE+ THE+TROLL+RESTS+AT+THE+HOLE+OF+LOSSES+ IT+IS+THERE+"
+                   "THAT+SHE+STORES+ROLES+OF+LEATHERS+ AFTER+SHE+SATISFIES+"
+                   "HER+HATE+OFF+THOSE+FEARS+ A+TASTE+RISES+AS+SHE+HEARS+THE+"
+                   "LEAST+FAR+HORSE+ THOSE+FAST+HORSES+THAT+FIRST+HEAR+THE+"
+                   "TROLL+FLEE+OFF+TO+THE+FOREST+ THE+HORSES+THAT+ALERTS+"
+                   "RAISE+THE+STARES+OF+THE+OTHERS+ AS+THE+TROLL+ASSAILS+AT+"
+                   "THE+TOTAL+SHIFT+ HER+TEETH+TEAR+HOOF+OFF+TORSO+AS+THE+"
+                   "LAST+HORSE+FORFEITS+ITS+LIFE+ THE+FIRST+FATHERS+HEAR+OF+"
+                   "THE+HORRORS+ THEIR+FEARS+THAT+THE+FIRES+FOR+THEIR+FEASTS+"
+                   "ARREST+ AS+THE+FIRST+FATHERS+RESETTLE+THE+LAST+OF+THE+"
+                   "FIRE+HORSES+ THE+LAST+TROLL+HARASSES+THE+FOREST+HEART+"
+                   "FREE+AT+LAST+OF+THE+LAST+TROLL+ ALL+OFFER+THEIR+FIRE+HEAT+"
+                   "TO+THE+ASSISTERS+FAR+OFF+THE+TROLL+FASTS+ITS+LIFE+SHORTER+"
+                   "AS+STARS+RISE+ THE+HORSES+REST+SAFE+AFTER+ALL+SHARE+HOT+"
+                   "FISH+ AS+THEIR+AFFILIATES+TAILOR+A+ROOFS+FOR+THEIR+SAFE=="
+                   "FORTRESSES")),
+            {"F": 5,
+             "S": 4,
+             "L": 2,
+             "I": 7,
+             "T": 9,
+             "H": 8,
+             "A": 1,
+             "R": 3,
+             "O": 6,
+             "E": 0})
+
 
 if __name__ == '__main__':
     unittest.main()
