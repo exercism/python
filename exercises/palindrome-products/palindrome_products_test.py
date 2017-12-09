bab45a068c4581f696a11c368cf9289dd8581869
Ejaaz Merali <emerali@users.noreply.github.com>
2017-12-09 14:12:56 -0500

    Return to using nested sets (for the sake of performance)

--- exercises/palindrome-products/palindrome_products_test.py
+++ exercises/palindrome-products/palindrome_products_test.py
@@ -22,50 +22,42 @@ class PalindromesTests(unittest.TestCase):
     def test_smallest_palindrome_from_single_digit_factors(self):
         value, factors = smallest_palindrome(min_factor=1, max_factor=9)
         self.assertEqual(value, 1)
-        self.assertEqual(self.normalize_factors(factors),
-                         {(1, 1)})
+        self.assertFactorsEqual(factors, {(1, 1)})
 
     def test_largest_palindrome_from_single_digit_factors(self):
         value, factors = largest_palindrome(min_factor=1, max_factor=9)
         self.assertEqual(value, 9)
-        self.assertEqual(self.normalize_factors(factors),
-                         {(1, 9), (3, 3)})
+        self.assertFactorsEqual(factors, {(1, 9), (3, 3)})
 
     def test_smallest_palindrome_from_double_digit_factors(self):
         value, factors = smallest_palindrome(min_factor=10, max_factor=99)
         self.assertEqual(value, 121)
-        self.assertEqual(self.normalize_factors(factors),
-                         {(11, 11)})
+        self.assertFactorsEqual(factors, {(11, 11)})
 
     def test_largest_palindrome_from_double_digit_factors(self):
         value, factors = largest_palindrome(min_factor=10, max_factor=99)
         self.assertEqual(value, 9009)
-        self.assertEqual(self.normalize_factors(factors),
-                         {(91, 99)})
+        self.assertFactorsEqual(factors, {(91, 99)})
 
     def test_smallest_palindrome_from_triple_digit_factors(self):
         value, factors = smallest_palindrome(min_factor=100, max_factor=999)
         self.assertEqual(value, 10201)
-        self.assertEqual(self.normalize_factors(factors),
-                         {(101, 101)})
+        self.assertFactorsEqual(factors, {(101, 101)})
 
     def test_largest_palindrome_from_triple_digit_factors(self):
         value, factors = largest_palindrome(min_factor=100, max_factor=999)
         self.assertEqual(value, 906609)
-        self.assertEqual(self.normalize_factors(factors),
-                         {(913, 993)})
+        self.assertFactorsEqual(factors, {(913, 993)})
 
     def test_smallest_palindrome_from_four_digit_factors(self):
         value, factors = smallest_palindrome(min_factor=1000, max_factor=9999)
         self.assertEqual(value, 1002001)
-        self.assertEqual(self.normalize_factors(factors),
-                         {(1001, 1001)})
+        self.assertFactorsEqual(factors, {(1001, 1001)})
 
     def test_largest_palindrome_from_four_digit_factors(self):
         value, factors = largest_palindrome(min_factor=1000, max_factor=9999)
         self.assertEqual(value, 99000099)
-        self.assertEqual(self.normalize_factors(factors),
-                         {(9901, 9999)})
+        self.assertFactorsEqual(factors, {(9901, 9999)})
 
     def test_empty_for_smallest_palindrome_if_none_in_range(self):
         with self.assertRaises(ValueError):
@@ -85,8 +77,11 @@ class PalindromesTests(unittest.TestCase):
         with self.assertRaises(ValueError):
             value, factors = largest_palindrome(min_factor=2, max_factor=1)
 
-    def normalize_factors(self, factors):
-        return {tuple(sorted(factor_pair)) for factor_pair in factors}
+    # Utility methods
+
+    def assertFactorsEqual(self, actual, expected):
+        self.assertEqual(set(map(frozenset, actual)),
+                         set(map(frozenset, expected)))
 
 
 if __name__ == '__main__':
