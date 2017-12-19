77a93d096f9454126bc17999bb490a44cdaddcd2
Corey McCandless <crm1994@gmail.com>
2017-12-19 08:38:17 -0600

    palindrome-products: conform to canonical data

--- exercises/palindrome-products/example.py
+++ exercises/palindrome-products/example.py
@@ -1,18 +1,29 @@
+from itertools import groupby
+
+
 def largest_palindrome(max_factor, min_factor=0):
-    return max(palindromes(max_factor, min_factor), key=lambda tup: tup[0])
+    return next(palindromes(max_factor, min_factor, True))
 
 
 def smallest_palindrome(max_factor, min_factor):
-    return min(palindromes(max_factor, min_factor), key=lambda tup: tup[0])
+    return next(palindromes(max_factor, min_factor))
 
 
-def palindromes(max_factor, min_factor):
-    return ((a * b, (a, b))
-            for a in range(min_factor, max_factor + 1)
-            for b in range(min_factor, a + 1)
-            if is_palindrome(a * b))
+def palindromes(max_factor, min_factor, reverse=False):
+    return (
+        (prod, list(zip(*grp))[1])
+        for prod, grp in groupby(sorted(
+            (
+                (a * b, (a, b))
+                for a in range(min_factor, max_factor + 1)
+                for b in range(min_factor, a + 1)
+                if is_palindrome(str(a * b))
+            ),
+            key=lambda tup: tup[0],
+            reverse=reverse
+        ), key=lambda tup: tup[0])
+    )
 
 
-def is_palindrome(n):
-    s = str(n)
-    return s == s[::-1]
+def is_palindrome(num_str):
+    return num_str == num_str[::-1]
--- exercises/palindrome-products/palindrome_products_test.py
+++ exercises/palindrome-products/palindrome_products_test.py
@@ -16,31 +16,48 @@ import unittest
 from palindrome_products import smallest_palindrome, largest_palindrome
 
 
+def set_recursive(collection):
+    return frozenset(
+        set_recursive(e) if hasattr(e, '__iter__') else e for e in collection
+    )
+
+
 class PalindromesTests(unittest.TestCase):
+    def assertSetEqualRecursive(self, actual, expected):
+        self.assertSetEqual(
+            set_recursive(actual),
+            set_recursive(expected)
+        )
+
+    def test_smallest_palindrome_from_single_digit_factors(self):
+        value, factors = smallest_palindrome(max_factor=9, min_factor=1)
+        self.assertEqual(value, 1)
+        self.assertSetEqualRecursive(factors, [{1, 1}])
+
     def test_largest_palindrome_from_single_digit_factors(self):
         value, factors = largest_palindrome(max_factor=9)
         self.assertEqual(value, 9)
-        self.assertIn(set(factors), [{1, 9}, {3, 3}])
-
-    def test_largest_palindrome_from_double_digit_factors(self):
-        value, factors = largest_palindrome(max_factor=99, min_factor=10)
-        self.assertEqual(value, 9009)
-        self.assertEqual(set(factors), {91, 99})
+        self.assertSetEqualRecursive(factors, [{1, 9}, {3, 3}])
 
     def test_smallest_palindrome_from_double_digit_factors(self):
         value, factors = smallest_palindrome(max_factor=99, min_factor=10)
         self.assertEqual(value, 121)
-        self.assertEqual(set(factors), {11})
+        self.assertSetEqualRecursive(factors, [{11, 11}])
 
-    def test_largest_palindrome_from_triple_digit_factors(self):
-        value, factors = largest_palindrome(max_factor=999, min_factor=100)
-        self.assertEqual(value, 906609)
-        self.assertEqual(set(factors), {913, 993})
+    def test_largest_palindrome_from_double_digit_factors(self):
+        value, factors = largest_palindrome(max_factor=99, min_factor=10)
+        self.assertEqual(value, 9009)
+        self.assertSetEqualRecursive(factors, [{91, 99}])
 
     def test_smallest_palindrome_from_triple_digit_factors(self):
         value, factors = smallest_palindrome(max_factor=999, min_factor=100)
         self.assertEqual(value, 10201)
-        self.assertEqual(set(factors), {101, 101})
+        self.assertSetEqualRecursive(factors, [{101, 101}])
+
+    def test_largest_palindrome_from_triple_digit_factors(self):
+        value, factors = largest_palindrome(max_factor=999, min_factor=100)
+        self.assertEqual(value, 906609)
+        self.assertSetEqualRecursive(factors, [{913, 993}])
 
 
 if __name__ == '__main__':
