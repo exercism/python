fd14531acd49978b8010445097672c6ec51d66b1
Khoa A. Tran <khoa102@gmail.com>
2018-01-10 08:43:11 +0700

    Update all files to follow PEP8 standard

---
+++ exercises/hangman/README.md
@@ -0,0 +1,8 @@
+# Need edit
+Implement the logic of the hangman game using functional reactive programming.
+
+Hangman is a simple word guessing game.
+
+Functional Reactive Programming is a way to write interactive programs. It differs from the usual perspective in that instead of saying "when the button is pressed increment the counter", you write "the value of the counter is the sum of the number of times the button is pressed."
+
+Implement the basic logic behind hangman using functional reactive programming. You'll need to install an FRP library for this, this will be described in the language/track specific files of the exercise.
--- exercises/hangman/hangman_test.py
+++ exercises/hangman/hangman_test.py
@@ -2,79 +2,81 @@ import unittest
 
 from example import Hangman
 
+
+# Adapt from c-sharp track
 class HangmanTests(unittest.TestCase):
-	def test_initially_9_failures_are_allowed(self):
-		game = Hangman('foo')
-		self.assertEqual(game.get_status(), 'busy')
-		self.assertEqual(game.remainingGuesses, 9)
+    def test_initially_9_failures_are_allowed(self):
+        game = Hangman('foo')
+        self.assertEqual(game.get_status(), 'busy')
+        self.assertEqual(game.remainingGuesses, 9)
 
-	def test_initially_no_letters_are_guessed(self):
-		game = Hangman('foo')
+    def test_initially_no_letters_are_guessed(self):
+        game = Hangman('foo')
 
-		self.assertEqual(game.get_masked_word(), '___')
+        self.assertEqual(game.get_masked_word(), '___')
 
-	def test_after_10_failures_the_game_is_over(self):
-		game = Hangman('foo')
+    def test_after_10_failures_the_game_is_over(self):
+        game = Hangman('foo')
 
-		for i in range(10):
-			game.guess('x')
+        for i in range(10):
+            game.guess('x')
 
-		self.assertEqual(game.get_status(), 'lose')
+        self.assertEqual(game.get_status(), 'lose')
 
-	def test_feeding_a_correct_letter_removes_underscores(self):
-		game = Hangman('foobar')
+    def test_feeding_a_correct_letter_removes_underscores(self):
+        game = Hangman('foobar')
 
-		game.guess('b')
-		self.assertEqual(game.get_status(), 'busy')
-		self.assertEqual(game.remainingGuesses, 9)
-		self.assertEqual(game.get_masked_word(),'___b__')
+        game.guess('b')
+        self.assertEqual(game.get_status(), 'busy')
+        self.assertEqual(game.remainingGuesses, 9)
+        self.assertEqual(game.get_masked_word(), '___b__')
 
-		game.guess('o')
-		self.assertEqual(game.get_status(), 'busy')
-		self.assertEqual(game.remainingGuesses, 9)
-		self.assertEqual(game.get_masked_word(),'_oob__')
+        game.guess('o')
+        self.assertEqual(game.get_status(), 'busy')
+        self.assertEqual(game.remainingGuesses, 9)
+        self.assertEqual(game.get_masked_word(), '_oob__')
 
-	def test_feeding_a_correct_letter_twice_counts_as_a_failure(self):
-		game = Hangman('foobar')
+    def test_feeding_a_correct_letter_twice_counts_as_a_failure(self):
+        game = Hangman('foobar')
 
-		game.guess('b')
-		self.assertEqual(game.get_status(), 'busy')
-		self.assertEqual(game.remainingGuesses, 9)
-		self.assertEqual(game.get_masked_word(),'___b__')
+        game.guess('b')
+        self.assertEqual(game.get_status(), 'busy')
+        self.assertEqual(game.remainingGuesses, 9)
+        self.assertEqual(game.get_masked_word(), '___b__')
 
-		game.guess('b')
-		self.assertEqual(game.get_status(), 'busy')
-		self.assertEqual(game.remainingGuesses, 8)
-		self.assertEqual(game.get_masked_word(),'___b__')
+        game.guess('b')
+        self.assertEqual(game.get_status(), 'busy')
+        self.assertEqual(game.remainingGuesses, 8)
+        self.assertEqual(game.get_masked_word(), '___b__')
 
-	def test_getting_all_the_letters_right_makes_for_a_win(self):
-		game = Hangman('hello')
+    def test_getting_all_the_letters_right_makes_for_a_win(self):
+        game = Hangman('hello')
 
-		game.guess('b')
-		self.assertEqual(game.get_status(), 'busy')
-		self.assertEqual(game.get_status(), 'busy')
-		self.assertEqual(game.remainingGuesses, 8)
-		self.assertEqual(game.get_masked_word(),'_____')
+        game.guess('b')
+        self.assertEqual(game.get_status(), 'busy')
+        self.assertEqual(game.get_status(), 'busy')
+        self.assertEqual(game.remainingGuesses, 8)
+        self.assertEqual(game.get_masked_word(), '_____')
 
-		game.guess('e')
-		self.assertEqual(game.get_status(), 'busy')
-		self.assertEqual(game.remainingGuesses, 8)
-		self.assertEqual(game.get_masked_word(),'_e___')
+        game.guess('e')
+        self.assertEqual(game.get_status(), 'busy')
+        self.assertEqual(game.remainingGuesses, 8)
+        self.assertEqual(game.get_masked_word(), '_e___')
 
-		game.guess('l')
-		self.assertEqual(game.get_status(), 'busy')
-		self.assertEqual(game.remainingGuesses, 8)
-		self.assertEqual(game.get_masked_word(),'_ell_')
+        game.guess('l')
+        self.assertEqual(game.get_status(), 'busy')
+        self.assertEqual(game.remainingGuesses, 8)
+        self.assertEqual(game.get_masked_word(), '_ell_')
 
-		game.guess('o')
-		self.assertEqual(game.get_status(), 'busy')
-		self.assertEqual(game.remainingGuesses, 8)
-		self.assertEqual(game.get_masked_word(),'_ello')
+        game.guess('o')
+        self.assertEqual(game.get_status(), 'busy')
+        self.assertEqual(game.remainingGuesses, 8)
+        self.assertEqual(game.get_masked_word(), '_ello')
 
-		game.guess('h')
-		self.assertEqual(game.get_status(), 'win')
-		self.assertEqual(game.get_masked_word(), 'hello')
+        game.guess('h')
+        self.assertEqual(game.get_status(), 'win')
+        self.assertEqual(game.get_masked_word(), 'hello')
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
