93d8ddcac840692063334ba892f0f4d60e2cf667
Khoa A. Tran <khoa102@gmail.com>
2018-01-10 08:23:14 +0700

    Implement exercise hangman

---
+++ exercises/hangman/hangman.py
@@ -0,0 +1,14 @@
+class Hangman(object):
+	def __init__(self, word):
+		self.remainingGuesses = 9
+		self.status = 'busy'
+	
+	def guess(self, char):
+		pass
+
+	def get_masked_word(self):
+		pass
+
+	def get_status(self):
+		pass
+
