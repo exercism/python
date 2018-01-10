28aef2cbb47ce8972795e01dd5cd07ad1845d714
Corey McCandless <crm1994@gmail.com>
2018-01-10 11:59:58 -0600

    formatting fix

--- exercises/beer-song/beer_song_test.py
+++ exercises/beer-song/beer_song_test.py
@@ -301,16 +301,10 @@ SONG = [
     "Take one down and pass it around, 1 bottle of beer on the wall.",
     "",
     "1 bottle of beer on the wall, 1 bottle of beer.",
-    (
-        "Take it down and pass it around, "
-        "no more bottles of beer on the wall."
-    ),
+    "Take it down and pass it around, no more bottles of beer on the wall.",
     "",
     "No more bottles of beer on the wall, no more bottles of beer.",
-    (
-        "Go to the store and buy some more, "
-        "99 bottles of beer on the wall."
-    )
+    "Go to the store and buy some more, 99 bottles of beer on the wall."
 ]
 
 
