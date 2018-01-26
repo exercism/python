9ed2ca66d87573b4144fe638e59595eb66b76734
mRcfps <1402491442@qq.com>
2018-01-26 14:30:18 +0800

    forth: Fix #1135, update test to v1.4.0

--- exercises/forth/forth_test.py
+++ exercises/forth/forth_test.py
@@ -3,20 +3,10 @@ import unittest
 from forth import evaluate, StackUnderflowError
 
 
-# Tests adapted from `problem-specifications//canonical-data.json` @ v1.2.0
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.4.0
 # Tests for case-insensitivity are track-specific
 
 class ForthParsingTest(unittest.TestCase):
-    def test_empty_input_empty_stack(self):
-        input_data = []
-        expected = []
-        self.assertEqual(evaluate(input_data), expected)
-
-    def test_empty_line_empty_stack(self):
-        input_data = [""]
-        expected = []
-        self.assertEqual(evaluate(input_data), expected)
-
     def test_numbers_just_get_pushed_to_stack(self):
         input_data = ["1 2 3 4 5"]
         expected = [1, 2, 3, 4, 5]
@@ -154,13 +144,13 @@ class ForthCombinedArithmeticTest(unittest.TestCase):
 
 
 class ForthDupTest(unittest.TestCase):
-    def test_copies_the_top_value_on_the_stack(self):
-        input_data = ["1 DUP"]
+    def test_copies_a_value_on_the_stack(self):
+        input_data = ["1 dup"]
         expected = [1, 1]
         self.assertEqual(evaluate(input_data), expected)
 
-    def test_is_case_insensitive(self):
-        input_data = ["1 2 Dup"]
+    def test_copies_the_top_value_on_the_stack(self):
+        input_data = ["1 2 dup"]
         expected = [1, 2, 2]
         self.assertEqual(evaluate(input_data), expected)
 
@@ -191,11 +181,6 @@ class ForthDropTest(unittest.TestCase):
         expected = [3]
         self.assertEqual(evaluate(input_data), expected)
 
-    def test_is_case_insensitive(self):
-        input_data = ["1 2 Drop"]
-        expected = [1]
-        self.assertEqual(evaluate(input_data), expected)
-
     def test_errors_if_there_is_nothing_on_the_stack(self):
         input_data = ["drop"]
         with self.assertRaisesWithMessage(StackUnderflowError):
@@ -221,11 +206,6 @@ class ForthSwapTest(unittest.TestCase):
     def test_swaps_two_two_values_on_stack(self):
         input_data = ["1 2 3 SWAP"]
         expected = [1, 3, 2]
-        self.assertEqual(evaluate(input_data), expected)
-
-    def test_is_case_insensitive(self):
-        input_data = ["3 4 Swap"]
-        expected = [4, 3]
         self.assertEqual(evaluate(input_data), expected)
 
     def test_errors_if_there_is_nothing_on_the_stack(self):
@@ -258,11 +238,6 @@ class ForthOverTest(unittest.TestCase):
     def test_copies_the_second_element_if_there_are_more_than_two(self):
         input_data = ["1 2 3 OVER"]
         expected = [1, 2, 3, 2]
-        self.assertEqual(evaluate(input_data), expected)
-
-    def test_is_case_insensitive(self):
-        input_data = ["3 4 Over"]
-        expected = [3, 4, 3]
         self.assertEqual(evaluate(input_data), expected)
 
     def test_errors_if_there_is_nothing_on_the_stack(self):
@@ -328,14 +303,6 @@ class ForthUserDefinedWordsTest(unittest.TestCase):
         expected = [12]
         self.assertEqual(evaluate(input_data), expected)
 
-    def test_is_case_insensitive(self):
-        input_data = [
-            ": foo dup ;",
-            "1 FOO"
-        ]
-        expected = [1, 1]
-        self.assertEqual(evaluate(input_data), expected)
-
     def test_cannot_redefine_numbers(self):
         input_data = [": 1 2 ;"]
         with self.assertRaisesWithMessage(ValueError):
@@ -355,6 +322,44 @@ class ForthUserDefinedWordsTest(unittest.TestCase):
 
     def assertRaisesWithMessage(self, exception):
         return self.assertRaisesRegex(exception, r".+")
+
+
+class ForthCaseInsensitivityTest(unittest.TestCase):
+    def test_dup_is_case_insensitive(self):
+        input_data = ["1 DUP Dup dup"]
+        expected = [1, 1, 1, 1]
+        self.assertEqual(evaluate(input_data), expected)
+
+    def test_drop_is_case_insensitive(self):
+        input_data = ["1 2 3 4 DROP Drop drop"]
+        expected = [1]
+        self.assertEqual(evaluate(input_data), expected)
+
+    def test_swap_is_case_insensitive(self):
+        input_data = ["1 2 SWAP 3 Swap 4 swap"]
+        expected = [2, 3, 4, 1]
+        self.assertEqual(evaluate(input_data), expected)
+
+    def test_over_is_case_insensitive(self):
+        input_data = ["1 2 OVER Over over"]
+        expected = [1, 2, 1, 2, 1]
+        self.assertEqual(evaluate(input_data), expected)
+
+    def test_user_defined_words_are_case_insensitive(self):
+        input_data = [
+            ": foo dup ;",
+            "1 FOO Foo foo"
+        ]
+        expected = [1, 1, 1, 1]
+        self.assertEqual(evaluate(input_data), expected)
+
+    def test_definitions_are_case_insensitive(self):
+        input_data = [
+            ": SWAP DUP Dup dup ;",
+            "1 swap"
+        ]
+        expected = [1, 1, 1, 1]
+        self.assertEqual(evaluate(input_data), expected)
 
 
 if __name__ == '__main__':
