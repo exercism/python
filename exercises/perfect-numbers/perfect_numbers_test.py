31ede3ac6419ed1e48432b840f4419f0178d98c0
mRcfps <1402491442@qq.com>
2018-02-14 18:51:05 +0800

    perfect-numbers: update tests to v1.1.0

--- exercises/perfect-numbers/perfect_numbers_test.py
+++ exercises/perfect-numbers/perfect_numbers_test.py
@@ -3,7 +3,7 @@ import unittest
 from perfect_numbers import classify
 
 
-# Tests adapted from `problem-specifications//canonical-data.json` @ v1.0.1
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
 
 class PerfectNumbersTest(unittest.TestCase):
     def test_smallest_perfect_number(self):
