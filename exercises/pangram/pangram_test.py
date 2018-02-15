a64db4638323cd4fa4fb2857faf0b5d3329dfb71
Marc Chan <mrc@powerformer.com>
2018-02-16 01:29:13 +0800

    pangram: update tests to v1.4.0 (#1267)

--- exercises/pangram/pangram_test.py
+++ exercises/pangram/pangram_test.py
@@ -3,7 +3,7 @@ import unittest
 from pangram import is_pangram
 
 
-# Tests adapted from `problem-specifications//canonical-data.json` @ v1.3.0
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.4.0
 
 class PangramTests(unittest.TestCase):
 
