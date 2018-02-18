96027e143bf36b0d4c4ab20a9f24a2206bac2a1c
mRcfps <1402491442@qq.com>
2018-02-18 23:09:53 +0800

    protein-translation: update tests to v1.1.0

--- exercises/protein-translation/protein_translation_test.py
+++ exercises/protein-translation/protein_translation_test.py
@@ -3,7 +3,7 @@ import unittest
 from protein_translation import proteins
 
 
-# Tests adapted from problem-specifications/canonical-data.json @ v1.0.0
+# Tests adapted from problem-specifications/canonical-data.json @ v1.1.0
 
 class ProteinTranslationTests(unittest.TestCase):
 
