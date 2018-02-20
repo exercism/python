274307bcd2d6096d95ce4060a99ba44d44a19090
Corey McCandless <crm1994@gmail.com>
2018-02-20 09:23:12 -0600

    acronym, all-your-base, roman-numerals: convert tests to ddt

--- exercises/acronym/acronym_test.py
+++ exercises/acronym/acronym_test.py
@@ -1,26 +1,40 @@
 import unittest
+from ddt import ddt, data, unpack
 
 from acronym import abbreviate
 
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.3.0
 
+class AnnotatedCase(dict):
+    case_args = ('phrase', 'expected')
+
+    def __init__(self, name, *args):
+        setattr(self, '__name__', name)
+        for k, v in zip(AnnotatedCase.case_args, args):
+            self[k] = v
+
+
+@ddt
 class AcronymTest(unittest.TestCase):
-    def test_basic(self):
-        self.assertEqual(abbreviate('Portable Network Graphics'), 'PNG')
-
-    def test_lowercase_words(self):
-        self.assertEqual(abbreviate('Ruby on Rails'), 'ROR')
-
-    def test_punctuation(self):
-        self.assertEqual(abbreviate('First In, First Out'), 'FIFO')
-
-    def test_all_caps_words(self):
-        self.assertEqual(abbreviate('GNU Image Manipulation Program'), 'GIMP')
-
-    def test_punctuation_without_whitespace(self):
-        self.assertEqual(
-            abbreviate('Complementary metal-oxide semiconductor'), 'CMOS')
+    @data(
+        AnnotatedCase('basic', "Portable Network Graphics", 'PNG'),
+        AnnotatedCase('lowercase words', 'Ruby on Rails', 'ROR'),
+        AnnotatedCase('punctuation', 'First In, First Out', 'FIFO'),
+        AnnotatedCase(
+            'all caps word',
+            'GNU Image Manipulation Program',
+            'GIMP'
+        ),
+        AnnotatedCase(
+            'punctuation without whitespace',
+            'Complementary metal-oxide semiconductor',
+            'CMOS'
+        ),
+    )
+    @unpack
+    def test_abbreviate(self, phrase, expected):
+        self.assertEqual(abbreviate(phrase), expected)
 
 
 if __name__ == '__main__':
--- exercises/all-your-base/all_your_base_test.py
+++ exercises/all-your-base/all_your_base_test.py
@@ -1,83 +1,62 @@
 import unittest
+from ddt import ddt, data, unpack
 
 from all_your_base import rebase
 
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v2.3.0
 
+
+class AnnotatedCase(dict):
+    case_args = ('input_base', 'digits', 'output_base', 'expected')
+
+    def __init__(self, name, *args):
+        setattr(self, '__name__', name)
+        for k, v in zip(AnnotatedCase.case_args, args):
+            self[k] = v
+
+
+@ddt
 class AllYourBaseTests(unittest.TestCase):
+    @data(
+        AnnotatedCase('single bit to one decimal', 2, [1], 10, [1]),
+        AnnotatedCase('binary to single decimal', 2, [1, 0, 1], 10, [5]),
+        AnnotatedCase('single decimal to binary', 10, [5], 2, [1, 0, 1]),
+        AnnotatedCase(
+            'binary to multiple decimal',
+            2, [1, 0, 1, 0, 1, 0], 10, [4, 2]
+        ),
+        AnnotatedCase('decimal to binary', 10, [4, 2], 2, [1, 0, 1, 0, 1, 0]),
+        AnnotatedCase('trinary to hexadecimal', 3, [1, 1, 2, 0], 16, [2, 10]),
+        AnnotatedCase('hexadecimal to trinary', 16, [2, 10], 3, [1, 1, 2, 0]),
+        AnnotatedCase('15 bit integer', 97, [3, 46, 60], 73, [6, 10, 45]),
+        AnnotatedCase('empty list', 2, [], 10, []),
+        AnnotatedCase('single zero', 10, [0], 2, []),
+        AnnotatedCase('multiple zeros', 10, [0, 0, 0], 2, []),
+        AnnotatedCase('leading zeros', 7, [0, 6, 0], 10, [4, 2])
+    )
+    @unpack
+    def test_rebase(self, input_base, digits, output_base, expected):
+        self.assertEqual(
+            rebase(input_base, digits, output_base),
+            expected
+        )
 
-    def test_single_bit_to_one_decimal(self):
-        self.assertEqual(rebase(2, [1], 10), [1])
-
-    def test_binary_to_single_decimal(self):
-        self.assertEqual(rebase(2, [1, 0, 1], 10), [5])
-
-    def test_single_decimal_to_binary(self):
-        self.assertEqual(rebase(10, [5], 2), [1, 0, 1])
-
-    def test_binary_to_multiple_decimal(self):
-        self.assertEqual(rebase(2, [1, 0, 1, 0, 1, 0], 10), [4, 2])
-
-    def test_decimal_to_binary(self):
-        self.assertEqual(rebase(10, [4, 2], 2), [1, 0, 1, 0, 1, 0])
-
-    def test_trinary_to_hexadecimal(self):
-        self.assertEqual(rebase(3, [1, 1, 2, 0], 16), [2, 10])
-
-    def test_hexadecimal_to_trinary(self):
-        self.assertEqual(rebase(16, [2, 10], 3), [1, 1, 2, 0])
-
-    def test_15_bit_integer(self):
-        self.assertEqual(rebase(97, [3, 46, 60], 73), [6, 10, 45])
-
-    def test_empty_list(self):
-        self.assertEqual(rebase(2, [], 10), [])
-
-    def test_single_zero(self):
-        self.assertEqual(rebase(10, [0], 2), [])
-
-    def test_multiple_zeroes(self):
-        self.assertEqual(rebase(10, [0, 0, 0], 2), [])
-
-    def test_leading_zeros(self):
-        self.assertEqual(rebase(7, [0, 6, 0], 10), [4, 2])
-
-    def test_input_base_is_one(self):
+    @data(
+        AnnotatedCase('base is one', 1, [0], 10),
+        AnnotatedCase('base is zero', 0, [], 10),
+        AnnotatedCase('base is negative', -2, [1], 10),
+        AnnotatedCase('negative digit', 2, [1, -1, 1, 0, 1, 0], 10),
+        AnnotatedCase('invalid positive digit', 2, [1, 2, 1, 0, 1, 0], 10),
+        AnnotatedCase('output base is one', 2, [1, 0, 1, 0, 1, 0], 1),
+        AnnotatedCase('output base is zero', 10, [7], 0),
+        AnnotatedCase('output base is negative', 2, [1], -7),
+        AnnotatedCase('bot hbase are negative', -2, [1], -7),
+    )
+    @unpack
+    def test_rebase_error(self, input_base, digits, output_base):
         with self.assertRaisesWithMessage(ValueError):
-            rebase(1, [0], 10)
-
-    def test_input_base_is_zero(self):
-        with self.assertRaisesWithMessage(ValueError):
-            rebase(0, [], 10)
-
-    def test_input_base_is_negative(self):
-        with self.assertRaisesWithMessage(ValueError):
-            rebase(-2, [1], 10)
-
-    def test_negative_digit(self):
-        with self.assertRaisesWithMessage(ValueError):
-            rebase(2, [1, -1, 1, 0, 1, 0], 10)
-
-    def test_invalid_positive_digit(self):
-        with self.assertRaisesWithMessage(ValueError):
-            rebase(2, [1, 2, 1, 0, 1, 0], 10)
-
-    def test_output_base_is_one(self):
-        with self.assertRaisesWithMessage(ValueError):
-            rebase(2, [1, 0, 1, 0, 1, 0], 1)
-
-    def test_output_base_is_zero(self):
-        with self.assertRaisesWithMessage(ValueError):
-            rebase(10, [7], 0)
-
-    def test_output_base_is_negative(self):
-        with self.assertRaisesWithMessage(ValueError):
-            rebase(2, [1], -7)
-
-    def test_both_bases_are_negative(self):
-        with self.assertRaisesWithMessage(ValueError):
-            rebase(-2, [1], -7)
+            rebase(input_base, digits, output_base)
 
     # Utility functions
     def setUp(self):
