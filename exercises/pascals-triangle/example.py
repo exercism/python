54c9694e31d967f459101a10569b2a5d0493dac6
Corey McCandless <cmccandless@users.noreply.github.com>
2018-04-03 10:06:48 -0400

    pascals-triangle: update tests to canonical-date v1.2.0 (#1164)

--- exercises/pascals-triangle/example.py
+++ exercises/pascals-triangle/example.py
@@ -1,14 +1,12 @@
-def triangle(nth):
-    return [row(i) for i in range(nth + 1)]
-
-
-def is_triangle(t):
-    new_t = triangle(len(t) - 1)
-    return t == new_t
-
-
-def row(nth):
-    r = [1]
-    for i in range(1, nth + 1):
-        r.append(int(r[-1] * (nth - i + 1) / i))
-    return " ".join([str(i) for i in r])
+def rows(row_count):
+    if row_count < 0:
+        return None
+    elif row_count == 0:
+        return []
+    r = []
+    for i in range(row_count):
+        rn = [1]
+        for j in range(i):
+            rn.append(sum(r[-1][j:j+2]))
+        r.append(rn)
+    return r
--- exercises/pascals-triangle/pascals_triangle.py
+++ exercises/pascals-triangle/pascals_triangle.py
@@ -1,10 +1,2 @@
-def triangle(row_count):
-    pass
-
-
-def is_triangle(triangle_rows_list):
-    pass
-
-
-def row(row_count):
+def rows(row_count):
     pass
