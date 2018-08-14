0993ca53ed7689f386e87fe49c6313f039814481
Corey McCandless <cmccandless@users.noreply.github.com>
2018-08-13 21:16:22 -0400

    tree-building: add error messages (#1456)

--- exercises/tree-building/tree_building.py
+++ exercises/tree-building/tree_building.py
@@ -16,9 +16,9 @@ def BuildTree(records):
     ordered_id = [i.record_id for i in records]
     if records:
         if ordered_id[-1] != len(ordered_id) - 1:
-            raise ValueError
+            raise ValueError('Tree must be continuous')
         if ordered_id[0] != 0:
-            raise ValueError
+            raise ValueError('Tree must start with id 0')
     trees = []
     parent = {}
     for i in range(len(ordered_id)):
@@ -26,12 +26,12 @@ def BuildTree(records):
             if ordered_id[i] == j.record_id:
                 if j.record_id == 0:
                     if j.parent_id != 0:
-                        raise ValueError
+                        raise ValueError('Root node cannot have a parent')
                 if j.record_id < j.parent_id:
-                    raise ValueError
+                    raise ValueError('Parent id must be lower than child id')
                 if j.record_id == j.parent_id:
                     if j.record_id != 0:
-                        raise ValueError
+                        raise ValueError('Tree is a cycle')
                 trees.append(Node(ordered_id[i]))
     for i in range(len(ordered_id)):
         for j in trees:
