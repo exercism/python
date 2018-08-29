de3e813db9ca1839495fbfaf0efead6ba1c6611c
Thomas Fan <thomasjpfan@gmail.com>
2018-08-29 09:44:36 -0400

    anagram: flake8

--- exercises/isogram/isogram_test.py
+++ exercises/isogram/isogram_test.py
@@ -25,7 +25,7 @@ class IsogramTest(unittest.TestCase):
     def test_word_with_duplicated_character_in_mixed_case(self):
         self.assertIs(is_isogram("Alphabet"), False)
 
-    def test_word_with_duplicated_character_in_mixed_case_lowercase_first(self):
+    def test_word_with_duplicated_letter_in_mixed_case_lowercase_first(self):
         self.assertIs(is_isogram("alphAbet"), False)
 
     def test_hypothetical_isogrammic_word_with_hyphen(self):
