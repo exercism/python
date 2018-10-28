3bf3b3bb3d4208bbc2d16668155abe715c973e37
Tammo Behrends <behr.tam@gmail.com>
2018-10-28 19:20:31 +0100

    high-scores: Implement new exercise

---
+++ exercises/high-scores/example.py
@@ -0,0 +1,23 @@
+class HighScores(object):
+    def __init__(self, scores):
+        self.scores = scores
+
+    @property
+    def latest(self):
+        return self.scores[-1]
+
+    @property
+    def highest(self):
+        return max(self.scores)
+
+    @property
+    def top(self):
+        return sorted(self.scores, reverse=True)[:3]
+
+    @property
+    def report(self):
+        difference = self.highest - self.latest
+        result_qualifier = "" if difference <= 0 else "{} short of ".format(difference)
+        return "Your latest score was {}. That's {}your personal best!".format(
+            self.latest, result_qualifier
+        )
