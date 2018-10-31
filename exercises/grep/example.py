9817f10963a309e224e9efb6a30116262a515162
Tammo Behrends <tammo.behrends@campanda.com>
2018-10-31 23:27:29 +0100

    grep: Fix missing flag combination

--- exercises/grep/example.py
+++ exercises/grep/example.py
@@ -5,7 +5,7 @@ def matches(line, pattern, flags):
 
     if '-x' in flags:  # match entire lines
         if len(pattern) != len(line.rstrip()):
-            return False
+            return False if '-v' not in flags else True  # ugly inverted
 
     if '-v' in flags:  # invert matching
         return pattern not in line
