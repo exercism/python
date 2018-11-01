e70dd276760baf1d48666739f8ed40ca8ff713ce
Tammo Behrends <behr.tam@gmail.com>
2018-11-01 16:45:46 +0100

    leap: Update tests to v1.4.0 (#1576)

--- exercises/leap/leap_test.py
+++ exercises/leap/leap_test.py
@@ -3,7 +3,7 @@ import unittest
 from leap import is_leap_year
 
 
-# Tests adapted from `problem-specifications//canonical-data.json` @ v1.3.0
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.4.0
 
 class LeapTest(unittest.TestCase):
     def test_year_not_divisible_by_4(self):
@@ -17,6 +17,9 @@ class LeapTest(unittest.TestCase):
 
     def test_year_divisible_by_400(self):
         self.assertIs(is_leap_year(2000), True)
+
+    def test_year_divisible_by_200_not_divisible_by_400(self):
+        self.assertIs(is_leap_year(1800), False)
 
 
 if __name__ == '__main__':
