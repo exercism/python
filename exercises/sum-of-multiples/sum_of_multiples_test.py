fd9743afd80124769c14cf04a38b754803bf5176
Kevin Gasca <gasca.kevin.a@gmail.com>
2018-11-27 15:23:11 -0800

    sum-of-multiples: pep8 formatting connotation
    Even smaller updates to conform to pep8

--- exercises/sum-of-multiples/sum_of_multiples_test.py
+++ exercises/sum-of-multiples/sum_of_multiples_test.py
@@ -62,7 +62,7 @@ class SumOfMultiplesTest(unittest.TestCase):
 
     def test_multiples_of_2_3_5_7_or_11_up_to_10000(self):
         self.assertEqual(sum_of_multiples(10000,
-                        [2, 3, 5, 7, 11]), 39614537)
+                         [2, 3, 5, 7, 11]), 39614537)
 
 
 if __name__ == '__main__':
