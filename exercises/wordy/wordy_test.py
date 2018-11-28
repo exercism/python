59057f0fa395b626c36fdb6d39280422daf39ebe
Corey McCandless <cmccandless@users.noreply.github.com>
2018-11-28 10:46:48 -0500

    wordy: bump canonical version (#1610)
    This was missed in #1608

--- exercises/wordy/wordy_test.py
+++ exercises/wordy/wordy_test.py
@@ -3,7 +3,7 @@ import unittest
 from wordy import calculate
 
 
-# Tests adapted from `problem-specifications//canonical-data.json` @ v1.2.0
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.5.0
 
 class WordyTest(unittest.TestCase):
 
