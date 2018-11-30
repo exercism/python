ef8cdd954704fda47e1b24bd5e12dc347846d0bf
Kevin Gasca <gasca.kevin.a@gmail.com>
2018-11-30 10:43:41 -0800

    sum-of-multiples: update tests to v1.5.0
    * Updated test case names to reflect significance of each case.
    * Updated example.py for brevity

--- exercises/sum-of-multiples/example.py
+++ exercises/sum-of-multiples/example.py
@@ -1,10 +1,6 @@
 def sum_of_multiples(limit, multiples):
 
-    # Sanitize in-coming list
-    for i, digits in enumerate(multiples):
-        if digits <= 0:
-            del multiples[i]
-
     return sum(value for value in range(limit)
                if any(value % multiple == 0
-                      for multiple in multiples))
+                      for multiple in multiples
+                      if multiple > 0))
