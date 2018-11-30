8b9d6d6e56c6ead606b23345cb32a45be7114028
Kevin Gasca <gasca.kevin.a@gmail.com>
2018-11-30 12:03:34 -0800

    sum-of-multiples: update
    * Delete blank line

--- exercises/sum-of-multiples/example.py
+++ exercises/sum-of-multiples/example.py
@@ -1,5 +1,4 @@
 def sum_of_multiples(limit, multiples):
-
     return sum(value for value in range(limit)
                if any(value % multiple == 0
                       for multiple in multiples
