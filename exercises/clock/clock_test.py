9a03499441265efffad0ba9843841df060acb00b
Kevin Gasca <gasca.kevin.a@gmail.com>
2018-12-08 14:31:52 -0800

    clock: update tests to v2.4.0
    Fixes #1618
    * Updated test suite to test for exactly negative 1 hour.

--- exercises/clock/clock_test.py
+++ exercises/clock/clock_test.py
@@ -58,6 +58,9 @@ class ClockTest(unittest.TestCase):
     def test_negative_minutes_roll_over_continuously(self):
         self.assertEqual(str(Clock(1, -4820)), '16:40')
 
+    def test_negative_sixty_minutes_is_previous_hour(self):
+        self.asertEqual(str(Clock(2, -60)), '01:00')
+
     def test_negative_hour_and_minutes_both_roll_over(self):
         self.assertEqual(str(Clock(-25, -160)), '20:20')
 
