bac93af5bbe7239f164852b7da444604e600809d
Kevin Gasca <gasca.kevin.a@gmail.com>
2018-12-08 14:07:38 -0800

    high-scores: update tests to v2.0.0
    Fixes #1616
    * Update high_scores_test.py test case names to fit with canonical data 
    * Update example.py to fit with new test case names.

--- exercises/high-scores/high_scores_test.py
+++ exercises/high-scores/high_scores_test.py
@@ -3,7 +3,7 @@ import unittest
 from high_scores import HighScores
 
 
-# Tests adapted from `problem-specifications//canonical-data.json` @ v1.0.0
+# Tests adapted from `problem-specifications//canonical-data.json` @ v2.0.0
 
 
 class HighScoreTest(unittest.TestCase):
@@ -17,40 +17,40 @@ class HighScoreTest(unittest.TestCase):
         expected = 30
         self.assertEqual(HighScores(scores).latest(), expected)
 
-    def test_highest_score(self):
+    def test_personal_best(self):
         scores = [40, 100, 70]
         expected = 100
-        self.assertEqual(HighScores(scores).highest(), expected)
+        self.assertEqual(HighScores(scores).personal_best(), expected)
 
-    def test_top_3_scores(self):
+    def test_personal_top(self):
         scores = [50, 30, 10]
         expected = [50, 30, 10]
-        self.assertEqual(HighScores(scores).top(), expected)
+        self.assertEqual(HighScores(scores).personal_top(), expected)
 
-    def test_personal_bests_highest_to_lowest(self):
+    def test_personal_top_highest_to_lowest(self):
         scores = [20, 10, 30]
         expected = [30, 20, 10]
-        self.assertEqual(HighScores(scores).top(), expected)
+        self.assertEqual(HighScores(scores).personal_top(), expected)
 
-    def test_personal_bests_when_there_is_a_tie(self):
+    def test_personal_top_when_there_is_a_tie(self):
         scores = [40, 20, 40, 30]
         expected = [40, 40, 30]
-        self.assertEqual(HighScores(scores).top(), expected)
+        self.assertEqual(HighScores(scores).personal_top(), expected)
 
-    def test_personal_bests_when_there_are_less_than_3(self):
+    def test_personal_top_when_there_are_less_than_3(self):
         scores = [30, 70]
         expected = [70, 30]
-        self.assertEqual(HighScores(scores).top(), expected)
+        self.assertEqual(HighScores(scores).personal_top(), expected)
 
-    def test_personal_bests_when_there_is_only_one(self):
+    def test_personal_top_when_there_is_only_one(self):
         scores = [40]
         expected = [40]
-        self.assertEqual(HighScores(scores).top(), expected)
+        self.assertEqual(HighScores(scores).personal_top(), expected)
 
-    def test_personal_bests_from_a_long_list(self):
+    def test_personal_top_from_a_long_list(self):
         scores = [10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70]
         expected = [100, 90, 70]
-        self.assertEqual(HighScores(scores).top(), expected)
+        self.assertEqual(HighScores(scores).personal_top(), expected)
 
     def test_message_for_new_personal_best(self):
         scores = [20, 40, 0, 30, 70]
