5edcba9490a242a9c5c17523ed4f135dd6627622
Kevin Gasca <gasca.kevin.a@gmail.com>
2018-12-13 10:59:44 -0800

    dnd-character: implement exercise v1.0.0 (#1613)
    * sum-of-multiples: update tests to v1.5.0
    
    Fixes exercism#1580
    * Updated 3 test cases to conform to canonical data (reordered list to fit the ascending order convention)
    * Updated example to check for ZeroDivisionError
    
    * sum-of-multiples: pep8 formatting connotation
    
    Small edits to conform to Pep8 standards
    
    * sum-of-multiples: pep8 formatting connotation
    
    Even smaller updates to conform to pep8
    
    * sum-of_multiples: update import list
    
    Change import to link to correct file
    
    * sum-of-multiples: update tests to v1.5.0
    
    * Updated test case names to reflect significance of each case.
    * Updated example.py for brevity
    
    * sum-of-multiples: update
    
    * Delete blank line
    
    * phone-number: update tests to v1.6.1
    
    Fixes #1582
    * Version increment.
    
    * dnd-character: implement exercise v1.0.0
    
    Closes #1593
    * DND exercise implemented with corresponding tests and example
    * Updated README.md
    
    * dnd-character: implement exercise v1.0.0
    
    Update #1613
    * Del character{} from dnd_character.py
    * Modified test cases to fit the exercise implementation in dnd_charact_test.py
    * Modified example.py to exemplify spirit of exercise and deleted the character{}
    * Implemented the README.md using the configlet tool
    
    * dnd-character: implement exercise v1.0.0
    
    Update config.json to reference this exercise.
    
    * Update config.json
    
    "unlocked_by" updated to reference "robot-name"
    
    Co-Authored-By: GascaK <gasca.kevin.a@gmail.com>
    
    * Update config.json
    
    unlocked_by: null
    
    Co-Authored-By: GascaK <gasca.kevin.a@gmail.com>
    
    * dnd-character: update tests to v1.1.0
    
    Closes #1593
    * Update config.json
    * Update _test.py to correctly reflect the canonical data.
    
    * Update _test.py
    
    Update to include max roll of 18.

---
+++ exercises/dnd-character/dnd_character.py
@@ -0,0 +1,3 @@
+class Character:
+    def __init__(self):
+        pass
---
+++ exercises/dnd-character/example.py
@@ -0,0 +1,23 @@
+import random
+import math
+
+
+class Character:
+    def __init__(self):
+        self.strength = self.ability()
+        self.dexterity = self.ability()
+        self.constitution = self.ability()
+        self.intelligence = self.ability()
+        self.wisdom = self.ability()
+        self.charisma = self.ability()
+        self.hitpoints = 10 + modifier(self.constitution)
+
+    def ability(self):
+        dice_rolls = sorted(
+            [random.randint(1, 6)
+                for _ in range(4)])
+        return sum(dice_rolls[1:])
+
+
+def modifier(value):
+    return math.floor((value-10)/2)
