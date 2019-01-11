f08280788664c6381a130ce7988949a6e3c9d0f1
AnAccountForReportingBugs <45908752+AnAccountForReportingBugs@users.noreply.github.com>
2019-01-11 17:06:10 -0600

    update binary-search to 1.3.0
    Changes version number, as descriptions aren't actually used.

--- exercises/binary-search/binary_search_test.py
+++ exercises/binary-search/binary_search_test.py
@@ -2,7 +2,7 @@ import unittest
 
 from binary_search import binary_search
 
-# Tests adapted from `problem-specifications//canonical-data.json` @ v1.2.0
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.3.0
 
 
 class BinarySearchTest(unittest.TestCase):
