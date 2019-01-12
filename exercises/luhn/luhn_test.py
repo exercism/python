021be6ed220a0abb876ede97db978cd18fbbb9e8
AnAccountForReportingBugs <45908752+AnAccountForReportingBugs@users.noreply.github.com>
2019-01-11 19:45:48 -0600

    luhn_test.py: fix typo

--- exercises/luhn/luhn_test.py
+++ exercises/luhn/luhn_test.py
@@ -36,7 +36,7 @@ class LuhnTest(unittest.TestCase):
         self.assertIs(Luhn("055a 444 285").is_valid(), False)
 
     def test_valid_strings_with_non_digit_added_at_end_become_invalid(self):
-        self.assertis(Luhn("059a").is_valid(), False)
+        self.assertIs(Luhn("059a").is_valid(), False)
 
     def test_valid_strings_with_punctuation_included_become_invalid(self):
         self.assertIs(Luhn("055-444-285").is_valid(), False)
