edfaff6759155f74a4757c073899c958af5975a3
AnAccountForReportingBugs <45908752+AnAccountForReportingBugs@users.noreply.github.com>
2019-01-14 10:27:59 -0600

    saddle-points: update tests to 1.5.0 (#1650)
    * saddle-points: update tests to 1.5.0
    
    * Fix example.py
    
    Matrices are 1-indexed
    
    * Fix missed test case

--- exercises/saddle-points/saddle_points_test.py
+++ exercises/saddle-points/saddle_points_test.py
@@ -10,19 +10,19 @@ import unittest
 from saddle_points import saddle_points
 
 
-# Tests adapted from `problem-specifications//canonical-data.json` @ v1.3.0
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.5.0
 
 class SaddlePointsTest(unittest.TestCase):
     def test_identify_single_saddle_point(self):
         matrix = [[9, 8, 7], [5, 3, 2], [6, 6, 7]]
-        self.assertEqual(saddle_points(matrix), set([(1, 0)]))
+        self.assertEqual(saddle_points(matrix), set([(2, 1)]))
 
     def test_empty_matrix_has_no_saddle_points(self):
         self.assertEqual(saddle_points([]), set())
 
     def test_matrix_with_one_elem_has_single_saddle_point(self):
         matrix = [[1]]
-        self.assertEqual(saddle_points(matrix), set([(0, 0)]))
+        self.assertEqual(saddle_points(matrix), set([(1, 1)]))
 
     def test_identify_lack_of_saddle_points_when_there_are_none(self):
         matrix = [[1, 2, 3], [3, 1, 2], [2, 3, 1]]
@@ -30,30 +30,30 @@ class SaddlePointsTest(unittest.TestCase):
 
     def test_identify_multiple_saddle_points_in_column(self):
         matrix = [[4, 5, 4], [3, 5, 5], [1, 5, 4]]
-        expected = set([(0, 1), (1, 1), (2, 1)])
+        expected = set([(1, 2), (2, 2), (3, 2)])
         self.assertEqual(saddle_points(matrix), expected)
 
     def test_identify_multiple_saddle_points_in_row(self):
         matrix = [[6, 7, 8], [5, 5, 5], [7, 5, 6]]
-        expected = set([(1, 0), (1, 1), (1, 2)])
+        expected = set([(2, 1), (2, 2), (2, 3)])
         self.assertEqual(saddle_points(matrix), expected)
 
     def test_identify_saddle_point_in_bottom_right_corner(self):
         matrix = [[8, 7, 9], [6, 7, 6], [3, 2, 5]]
-        expected = set([(2, 2)])
+        expected = set([(3, 3)])
         self.assertEqual(saddle_points(matrix), expected)
 
     def test_non_square_matrix_with_2_saddle_points(self):
         matrix = [[3, 1, 3], [3, 2, 4]]
-        self.assertEqual(saddle_points(matrix), set([(0, 2), (0, 0)]))
+        self.assertEqual(saddle_points(matrix), set([(1, 3), (1, 1)]))
 
     def test_single_column_matrix_has_saddle_point_min_value(self):
         matrix = [[2], [1], [4], [1]]
-        self.assertEqual(saddle_points(matrix), set([(1, 0), (3, 0)]))
+        self.assertEqual(saddle_points(matrix), set([(2, 1), (4, 1)]))
 
     def test_single_row_matrix_has_saddle_point_in_max_value(self):
         matrix = [[2, 5, 3, 5]]
-        self.assertEqual(saddle_points(matrix), set([(0, 1), (0, 3)]))
+        self.assertEqual(saddle_points(matrix), set([(1, 2), (1, 4)]))
 
     # Additional tests for this track
 
