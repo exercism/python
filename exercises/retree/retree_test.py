482a5ea85cfd44ceb2798b06767e7637bb570bd2
AnAccountForReportingBugs <45908752+AnAccountForReportingBugs@users.noreply.github.com>
2019-01-18 17:21:55 -0600

    Update retree_test.py

--- exercises/retree/retree_test.py
+++ exercises/retree/retree_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from retree import TreeFromTraversals
+from retree import treeFromTraversals
 
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.0.0
@@ -10,13 +10,13 @@ class TwoFerTest(unittest.TestCase):
         preorder = []
         inorder = []
         expected = {}
-        self.assertEqual(TreeFromTraversals(preorder, inorder), expected)
+        self.assertEqual(treeFromTraversals(preorder, inorder), expected)
 
     def test_tree_with_one_item(self):
         preorder = ["a"]
         inorder = ["a"]
         expected = {"v": "a", "l": {}, "r": {}}
-        self.assertEqual(TreeFromTraversals(preorder, inorder), expected)
+        self.assertEqual(treeFromTraversals(preorder, inorder), expected)
 
     def test_tree_with_many_items(self):
         preorder = ["a", "i", "x", "f", "r"]
@@ -24,25 +24,25 @@ class TwoFerTest(unittest.TestCase):
         expected = {"v": "a", "l": {"v": "i", "l": {}, "r": {}},
                     "r": {"v": "x", "l": {"v": "f", "l": {}, "r": {}},
                           "r": {"v": "r", "l": {}, "r": {}}}}
-        self.assertEqual(TreeFromTraversals(preorder, inorder), expected)
+        self.assertEqual(treeFromTraversals(preorder, inorder), expected)
 
     def test_reject_traverals_of_different_length(self):
         preorder = ["a", "b"]
         inorder = ["b", "a", "r"]
         with self.assertRaisesWithMessage(ValueError):
-            TreeFromTraversals(preorder, inorder)
+            treeFromTraversals(preorder, inorder)
 
     def test_reject_inconsistent_traversals_of_same_length(self):
         preorder = ["x", "y", "z"]
         inorder = ["a", "b", "c"]
         with self.assertRaisesWithMessage(ValueError):
-            TreeFromTraversals(preorder, inorder)
+            treeFromTraversals(preorder, inorder)
 
     def test_reject_traverals_with_repeated_items(self):
         preorder = ["a", "b", "a"]
         inorder = ["b", "a", "a"]
         with self.assertRaisesWithMessage(ValueError):
-            TreeFromTraversals(preorder, inorder)
+            treeFromTraversals(preorder, inorder)
 
     # Utility functions
     def setUp(self):
