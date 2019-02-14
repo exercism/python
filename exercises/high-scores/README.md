ad758c70a2585f99bddfe4e692568f92383c7bd3
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-02-14 01:00:26 -0200

    high-scores: update test suite to v4.0.0

--- exercises/high-scores/README.md
+++ exercises/high-scores/README.md
@@ -2,7 +2,7 @@
 
 Manage a game player's High Score list.
 
-Your task is to build a high-score component of the classic Frogger game, one of the highest selling and addictive games of all time, and a classic of the arcade era. Your task is to write methods that return the highest score from the list, the last added score, the three highest scores, and a report on the difference between the last and the highest scores.
+Your task is to build a high-score component of the classic Frogger game, one of the highest selling and addictive games of all time, and a classic of the arcade era. Your task is to write methods that return the highest score from the list, the last added score and the three highest scores.
 
 ## Submitting Exercises
 
--- exercises/high-scores/high_scores_test.py
+++ exercises/high-scores/high_scores_test.py
@@ -3,7 +3,7 @@ import unittest
 from high_scores import HighScores
 
 
-# Tests adapted from `problem-specifications//canonical-data.json` @ v2.0.0
+# Tests adapted from `problem-specifications//canonical-data.json` @ v4.0.0
 
 
 class HighScoreTest(unittest.TestCase):
@@ -22,54 +22,30 @@ class HighScoreTest(unittest.TestCase):
         expected = 100
         self.assertEqual(HighScores(scores).personal_best(), expected)
 
-    def test_personal_top(self):
-        scores = [50, 30, 10]
-        expected = [50, 30, 10]
-        self.assertEqual(HighScores(scores).personal_top(), expected)
-
-    def test_personal_top_highest_to_lowest(self):
-        scores = [20, 10, 30]
-        expected = [30, 20, 10]
-        self.assertEqual(HighScores(scores).personal_top(), expected)
-
-    def test_personal_top_when_there_is_a_tie(self):
-        scores = [40, 20, 40, 30]
-        expected = [40, 40, 30]
-        self.assertEqual(HighScores(scores).personal_top(), expected)
-
-    def test_personal_top_when_there_are_less_than_3(self):
-        scores = [30, 70]
-        expected = [70, 30]
-        self.assertEqual(HighScores(scores).personal_top(), expected)
-
-    def test_personal_top_when_there_is_only_one(self):
-        scores = [40]
-        expected = [40]
-        self.assertEqual(HighScores(scores).personal_top(), expected)
-
-    def test_personal_top_from_a_long_list(self):
+    def test_personal_top_three_from_a_long_list(self):
         scores = [10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70]
         expected = [100, 90, 70]
-        self.assertEqual(HighScores(scores).personal_top(), expected)
+        self.assertEqual(HighScores(scores).personal_top_three(), expected)
 
-    def test_message_for_new_personal_best(self):
-        scores = [20, 40, 0, 30, 70]
-        expected = "Your latest score was 70. That's your personal best!"
-        self.assertEqual(HighScores(scores).report(), expected)
+    def test_personal_top_three_highest_to_lowest(self):
+        scores = [20, 10, 30]
+        expected = [30, 20, 10]
+        self.assertEqual(HighScores(scores).personal_top_three(), expected)
 
-    def test_message_when_latest_score_is_not_the_highest_score(self):
-        scores = [20, 100, 0, 30, 70]
-        expected = (
-            "Your latest score was 70. That's 30 short of your personal best!"
-        )
-        self.assertEqual(HighScores(scores).report(), expected)
+    def test_personal_top_three_when_there_is_a_tie(self):
+        scores = [40, 20, 40, 30]
+        expected = [40, 40, 30]
+        self.assertEqual(HighScores(scores).personal_top_three(), expected)
 
-    def test_message_for_repeated_personal_best(self):
-        scores = [20, 70, 50, 70, 30]
-        expected = (
-            "Your latest score was 30. That's 40 short of your personal best!"
-        )
-        self.assertEqual(HighScores(scores).report(), expected)
+    def test_personal_top_three_when_there_are_less_than_3(self):
+        scores = [30, 70]
+        expected = [70, 30]
+        self.assertEqual(HighScores(scores).personal_top_three(), expected)
+
+    def test_personal_top_three_when_there_is_only_one(self):
+        scores = [40]
+        expected = [40]
+        self.assertEqual(HighScores(scores).personal_top_three(), expected)
 
 
 if __name__ == "__main__":
