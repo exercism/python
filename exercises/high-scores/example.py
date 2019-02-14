1916cc367defb734a5b0c2cd5a00a202dead801d
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-02-14 13:26:37 -0200

    high-scores: update test suite to v4.0.0 (#1681)

--- exercises/high-scores/example.py
+++ exercises/high-scores/example.py
@@ -8,14 +8,5 @@ class HighScores(object):
     def personal_best(self):
         return max(self.scores)
 
-    def personal_top(self):
+    def personal_top_three(self):
         return sorted(self.scores, reverse=True)[:3]
-
-    def report(self):
-        difference = self.personal_best() - self.latest()
-        result_qualifier = (
-            "" if difference <= 0 else "{} short of ".format(difference)
-        )
-        return "Your latest score was {}. That's {}your personal best!".format(
-            self.latest(), result_qualifier
-        )
