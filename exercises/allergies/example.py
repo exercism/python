5c2f5b3a5826f3cd0ba752020dc270884ed15b3a
spencer51324312 <chester00325@gmail.com>
2019-05-08 19:54:27 -0400

    Update example.py
    made it so it matches canon data

--- exercises/allergies/example.py
+++ exercises/allergies/example.py
@@ -14,8 +14,9 @@ class Allergies(object):
     def __init__(self, score):
         self.score = score
 
-    def is_allergic_to(self, allergy):
+    def allergic_to(self, allergy):
         return bool(self.score & 1 << self._allergies.index(allergy))
+
 
     @property
     def lst(self):
