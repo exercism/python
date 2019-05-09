766b40ecc49f6e3512c473528c455b7ccd9750be
Kaweh Ebrahimi-Far <kaweh@efar.nl>
2019-05-09 11:09:36 +0200

    Fixed: testing wrong plants for given student

--- exercises/kindergarten-garden/kindergarten_garden_test.py
+++ exercises/kindergarten-garden/kindergarten_garden_test.py
@@ -44,10 +44,10 @@ class KindergartenGardenTest(unittest.TestCase):
             "VCRRGVRG\nRVGCCGCV",
             students="Samantha Patricia Xander Roger".split())
         self.assertEqual(
-            garden.plants("Patricia"),
+            garden.plants("Samantha"),
             "Violets Clover Radishes Violets".split())
         self.assertEqual(
-            garden.plants("Xander"), "Radishes Grass Clover Violets".split())
+            garden.plants("Roger"), "Radishes Grass Clover Violets".split())
 
 
 if __name__ == '__main__':
