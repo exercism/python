e052f92494f76672ecc3f960435eafff4d2aed2b
Bethany Garcia <bethanymgarcia@gmail.com>
2019-05-26 13:11:24 -0700

    grains: sync expected test results and input data with problem-specifications.

--- exercises/grains/grains_test.py
+++ exercises/grains/grains_test.py
@@ -1,55 +1,52 @@
 import unittest
 
-from grains import (
-    on_square,
-    total_after,
-)
+from grains import square, total
 
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.2.0
 
 class GrainsTest(unittest.TestCase):
     def test_square_1(self):
-        self.assertEqual(on_square(1), 1)
+        self.assertEqual(square(1), 1)
 
     def test_square_2(self):
-        self.assertEqual(on_square(2), 2)
+        self.assertEqual(square(2), 2)
 
     def test_square_3(self):
-        self.assertEqual(on_square(3), 4)
+        self.assertEqual(square(3), 4)
 
     def test_square_4(self):
-        self.assertEqual(on_square(4), 8)
+        self.assertEqual(square(4), 8)
 
     def test_square_16(self):
-        self.assertEqual(on_square(16), 32768)
+        self.assertEqual(square(16), 32768)
 
     def test_square_32(self):
-        self.assertEqual(on_square(32), 2147483648)
+        self.assertEqual(square(32), 2147483648)
 
     def test_square_64(self):
-        self.assertEqual(on_square(64), 9223372036854775808)
+        self.assertEqual(square(64), 9223372036854775808)
 
     def test_square_0_raises_exception(self):
         with self.assertRaisesWithMessage(ValueError):
-            on_square(0)
+            square(0)
         with self.assertRaisesWithMessage(ValueError):
-            total_after(0)
+            total(0)
 
     def test_square_negative_raises_exception(self):
         with self.assertRaisesWithMessage(ValueError):
-            on_square(-1)
+            square(-1)
         with self.assertRaisesWithMessage(ValueError):
-            total_after(-1)
+            total(-1)
 
     def test_square_gt_64_raises_exception(self):
         with self.assertRaisesWithMessage(ValueError):
-            on_square(65)
+            square(65)
         with self.assertRaisesWithMessage(ValueError):
-            total_after(65)
+            total(65)
 
     def test_total(self):
-        self.assertEqual(total_after(64), 18446744073709551615)
+        self.assertEqual(total(64), 18446744073709551615)
 
     # Utility functions
     def setUp(self):
