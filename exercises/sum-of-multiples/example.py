10feae5d30310ddfcceea7c7ef509043728273cd
Grociu <jan.grottel@gmail.com>
2019-05-31 21:34:56 +0200

    sum_of_multiples sync

--- exercises/sum-of-multiples/example.py
+++ exercises/sum-of-multiples/example.py
@@ -1,5 +1,8 @@
-def sum_of_multiples(limit, multiples):
-    return sum(value for value in range(limit)
-               if any(value % multiple == 0
-                      for multiple in multiples
-                      if multiple > 0))
+old_sum = sum
+
+
+def sum(factors, limit):
+    return old_sum(value for value in range(limit)
+                   if any(value % multiple == 0
+                          for multiple in factors
+                          if multiple > 0))
--- exercises/sum-of-multiples/sum_of_multiples.py
+++ exercises/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,2 @@
-def sum_of_multiples(limit, multiples):
+def sum(factors, limit):
     pass
