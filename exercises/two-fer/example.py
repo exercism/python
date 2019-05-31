eb626391e00b71a455373c21df6aa4abf066b313
Grociu <jan.grottel@gmail.com>
2019-05-31 17:10:48 +0200

    two_fer sync with canon

--- exercises/two-fer/example.py
+++ exercises/two-fer/example.py
@@ -1,5 +1,5 @@
-def two_fer(name=""):
-    if not name.strip():
+def two_fer(name=None):
+    if not name:
         return "One for you, one for me."
     else:
         return "One for {}, one for me.".format(name)
