569beb1bc285be19ada7244b2c3dabded2f13237
Grociu <jan.grottel@gmail.com>
2019-06-01 20:57:32 +0200

    secret handshake: sync with canonical data

--- exercises/secret-handshake/example.py
+++ exercises/secret-handshake/example.py
@@ -1,10 +1,10 @@
 gestures = ['wink', 'double blink', 'close your eyes', 'jump']
 
 
-def handshake(code):
+def commands(number):
     actions = [gestures[i] for i in range(len(gestures))
-               if (code >> i) % 2 == 1]
-    return actions if code < 16 else list(reversed(actions))
+               if (number >> i) % 2 == 1]
+    return actions if number < 16 else list(reversed(actions))
 
 
 def secret_code(actions):
