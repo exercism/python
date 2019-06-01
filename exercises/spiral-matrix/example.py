2b4fc2710e50810043a73823cdb0b063e77c1810
Grociu <jan.grottel@gmail.com>
2019-06-01 10:08:51 +0200

    spiral-matrix-sync-2 first pass

--- exercises/spiral-matrix/example.py
+++ exercises/spiral-matrix/example.py
@@ -1,11 +1,11 @@
-def spiral(size):
-    sm = [[0]*size for k in range(size)]
-    i, j, el = 0, -1, 1
+def spiral_matrix(size):
+    matrix = [[0]*size for row in range(size)]
+    i, j, element = 0, -1, 1
     di, dj = [0, 1, 0, -1], [1, 0, -1, 0]
     for x in range(2*size - 1):
         for y in range((2*size - x) // 2):
             i += di[x % 4]
             j += dj[x % 4]
-            sm[i][j] = el
-            el += 1
-    return sm
+            matrix[i][j] = element
+            element += 1
+    return matrix
