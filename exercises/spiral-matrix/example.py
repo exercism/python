82752861897ad7ba90762c5178cc515cc4d5c664
Jan <49829764+Grociu@users.noreply.github.com>
2019-06-10 15:10:26 +0200

    Update exercises/spiral-matrix/example.py
    variable y otherwise unused, switched to _

--- exercises/spiral-matrix/example.py
+++ exercises/spiral-matrix/example.py
@@ -3,7 +3,7 @@ def spiral_matrix(size):
     i, j, element = 0, -1, 1
     di, dj = [0, 1, 0, -1], [1, 0, -1, 0]
     for x in range(2*size - 1):
-        for y in range((2*size - x) // 2):
+        for _ in range((2*size - x) // 2):
             i += di[x % 4]
             j += dj[x % 4]
             matrix[i][j] = element
