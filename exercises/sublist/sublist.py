7f7878aa8f07eb168ee26ff59080d1c00a57721a
Grociu <jan.grottel@gmail.com>
2019-06-19 20:44:09 +0200

    third pass, moving the hints into the stub

--- exercises/sublist/sublist.py
+++ exercises/sublist/sublist.py
@@ -1,3 +1,16 @@
+"""
+This exercise stub and the test suite contain several enumerated constants.
+
+Since Python 2 does not have the enum module, the idiomatic way to write
+enumerated constants has traditionally been a NAME assigned to an arbitrary,
+but unique value. An integer is traditionally used because it’s memory
+efficient.
+It is a common practice to export both constants and functions that work with
+those constants (ex. the constants in the os, subprocess and re modules).
+
+You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
+"""
+
 # Possible sublist categories.
 # Change the values as you see fit.
 SUBLIST = None
