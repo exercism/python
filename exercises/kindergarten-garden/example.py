94de775fb18b2ff1c372ceddddced864378d5077
Michael Morehouse <640167+yawpitch@users.noreply.github.com>
2019-07-10 13:44:04 +0100

    kindergarten-garden: improve example
    Eliminate mutable default value and incorrect use of name mangling.
    Minimize unused instance attributes and speed up performance.

--- exercises/kindergarten-garden/example.py
+++ exercises/kindergarten-garden/example.py
@@ -1,18 +1,31 @@
 class Garden(object):
 
-    __plant_names = {"C": "Clover", "G": "Grass",
-                     "R": "Radishes", "V": "Violets"}
+    STUDENTS = [
+        "Alice",
+        "Bob",
+        "Charlie",
+        "David",
+        "Eve",
+        "Fred",
+        "Ginny",
+        "Harriet",
+        "Ileana",
+        "Joseph",
+        "Kincaid",
+        "Larry",
+    ]
+    PLANTS = {"C": "Clover", "G": "Grass", "R": "Radishes", "V": "Violets"}
 
-    def __init__(self, diagram,
-                 students=("Alice Bob Charlie David "
-                           "Eve Fred Ginny Harriet "
-                           "Ileana Joseph Kincaid Larry").split()):
-        self.plant_rows = diagram.split()
-        self.students = sorted(students)
+    def __init__(self, diagram, students=None):
+        students = sorted(students or self.STUDENTS)
+        front, back = diagram.splitlines()
+        self.cups = {}
+        for idx, student in enumerate(students[: len(front)]):
+            start = idx * 2
+            stop = start + 2
+            self.cups.setdefault(student, [])
+            self.cups[student].extend(self.PLANTS[p] for p in front[start:stop])
+            self.cups[student].extend(self.PLANTS[p] for p in back[start:stop])
 
     def plants(self, student):
-        slot_start = self.students.index(student) * 2
-        slot = slice(slot_start, slot_start + 2)
-        return [self.__plant_names[abbrev]
-                for abbrev in (self.plant_rows[0][slot] +
-                               self.plant_rows[1][slot])]
+        return self.cups.get(student, [])
