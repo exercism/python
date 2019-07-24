2e2231721895688a4ecd51bfa31dfb659240fcee
Corey McCandless <corey.mccandless@atlasied.com>
2019-07-24 10:10:39 -0400

    hello-world: add test template

--- exercises/hello-world/hello_world_test.py
+++ exercises/hello-world/hello_world_test.py
@@ -2,13 +2,13 @@ import unittest
 
 import hello_world
 
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
 
+
 class HelloWorldTest(unittest.TestCase):
-    def test_hello(self):
-        self.assertEqual(hello_world.hello(), 'Hello, World!')
+    def test_say_hi(self):
+        self.assertEqual(hello_world.hello(), "Hello, World!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
