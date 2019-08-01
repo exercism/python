e05113235ffd784b5a99add959727ec076076783
Michael Morehouse <640167+yawpitch@users.noreply.github.com>
2019-08-01 13:38:26 +0100

    rna-transcription: fix example.py
    Remove valid character test, as that's not required by spec any longer.
    Also removed some unwieldy Python 2 compatability code.

--- exercises/rna-transcription/example.py
+++ exercises/rna-transcription/example.py
@@ -1,18 +1,4 @@
-import sys
-
-if sys.version_info[0] == 2:
-    from string import maketrans
-else:
-    maketrans = str.maketrans
-
-
-DNA_CHARS = 'AGCT'
-DNA_TO_RNA = maketrans(DNA_CHARS, 'UCGA')
-
+DNA_TO_RNA = {ord(d): r for d, r in zip("AGCT", "UCGA")}
 
 def to_rna(dna_strand):
-    valid_chars = set(DNA_CHARS)
-    if any(char not in valid_chars for char in dna_strand):
-        raise ValueError("Input DNA strand contains invalid bases")
-
     return dna_strand.translate(DNA_TO_RNA)
