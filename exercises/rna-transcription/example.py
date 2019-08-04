500f7eda9482a1c42c7e1c43856cd571d3f479fe
Michael Morehouse <640167+yawpitch@users.noreply.github.com>
2019-08-04 16:46:34 +0100

    rna-transcription: fix example.py
    Reinstate Python 2 fallback code.

--- exercises/rna-transcription/example.py
+++ exercises/rna-transcription/example.py
@@ -1,4 +1,11 @@
-DNA_TO_RNA = {ord(d): r for d, r in zip("AGCT", "UCGA")}
+import sys
+
+if sys.version_info[0] == 2:
+    from string import maketrans
+else:
+    maketrans = str.maketrans
+
+DNA_TO_RNA = maketrans("AGCT", "UCGA")
 
 def to_rna(dna_strand):
     return dna_strand.translate(DNA_TO_RNA)
