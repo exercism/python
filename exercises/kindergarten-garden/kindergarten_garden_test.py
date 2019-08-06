186e189b7ceaccdd4185cb3e1af9bf3c35756b4b
Corey McCandless <cmccandless@users.noreply.github.com>
2019-08-06 16:01:59 -0400

    kindergarten-garden: add test template (#1891)

--- exercises/kindergarten-garden/kindergarten_garden_test.py
+++ exercises/kindergarten-garden/kindergarten_garden_test.py
@@ -2,53 +2,76 @@ import unittest
 
 from kindergarten_garden import Garden
 
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.1
 
+
 class KindergartenGardenTest(unittest.TestCase):
-    def test_garden_with_single_student(self):
+    def test_partial_garden_garden_with_single_student(self):
+        garden = Garden("RC\nGG")
         self.assertEqual(
-            Garden("RC\nGG").plants("Alice"),
-            "Radishes Clover Grass Grass".split())
+            garden.plants("Alice"), ["Radishes", "Clover", "Grass", "Grass"]
+        )
 
-    def test_different_garden_with_single_student(self):
+    def test_partial_garden_different_garden_with_single_student(self):
+        garden = Garden("VC\nRC")
         self.assertEqual(
-            Garden("VC\nRC").plants("Alice"),
-            "Violets Clover Radishes Clover".split())
+            garden.plants("Alice"), ["Violets", "Clover", "Radishes", "Clover"]
+        )
 
-    def test_garden_with_two_students(self):
+    def test_partial_garden_garden_with_two_students(self):
         garden = Garden("VVCG\nVVRC")
         self.assertEqual(
-            garden.plants("Bob"), "Clover Grass Radishes Clover".split())
+            garden.plants("Bob"), ["Clover", "Grass", "Radishes", "Clover"]
+        )
 
-    def test_multiple_students_for_the_same_garden_with_three_students(self):
+    def test_partial_garden_second_student_s_garden(self):
         garden = Garden("VVCCGG\nVVCCGG")
-        self.assertEqual(garden.plants("Bob"), ["Clover"] * 4)
-        self.assertEqual(garden.plants("Charlie"), ["Grass"] * 4)
+        self.assertEqual(garden.plants("Bob"), ["Clover", "Clover", "Clover", "Clover"])
 
-    def test_full_garden(self):
+    def test_partial_garden_third_student_s_garden(self):
+        garden = Garden("VVCCGG\nVVCCGG")
+        self.assertEqual(garden.plants("Charlie"), ["Grass", "Grass", "Grass", "Grass"])
+
+    def test_full_garden_first_student_s_garden(self):
         garden = Garden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV")
         self.assertEqual(
-            garden.plants("Alice"),
-            "Violets Radishes Violets Radishes".split())
+            garden.plants("Alice"), ["Violets", "Radishes", "Violets", "Radishes"]
+        )
+
+    def test_full_garden_second_student_s_garden(self):
+        garden = Garden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV")
+        self.assertEqual(garden.plants("Bob"), ["Clover", "Grass", "Clover", "Clover"])
+
+    def test_full_garden_second_to_last_student_s_garden(self):
+        garden = Garden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV")
         self.assertEqual(
-            garden.plants("Bob"), "Clover Grass Clover Clover".split())
+            garden.plants("Kincaid"), ["Grass", "Clover", "Clover", "Grass"]
+        )
+
+    def test_full_garden_last_student_s_garden(self):
+        garden = Garden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV")
         self.assertEqual(
-            garden.plants("Kincaid"), "Grass Clover Clover Grass".split())
-        self.assertEqual(
-            garden.plants("Larry"), "Grass Violets Clover Violets".split())
+            garden.plants("Larry"), ["Grass", "Violets", "Clover", "Violets"]
+        )
 
     # Additional tests for this track
-    def test_disordered_test(self):
+
+    def test_students_are_unordered_first_student(self):
         garden = Garden(
-            "VCRRGVRG\nRVGCCGCV",
-            students="Samantha Patricia Xander Roger".split())
+            "VCRRGVRG\nRVGCCGCV", students=["Samantha", "Patricia", "Xander", "Roger"]
+        )
         self.assertEqual(
-            garden.plants("Patricia"),
-            "Violets Clover Radishes Violets".split())
+            garden.plants("Patricia"), ["Violets", "Clover", "Radishes", "Violets"]
+        )
+
+    def test_students_are_unordered_last_student(self):
+        garden = Garden(
+            "VCRRGVRG\nRVGCCGCV", students=["Samantha", "Patricia", "Xander", "Roger"]
+        )
         self.assertEqual(
-            garden.plants("Xander"), "Radishes Grass Clover Violets".split())
+            garden.plants("Xander"), ["Radishes", "Grass", "Clover", "Violets"]
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
