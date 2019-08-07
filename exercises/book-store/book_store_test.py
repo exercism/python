79711d5221cf00676f6ef6e3d458fd5dbefcd243
Corey McCandless <cmccandless@users.noreply.github.com>
2019-08-07 10:16:01 -0400

    book-store: add test template (#1899)

--- exercises/book-store/book_store_test.py
+++ exercises/book-store/book_store_test.py
@@ -2,59 +2,87 @@ import unittest
 
 from book_store import total
 
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.4.0
+
 
 class BookStoreTest(unittest.TestCase):
     def test_only_a_single_book(self):
-        self.assertEqual(total([1]), 800)
+        results = [1]
+        table = 800
+        self.assertEqual(total(results), table)
 
     def test_two_of_the_same_book(self):
-        self.assertEqual(total([2, 2]), 1600)
+        results = [2, 2]
+        table = 1600
+        self.assertEqual(total(results), table)
 
     def test_empty_basket(self):
-        self.assertEqual(total([]), 0)
+        results = []
+        table = 0
+        self.assertEqual(total(results), table)
 
     def test_two_different_books(self):
-        self.assertEqual(total([1, 2]), 1520)
+        results = [1, 2]
+        table = 1520
+        self.assertEqual(total(results), table)
 
     def test_three_different_books(self):
-        self.assertEqual(total([1, 2, 3]), 2160)
+        results = [1, 2, 3]
+        table = 2160
+        self.assertEqual(total(results), table)
 
     def test_four_different_books(self):
-        self.assertEqual(total([1, 2, 3, 4]), 2560)
+        results = [1, 2, 3, 4]
+        table = 2560
+        self.assertEqual(total(results), table)
 
     def test_five_different_books(self):
-        self.assertEqual(total([1, 2, 3, 4, 5]), 3000)
+        results = [1, 2, 3, 4, 5]
+        table = 3000
+        self.assertEqual(total(results), table)
 
-    def test_two_groups_of_4_is_cheaper_than_group_of_5_plus_group_of_3(self):
-        self.assertEqual(total([1, 1, 2, 2, 3, 3, 4, 5]), 5120)
+    def test_two_groups_of_four_is_cheaper_than_group_of_five_plus_group_of_three(self):
+        results = [1, 1, 2, 2, 3, 3, 4, 5]
+        table = 5120
+        self.assertEqual(total(results), table)
 
-    def test_two_groups_of_4_is_cheaper_than_groups_of_5_and_3(self):
-        self.assertEqual(total([1, 1, 2, 3, 4, 4, 5, 5]), 5120)
+    def test_two_groups_of_four_is_cheaper_than_groups_of_five_and_three(self):
+        results = [1, 1, 2, 3, 4, 4, 5, 5]
+        table = 5120
+        self.assertEqual(total(results), table)
 
-    def test_group_of_4_plus_group_of_2_is_cheaper_than_2_groups_of_3(self):
-        self.assertEqual(total([1, 1, 2, 2, 3, 4]), 4080)
+    def test_group_of_four_plus_group_of_two_is_cheaper_than_two_groups_of_three(self):
+        results = [1, 1, 2, 2, 3, 4]
+        table = 4080
+        self.assertEqual(total(results), table)
 
     def test_two_each_of_first_4_books_and_1_copy_each_of_rest(self):
-        self.assertEqual(total([1, 1, 2, 2, 3, 3, 4, 4, 5]), 5560)
+        results = [1, 1, 2, 2, 3, 3, 4, 4, 5]
+        table = 5560
+        self.assertEqual(total(results), table)
 
     def test_two_copies_of_each_book(self):
-        self.assertEqual(total([1, 1, 2, 2, 3, 3, 4, 4, 5, 5]), 6000)
+        results = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+        table = 6000
+        self.assertEqual(total(results), table)
 
     def test_three_copies_of_first_book_and_2_each_of_remaining(self):
-        self.assertEqual(
-            total([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1]), 6800)
+        results = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1]
+        table = 6800
+        self.assertEqual(total(results), table)
 
     def test_three_each_of_first_2_books_and_2_each_of_remaining_books(self):
-        self.assertEqual(
-            total([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1, 2]), 7520)
+        results = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1, 2]
+        table = 7520
+        self.assertEqual(total(results), table)
 
-    def test_four_groups_of_4_are_cheaper_than_2_groups_each_of_5_and_3(self):
-        self.assertEqual(
-            total([1, 1, 2, 2, 3, 3, 4, 5, 1, 1, 2, 2, 3, 3, 4, 5]),
-            10240)
+    def test_four_groups_of_four_are_cheaper_than_two_groups_each_of_five_and_three(
+        self
+    ):
+        results = [1, 1, 2, 2, 3, 3, 4, 5, 1, 1, 2, 2, 3, 3, 4, 5]
+        table = 10240
+        self.assertEqual(total(results), table)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
