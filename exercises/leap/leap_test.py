2ae116500bcba1b783d3434ddd5ab2f4f8c7bad1
Corey McCandless <cmccandless@users.noreply.github.com>
2019-08-16 11:41:55 -0400

    leap: add test template (#1903)

--- exercises/leap/leap_test.py
+++ exercises/leap/leap_test.py
@@ -2,29 +2,28 @@ import unittest
 
 from leap import leap_year
 
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.5.1
 
 
 class LeapTest(unittest.TestCase):
-    def test_year_not_divisible_by_4(self):
+    def test_year_not_divisible_by_4_in_common_year(self):
         self.assertIs(leap_year(2015), False)
 
-    def test_year_divisible_by_2_not_divisible_by_4(self):
+    def test_year_divisible_by_2_not_divisible_by_4_in_common_year(self):
         self.assertIs(leap_year(1970), False)
 
-    def test_year_divisible_by_4_not_divisible_by_100(self):
+    def test_year_divisible_by_4_not_divisible_by_100_in_leap_year(self):
         self.assertIs(leap_year(1996), True)
 
-    def test_year_divisible_by_100_not_divisible_by_400(self):
+    def test_year_divisible_by_100_not_divisible_by_400_in_common_year(self):
         self.assertIs(leap_year(2100), False)
 
-    def test_year_divisible_by_400(self):
+    def test_year_divisible_by_400_in_leap_year(self):
         self.assertIs(leap_year(2000), True)
 
-    def test_year_divisible_by_200_not_divisible_by_400(self):
+    def test_year_divisible_by_200_not_divisible_by_400_in_common_year(self):
         self.assertIs(leap_year(1800), False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
