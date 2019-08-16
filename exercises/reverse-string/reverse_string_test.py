7a5a0a7d01fdc793fe7633e1e8d85df24655d3e7
Corey McCandless <corey.mccandless@atlasied.com>
2019-08-16 11:44:56 -0400

    reverse-string: add test template

--- exercises/reverse-string/reverse_string_test.py
+++ exercises/reverse-string/reverse_string_test.py
@@ -2,28 +2,28 @@ import unittest
 
 from reverse_string import reverse
 
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.2.0
 
+
 class ReverseStringTest(unittest.TestCase):
-    def test_empty_string(self):
-        self.assertEqual(reverse(''), '')
+    def test_an_empty_string(self):
+        self.assertEqual(reverse(""), "")
 
     def test_a_word(self):
-        self.assertEqual(reverse('robot'), 'tobor')
+        self.assertEqual(reverse("robot"), "tobor")
 
     def test_a_capitalized_word(self):
-        self.assertEqual(reverse('Ramen'), 'nemaR')
+        self.assertEqual(reverse("Ramen"), "nemaR")
 
     def test_a_sentence_with_punctuation(self):
-        self.assertEqual(reverse('I\'m hungry!'), '!yrgnuh m\'I')
+        self.assertEqual(reverse("I'm hungry!"), "!yrgnuh m'I")
 
     def test_a_palindrome(self):
-        self.assertEqual(reverse('racecar'), 'racecar')
+        self.assertEqual(reverse("racecar"), "racecar")
 
     def test_an_even_sized_word(self):
-        self.assertEqual(reverse('drawer'), 'reward')
+        self.assertEqual(reverse("drawer"), "reward")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
