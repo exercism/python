e25fe7c1aac6b7a127b54189e8af2d6e63142911
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-09-19 09:07:18 +0200

    updated leap tests to v1.6.0 canonical data

--- exercises/leap/leap_test.py
+++ exercises/leap/leap_test.py
@@ -2,7 +2,7 @@ import unittest
 
 from leap import leap_year
 
-# Tests adapted from `problem-specifications//canonical-data.json` @ v1.5.1
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.6.0
 
 
 class LeapTest(unittest.TestCase):
@@ -15,11 +15,20 @@ class LeapTest(unittest.TestCase):
     def test_year_divisible_by_4_not_divisible_by_100_in_leap_year(self):
         self.assertIs(leap_year(1996), True)
 
+    def test_year_divisible_by_4_and_5_is_still_a_leap_year(self):
+        self.assertIs(leap_year(1960), True)
+
     def test_year_divisible_by_100_not_divisible_by_400_in_common_year(self):
         self.assertIs(leap_year(2100), False)
 
+    def test_year_divisible_by_100_but_not_by_3_is_still_not_a_leap_year(self):
+        self.assertIs(leap_year(1900), False)
+
     def test_year_divisible_by_400_in_leap_year(self):
         self.assertIs(leap_year(2000), True)
+
+    def test_year_divisible_by_400_but_not_by_125_is_still_a_leap_year(self):
+        self.assertIs(leap_year(2400), True)
 
     def test_year_divisible_by_200_not_divisible_by_400_in_common_year(self):
         self.assertIs(leap_year(1800), False)
