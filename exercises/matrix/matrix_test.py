3e66c8dda5b2964a38e80d01e0c15e1f54f98ce4
Corey McCandless <corey.mccandless@atlasied.com>
2019-09-19 11:01:22 -0400

    matrix: update tests to v1.3.0
    Resolves #1912

--- exercises/matrix/matrix_test.py
+++ exercises/matrix/matrix_test.py
@@ -2,7 +2,7 @@ import unittest
 
 from matrix import Matrix
 
-# Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.3.0
 
 
 class MatrixTest(unittest.TestCase):
@@ -18,9 +18,9 @@ class MatrixTest(unittest.TestCase):
         matrix = Matrix("1 2\n10 20")
         self.assertEqual(matrix.row(2), [10, 20])
 
-    def test_can_extract_row_from_non_square_matrix(self):
+    def test_can_extract_row_from_non_square_matrix_with_no_corresponding_column(self):
         matrix = Matrix("1 2 3\n4 5 6\n7 8 9\n8 7 6")
-        self.assertEqual(matrix.row(3), [7, 8, 9])
+        self.assertEqual(matrix.row(4), [8, 7, 6])
 
     def test_extract_column_from_one_number_matrix(self):
         matrix = Matrix("1")
@@ -30,9 +30,9 @@ class MatrixTest(unittest.TestCase):
         matrix = Matrix("1 2 3\n4 5 6\n7 8 9")
         self.assertEqual(matrix.column(3), [3, 6, 9])
 
-    def test_can_extract_column_from_non_square_matrix(self):
-        matrix = Matrix("1 2 3\n4 5 6\n7 8 9\n8 7 6")
-        self.assertEqual(matrix.column(3), [3, 6, 9, 6])
+    def test_can_extract_column_from_non_square_matrix_with_no_corresponding_row(self):
+        matrix = Matrix("1 2 3 4\n5 6 7 8\n9 8 7 6")
+        self.assertEqual(matrix.column(4), [4, 8, 6])
 
     def test_extract_column_where_numbers_have_different_widths(self):
         matrix = Matrix("89 1903 3\n18 3 1\n9 4 800")
