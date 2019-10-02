f3852b2e9961aa2cf66cc01a1a1e2ddd991426d6
MrNaif2018 <chuff184@gmail.com>
2019-10-02 18:39:37 +0300

    Fixes

--- exercises/crypto-square/crypto_square_test.py
+++ exercises/crypto-square/crypto_square_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from crypto_square import ciphertext
+from crypto_square import cipher_text
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v3.2.0
 
@@ -9,41 +9,41 @@ class CryptoSquareTest(unittest.TestCase):
     def test_empty_plaintext_results_in_an_empty_ciphertext(self):
         value = ""
         expected = ""
-        self.assertEqual(ciphertext(value), expected)
+        self.assertEqual(cipher_text(value), expected)
 
     def test_lowercase(self):
-        value = ""
+        value = "A"
         expected = "a"
-        self.assertEqual(ciphertext(value), expected)
+        self.assertEqual(cipher_text(value), expected)
 
     def test_remove_spaces(self):
-        value = ""
+        value = "  b "
         expected = "b"
-        self.assertEqual(ciphertext(value), expected)
+        self.assertEqual(cipher_text(value), expected)
 
     def test_remove_punctuation(self):
-        value = ""
+        value = "@1,%!"
         expected = "1"
-        self.assertEqual(ciphertext(value), expected)
+        self.assertEqual(cipher_text(value), expected)
 
     def test_9_character_plaintext_results_in_3_chunks_of_3_characters(self):
-        value = ""
+        value = "This is fun!"
         expected = "tsf hiu isn"
-        self.assertEqual(ciphertext(value), expected)
+        self.assertEqual(cipher_text(value), expected)
 
     def test_8_character_plaintext_results_in_3_chunks_the_last_one_with_a_trailing_space(
         self
     ):
-        value = ""
+        value = "Chill out."
         expected = "clu hlt io "
-        self.assertEqual(ciphertext(value), expected)
+        self.assertEqual(cipher_text(value), expected)
 
     def test_54_character_plaintext_results_in_7_chunks_the_last_two_with_trailing_spaces(
         self
     ):
-        value = ""
+        value = "If man was meant to stay on the ground, god would have given us roots."
         expected = "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau "
-        self.assertEqual(ciphertext(value), expected)
+        self.assertEqual(cipher_text(value), expected)
 
 
 if __name__ == "__main__":
