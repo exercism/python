8d082a056629767efd536bbfe3d4c0dea5bd451c
MrNaif2018 <chuff184@gmail.com>
2019-10-02 17:57:37 +0300

    Added crypto-square test template

--- exercises/crypto-square/crypto_square_test.py
+++ exercises/crypto-square/crypto_square_test.py
@@ -1,36 +1,50 @@
 import unittest
 
-from crypto_square import cipher_text
-
+from crypto_square import ciphertext
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v3.2.0
 
+
 class CryptoSquareTest(unittest.TestCase):
-    def test_empty_string(self):
-        self.assertEqual(cipher_text(''), '')
+    def test_empty_plaintext_results_in_an_empty_ciphertext(self):
+        value = ""
+        expected = ""
+        self.assertEqual(ciphertext(value), expected)
 
     def test_lowercase(self):
-        self.assertEqual(cipher_text('A'), 'a')
+        value = ""
+        expected = "a"
+        self.assertEqual(ciphertext(value), expected)
 
     def test_remove_spaces(self):
-        self.assertEqual(cipher_text('  b '), 'b')
+        value = ""
+        expected = "b"
+        self.assertEqual(ciphertext(value), expected)
 
     def test_remove_punctuation(self):
-        self.assertEqual(cipher_text('@1,%!'), '1')
+        value = ""
+        expected = "1"
+        self.assertEqual(ciphertext(value), expected)
 
-    def test_9chars_results_3chunks(self):
-        self.assertEqual(cipher_text('This is fun!'), 'tsf hiu isn')
+    def test_9_character_plaintext_results_in_3_chunks_of_3_characters(self):
+        value = ""
+        expected = "tsf hiu isn"
+        self.assertEqual(ciphertext(value), expected)
 
-    def test_8chars_results_3chunks_ending_space(self):
-        self.assertEqual(cipher_text('Chill out.'), 'clu hlt io ')
+    def test_8_character_plaintext_results_in_3_chunks_the_last_one_with_a_trailing_space(
+        self
+    ):
+        value = ""
+        expected = "clu hlt io "
+        self.assertEqual(ciphertext(value), expected)
 
-    def test_54chars_results_7chunks_2ending_space(self):
-        self.assertEqual(
-            cipher_text('If man was meant to stay on the ground, '
-                        'god would have given us roots.'),
-            'imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau '
-        )
+    def test_54_character_plaintext_results_in_7_chunks_the_last_two_with_trailing_spaces(
+        self
+    ):
+        value = ""
+        expected = "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau "
+        self.assertEqual(ciphertext(value), expected)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
