82ca44188f36124aaec5b3fa086a068608945ef9
dvermd <315743+dvermd@users.noreply.github.com>
2019-10-03 00:35:45 +0200

    word-search: add test template

--- exercises/word-search/word_search_test.py
+++ exercises/word-search/word_search_test.py
@@ -2,175 +2,324 @@ import unittest
 
 from word_search import WordSearch, Point
 
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.2.1
 
+
 class WordSearchTest(unittest.TestCase):
+    def test_should_accept_an_initial_game_grid_and_a_target_search_word(self):
+        puzzle = ["jefblpepre"]
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertIsNone(search_answer)
 
-    def test_initial_game_grid(self):
-        puzzle = ['jefblpepre']
-        searchAnswer = WordSearch(puzzle).search('clojure')
-        self.assertIsNone(searchAnswer)
+    def test_should_locate_one_word_written_left_to_right(self):
+        puzzle = ["clojurermt"]
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertEqual(search_answer, (Point(0, 0), Point(6, 0)))
 
-    def test_left_to_right_word(self):
-        puzzle = ['clojurermt']
-        searchAnswer = WordSearch(puzzle).search('clojure')
-        self.assertEqual(searchAnswer, (Point(0, 0), Point(6, 0)))
+    def test_should_locate_the_same_word_written_left_to_right_in_a_different_position(
+        self
+    ):
+        puzzle = ["mtclojurer"]
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertEqual(search_answer, (Point(2, 0), Point(8, 0)))
 
-    def test_left_to_right_word_different_position(self):
-        puzzle = ['mtclojurer']
-        searchAnswer = WordSearch(puzzle).search('clojure')
-        self.assertEqual(searchAnswer, (Point(2, 0), Point(8, 0)))
+    def test_should_locate_a_different_left_to_right_word(self):
+        puzzle = ["coffeelplx"]
+        search_answer = WordSearch(puzzle).search("coffee")
+        self.assertEqual(search_answer, (Point(0, 0), Point(5, 0)))
 
-    def test_different_left_to_right_word(self):
-        puzzle = ['coffeelplx']
-        searchAnswer = WordSearch(puzzle).search('coffee')
-        self.assertEqual(searchAnswer, (Point(0, 0), Point(5, 0)))
+    def test_should_locate_that_different_left_to_right_word_in_a_different_position(
+        self
+    ):
+        puzzle = ["xcoffeezlp"]
+        search_answer = WordSearch(puzzle).search("coffee")
+        self.assertEqual(search_answer, (Point(1, 0), Point(6, 0)))
 
-    def test_different_left_to_right_word_different_position(self):
-        puzzle = ['xcoffeezlp']
-        searchAnswer = WordSearch(puzzle).search('coffee')
-        self.assertEqual(searchAnswer, (Point(1, 0), Point(6, 0)))
+    def test_should_locate_a_left_to_right_word_in_two_line_grid(self):
+        puzzle = ["jefblpepre", "tclojurerm"]
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertEqual(search_answer, (Point(1, 1), Point(7, 1)))
 
-    def test_left_to_right_word_two_lines(self):
-        puzzle = ['jefblpepre',
-                  'tclojurerm']
-        searchAnswer = WordSearch(puzzle).search('clojure')
-        self.assertEqual(searchAnswer, (Point(1, 1), Point(7, 1)))
+    def test_should_locate_a_left_to_right_word_in_three_line_grid(self):
+        puzzle = ["camdcimgtc", "jefblpepre", "clojurermt"]
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertEqual(search_answer, (Point(0, 2), Point(6, 2)))
 
-    def test_left_to_right_word_three_lines(self):
-        puzzle = ['camdcimgtc',
-                  'jefblpepre',
-                  'clojurermt']
-        searchAnswer = WordSearch(puzzle).search('clojure')
-        self.assertEqual(searchAnswer, (Point(0, 2), Point(6, 2)))
+    def test_should_locate_a_left_to_right_word_in_ten_line_grid(self):
+        puzzle = [
+            "jefblpepre",
+            "camdcimgtc",
+            "oivokprjsm",
+            "pbwasqroua",
+            "rixilelhrs",
+            "wolcqlirpc",
+            "screeaumgr",
+            "alxhpburyi",
+            "jalaycalmp",
+            "clojurermt",
+        ]
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertEqual(search_answer, (Point(0, 9), Point(6, 9)))
 
-    def test_left_to_right_word_ten_lines(self):
-        puzzle = ['jefblpepre',
-                  'camdcimgtc',
-                  'oivokprjsm',
-                  'pbwasqroua',
-                  'rixilelhrs',
-                  'wolcqlirpc',
-                  'screeaumgr',
-                  'alxhpburyi',
-                  'jalaycalmp',
-                  'clojurermt']
-        searchAnswer = WordSearch(puzzle).search('clojure')
-        self.assertEqual(searchAnswer, (Point(0, 9), Point(6, 9)))
+    def test_should_locate_that_left_to_right_word_in_a_different_position_in_a_ten_line_grid(
+        self
+    ):
+        puzzle = [
+            "jefblpepre",
+            "camdcimgtc",
+            "oivokprjsm",
+            "pbwasqroua",
+            "rixilelhrs",
+            "wolcqlirpc",
+            "screeaumgr",
+            "alxhpburyi",
+            "clojurermt",
+            "jalaycalmp",
+        ]
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertEqual(search_answer, (Point(0, 8), Point(6, 8)))
 
-    def test_left_to_right_word_ten_lines_different_position(self):
-        puzzle = ['jefblpepre',
-                  'camdcimgtc',
-                  'oivokprjsm',
-                  'pbwasqroua',
-                  'rixilelhrs',
-                  'wolcqlirpc',
-                  'screeaumgr',
-                  'alxhpburyi',
-                  'clojurermt',
-                  'jalaycalmp']
-        searchAnswer = WordSearch(puzzle).search('clojure')
-        self.assertEqual(searchAnswer, (Point(0, 8), Point(6, 8)))
+    def test_should_locate_a_different_left_to_right_word_in_a_ten_line_grid(self):
+        puzzle = [
+            "jefblpepre",
+            "camdcimgtc",
+            "oivokprjsm",
+            "pbwasqroua",
+            "rixilelhrs",
+            "wolcqlirpc",
+            "fortranftw",
+            "alxhpburyi",
+            "clojurermt",
+            "jalaycalmp",
+        ]
+        search_answer = WordSearch(puzzle).search("fortran")
+        self.assertEqual(search_answer, (Point(0, 6), Point(6, 6)))
 
-    def test_different_left_to_right_word_ten_lines(self):
-        puzzle = ['jefblpepre',
-                  'camdcimgtc',
-                  'oivokprjsm',
-                  'pbwasqroua',
-                  'rixilelhrs',
-                  'wolcqlirpc',
-                  'fortranftw',
-                  'alxhpburyi',
-                  'clojurermt',
-                  'jalaycalmp']
-        searchAnswer = WordSearch(puzzle).search('fortran')
-        self.assertEqual(searchAnswer, (Point(0, 6), Point(6, 6)))
+    def test_should_locate_multiple_words(self):
+        puzzle = [
+            "jefblpepre",
+            "camdcimgtc",
+            "oivokprjsm",
+            "pbwasqroua",
+            "rixilelhrs",
+            "wolcqlirpc",
+            "fortranftw",
+            "alxhpburyi",
+            "jalaycalmp",
+            "clojurermt",
+        ]
+        search_answer = WordSearch(puzzle).search("fortran")
+        self.assertEqual(search_answer, (Point(0, 6), Point(6, 6)))
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertEqual(search_answer, (Point(0, 9), Point(6, 9)))
 
-    def test_multiple_words(self):
-        puzzle = ['jefblpepre',
-                  'camdcimgtc',
-                  'oivokprjsm',
-                  'pbwasqroua',
-                  'rixilelhrs',
-                  'wolcqlirpc',
-                  'fortranftw',
-                  'alxhpburyi',
-                  'jalaycalmp',
-                  'clojurermt']
-        searchAnswer = WordSearch(puzzle).search('fortran')
-        self.assertEqual(searchAnswer, (Point(0, 6), Point(6, 6)))
-        searchAnswer = WordSearch(puzzle).search('clojure')
-        self.assertEqual(searchAnswer, (Point(0, 9), Point(6, 9)))
+    def test_should_locate_a_single_word_written_right_to_left(self):
+        puzzle = ["rixilelhrs"]
+        search_answer = WordSearch(puzzle).search("elixir")
+        self.assertEqual(search_answer, (Point(5, 0), Point(0, 0)))
 
-    def test_single_word_right_to_left(self):
-        puzzle = ['rixilelhrs']
-        searchAnswer = WordSearch(puzzle).search('elixir')
-        self.assertEqual(searchAnswer, (Point(5, 0), Point(0, 0)))
+    def test_should_locate_multiple_words_written_in_different_horizontal_directions(
+        self
+    ):
+        puzzle = [
+            "jefblpepre",
+            "camdcimgtc",
+            "oivokprjsm",
+            "pbwasqroua",
+            "rixilelhrs",
+            "wolcqlirpc",
+            "screeaumgr",
+            "alxhpburyi",
+            "jalaycalmp",
+            "clojurermt",
+        ]
+        search_answer = WordSearch(puzzle).search("elixir")
+        self.assertEqual(search_answer, (Point(5, 4), Point(0, 4)))
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertEqual(search_answer, (Point(0, 9), Point(6, 9)))
 
-    @classmethod
-    def setUpClass(cls):
-        puzzle = ['jefblpepre',
-                  'camdcimgtc',
-                  'oivokprjsm',
-                  'pbwasqroua',
-                  'rixilelhrs',
-                  'wolcqlirpc',
-                  'screeaumgr',
-                  'alxhpburyi',
-                  'jalaycalmp',
-                  'clojurermt']
-        cls.example = WordSearch(puzzle)
+    def test_should_locate_words_written_top_to_bottom(self):
+        puzzle = [
+            "jefblpepre",
+            "camdcimgtc",
+            "oivokprjsm",
+            "pbwasqroua",
+            "rixilelhrs",
+            "wolcqlirpc",
+            "screeaumgr",
+            "alxhpburyi",
+            "jalaycalmp",
+            "clojurermt",
+        ]
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertEqual(search_answer, (Point(0, 9), Point(6, 9)))
+        search_answer = WordSearch(puzzle).search("elixir")
+        self.assertEqual(search_answer, (Point(5, 4), Point(0, 4)))
+        search_answer = WordSearch(puzzle).search("ecmascript")
+        self.assertEqual(search_answer, (Point(9, 0), Point(9, 9)))
 
-    def test_horizontal_words_different_directions(self):
-        self.assertEqual(
-            self.example.search('clojure'),
-            (Point(0, 9), Point(6, 9))
-        )
-        self.assertEqual(
-            self.example.search('elixir'),
-            (Point(5, 4), Point(0, 4))
-        )
+    def test_should_locate_words_written_bottom_to_top(self):
+        puzzle = [
+            "jefblpepre",
+            "camdcimgtc",
+            "oivokprjsm",
+            "pbwasqroua",
+            "rixilelhrs",
+            "wolcqlirpc",
+            "screeaumgr",
+            "alxhpburyi",
+            "jalaycalmp",
+            "clojurermt",
+        ]
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertEqual(search_answer, (Point(0, 9), Point(6, 9)))
+        search_answer = WordSearch(puzzle).search("elixir")
+        self.assertEqual(search_answer, (Point(5, 4), Point(0, 4)))
+        search_answer = WordSearch(puzzle).search("ecmascript")
+        self.assertEqual(search_answer, (Point(9, 0), Point(9, 9)))
+        search_answer = WordSearch(puzzle).search("rust")
+        self.assertEqual(search_answer, (Point(8, 4), Point(8, 1)))
 
-    def test_vertical_words_top_to_bottom(self):
-        self.assertEqual(
-            self.example.search('ecmascript'),
-            (Point(9, 0), Point(9, 9))
-        )
+    def test_should_locate_words_written_top_left_to_bottom_right(self):
+        puzzle = [
+            "jefblpepre",
+            "camdcimgtc",
+            "oivokprjsm",
+            "pbwasqroua",
+            "rixilelhrs",
+            "wolcqlirpc",
+            "screeaumgr",
+            "alxhpburyi",
+            "jalaycalmp",
+            "clojurermt",
+        ]
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertEqual(search_answer, (Point(0, 9), Point(6, 9)))
+        search_answer = WordSearch(puzzle).search("elixir")
+        self.assertEqual(search_answer, (Point(5, 4), Point(0, 4)))
+        search_answer = WordSearch(puzzle).search("ecmascript")
+        self.assertEqual(search_answer, (Point(9, 0), Point(9, 9)))
+        search_answer = WordSearch(puzzle).search("rust")
+        self.assertEqual(search_answer, (Point(8, 4), Point(8, 1)))
+        search_answer = WordSearch(puzzle).search("java")
+        self.assertEqual(search_answer, (Point(0, 0), Point(3, 3)))
 
-    def test_vertical_words_bottom_to_top(self):
-        self.assertEqual(
-            self.example.search('rust'),
-            (Point(8, 4), Point(8, 1))
-        )
+    def test_should_locate_words_written_bottom_right_to_top_left(self):
+        puzzle = [
+            "jefblpepre",
+            "camdcimgtc",
+            "oivokprjsm",
+            "pbwasqroua",
+            "rixilelhrs",
+            "wolcqlirpc",
+            "screeaumgr",
+            "alxhpburyi",
+            "jalaycalmp",
+            "clojurermt",
+        ]
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertEqual(search_answer, (Point(0, 9), Point(6, 9)))
+        search_answer = WordSearch(puzzle).search("elixir")
+        self.assertEqual(search_answer, (Point(5, 4), Point(0, 4)))
+        search_answer = WordSearch(puzzle).search("ecmascript")
+        self.assertEqual(search_answer, (Point(9, 0), Point(9, 9)))
+        search_answer = WordSearch(puzzle).search("rust")
+        self.assertEqual(search_answer, (Point(8, 4), Point(8, 1)))
+        search_answer = WordSearch(puzzle).search("java")
+        self.assertEqual(search_answer, (Point(0, 0), Point(3, 3)))
+        search_answer = WordSearch(puzzle).search("lua")
+        self.assertEqual(search_answer, (Point(7, 8), Point(5, 6)))
 
-    def test_diagonal_words_top_left_to_bottom_right(self):
-        self.assertEqual(
-            self.example.search('java'),
-            (Point(0, 0), Point(3, 3))
-        )
+    def test_should_locate_words_written_bottom_left_to_top_right(self):
+        puzzle = [
+            "jefblpepre",
+            "camdcimgtc",
+            "oivokprjsm",
+            "pbwasqroua",
+            "rixilelhrs",
+            "wolcqlirpc",
+            "screeaumgr",
+            "alxhpburyi",
+            "jalaycalmp",
+            "clojurermt",
+        ]
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertEqual(search_answer, (Point(0, 9), Point(6, 9)))
+        search_answer = WordSearch(puzzle).search("elixir")
+        self.assertEqual(search_answer, (Point(5, 4), Point(0, 4)))
+        search_answer = WordSearch(puzzle).search("ecmascript")
+        self.assertEqual(search_answer, (Point(9, 0), Point(9, 9)))
+        search_answer = WordSearch(puzzle).search("rust")
+        self.assertEqual(search_answer, (Point(8, 4), Point(8, 1)))
+        search_answer = WordSearch(puzzle).search("java")
+        self.assertEqual(search_answer, (Point(0, 0), Point(3, 3)))
+        search_answer = WordSearch(puzzle).search("lua")
+        self.assertEqual(search_answer, (Point(7, 8), Point(5, 6)))
+        search_answer = WordSearch(puzzle).search("lisp")
+        self.assertEqual(search_answer, (Point(2, 5), Point(5, 2)))
 
-    def test_diagonal_upper_bottom_right_to_top_left(self):
-        self.assertEqual(
-            self.example.search('lua'),
-            (Point(7, 8), Point(5, 6))
-        )
+    def test_should_locate_words_written_top_right_to_bottom_left(self):
+        puzzle = [
+            "jefblpepre",
+            "camdcimgtc",
+            "oivokprjsm",
+            "pbwasqroua",
+            "rixilelhrs",
+            "wolcqlirpc",
+            "screeaumgr",
+            "alxhpburyi",
+            "jalaycalmp",
+            "clojurermt",
+        ]
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertEqual(search_answer, (Point(0, 9), Point(6, 9)))
+        search_answer = WordSearch(puzzle).search("elixir")
+        self.assertEqual(search_answer, (Point(5, 4), Point(0, 4)))
+        search_answer = WordSearch(puzzle).search("ecmascript")
+        self.assertEqual(search_answer, (Point(9, 0), Point(9, 9)))
+        search_answer = WordSearch(puzzle).search("rust")
+        self.assertEqual(search_answer, (Point(8, 4), Point(8, 1)))
+        search_answer = WordSearch(puzzle).search("java")
+        self.assertEqual(search_answer, (Point(0, 0), Point(3, 3)))
+        search_answer = WordSearch(puzzle).search("lua")
+        self.assertEqual(search_answer, (Point(7, 8), Point(5, 6)))
+        search_answer = WordSearch(puzzle).search("lisp")
+        self.assertEqual(search_answer, (Point(2, 5), Point(5, 2)))
+        search_answer = WordSearch(puzzle).search("ruby")
+        self.assertEqual(search_answer, (Point(7, 5), Point(4, 8)))
 
-    def test_diagonal_upper_bottom_left_to_top_right(self):
-        self.assertEqual(
-            self.example.search('lisp'),
-            (Point(2, 5), Point(5, 2))
-        )
+    def test_should_fail_to_locate_a_word_that_is_not_in_the_puzzle(self):
+        puzzle = [
+            "jefblpepre",
+            "camdcimgtc",
+            "oivokprjsm",
+            "pbwasqroua",
+            "rixilelhrs",
+            "wolcqlirpc",
+            "screeaumgr",
+            "alxhpburyi",
+            "jalaycalmp",
+            "clojurermt",
+        ]
+        search_answer = WordSearch(puzzle).search("clojure")
+        self.assertEqual(search_answer, (Point(0, 9), Point(6, 9)))
+        search_answer = WordSearch(puzzle).search("elixir")
+        self.assertEqual(search_answer, (Point(5, 4), Point(0, 4)))
+        search_answer = WordSearch(puzzle).search("ecmascript")
+        self.assertEqual(search_answer, (Point(9, 0), Point(9, 9)))
+        search_answer = WordSearch(puzzle).search("rust")
+        self.assertEqual(search_answer, (Point(8, 4), Point(8, 1)))
+        search_answer = WordSearch(puzzle).search("java")
+        self.assertEqual(search_answer, (Point(0, 0), Point(3, 3)))
+        search_answer = WordSearch(puzzle).search("lua")
+        self.assertEqual(search_answer, (Point(7, 8), Point(5, 6)))
+        search_answer = WordSearch(puzzle).search("lisp")
+        self.assertEqual(search_answer, (Point(2, 5), Point(5, 2)))
+        search_answer = WordSearch(puzzle).search("ruby")
+        self.assertEqual(search_answer, (Point(7, 5), Point(4, 8)))
+        search_answer = WordSearch(puzzle).search("haskell")
+        self.assertIsNone(search_answer)
 
-    def test_diagonal_upper_top_right_to_bottom_left(self):
-        self.assertEqual(
-            self.example.search('ruby'),
-            (Point(7, 5), Point(4, 8))
-        )
 
-    def test_words_that_are_not_in_the_puzzle(self):
-        self.assertIsNone(self.example.search('haskell'))
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
