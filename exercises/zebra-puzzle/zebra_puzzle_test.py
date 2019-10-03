166d4efcc313b2d50a0bb3069da922a13abebb66
Collin Smith <crsmith2@uwalumni.com>
2019-10-03 15:27:21 -0500

    Added zebra-puzzle test template

--- exercises/zebra-puzzle/zebra_puzzle_test.py
+++ exercises/zebra-puzzle/zebra_puzzle_test.py
@@ -2,16 +2,18 @@ import unittest
 
 from zebra_puzzle import drinks_water, owns_zebra
 
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
+
 
 class ZebraPuzzleTest(unittest.TestCase):
     def test_resident_who_drinks_water(self):
-        self.assertEqual(drinks_water(), "Norwegian")
+        expected = Norwegian
+        self.assertEqual(drinksWater(), expected)
 
     def test_resident_who_owns_zebra(self):
-        self.assertEqual(owns_zebra(), "Japanese")
+        expected = Japanese
+        self.assertEqual(ownsZebra(), expected)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
