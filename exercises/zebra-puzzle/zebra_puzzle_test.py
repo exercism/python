ee0f2da356b2736fa173c452272738aacd45d4ca
Collin Smith <crsmith2@uwalumni.com>
2019-10-03 17:15:48 -0500

    Zebra-puzzle: fix function casing in test

--- exercises/zebra-puzzle/zebra_puzzle_test.py
+++ exercises/zebra-puzzle/zebra_puzzle_test.py
@@ -8,11 +8,11 @@ from zebra_puzzle import drinks_water, owns_zebra
 class ZebraPuzzleTest(unittest.TestCase):
     def test_resident_who_drinks_water(self):
         expected = "Norwegian"
-        self.assertEqual(drinksWater(), expected)
+        self.assertEqual(drinks_water(), expected)
 
     def test_resident_who_owns_zebra(self):
         expected = "Japanese"
-        self.assertEqual(ownsZebra(), expected)
+        self.assertEqual(owns_zebra(), expected)
 
 
 if __name__ == "__main__":
