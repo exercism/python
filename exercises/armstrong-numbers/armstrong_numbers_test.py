2681e4123d6926042809be6ea63e1afc79123a88
Jens <inhetpan.jd@gmail.com>
2019-10-03 23:12:21 +0200

    add generated test file

--- exercises/armstrong-numbers/armstrong_numbers_test.py
+++ exercises/armstrong-numbers/armstrong_numbers_test.py
@@ -2,38 +2,55 @@ import unittest
 
 from armstrong_numbers import is_armstrong_number
 
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
 
-class ArmstrongNumbersTest(unittest.TestCase):
 
+class ArmstrongNumbersTest(unittest.TestCase):
     def test_zero_is_an_armstrong_number(self):
-        self.assertIs(is_armstrong_number(0), True)
+        input = 0
+        expected = True
+        self.assertEqual(is_armstrong_number(input), expected)
 
     def test_single_digit_numbers_are_armstrong_numbers(self):
-        self.assertIs(is_armstrong_number(5), True)
+        input = 5
+        expected = True
+        self.assertEqual(is_armstrong_number(input), expected)
 
-    def test_there_are_no_two_digit_armstrong_numbers(self):
-        self.assertIs(is_armstrong_number(10), False)
+    def test_there_are_no_2_digit_armstrong_numbers(self):
+        input = 10
+        expected = False
+        self.assertEqual(is_armstrong_number(input), expected)
 
     def test_three_digit_number_that_is_an_armstrong_number(self):
-        self.assertIs(is_armstrong_number(153), True)
+        input = 153
+        expected = True
+        self.assertEqual(is_armstrong_number(input), expected)
 
     def test_three_digit_number_that_is_not_an_armstrong_number(self):
-        self.assertIs(is_armstrong_number(100), False)
+        input = 100
+        expected = False
+        self.assertEqual(is_armstrong_number(input), expected)
 
     def test_four_digit_number_that_is_an_armstrong_number(self):
-        self.assertIs(is_armstrong_number(9474), True)
+        input = 9474
+        expected = True
+        self.assertEqual(is_armstrong_number(input), expected)
 
     def test_four_digit_number_that_is_not_an_armstrong_number(self):
-        self.assertIs(is_armstrong_number(9475), False)
+        input = 9475
+        expected = False
+        self.assertEqual(is_armstrong_number(input), expected)
 
     def test_seven_digit_number_that_is_an_armstrong_number(self):
-        self.assertIs(is_armstrong_number(9926315), True)
+        input = 9926315
+        expected = True
+        self.assertEqual(is_armstrong_number(input), expected)
 
     def test_seven_digit_number_that_is_not_an_armstrong_number(self):
-        self.assertIs(is_armstrong_number(9926314), False)
+        input = 9926314
+        expected = False
+        self.assertEqual(is_armstrong_number(input), expected)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
