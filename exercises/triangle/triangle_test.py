e52fdff4c4bfc58ccb09355d6f20a0d52a0b3ad9
manasap <manasa.patibandla@iga.in.gov>
2019-10-03 17:10:12 -0400

    updated template with variables removed

--- exercises/triangle/triangle_test.py
+++ exercises/triangle/triangle_test.py
@@ -7,103 +7,65 @@ from triangle import equilateral, isosceles, scalene
 
 class EquilateralTriangleTest(unittest.TestCase):
     def test_all_sides_are_equal(self):
-        input = [2, 2, 2]
-        expected = True
-        self.assertEqual(equilateral(input), expected)
+        self.assertEqual(equilateral([2, 2, 2]), True)
 
     def test_any_side_is_unequal(self):
-        input = [2, 3, 2]
-        expected = False
-        self.assertEqual(equilateral(input), expected)
+        self.assertEqual(equilateral([2, 3, 2]), False)
 
     def test_no_sides_are_equal(self):
-        input = [5, 4, 6]
-        expected = False
-        self.assertEqual(equilateral(input), expected)
+        self.assertEqual(equilateral([5, 4, 6]), False)
 
     def test_all_zero_sides_is_not_a_triangle(self):
-        input = [0, 0, 0]
-        expected = False
-        self.assertEqual(equilateral(input), expected)
+        self.assertEqual(equilateral([0, 0, 0]), False)
 
     def test_sides_may_be_floats(self):
-        input = [0.5, 0.5, 0.5]
-        expected = True
-        self.assertEqual(equilateral(input), expected)
+        self.assertEqual(equilateral([0.5, 0.5, 0.5]), True)
 
 
 class IsoscelesTriangleTest(unittest.TestCase):
     def test_last_two_sides_are_equal(self):
-        input = [3, 4, 4]
-        expected = True
-        self.assertEqual(isosceles(input), expected)
+        self.assertEqual(isosceles([3, 4, 4]), True)
 
     def test_first_two_sides_are_equal(self):
-        input = [4, 4, 3]
-        expected = True
-        self.assertEqual(isosceles(input), expected)
+        self.assertEqual(isosceles([4, 4, 3]), True)
 
     def test_first_and_last_sides_are_equal(self):
-        input = [4, 3, 4]
-        expected = True
-        self.assertEqual(isosceles(input), expected)
+        self.assertEqual(isosceles([4, 3, 4]), True)
 
     def test_equilateral_triangles_are_also_isosceles(self):
-        input = [4, 4, 4]
-        expected = True
-        self.assertEqual(isosceles(input), expected)
+        self.assertEqual(isosceles([4, 4, 4]), True)
 
     def test_no_sides_are_equal(self):
-        input = [2, 3, 4]
-        expected = False
-        self.assertEqual(isosceles(input), expected)
+        self.assertEqual(isosceles([2, 3, 4]), False)
 
     def test_first_triangle_inequality_violation(self):
-        input = [1, 1, 3]
-        expected = False
-        self.assertEqual(isosceles(input), expected)
+        self.assertEqual(isosceles([1, 1, 3]), False)
 
     def test_second_triangle_inequality_violation(self):
-        input = [1, 3, 1]
-        expected = False
-        self.assertEqual(isosceles(input), expected)
+        self.assertEqual(isosceles([1, 3, 1]), False)
 
     def test_third_triangle_inequality_violation(self):
-        input = [3, 1, 1]
-        expected = False
-        self.assertEqual(isosceles(input), expected)
+        self.assertEqual(isosceles([3, 1, 1]), False)
 
     def test_sides_may_be_floats(self):
-        input = [0.5, 0.4, 0.5]
-        expected = True
-        self.assertEqual(isosceles(input), expected)
+        self.assertEqual(isosceles([0.5, 0.4, 0.5]), True)
 
 
 class ScaleneTriangleTest(unittest.TestCase):
     def test_no_sides_are_equal(self):
-        input = [5, 4, 6]
-        expected = True
-        self.assertEqual(scalene(input), expected)
+        self.assertEqual(scalene([5, 4, 6]), True)
 
     def test_all_sides_are_equal(self):
-        input = [4, 4, 4]
-        expected = False
-        self.assertEqual(scalene(input), expected)
+        self.assertEqual(scalene([4, 4, 4]), False)
 
     def test_two_sides_are_equal(self):
-        input = [4, 4, 3]
-        expected = False
-        self.assertEqual(scalene(input), expected)
+        self.assertEqual(scalene([4, 4, 3]), False)
 
     def test_may_not_violate_triangle_inequality(self):
-        input = [7, 3, 2]
-        expected = False
-        self.assertEqual(scalene(input), expected)
+        self.assertEqual(scalene([7, 3, 2]), False)
 
     def test_sides_may_be_floats(self):
-        input = [0.5, 0.4, 0.6]
-        expected = True
-        self.assertEqual(scalene(input), expected)
+        self.assertEqual(scalene([0.5, 0.4, 0.6]), True)
 
 
 if __name__ == "__main__":
