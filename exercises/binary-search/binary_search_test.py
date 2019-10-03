47380738cc877114b4007e9b1752e707dad7e3bf
manasap <manasa.patibandla@iga.in.gov>
2019-10-03 10:56:11 -0400

    Updated test template with error checking

--- exercises/binary-search/binary_search_test.py
+++ exercises/binary-search/binary_search_test.py
@@ -7,70 +7,63 @@ from binary_search import find
 
 class BinarySearchTest(unittest.TestCase):
     def test_finds_a_value_in_an_array_with_one_element(self):
-        input_array = [6]
-        input_search_value = 6
         expected = 0
-        self.assertEqual(find(input_array, input_search_value), expected)
+        self.assertEqual(find("[6]", "6"), expected)
 
     def test_finds_a_value_in_the_middle_of_an_array(self):
-        input_array = [1, 3, 4, 6, 8, 9, 11]
-        input_search_value = 6
         expected = 3
-        self.assertEqual(find(input_array, input_search_value), expected)
+        self.assertEqual(find("[1, 3, 4, 6, 8, 9, 11]", "6"), expected)
 
     def test_finds_a_value_at_the_beginning_of_an_array(self):
-        input_array = [1, 3, 4, 6, 8, 9, 11]
-        input_search_value = 1
         expected = 0
-        self.assertEqual(find(input_array, input_search_value), expected)
+        self.assertEqual(find("[1, 3, 4, 6, 8, 9, 11]", "1"), expected)
 
     def test_finds_a_value_at_the_end_of_an_array(self):
-        input_array = [1, 3, 4, 6, 8, 9, 11]
-        input_search_value = 11
         expected = 6
-        self.assertEqual(find(input_array, input_search_value), expected)
+        self.assertEqual(find("[1, 3, 4, 6, 8, 9, 11]", "11"), expected)
 
     def test_finds_a_value_in_an_array_of_odd_length(self):
-        input_array = [1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634]
-        input_search_value = 144
         expected = 9
-        self.assertEqual(find(input_array, input_search_value), expected)
+        self.assertEqual(
+            find("[1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634]", "144"),
+            expected,
+        )
 
     def test_finds_a_value_in_an_array_of_even_length(self):
-        input_array = [1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
-        input_search_value = 21
         expected = 5
-        self.assertEqual(find(input_array, input_search_value), expected)
+        self.assertEqual(
+            find("[1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]", "21"), expected
+        )
 
     def test_identifies_that_a_value_is_not_included_in_the_array(self):
-        input_array = [1, 3, 4, 6, 8, 9, 11]
-        input_search_value = 7
-        expected = {"error": "value not in array"}
-        self.assertEqual(find(input_array, input_search_value), expected)
+        with self.assertRaisesWithMessage(ValueError):
+            find("[1, 3, 4, 6, 8, 9, 11]", "7")
 
     def test_a_value_smaller_than_the_array_s_smallest_value_is_not_found(self):
-        input_array = [1, 3, 4, 6, 8, 9, 11]
-        input_search_value = 0
-        expected = {"error": "value not in array"}
-        self.assertEqual(find(input_array, input_search_value), expected)
+        with self.assertRaisesWithMessage(ValueError):
+            find("[1, 3, 4, 6, 8, 9, 11]", "0")
 
     def test_a_value_larger_than_the_array_s_largest_value_is_not_found(self):
-        input_array = [1, 3, 4, 6, 8, 9, 11]
-        input_search_value = 13
-        expected = {"error": "value not in array"}
-        self.assertEqual(find(input_array, input_search_value), expected)
+        with self.assertRaisesWithMessage(ValueError):
+            find("[1, 3, 4, 6, 8, 9, 11]", "13")
 
     def test_nothing_is_found_in_an_empty_array(self):
-        input_array = []
-        input_search_value = 1
-        expected = {"error": "value not in array"}
-        self.assertEqual(find(input_array, input_search_value), expected)
+        with self.assertRaisesWithMessage(ValueError):
+            find("[]", "1")
 
     def test_nothing_is_found_when_the_left_and_right_bounds_cross(self):
-        input_array = [1, 2]
-        input_search_value = 0
-        expected = {"error": "value not in array"}
-        self.assertEqual(find(input_array, input_search_value), expected)
+        with self.assertRaisesWithMessage(ValueError):
+            find("[1, 2]", "0")
+
+    # Utility functions
+    def setUp(self):
+        try:
+            self.assertRaisesRegex
+        except AttributeError:
+            self.assertRaisesRegex = self.assertRaisesRegexp
+
+    def assertRaisesWithMessage(self, exception):
+        return self.assertRaisesRegex(exception, r".+")
 
 
 if __name__ == "__main__":
