6e0bab1ce0bbcc04537430f7de9ca8cc364eeb3b
Collin Smith <crsmith2@uwalumni.com>
2019-10-03 17:18:51 -0500

    Zebra-puzzle: Condense test cases to one line

--- exercises/zebra-puzzle/zebra_puzzle_test.py
+++ exercises/zebra-puzzle/zebra_puzzle_test.py
@@ -7,12 +7,10 @@ from zebra_puzzle import drinks_water, owns_zebra
 
 class ZebraPuzzleTest(unittest.TestCase):
     def test_resident_who_drinks_water(self):
-        expected = "Norwegian"
-        self.assertEqual(drinks_water(), expected)
+        self.assertEqual(drinks_water(), "Norwegian")
 
     def test_resident_who_owns_zebra(self):
-        expected = "Japanese"
-        self.assertEqual(owns_zebra(), expected)
+        self.assertEqual(owns_zebra(), "Japanese")
 
 
 if __name__ == "__main__":
