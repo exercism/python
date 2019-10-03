42b8281082fe152716e148462fc47dce4f55c697
Jens <inhetpan.jd@gmail.com>
2019-10-03 23:53:47 +0200

    Updated assertion method according to conventions

--- exercises/armstrong-numbers/armstrong_numbers_test.py
+++ exercises/armstrong-numbers/armstrong_numbers_test.py
@@ -7,31 +7,31 @@ from armstrong_numbers import is_armstrong_number
 
 class ArmstrongNumbersTest(unittest.TestCase):
     def test_zero_is_an_armstrong_number(self):
-        self.assertEqual(is_armstrong_number(0), True)
+        self.assertIs(is_armstrong_number(0), True)
 
     def test_single_digit_numbers_are_armstrong_numbers(self):
-        self.assertEqual(is_armstrong_number(5), True)
+        self.assertIs(is_armstrong_number(5), True)
 
     def test_there_are_no_2_digit_armstrong_numbers(self):
-        self.assertEqual(is_armstrong_number(10), False)
+        self.assertIs(is_armstrong_number(10), False)
 
     def test_three_digit_number_that_is_an_armstrong_number(self):
-        self.assertEqual(is_armstrong_number(153), True)
+        self.assertIs(is_armstrong_number(153), True)
 
     def test_three_digit_number_that_is_not_an_armstrong_number(self):
-        self.assertEqual(is_armstrong_number(100), False)
+        self.assertIs(is_armstrong_number(100), False)
 
     def test_four_digit_number_that_is_an_armstrong_number(self):
-        self.assertEqual(is_armstrong_number(9474), True)
+        self.assertIs(is_armstrong_number(9474), True)
 
     def test_four_digit_number_that_is_not_an_armstrong_number(self):
-        self.assertEqual(is_armstrong_number(9475), False)
+        self.assertIs(is_armstrong_number(9475), False)
 
     def test_seven_digit_number_that_is_an_armstrong_number(self):
-        self.assertEqual(is_armstrong_number(9926315), True)
+        self.assertIs(is_armstrong_number(9926315), True)
 
     def test_seven_digit_number_that_is_not_an_armstrong_number(self):
-        self.assertEqual(is_armstrong_number(9926314), False)
+        self.assertIs(is_armstrong_number(9926314), False)
 
 
 if __name__ == "__main__":
