b9d28ce91e70ef90ddbf5e1829c08272897388d5
dvermd <315743+dvermd@users.noreply.github.com>
2019-10-05 04:31:34 +0200

    sublist: add test template (#2028)

--- exercises/sublist/sublist_test.py
+++ exercises/sublist/sublist_test.py
@@ -2,141 +2,82 @@ import unittest
 
 from sublist import sublist, SUBLIST, SUPERLIST, EQUAL, UNEQUAL
 
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
+
 
 class SublistTest(unittest.TestCase):
     def test_empty_lists(self):
-        self.assertEqual(
-            sublist([], []),
-            EQUAL
-        )
+        self.assertEqual(sublist([], []), EQUAL)
 
     def test_empty_list_within_non_empty_list(self):
-        self.assertEqual(
-            sublist([], [1, 2, 3]),
-            SUBLIST
-        )
+        self.assertEqual(sublist([], [1, 2, 3]), SUBLIST)
 
     def test_non_empty_list_contains_empty_list(self):
-        self.assertEqual(
-            sublist([1, 2, 3], []),
-            SUPERLIST
-        )
+        self.assertEqual(sublist([1, 2, 3], []), SUPERLIST)
 
     def test_list_equals_itself(self):
-        self.assertEqual(
-            sublist([1, 2, 3], [1, 2, 3]),
-            EQUAL
-        )
+        self.assertEqual(sublist([1, 2, 3], [1, 2, 3]), EQUAL)
 
     def test_different_lists(self):
-        self.assertEqual(
-            sublist([1, 2, 3], [2, 3, 4]),
-            UNEQUAL
-        )
+        self.assertEqual(sublist([1, 2, 3], [2, 3, 4]), UNEQUAL)
 
     def test_false_start(self):
-        self.assertEqual(
-            sublist([1, 2, 5], [0, 1, 2, 3, 1, 2, 5, 6]),
-            SUBLIST
-        )
+        self.assertEqual(sublist([1, 2, 5], [0, 1, 2, 3, 1, 2, 5, 6]), SUBLIST)
 
     def test_consecutive(self):
-        self.assertEqual(
-            sublist([1, 1, 2], [0, 1, 1, 1, 2, 1, 2]),
-            SUBLIST
-        )
+        self.assertEqual(sublist([1, 1, 2], [0, 1, 1, 1, 2, 1, 2]), SUBLIST)
 
     def test_sublist_at_start(self):
-        self.assertEqual(
-            sublist([0, 1, 2], [0, 1, 2, 3, 4, 5]),
-            SUBLIST
-        )
+        self.assertEqual(sublist([0, 1, 2], [0, 1, 2, 3, 4, 5]), SUBLIST)
 
     def test_sublist_in_middle(self):
-        self.assertEqual(
-            sublist([2, 3, 4], [0, 1, 2, 3, 4, 5]),
-            SUBLIST
-        )
+        self.assertEqual(sublist([2, 3, 4], [0, 1, 2, 3, 4, 5]), SUBLIST)
 
     def test_sublist_at_end(self):
-        self.assertEqual(
-            sublist([3, 4, 5], [0, 1, 2, 3, 4, 5]),
-            SUBLIST
-        )
+        self.assertEqual(sublist([3, 4, 5], [0, 1, 2, 3, 4, 5]), SUBLIST)
 
     def test_at_start_of_superlist(self):
-        self.assertEqual(
-            sublist([0, 1, 2, 3, 4, 5], [0, 1, 2]),
-            SUPERLIST
-        )
+        self.assertEqual(sublist([0, 1, 2, 3, 4, 5], [0, 1, 2]), SUPERLIST)
 
     def test_in_middle_of_superlist(self):
-        self.assertEqual(
-            sublist([0, 1, 2, 3, 4, 5], [2, 3]),
-            SUPERLIST
-        )
+        self.assertEqual(sublist([0, 1, 2, 3, 4, 5], [2, 3]), SUPERLIST)
 
     def test_at_end_of_superlist(self):
-        self.assertEqual(
-            sublist([0, 1, 2, 3, 4, 5], [3, 4, 5]),
-            SUPERLIST
-        )
+        self.assertEqual(sublist([0, 1, 2, 3, 4, 5], [3, 4, 5]), SUPERLIST)
 
     def test_first_list_missing_element_from_second_list(self):
-        self.assertEqual(
-            sublist([1, 3], [1, 2, 3]),
-            UNEQUAL
-        )
+        self.assertEqual(sublist([1, 3], [1, 2, 3]), UNEQUAL)
 
     def test_second_list_missing_element_from_first_list(self):
-        self.assertEqual(
-            sublist([1, 2, 3], [1, 3]),
-            UNEQUAL
-        )
+        self.assertEqual(sublist([1, 2, 3], [1, 3]), UNEQUAL)
 
     def test_order_matters_to_a_list(self):
-        self.assertEqual(
-            sublist([1, 2, 3], [3, 2, 1]),
-            UNEQUAL
-        )
+        self.assertEqual(sublist([1, 2, 3], [3, 2, 1]), UNEQUAL)
 
     def test_same_digits_but_different_numbers(self):
-        self.assertEqual(
-            sublist([1, 0, 1], [10, 1]),
-            UNEQUAL
-        )
+        self.assertEqual(sublist([1, 0, 1], [10, 1]), UNEQUAL)
 
-    # additional track specific test
+    # Additional tests for this track
     def test_unique_return_values(self):
         self.assertEqual(len(set([SUBLIST, SUPERLIST, EQUAL, UNEQUAL])), 4)
 
-    # additional track specific test
     def test_inner_spaces(self):
-        self.assertEqual(
-            sublist(['a c'], ['a', 'c']),
-            UNEQUAL
-        )
+        self.assertEqual(sublist(["a c"], ["a", "c"]), UNEQUAL)
 
-    # additional track specific test
     def test_large_lists(self):
-        list_1 = list(range(1000)) * 1000 + list(range(1000, 1100))
-        list_2 = list(range(900, 1050))
         self.assertEqual(
-            sublist(list_1, list_2),
-            SUPERLIST
+            sublist(
+                list(range(1000)) * 1000 + list(range(1000, 1100)),
+                list(range(900, 1050)),
+            ),
+            SUPERLIST,
         )
 
-    # additional track specific test
     def test_spread_sublist(self):
-        multiples_of_3 = list(range(3, 200, 3))
-        multiples_of_15 = list(range(15, 200, 15))
         self.assertEqual(
-            sublist(multiples_of_15, multiples_of_3),
-            UNEQUAL
+            sublist(list(range(3, 200, 3)), list(range(15, 200, 15))), UNEQUAL
         )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
