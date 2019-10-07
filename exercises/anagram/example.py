5078354aded585c0e5ff23d5c1faa82db49360b9
Nishant Ingle <nishantingle5@gmail.com>
2019-10-07 19:06:23 +0530

    Added Comment

--- exercises/anagram/example.py
+++ exercises/anagram/example.py
@@ -4,6 +4,7 @@ def find_anagrams(word, candidates):
             if _letters(candidate) == _letters(word)
             if candidate.lower() != word.lower()]
 
-
 def _letters(word):
+	# lower is convert string to lowercase and sorted converted the string to 
+	# list with elements arranged in sorted order
     return sorted(word.lower())
