13fe001f71a28e92dcb468b17ae344f416bb90d0
Collin Smith <crsmith2@uwalumni.com>
2019-10-15 20:36:12 -0500

    saddle-points: clean up template Improve readability and maintainability. Handle error_case without overriding built-in. [{}] -> []: better represents an empty matrix.

--- exercises/saddle-points/saddle_points_test.py
+++ exercises/saddle-points/saddle_points_test.py
@@ -20,31 +20,25 @@ class SaddlePointsTest(unittest.TestCase):
 
     def test_can_identify_that_empty_matrix_has_no_saddle_points(self):
         matrix = []
-        self.assertEqual(saddle_points(matrix), [{}])
+        self.assertEqual(saddle_points(matrix), [])
 
     def test_can_identify_lack_of_saddle_points_when_there_are_none(self):
         matrix = [[1, 2, 3], [3, 1, 2], [2, 3, 1]]
-        self.assertEqual(saddle_points(matrix), [{}])
+        self.assertEqual(saddle_points(matrix), [])
 
     def test_can_identify_multiple_saddle_points_in_a_column(self):
         matrix = [[4, 5, 4], [3, 5, 5], [1, 5, 4]]
-        expected = [
-            {"row": 1, "column": 2},
-            {"row": 2, "column": 2},
-            {"row": 3, "column": 2},
-        ]
-
-        self.assertEqual(saddle_points(matrix), expected)
+        self.assertEqual(
+            saddle_points(matrix),
+            [{"row": 1, "column": 2}, {"row": 2, "column": 2}, {"row": 3, "column": 2}],
+        )
 
     def test_can_identify_multiple_saddle_points_in_a_row(self):
         matrix = [[6, 7, 8], [5, 5, 5], [7, 5, 6]]
-        expected = [
-            {"row": 2, "column": 1},
-            {"row": 2, "column": 2},
-            {"row": 2, "column": 3},
-        ]
-
-        self.assertEqual(saddle_points(matrix), expected)
+        self.assertEqual(
+            saddle_points(matrix),
+            [{"row": 2, "column": 1}, {"row": 2, "column": 2}, {"row": 2, "column": 3}],
+        )
 
     def test_can_identify_saddle_point_in_bottom_right_corner(self):
         matrix = [[8, 7, 9], [6, 7, 6], [3, 2, 5]]
@@ -53,7 +47,7 @@ class SaddlePointsTest(unittest.TestCase):
     def test_can_identify_saddle_points_in_a_non_square_matrix(self):
         matrix = [[3, 1, 3], [3, 2, 4]]
         self.assertEqual(
-            saddle_points(matrix), [{"row": 1, "column": 1}, {"row": 1, "column": 3}]
+            saddle_points(matrix), [{"row": 1, "column": 3}, {"row": 1, "column": 1}]
         )
 
     def test_can_identify_that_saddle_points_in_a_single_column_matrix_are_those_with_the_minimum_value(
