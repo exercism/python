bbe930798da30b5d288c7e05d0e7d6119d214c72
Simon Bukin <simonbukin@gmail.com>
2019-10-18 20:02:11 -0700

    Add gigasecond test template and generate tests.

--- exercises/gigasecond/gigasecond_test.py
+++ exercises/gigasecond/gigasecond_test.py
@@ -1,44 +1,36 @@
 import unittest
-from datetime import datetime
 
 from gigasecond import add
 
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v2.0.0
 
+
 class GigasecondTest(unittest.TestCase):
-    def test_date_only_specification_of_time(self):
-        self.assertEqual(
-            add(datetime(2011, 4, 25)),
-            datetime(2043, 1, 1, 1, 46, 40))
+    def date_only_specification_of_time(self):
+        test_value = "2011-04-25"
+        expected = "2043-01-01T01:46:40"
+        self.assertEqual(add(test_value), expected)
 
-    def test_another_date_only_specification_of_time(self):
-        self.assertEqual(
-            add(datetime(1977, 6, 13)),
-            datetime(2009, 2, 19, 1, 46, 40))
+    def second_test_for_date_only_specification_of_time(self):
+        test_value = "1977-06-13"
+        expected = "2009-02-19T01:46:40"
+        self.assertEqual(add(test_value), expected)
 
-    def test_one_more_date_only_specification_of_time(self):
-        self.assertEqual(
-            add(datetime(1959, 7, 19)),
-            datetime(1991, 3, 27, 1, 46, 40))
+    def third_test_for_date_only_specification_of_time(self):
+        test_value = "1959-07-19"
+        expected = "1991-03-27T01:46:40"
+        self.assertEqual(add(test_value), expected)
 
-    def test_full_time_specified(self):
-        self.assertEqual(
-            add(datetime(2015, 1, 24, 22, 0, 0)),
-            datetime(2046, 10, 2, 23, 46, 40))
+    def full_time_specified(self):
+        test_value = "2015-01-24T22:00:00"
+        expected = "2046-10-02T23:46:40"
+        self.assertEqual(add(test_value), expected)
 
-    def test_full_time_with_day_roll_over(self):
-        self.assertEqual(
-            add(datetime(2015, 1, 24, 23, 59, 59)),
-            datetime(2046, 10, 3, 1, 46, 39))
-
-    def test_yourself(self):
-        # customize this to test your birthday and find your gigasecond date:
-        your_birthday = datetime(1970, 1, 1)
-        your_gigasecond = datetime(2001, 9, 9, 1, 46, 40)
-
-        self.assertEqual(add(your_birthday), your_gigasecond)
+    def full_time_with_day_roll_over(self):
+        test_value = "2015-01-24T23:59:59"
+        expected = "2046-10-03T01:46:39"
+        self.assertEqual(add(test_value), expected)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
