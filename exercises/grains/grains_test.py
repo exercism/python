cdb33d1ac19390da2aa6339c97b73e2a24596b3d
Sean Malley <sean@seanmalley.com>
2019-10-19 22:31:07 -0700

    grains: add test template
    Add test template for grains which adds all square() tests and the method to generate the only existing total() test. total() should not take a parameter so parameter removed from grains.py template and example.py. Collapse check function into square() since total will no longer take a parameter.
    
    Closes #1952.

--- exercises/grains/grains_test.py
+++ exercises/grains/grains_test.py
@@ -2,51 +2,45 @@ import unittest
 
 from grains import square, total
 
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.2.0
 
+
 class GrainsTest(unittest.TestCase):
-    def test_square_1(self):
+    def test_1(self):
         self.assertEqual(square(1), 1)
 
-    def test_square_2(self):
+    def test_2(self):
         self.assertEqual(square(2), 2)
 
-    def test_square_3(self):
+    def test_3(self):
         self.assertEqual(square(3), 4)
 
-    def test_square_4(self):
+    def test_4(self):
         self.assertEqual(square(4), 8)
 
-    def test_square_16(self):
+    def test_16(self):
         self.assertEqual(square(16), 32768)
 
-    def test_square_32(self):
+    def test_32(self):
         self.assertEqual(square(32), 2147483648)
 
-    def test_square_64(self):
+    def test_64(self):
         self.assertEqual(square(64), 9223372036854775808)
 
-    def test_square_0_raises_exception(self):
+    def test_square_0_raises_an_exception(self):
         with self.assertRaisesWithMessage(ValueError):
             square(0)
-        with self.assertRaisesWithMessage(ValueError):
-            total(0)
 
-    def test_square_negative_raises_exception(self):
+    def test_negative_square_raises_an_exception(self):
         with self.assertRaisesWithMessage(ValueError):
             square(-1)
-        with self.assertRaisesWithMessage(ValueError):
-            total(-1)
 
-    def test_square_gt_64_raises_exception(self):
+    def test_square_greater_than_64_raises_an_exception(self):
         with self.assertRaisesWithMessage(ValueError):
             square(65)
-        with self.assertRaisesWithMessage(ValueError):
-            total(65)
 
-    def test_total(self):
-        self.assertEqual(total(64), 18446744073709551615)
+    def test_returns_the_total_number_of_grains_on_the_board(self):
+        self.assertEqual(total(), 18446744073709551615)
 
     # Utility functions
     def setUp(self):
@@ -59,5 +53,5 @@ class GrainsTest(unittest.TestCase):
         return self.assertRaisesRegex(exception, r".+")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
