da1b749af6e5fead635514860ed5e47568afa83e
Sean Malley <sean@seanmalley.com>
2019-10-19 23:29:15 -0700

    perfect-numbers: Add Test Template.
    Add test template to generate perfect numbers tests from canonical-data.json. Closes #1965

--- exercises/perfect-numbers/perfect_numbers_test.py
+++ exercises/perfect-numbers/perfect_numbers_test.py
@@ -2,54 +2,54 @@ import unittest
 
 from perfect_numbers import classify
 
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
 
+
 class PerfectNumbersTest(unittest.TestCase):
-    def test_smallest_perfect_number(self):
+    def test_smallest_perfect_number_is_classified_correctly(self):
         self.assertIs(classify(6), "perfect")
 
-    def test_medium_perfect_number(self):
+    def test_medium_perfect_number_is_classified_correctly(self):
         self.assertIs(classify(28), "perfect")
 
-    def test_large_perfect_number(self):
+    def test_large_perfect_number_is_classified_correctly(self):
         self.assertIs(classify(33550336), "perfect")
 
 
 class AbundantNumbersTest(unittest.TestCase):
-    def test_smallest_abundant_number(self):
+    def test_smallest_abundant_number_is_classified_correctly(self):
         self.assertIs(classify(12), "abundant")
 
-    def test_medium_abundant_number(self):
+    def test_medium_abundant_number_is_classified_correctly(self):
         self.assertIs(classify(30), "abundant")
 
-    def test_large_abundant_number(self):
+    def test_large_abundant_number_is_classified_correctly(self):
         self.assertIs(classify(33550335), "abundant")
 
 
 class DeficientNumbersTest(unittest.TestCase):
-    def test_smallest_prime_deficient_number(self):
+    def test_smallest_prime_deficient_number_is_classified_correctly(self):
         self.assertIs(classify(2), "deficient")
 
-    def test_smallest_nonprime_deficient_number(self):
+    def test_smallest_non_prime_deficient_number_is_classified_correctly(self):
         self.assertIs(classify(4), "deficient")
 
-    def test_medium_deficient_number(self):
+    def test_medium_deficient_number_is_classified_correctly(self):
         self.assertIs(classify(32), "deficient")
 
-    def test_large_deficient_number(self):
+    def test_large_deficient_number_is_classified_correctly(self):
         self.assertIs(classify(33550337), "deficient")
 
-    def test_edge_case(self):
+    def test_edge_case_no_factors_other_than_itself_is_classified_correctly(self):
         self.assertIs(classify(1), "deficient")
 
 
 class InvalidInputsTest(unittest.TestCase):
-    def test_zero(self):
+    def test_zero_is_rejected_not_a_natural_number(self):
         with self.assertRaisesWithMessage(ValueError):
             classify(0)
 
-    def test_negative(self):
+    def test_negative_integer_is_rejected_not_a_natural_number(self):
         with self.assertRaisesWithMessage(ValueError):
             classify(-1)
 
@@ -64,5 +64,5 @@ class InvalidInputsTest(unittest.TestCase):
         return self.assertRaisesRegex(exception, r".+")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
