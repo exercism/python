cf26a09a6be3d5f89ceb5bc4abc5e66f89814b32
beatorizu <betrizaf@gmail.com>
2019-10-20 00:15:29 -0300

    Generate simple-cipher tests from template

--- exercises/simple-cipher/simple_cipher_test.py
+++ exercises/simple-cipher/simple_cipher_test.py
@@ -1,10 +1,10 @@
-import unittest
 import re
+import unittest
 
 from simple_cipher import Cipher
 
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v2.0.0
+
 
 class SimpleCipherTest(unittest.TestCase):
     # Utility functions
@@ -21,58 +21,61 @@ class SimpleCipherTest(unittest.TestCase):
 class RandomKeyCipherTest(SimpleCipherTest):
     def test_can_encode(self):
         cipher = Cipher()
-        plaintext = 'aaaaaaaaaa'
-        self.assertEqual(cipher.encode(plaintext), cipher.key[:len(plaintext)])
+        plaintext = "aaaaaaaaaa"
+        self.assertEqual(cipher.encode(plaintext), cipher.key[: len(plaintext)])
 
     def test_can_decode(self):
         cipher = Cipher()
-        plaintext = 'aaaaaaaaaa'
-        self.assertEqual(cipher.decode(cipher.key[:len(plaintext)]), plaintext)
+        plaintext = "aaaaaaaaaa"
+        self.assertEqual(cipher.decode(cipher.key[: len(plaintext)]), plaintext)
 
     def test_is_reversible(self):
         cipher = Cipher()
-        plaintext = 'abcdefghij'
+        plaintext = "abcdefghij"
         self.assertEqual(cipher.decode(cipher.encode(plaintext)), plaintext)
 
-    def test_key_is_only_made_of_lowercase_letters(self):
-        self.assertIsNotNone(re.match('^[a-z]+$', Cipher().key))
+    def test_key_is_made_only_of_lowercase_letters(self):
+        self.assertIsNotNone(re.match("^[a-z]+$", Cipher().key))
 
 
 class SubstitutionCipherTest(SimpleCipherTest):
     def test_can_encode(self):
-        cipher = Cipher('abcdefghij')
-        self.assertEqual(cipher.encode('aaaaaaaaaa'), cipher.key)
+        cipher = Cipher("abcdefghij")
+        plaintext = "aaaaaaaaaa"
+        self.assertEqual(cipher.encode(plaintext), cipher.key)
 
     def test_can_decode(self):
-        cipher = Cipher('abcdefghij')
-        self.assertEqual(cipher.decode(cipher.key), 'aaaaaaaaaa')
+        cipher = Cipher("abcdefghij")
+        self.assertEqual(cipher.decode(cipher.key), "aaaaaaaaaa")
 
     def test_is_reversible(self):
-        cipher = Cipher('abcdefghij')
-        plaintext = 'abcdefghij'
+        cipher = Cipher("abcdefghij")
+        plaintext = "abcdefghij"
         self.assertEqual(cipher.decode(cipher.encode(plaintext)), plaintext)
 
     def test_can_double_shift_encode(self):
-        plaintext = 'iamapandabear'
-        cipher = Cipher(plaintext)
-        self.assertEqual(cipher.encode(plaintext), 'qayaeaagaciai')
+        cipher = Cipher("iamapandabear")
+        plaintext = "iamapandabear"
+        self.assertEqual(cipher.encode(plaintext), "qayaeaagaciai")
 
     def test_can_wrap_on_encode(self):
-        cipher = Cipher('abcdefghij')
-        self.assertEqual(cipher.encode('zzzzzzzzzz'), 'zabcdefghi')
+        cipher = Cipher("abcdefghij")
+        plaintext = "zzzzzzzzzz"
+        self.assertEqual(cipher.encode(plaintext), "zabcdefghi")
 
     def test_can_wrap_on_decode(self):
-        cipher = Cipher('abcdefghij')
-        self.assertEqual(cipher.decode('zabcdefghi'), 'zzzzzzzzzz')
+        cipher = Cipher("abcdefghij")
+        self.assertEqual(cipher.decode("zabcdefghi"), "zzzzzzzzzz")
 
-    def test_can_encode_messages_longer_than_key(self):
-        cipher = Cipher('abc')
-        self.assertEqual(cipher.encode('iamapandabear'), 'iboaqcnecbfcr')
+    def test_can_encode_messages_longer_than_the_key(self):
+        cipher = Cipher("abc")
+        plaintext = "iamapandabear"
+        self.assertEqual(cipher.encode(plaintext), "iboaqcnecbfcr")
 
-    def test_can_decode_messages_longer_than_key(self):
-        cipher = Cipher('abc')
-        self.assertEqual(cipher.decode('iboaqcnecbfcr'), 'iamapandabear')
+    def test_can_decode_messages_longer_than_the_key(self):
+        cipher = Cipher("abc")
+        self.assertEqual(cipher.decode("iboaqcnecbfcr"), "iamapandabear")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
