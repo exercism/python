ea52750e431e6bcd6f39a18d44c567fec44b691e
Wiktor Matuszewski <pywkm@wukaem.pl>
2019-10-22 16:56:10 +0200

    palindrome-products: Move tests docstring to hints.md; refresh readme

--- exercises/palindrome-products/palindrome_products_test.py
+++ exercises/palindrome-products/palindrome_products_test.py
@@ -1,15 +1,3 @@
-"""	
-Notes regarding the implementation of smallest and largest:
-
-Both functions must take two keyword arguments:
-    max_factor -- int
-    min_factor -- int, default 0
-
-Their return value must be a tuple (value, factors) where value is the
-palindrome itself, and factors is an iterable containing both factors of the
-palindrome in arbitrary order.
-"""
-
 import unittest
 
 from palindrome_products import largest, smallest
