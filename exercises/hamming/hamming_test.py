8557941c087961e1416962f4f5ce01ca358f6500
Wiktor Matuszewski <pywkm@wukaem.pl>
2019-10-22 21:21:29 +0200

    hamming: remove extra utility methods

--- exercises/hamming/hamming_test.py
+++ exercises/hamming/hamming_test.py
@@ -47,16 +47,6 @@ class HammingTest(unittest.TestCase):
     def assertRaisesWithMessage(self, exception):
         return self.assertRaisesRegex(exception, r".+")
 
-    # Utility functions
-    def setUp(self):
-        try:
-            self.assertRaisesRegex
-        except AttributeError:
-            self.assertRaisesRegex = self.assertRaisesRegexp
-
-    def assertRaisesWithMessage(self, exception):
-        return self.assertRaisesRegex(exception, r".+")
-
 
 if __name__ == "__main__":
     unittest.main()
