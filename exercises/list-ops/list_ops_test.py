8cf7db38e431fe074516435e83949692b3e0cf0c
dvermd <315743+dvermd@users.noreply.github.com>
2019-10-31 00:52:46 +0100

    list-ops: add test template (#1958)

--- exercises/list-ops/list_ops_test.py
+++ exercises/list-ops/list_ops_test.py
@@ -1,104 +1,99 @@
 import unittest
-import operator
 
-import list_ops
-
+from list_ops import (
+    append,
+    concat,
+    foldl,
+    foldr,
+    length,
+    reverse,
+    map as list_ops_map,
+    filter as list_ops_filter,
+)
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v2.4.0
 
-class ListOpsTest(unittest.TestCase):
 
-    # test for append
+class ListOpsTest(unittest.TestCase):
     def test_append_empty_lists(self):
-        self.assertEqual(list_ops.append([], []), [])
+        self.assertEqual(append([], []), [])
 
     def test_append_empty_list_to_list(self):
-        self.assertEqual(list_ops.append([], [1, 2, 3, 4]), [1, 2, 3, 4])
+        self.assertEqual(append([], [1, 2, 3, 4]), [1, 2, 3, 4])
 
-    def test_append_nonempty_lists(self):
-        self.assertEqual(list_ops.append([1, 2], [2, 3, 4, 5]),
-                         [1, 2, 2, 3, 4, 5])
+    def test_append_non_empty_lists(self):
+        self.assertEqual(append([1, 2], [2, 3, 4, 5]), [1, 2, 2, 3, 4, 5])
 
-    # tests for concat
     def test_concat_empty_list(self):
-        self.assertEqual(list_ops.concat([]), [])
+        self.assertEqual(concat([]), [])
 
     def test_concat_list_of_lists(self):
-        self.assertEqual(list_ops.concat([[1, 2], [3], [], [4, 5, 6]]),
-                         [1, 2, 3, 4, 5, 6])
+        self.assertEqual(concat([[1, 2], [3], [], [4, 5, 6]]), [1, 2, 3, 4, 5, 6])
 
     def test_concat_list_of_nested_lists(self):
         self.assertEqual(
-            list_ops.concat([[[1], [2]], [[3]], [[]], [[4, 5, 6]]]),
-            [[1], [2], [3], [], [4, 5, 6]])
+            concat([[[1], [2]], [[3]], [[]], [[4, 5, 6]]]),
+            [[1], [2], [3], [], [4, 5, 6]],
+        )
 
-    # tests for filter
     def test_filter_empty_list(self):
-        self.assertEqual(list_ops.filter(lambda x: x % 2 == 1, []), [])
+        self.assertEqual(list_ops_filter(lambda x: x % 2 == 1, []), [])
 
-    def test_filter_nonempty_list(self):
-        self.assertEqual(
-            list_ops.filter(lambda x: x % 2 == 1, [1, 2, 3, 4, 5]),
-            [1, 3, 5])
+    def test_filter_non_empty_list(self):
+        self.assertEqual(list_ops_filter(lambda x: x % 2 == 1, [1, 2, 3, 5]), [1, 3, 5])
 
-    # tests for length
     def test_length_empty_list(self):
-        self.assertEqual(list_ops.length([]), 0)
+        self.assertEqual(length([]), 0)
 
-    def test_length_nonempty_list(self):
-        self.assertEqual(list_ops.length([1, 2, 3, 4]), 4)
+    def test_length_non_empty_list(self):
+        self.assertEqual(length([1, 2, 3, 4]), 4)
 
-    # tests for map
     def test_map_empty_list(self):
-        self.assertEqual(list_ops.map(lambda x: x + 1, []), [])
+        self.assertEqual(list_ops_map(lambda x: x + 1, []), [])
 
-    def test_map_nonempty_list(self):
-        self.assertEqual(list_ops.map(lambda x: x + 1, [1, 3, 5, 7]),
-                         [2, 4, 6, 8])
+    def test_map_non_empty_list(self):
+        self.assertEqual(list_ops_map(lambda x: x + 1, [1, 3, 5, 7]), [2, 4, 6, 8])
 
-    # tests for foldl
     def test_foldl_empty_list(self):
-        self.assertEqual(list_ops.foldl(operator.mul, [], 2), 2)
+        self.assertEqual(foldl(lambda x, y: x * y, [], 2), 2)
 
-    def test_foldl_nonempty_list_addition(self):
-        self.assertEqual(list_ops.foldl(operator.add, [1, 2, 3, 4], 5), 15)
+    def test_foldl_direction_independent_function_applied_to_non_empty_list(self):
+        self.assertEqual(foldl(lambda x, y: x + y, [1, 2, 3, 4], 5), 15)
 
-    def test_foldl_nonempty_list_floordiv(self):
-        self.assertEqual(list_ops.foldl(operator.floordiv, [2, 5], 5), 0)
+    def test_foldl_direction_dependent_function_applied_to_non_empty_list(self):
+        self.assertEqual(foldl(lambda x, y: x // y, [2, 5], 5), 0)
 
-    # tests for foldr
     def test_foldr_empty_list(self):
-        self.assertEqual(list_ops.foldr(operator.mul, [], 2), 2)
+        self.assertEqual(foldr(lambda x, y: x * y, [], 2), 2)
 
-    def test_foldr_nonempty_list_addition(self):
-        self.assertEqual(list_ops.foldr(operator.add, [1, 2, 3, 4], 5), 15)
+    def test_foldr_direction_independent_function_applied_to_non_empty_list(self):
+        self.assertEqual(foldr(lambda x, y: x + y, [1, 2, 3, 4], 5), 15)
 
-    def test_foldr_nonempty_list_floordiv(self):
-        self.assertEqual(list_ops.foldr(operator.floordiv, [2, 5], 5), 2)
+    def test_foldr_direction_dependent_function_applied_to_non_empty_list(self):
+        self.assertEqual(foldr(lambda x, y: x // y, [2, 5], 5), 2)
 
-    # additional test for foldr
-    def test_foldr_add_str(self):
-        self.assertEqual(
-            list_ops.foldr(operator.add,
-                           ["e", "x", "e", "r", "c", "i", "s", "m"], "!"),
-            "exercism!")
-
-    # tests for reverse
     def test_reverse_empty_list(self):
-        self.assertEqual(list_ops.reverse([]), [])
+        self.assertEqual(reverse([]), [])
 
-    def test_reverse_nonempty_list(self):
-        self.assertEqual(list_ops.reverse([1, 3, 5, 7]), [7, 5, 3, 1])
+    def test_reverse_non_empty_list(self):
+        self.assertEqual(reverse([1, 3, 5, 7]), [7, 5, 3, 1])
 
-    def test_reverse_list_of_lists_not_flattened(self):
-        self.assertEqual(list_ops.reverse([[1, 2], [3], [], [4, 5, 6]]),
-                         [[4, 5, 6], [], [3], [1, 2]])
-
-    # additional test for reverse
-    def test_reverse_mixed_types(self):
+    def test_reverse_list_of_lists_is_not_flattened(self):
         self.assertEqual(
-            list_ops.reverse(["xyz", 4.0, "cat", 1]), [1, "cat", 4.0, "xyz"])
+            reverse([[1, 2], [3], [], [4, 5, 6]]), [[4, 5, 6], [], [3], [1, 2]]
+        )
+
+    # Additional tests for this track
+
+    def test_foldr_foldr_add_string(self):
+        self.assertEqual(
+            foldr(lambda x, y: x + y, ["e", "x", "e", "r", "c", "i", "s", "m"], "!"),
+            "exercism!",
+        )
+
+    def test_reverse_reverse_mixed_types(self):
+        self.assertEqual(reverse(["xyz", 4.0, "cat", 1]), [1, "cat", 4.0, "xyz"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
