ec7625e1538acf2ffb7d80e22cd0436abfdd0886
Alex <alexpjohnson@gmail.com>
2019-10-29 21:01:29 -0700

    Add additional tests json and template. Regenerate README.md

--- exercises/minesweeper/README.md
+++ exercises/minesweeper/README.md
@@ -1,30 +1,46 @@
 # Minesweeper
 
-Add the numbers to a minesweeper board.
+Add the mine counts to a completed Minesweeper board.
 
 Minesweeper is a popular game where the user has to find the mines using
 numeric hints that indicate how many mines are directly adjacent
 (horizontally, vertically, diagonally) to a square.
 
 In this exercise you have to create some code that counts the number of
-mines adjacent to a square and transforms boards like this (where `*`
-indicates a mine):
+mines adjacent to a given empty square and replaces that square with the
+count.
 
-    +-----+
-    | * * |
-    |  *  |
-    |  *  |
-    |     |
-    +-----+
+The board is a rectangle composed of blank space (' ') characters. A mine 
+is represented by an asterisk ('\*') character.
 
-into this:
+If a given space has no adjacent mines at all, leave that square blank.
 
-    +-----+
-    |1*3*1|
-    |13*31|
-    | 2*2 |
-    | 111 |
-    +-----+
+## Examples
+
+For example you may receive a 5 x 4 board like this (empty spaces are
+represented here with the '·' character for display on screen):
+
+```
+·*·*·
+··*··
+··*··
+·····
+```
+
+And your code will transform it into this:
+
+```
+1*3*1
+13*31
+·2*2·
+·111·
+```
+
+Implementation note:
+The board function must validate its input and raise a
+ValueError with a *meaningful* error message if the
+input turns out to be malformed.
+
 
 ## Exception messages
 
--- exercises/minesweeper/minesweeper_test.py
+++ exercises/minesweeper/minesweeper_test.py
@@ -7,64 +7,61 @@ from minesweeper import annotate
 
 class MinesweeperTest(unittest.TestCase):
     def test_no_rows(self):
-        minefield = []
-        expected = []
-        self.assertEqual(annotate(minefield), expected)
+        self.assertEqual(annotate([]), [])
 
     def test_no_columns(self):
-        minefield = [""]
-        expected = [""]
-        self.assertEqual(annotate(minefield), expected)
+        self.assertEqual(annotate([""]), [""])
 
     def test_no_mines(self):
-        minefield = ["   ", "   ", "   "]
-        expected = ["   ", "   ", "   "]
-        self.assertEqual(annotate(minefield), expected)
+        self.assertEqual(annotate(["   ", "   ", "   "]), ["   ", "   ", "   "])
 
     def test_minefield_with_only_mines(self):
-        minefield = ["***", "***", "***"]
-        expected = ["***", "***", "***"]
-        self.assertEqual(annotate(minefield), expected)
+        self.assertEqual(annotate(["***", "***", "***"]), ["***", "***", "***"])
 
     def test_mine_surrounded_by_spaces(self):
-        minefield = ["   ", " * ", "   "]
-        expected = ["111", "1*1", "111"]
-        self.assertEqual(annotate(minefield), expected)
+        self.assertEqual(annotate(["   ", " * ", "   "]), ["111", "1*1", "111"])
 
     def test_space_surrounded_by_mines(self):
-        minefield = ["***", "* *", "***"]
-        expected = ["***", "*8*", "***"]
-        self.assertEqual(annotate(minefield), expected)
+        self.assertEqual(annotate(["***", "* *", "***"]), ["***", "*8*", "***"])
 
     def test_horizontal_line(self):
-        minefield = [" * * "]
-        expected = ["1*2*1"]
-        self.assertEqual(annotate(minefield), expected)
+        self.assertEqual(annotate([" * * "]), ["1*2*1"])
 
     def test_horizontal_line_mines_at_edges(self):
-        minefield = ["*   *"]
-        expected = ["*1 1*"]
-        self.assertEqual(annotate(minefield), expected)
+        self.assertEqual(annotate(["*   *"]), ["*1 1*"])
 
     def test_vertical_line(self):
-        minefield = [" ", "*", " ", "*", " "]
-        expected = ["1", "*", "2", "*", "1"]
-        self.assertEqual(annotate(minefield), expected)
+        self.assertEqual(annotate([" ", "*", " ", "*", " "]), ["1", "*", "2", "*", "1"])
 
     def test_vertical_line_mines_at_edges(self):
-        minefield = ["*", " ", " ", " ", "*"]
-        expected = ["*", "1", " ", "1", "*"]
-        self.assertEqual(annotate(minefield), expected)
+        self.assertEqual(annotate(["*", " ", " ", " ", "*"]), ["*", "1", " ", "1", "*"])
 
     def test_cross(self):
-        minefield = ["  *  ", "  *  ", "*****", "  *  ", "  *  "]
-        expected = [" 2*2 ", "25*52", "*****", "25*52", " 2*2 "]
-        self.assertEqual(annotate(minefield), expected)
+        self.assertEqual(
+            annotate(["  *  ", "  *  ", "*****", "  *  ", "  *  "]),
+            [" 2*2 ", "25*52", "*****", "25*52", " 2*2 "],
+        )
 
     def test_large_minefield(self):
-        minefield = [" *  * ", "  *   ", "    * ", "   * *", " *  * ", "      "]
-        expected = ["1*22*1", "12*322", " 123*2", "112*4*", "1*22*2", "111111"]
-        self.assertEqual(annotate(minefield), expected)
+        self.assertEqual(
+            annotate([" *  * ", "  *   ", "    * ", "   * *", " *  * ", "      "]),
+            ["1*22*1", "12*322", " 123*2", "112*4*", "1*22*2", "111111"],
+        )
+
+    # Additional tests for this track
+    def test_annotate_9(self):
+        self.assertEqual(
+            annotate(["     ", "   * ", "     ", "     ", " *   "]),
+            ["  111", "  1*1", "  111", "111  ", "1*1  "],
+        )
+
+    def test_different_len(self):
+        with self.assertRaisesWithMessage(ValueError):
+            annotate([" ", "*  ", "  "])
+
+    def test_invalid_char(self):
+        with self.assertRaisesWithMessage(ValueError):
+            annotate(["X  * "])
 
 
 if __name__ == "__main__":
