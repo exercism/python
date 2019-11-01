45186a790c911abfba84ef52af705ce6add47754
Dat Nguyen <datsama@protonmail.com>
2019-11-01 16:29:23 +0700

    two-fer: Add test for handling empty string param
    Ref: https://exercism.io/solutions/a1ec5f564aba409b8377f25ef1b80cee/comments/162304

--- exercises/two-fer/two_fer_test.py
+++ exercises/two-fer/two_fer_test.py
@@ -14,6 +14,9 @@ class TwoFerTest(unittest.TestCase):
 
     def test_another_name_given(self):
         self.assertEqual(two_fer("Bob"), "One for Bob, one for me.")
+        
+    def test_name_empty_given(self):
+        self.assertEqual(two_fer(""), "One for you, one for me.")
 
 
 if __name__ == "__main__":
