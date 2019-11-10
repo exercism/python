7a24eb0ebec589809911b705a7a54bb0265c85ad
dvermd <315743+dvermd@users.noreply.github.com>
2019-11-10 21:42:53 +0100

    go_counting: use a macro instead of generating map(...) (#1951)

--- exercises/go-counting/go_counting_test.py
+++ exercises/go-counting/go_counting_test.py
@@ -10,29 +10,25 @@ class GoCountingTest(unittest.TestCase):
         board = Board(["  B  ", " B B ", "B W B", " W W ", "  W  "])
         stone, territory = board.territory(x=0, y=1)
         self.assertEqual(stone, BLACK)
-        self.assertSetEqual(
-            territory, set(map(lambda x: tuple(x), [[0, 0], [0, 1], [1, 0]]))
-        )
+        self.assertSetEqual(territory, set([(0, 0), (0, 1), (1, 0)]))
 
     def test_white_center_territory_on_5x5_board(self):
         board = Board(["  B  ", " B B ", "B W B", " W W ", "  W  "])
         stone, territory = board.territory(x=2, y=3)
         self.assertEqual(stone, WHITE)
-        self.assertSetEqual(territory, set(map(lambda x: tuple(x), [[2, 3]])))
+        self.assertSetEqual(territory, set([(2, 3)]))
 
     def test_open_corner_territory_on_5x5_board(self):
         board = Board(["  B  ", " B B ", "B W B", " W W ", "  W  "])
         stone, territory = board.territory(x=1, y=4)
         self.assertEqual(stone, NONE)
-        self.assertSetEqual(
-            territory, set(map(lambda x: tuple(x), [[0, 3], [0, 4], [1, 4]]))
-        )
+        self.assertSetEqual(territory, set([(0, 3), (0, 4), (1, 4)]))
 
     def test_a_stone_and_not_a_territory_on_5x5_board(self):
         board = Board(["  B  ", " B B ", "B W B", " W W ", "  W  "])
         stone, territory = board.territory(x=1, y=1)
         self.assertEqual(stone, NONE)
-        self.assertSetEqual(territory, set(map(lambda x: tuple(x), [])))
+        self.assertSetEqual(territory, set([]))
 
     def test_invalid_because_x_is_too_low_for_5x5_board(self):
         board = Board(["  B  ", " B B ", "B W B", " W W ", "  W  "])
@@ -57,29 +53,23 @@ class GoCountingTest(unittest.TestCase):
     def test_one_territory_is_the_whole_board(self):
         board = Board([" "])
         territories = board.territories()
-        self.assertSetEqual(territories[BLACK], set(map(lambda x: tuple(x), [])))
-        self.assertSetEqual(territories[WHITE], set(map(lambda x: tuple(x), [])))
-        self.assertSetEqual(territories[NONE], set(map(lambda x: tuple(x), [[0, 0]])))
+        self.assertSetEqual(territories[BLACK], set([]))
+        self.assertSetEqual(territories[WHITE], set([]))
+        self.assertSetEqual(territories[NONE], set([(0, 0)]))
 
     def test_two_territory_rectangular_board(self):
         board = Board([" BW ", " BW "])
         territories = board.territories()
-        self.assertSetEqual(
-            territories[BLACK], set(map(lambda x: tuple(x), [[0, 0], [0, 1]]))
-        )
-        self.assertSetEqual(
-            territories[WHITE], set(map(lambda x: tuple(x), [[3, 0], [3, 1]]))
-        )
-        self.assertSetEqual(territories[NONE], set(map(lambda x: tuple(x), [])))
+        self.assertSetEqual(territories[BLACK], set([(0, 0), (0, 1)]))
+        self.assertSetEqual(territories[WHITE], set([(3, 0), (3, 1)]))
+        self.assertSetEqual(territories[NONE], set([]))
 
     def test_two_region_rectangular_board(self):
         board = Board([" B "])
         territories = board.territories()
-        self.assertSetEqual(
-            territories[BLACK], set(map(lambda x: tuple(x), [[0, 0], [2, 0]]))
-        )
-        self.assertSetEqual(territories[WHITE], set(map(lambda x: tuple(x), [])))
-        self.assertSetEqual(territories[NONE], set(map(lambda x: tuple(x), [])))
+        self.assertSetEqual(territories[BLACK], set([(0, 0), (2, 0)]))
+        self.assertSetEqual(territories[WHITE], set([]))
+        self.assertSetEqual(territories[NONE], set([]))
 
     # Utility functions
     def assertRaisesWithMessage(self, exception):
