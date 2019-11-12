adbaf1bff35962c5d33dbc40aa01c63b800b8008
TQA <trinhquocanh94@gmail.com>
2019-11-12 21:42:06 +0100

    Fix assert type

--- exercises/atbash-cipher/atbash_cipher_test.py
+++ exercises/atbash-cipher/atbash_cipher_test.py
@@ -7,55 +7,57 @@ from atbash_cipher import decode, encode
 
 class AtbashCipherTest(unittest.TestCase):
     def test_encode_yes(self):
-        self.assertEqual(encode("yes"), "bvh")
+        self.assertMultiLineEqual(encode("yes"), "bvh")
 
     def test_encode_no(self):
-        self.assertEqual(encode("no"), "ml")
+        self.assertMultiLineEqual(encode("no"), "ml")
 
     def test_encode_omg(self):
-        self.assertEqual(encode("OMG"), "lnt")
+        self.assertMultiLineEqual(encode("OMG"), "lnt")
 
     def test_encode_spaces(self):
-        self.assertEqual(encode("O M G"), "lnt")
+        self.assertMultiLineEqual(encode("O M G"), "lnt")
 
     def test_encode_mindblowingly(self):
-        self.assertEqual(encode("mindblowingly"), "nrmwy oldrm tob")
+        self.assertMultiLineEqual(encode("mindblowingly"), "nrmwy oldrm tob")
 
     def test_encode_numbers(self):
-        self.assertEqual(encode("Testing,1 2 3, testing."), "gvhgr mt123 gvhgr mt")
+        self.assertMultiLineEqual(
+            encode("Testing,1 2 3, testing."), "gvhgr mt123 gvhgr mt"
+        )
 
     def test_encode_deep_thought(self):
-        self.assertEqual(encode("Truth is fiction."), "gifgs rhurx grlm")
+        self.assertMultiLineEqual(encode("Truth is fiction."), "gifgs rhurx grlm")
 
     def test_encode_all_the_letters(self):
-        self.assertEqual(
+        self.assertMultiLineEqual(
             encode("The quick brown fox jumps over the lazy dog."),
             "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt",
         )
 
     def test_decode_exercism(self):
-        self.assertEqual(decode("vcvix rhn"), "exercism")
+        self.assertMultiLineEqual(decode("vcvix rhn"), "exercism")
 
     def test_decode_a_sentence(self):
-        self.assertEqual(
+        self.assertMultiLineEqual(
             decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v"),
             "anobstacleisoftenasteppingstone",
         )
 
     def test_decode_numbers(self):
-        self.assertEqual(decode("gvhgr mt123 gvhgr mt"), "testing123testing")
+        self.assertMultiLineEqual(decode("gvhgr mt123 gvhgr mt"), "testing123testing")
 
     def test_decode_all_the_letters(self):
-        self.assertEqual(
+        self.assertMultiLineEqual(
             decode("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"),
             "thequickbrownfoxjumpsoverthelazydog",
         )
 
     def test_decode_with_too_many_spaces(self):
-        self.assertEqual(decode("vc vix    r hn"), "exercism")
+        self.assertMultiLineEqual(decode("vc vix    r hn"), "exercism")
 
     def test_decode_with_no_spaces(self):
-        self.assertEqual(
+        self.assertMultiLineEqual(
             decode("zmlyhgzxovrhlugvmzhgvkkrmthglmv"), "anobstacleisoftenasteppingstone"
         )
 
