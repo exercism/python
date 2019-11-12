52b14661f8a1d8dcf397d3954ec1b9e48c8d5641
dvermd <315743+dvermd@users.noreply.github.com>
2019-11-12 16:05:17 +0100

    dominoes: add test template (#2116)
    Resolves: #1946

--- exercises/dominoes/dominoes_test.py
+++ exercises/dominoes/dominoes_test.py
@@ -2,8 +2,8 @@ import unittest
 
 from dominoes import can_chain
 
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v2.1.0
+
 
 class DominoesTest(unittest.TestCase):
     def test_empty_input_empty_output(self):
@@ -16,7 +16,7 @@ class DominoesTest(unittest.TestCase):
         output_chain = can_chain(input_dominoes)
         self.assert_correct_chain(input_dominoes, output_chain)
 
-    def test_singleton_cant_be_chained(self):
+    def test_singleton_that_can_t_be_chained(self):
         input_dominoes = [(1, 2)]
         output_chain = can_chain(input_dominoes)
         self.refute_correct_chain(input_dominoes, output_chain)
@@ -31,7 +31,7 @@ class DominoesTest(unittest.TestCase):
         output_chain = can_chain(input_dominoes)
         self.assert_correct_chain(input_dominoes, output_chain)
 
-    def test_cant_be_chained(self):
+    def test_can_t_be_chained(self):
         input_dominoes = [(1, 2), (4, 1), (2, 3)]
         output_chain = can_chain(input_dominoes)
         self.refute_correct_chain(input_dominoes, output_chain)
@@ -62,8 +62,17 @@ class DominoesTest(unittest.TestCase):
         self.assert_correct_chain(input_dominoes, output_chain)
 
     def test_nine_elements(self):
-        input_dominoes = [(1, 2), (5, 3), (3, 1), (1, 2), (2, 4), (1, 6),
-                          (2, 3), (3, 4), (5, 6)]
+        input_dominoes = [
+            (1, 2),
+            (5, 3),
+            (3, 1),
+            (1, 2),
+            (2, 4),
+            (1, 6),
+            (2, 3),
+            (3, 4),
+            (5, 6),
+        ]
         output_chain = can_chain(input_dominoes)
         self.assert_correct_chain(input_dominoes, output_chain)
 
@@ -73,31 +82,35 @@ class DominoesTest(unittest.TestCase):
         return list(sorted(tuple(sorted(domino)) for domino in dominoes))
 
     def assert_same_dominoes(self, input_dominoes, output_chain):
-        msg = ('Dominoes used in the output must be the same '
-               'as the ones given in the input')
+        msg = (
+            "Dominoes used in the output must be the same "
+            "as the ones given in the input"
+        )
         input_normal = self.normalize_dominoes(input_dominoes)
         output_normal = self.normalize_dominoes(output_chain)
         self.assertEqual(input_normal, output_normal, msg)
 
     def assert_consecutive_dominoes_match(self, output_chain):
         for i in range(len(output_chain) - 1):
-            msg = ("In chain {}, right end of domino {} ({}) "
-                   "and left end of domino {} ({}) must match")
-            msg = msg.format(output_chain,
-                             i,
-                             output_chain[i],
-                             i + 1,
-                             output_chain[i + 1])
+            msg = (
+                "In chain {}, right end of domino {} ({}) "
+                "and left end of domino {} ({}) must match"
+            )
+            msg = msg.format(
+                output_chain, i, output_chain[i], i + 1, output_chain[i + 1]
+            )
             self.assertEqual(output_chain[i][1], output_chain[i + 1][0], msg)
 
     def assert_dominoes_at_ends_match(self, output_chain):
-        msg = ("In chain {}, left end of first domino ({}) and "
-               "right end of last domino ({}) must match")
+        msg = (
+            "In chain {}, left end of first domino ({}) and "
+            "right end of last domino ({}) must match"
+        )
         msg = msg.format(output_chain, output_chain[0], output_chain[-1])
         self.assertEqual(output_chain[0][0], output_chain[-1][1], msg)
 
     def assert_correct_chain(self, input_dominoes, output_chain):
-        msg = 'There should be a chain for {}'.format(input_dominoes)
+        msg = "There should be a chain for {}".format(input_dominoes)
         self.assertIsNotNone(output_chain, msg)
         self.assert_same_dominoes(input_dominoes, output_chain)
         if not any(output_chain):
@@ -106,9 +119,9 @@ class DominoesTest(unittest.TestCase):
         self.assert_dominoes_at_ends_match(output_chain)
 
     def refute_correct_chain(self, input_dominoes, output_chain):
-        msg = 'There should be no valid chain for {}'.format(input_dominoes)
+        msg = "There should be no valid chain for {}".format(input_dominoes)
         self.assertIsNone(output_chain, msg)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
