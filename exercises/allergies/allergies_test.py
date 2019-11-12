eb785dc64d41c5a852473a9433ac08b48b8a6dae
TQA <trinhquocanh94@gmail.com>
2019-11-12 18:34:06 +0100

    Fix test names

--- exercises/allergies/allergies_test.py
+++ exercises/allergies/allergies_test.py
@@ -6,7 +6,7 @@ from allergies import Allergies
 
 
 class AllergiesTest(unittest.TestCase):
-    def test_not_allergic_to_anything(self):
+    def test_eggs_not_allergic_to_anything(self):
         self.assertIs(Allergies(0).allergic_to("eggs"), False)
 
     def test_allergic_only_to_eggs(self):
@@ -18,10 +18,10 @@ class AllergiesTest(unittest.TestCase):
     def test_allergic_to_something_but_not_eggs(self):
         self.assertIs(Allergies(2).allergic_to("eggs"), False)
 
-    def test_allergic_to_everything(self):
+    def test_eggs_allergic_to_everything(self):
         self.assertIs(Allergies(255).allergic_to("eggs"), True)
 
-    def test_not_allergic_to_anything(self):
+    def test_peanuts_not_allergic_to_anything(self):
         self.assertIs(Allergies(0).allergic_to("peanuts"), False)
 
     def test_allergic_only_to_peanuts(self):
@@ -33,10 +33,10 @@ class AllergiesTest(unittest.TestCase):
     def test_allergic_to_something_but_not_peanuts(self):
         self.assertIs(Allergies(5).allergic_to("peanuts"), False)
 
-    def test_allergic_to_everything(self):
+    def test_peanuts_allergic_to_everything(self):
         self.assertIs(Allergies(255).allergic_to("peanuts"), True)
 
-    def test_not_allergic_to_anything(self):
+    def test_shellfish_not_allergic_to_anything(self):
         self.assertIs(Allergies(0).allergic_to("shellfish"), False)
 
     def test_allergic_only_to_shellfish(self):
@@ -48,10 +48,10 @@ class AllergiesTest(unittest.TestCase):
     def test_allergic_to_something_but_not_shellfish(self):
         self.assertIs(Allergies(10).allergic_to("shellfish"), False)
 
-    def test_allergic_to_everything(self):
+    def test_shellfish_allergic_to_everything(self):
         self.assertIs(Allergies(255).allergic_to("shellfish"), True)
 
-    def test_not_allergic_to_anything(self):
+    def test_strawberries_not_allergic_to_anything(self):
         self.assertIs(Allergies(0).allergic_to("strawberries"), False)
 
     def test_allergic_only_to_strawberries(self):
@@ -63,10 +63,10 @@ class AllergiesTest(unittest.TestCase):
     def test_allergic_to_something_but_not_strawberries(self):
         self.assertIs(Allergies(20).allergic_to("strawberries"), False)
 
-    def test_allergic_to_everything(self):
+    def test_strawberries_allergic_to_everything(self):
         self.assertIs(Allergies(255).allergic_to("strawberries"), True)
 
-    def test_not_allergic_to_anything(self):
+    def test_tomatoes_not_allergic_to_anything(self):
         self.assertIs(Allergies(0).allergic_to("tomatoes"), False)
 
     def test_allergic_only_to_tomatoes(self):
@@ -78,10 +78,10 @@ class AllergiesTest(unittest.TestCase):
     def test_allergic_to_something_but_not_tomatoes(self):
         self.assertIs(Allergies(40).allergic_to("tomatoes"), False)
 
-    def test_allergic_to_everything(self):
+    def test_tomatoes_allergic_to_everything(self):
         self.assertIs(Allergies(255).allergic_to("tomatoes"), True)
 
-    def test_not_allergic_to_anything(self):
+    def test_chocolate_not_allergic_to_anything(self):
         self.assertIs(Allergies(0).allergic_to("chocolate"), False)
 
     def test_allergic_only_to_chocolate(self):
@@ -93,10 +93,10 @@ class AllergiesTest(unittest.TestCase):
     def test_allergic_to_something_but_not_chocolate(self):
         self.assertIs(Allergies(80).allergic_to("chocolate"), False)
 
-    def test_allergic_to_everything(self):
+    def test_chocolate_allergic_to_everything(self):
         self.assertIs(Allergies(255).allergic_to("chocolate"), True)
 
-    def test_not_allergic_to_anything(self):
+    def test_pollen_not_allergic_to_anything(self):
         self.assertIs(Allergies(0).allergic_to("pollen"), False)
 
     def test_allergic_only_to_pollen(self):
@@ -108,10 +108,10 @@ class AllergiesTest(unittest.TestCase):
     def test_allergic_to_something_but_not_pollen(self):
         self.assertIs(Allergies(160).allergic_to("pollen"), False)
 
-    def test_allergic_to_everything(self):
+    def test_pollen_allergic_to_everything(self):
         self.assertIs(Allergies(255).allergic_to("pollen"), True)
 
-    def test_not_allergic_to_anything(self):
+    def test_cats_not_allergic_to_anything(self):
         self.assertIs(Allergies(0).allergic_to("cats"), False)
 
     def test_allergic_only_to_cats(self):
@@ -123,35 +123,35 @@ class AllergiesTest(unittest.TestCase):
     def test_allergic_to_something_but_not_cats(self):
         self.assertIs(Allergies(64).allergic_to("cats"), False)
 
-    def test_allergic_to_everything(self):
+    def test_cats_allergic_to_everything(self):
         self.assertIs(Allergies(255).allergic_to("cats"), True)
 
-    def test_no_allergies(self):
-        self.assertCountEqual(Allergies(0).lst, [])
+    def test_(self):
+        self.assertEqual(Allergies(0).lst, [])
 
-    def test_just_eggs(self):
-        self.assertCountEqual(Allergies(1).lst, ["eggs"])
+    def test_(self):
+        self.assertEqual(Allergies(1).lst, ["eggs"])
 
-    def test_just_peanuts(self):
-        self.assertCountEqual(Allergies(2).lst, ["peanuts"])
+    def test_(self):
+        self.assertEqual(Allergies(2).lst, ["peanuts"])
 
-    def test_just_strawberries(self):
-        self.assertCountEqual(Allergies(8).lst, ["strawberries"])
+    def test_(self):
+        self.assertEqual(Allergies(8).lst, ["strawberries"])
 
-    def test_eggs_and_peanuts(self):
-        self.assertCountEqual(Allergies(3).lst, ["eggs", "peanuts"])
+    def test_(self):
+        self.assertEqual(Allergies(3).lst, ["eggs", "peanuts"])
 
-    def test_more_than_eggs_but_not_peanuts(self):
-        self.assertCountEqual(Allergies(5).lst, ["eggs", "shellfish"])
+    def test_(self):
+        self.assertEqual(Allergies(5).lst, ["eggs", "shellfish"])
 
-    def test_lots_of_stuff(self):
-        self.assertCountEqual(
+    def test_(self):
+        self.assertEqual(
             Allergies(248).lst,
             ["strawberries", "tomatoes", "chocolate", "pollen", "cats"],
         )
 
-    def test_everything(self):
-        self.assertCountEqual(
+    def test_(self):
+        self.assertEqual(
             Allergies(255).lst,
             [
                 "eggs",
@@ -165,8 +165,8 @@ class AllergiesTest(unittest.TestCase):
             ],
         )
 
-    def test_no_allergen_score_parts(self):
-        self.assertCountEqual(
+    def test_(self):
+        self.assertEqual(
             Allergies(509).lst,
             [
                 "eggs",
