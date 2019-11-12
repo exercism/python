f23687f93816bf7d69ba06530684fba05dcfa59c
TQA <trinhquocanh94@gmail.com>
2019-11-12 21:35:58 +0100

    Add test template for atbash cipher

--- exercises/atbash-cipher/atbash_cipher_test.py
+++ exercises/atbash-cipher/atbash_cipher_test.py
@@ -2,68 +2,80 @@ import unittest
 
 from atbash_cipher import decode, encode
 
-
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.2.0
 
+
 class AtbashCipherTest(unittest.TestCase):
-    def test_encode_no(self):
-        self.assertMultiLineEqual(encode("no"), "ml")
-
     def test_encode_yes(self):
-        self.assertMultiLineEqual(encode("yes"), "bvh")
+        value = "yes"
+        expected = "bvh"
+        self.assertEqual(encode(value), expected)
 
-    def test_encode_OMG(self):
-        self.assertMultiLineEqual(encode("OMG"), "lnt")
+    def test_encode_no(self):
+        value = "no"
+        expected = "ml"
+        self.assertEqual(encode(value), expected)
 
-    def test_encode_O_M_G(self):
-        self.assertMultiLineEqual(encode("O M G"), "lnt")
+    def test_encode_omg(self):
+        value = "OMG"
+        expected = "lnt"
+        self.assertEqual(encode(value), expected)
 
-    def test_encode_long_word(self):
-        self.assertMultiLineEqual(encode("mindblowingly"), "nrmwy oldrm tob")
+    def test_encode_spaces(self):
+        value = "O M G"
+        expected = "lnt"
+        self.assertEqual(encode(value), expected)
+
+    def test_encode_mindblowingly(self):
+        value = "mindblowingly"
+        expected = "nrmwy oldrm tob"
+        self.assertEqual(encode(value), expected)
 
     def test_encode_numbers(self):
-        self.assertMultiLineEqual(
-            encode("Testing, 1 2 3, testing."), "gvhgr mt123 gvhgr mt")
+        value = "Testing,1 2 3, testing."
+        expected = "gvhgr mt123 gvhgr mt"
+        self.assertEqual(encode(value), expected)
 
-    def test_encode_sentence(self):
-        self.assertMultiLineEqual(
-            encode("Truth is fiction."), "gifgs rhurx grlm")
+    def test_encode_deep_thought(self):
+        value = "Truth is fiction."
+        expected = "gifgs rhurx grlm"
+        self.assertEqual(encode(value), expected)
 
-    def test_encode_all_things(self):
-        plaintext = "The quick brown fox jumps over the lazy dog."
-        ciphertext = "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
-        self.assertMultiLineEqual(encode(plaintext), ciphertext)
+    def test_encode_all_the_letters(self):
+        value = "The quick brown fox jumps over the lazy dog."
+        expected = "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
+        self.assertEqual(encode(value), expected)
 
-    def test_decode_word(self):
-        self.assertMultiLineEqual(decode("vcvix rhn"), "exercism")
+    def test_decode_exercism(self):
+        value = "vcvix rhn"
+        expected = "exercism"
+        self.assertEqual(decode(value), expected)
 
-    def test_decode_sentence(self):
-        self.assertMultiLineEqual(
-            decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v"),
-            "anobstacleisoftenasteppingstone")
+    def test_decode_a_sentence(self):
+        value = "zmlyh gzxov rhlug vmzhg vkkrm thglm v"
+        expected = "anobstacleisoftenasteppingstone"
+        self.assertEqual(decode(value), expected)
 
     def test_decode_numbers(self):
-        self.assertMultiLineEqual(
-            decode("gvhgr mt123 gvhgr mt"), "testing123testing")
+        value = "gvhgr mt123 gvhgr mt"
+        expected = "testing123testing"
+        self.assertEqual(decode(value), expected)
 
     def test_decode_all_the_letters(self):
-        ciphertext = "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
-        plaintext = "thequickbrownfoxjumpsoverthelazydog"
-        self.assertMultiLineEqual(decode(ciphertext), plaintext)
+        value = "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
+        expected = "thequickbrownfoxjumpsoverthelazydog"
+        self.assertEqual(decode(value), expected)
 
     def test_decode_with_too_many_spaces(self):
-        self.assertMultiLineEqual(decode("vc vix    r hn"), "exercism")
+        value = "vc vix    r hn"
+        expected = "exercism"
+        self.assertEqual(decode(value), expected)
 
     def test_decode_with_no_spaces(self):
-        ciphertext = "zmlyhgzxovrhlugvmzhgvkkrmthglmv"
-        plaintext = "anobstacleisoftenasteppingstone"
-        self.assertMultiLineEqual(decode(ciphertext), plaintext)
-
-    # additional track specific test
-    def test_encode_decode(self):
-        self.assertMultiLineEqual(
-            decode(encode("Testing, 1 2 3, testing.")), "testing123testing")
+        value = "zmlyhgzxovrhlugvmzhgvkkrmthglmv"
+        expected = "anobstacleisoftenasteppingstone"
+        self.assertEqual(decode(value), expected)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
