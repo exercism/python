bce270fa1c9e73a026bd57e154b88ae38f208695
TQA <trinhquocanh94@gmail.com>
2019-11-20 18:34:44 +0100

    Fix import statement

--- exercises/dnd-character/dnd_character_test.py
+++ exercises/dnd-character/dnd_character_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from dnd_character import ability, character, modifier, strength
+from dnd_character import Character, modifier
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
 
