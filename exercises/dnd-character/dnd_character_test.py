c774fee89042d626e2e5070dcbfac7f3ed47ece2
TQA <trinhquocanh94@gmail.com>
2019-11-20 20:17:08 +0100

    Write templates for the last 3 tests

--- exercises/dnd-character/dnd_character_test.py
+++ exercises/dnd-character/dnd_character_test.py
@@ -55,21 +55,22 @@ class DndCharacterTest(unittest.TestCase):
         self.assertEqual(modifier(18), 4)
 
     def test_random_ability_is_within_range(self):
-        self.assertIn(Character().ability(), range(3, 19))
+        score = Character().ability()
+        self.assertTrue(score >= 3 and score <= 18)
 
     def test_random_character_is_valid(self):
         Char = Character()
-        self.assertIn(Char.strength, range(3, 19))
-        self.assertIn(Char.dexterity, range(3, 19))
-        self.assertIn(Char.constitution, range(3, 19))
-        self.assertIn(Char.intelligence, range(3, 19))
-        self.assertIn(Char.wisdom, range(3, 19))
-        self.assertIn(Char.charisma, range(3, 19))
-        self.assertEqual(Char.hitpoints, 10 + modifier(Char.constitution))
+        self.assertTrue(Char.strength >= 3 and Char.strength <= 18)
+        self.assertTrue(Char.dexterity >= 3 and Char.dexterity <= 18)
+        self.assertTrue(Char.constitution >= 3 and Char.constitution <= 18)
+        self.assertTrue(Char.intelligence >= 3 and Char.intelligence <= 18)
+        self.assertTrue(Char.wisdom >= 3 and Char.wisdom <= 18)
+        self.assertTrue(Char.charisma >= 3 and Char.charisma <= 18)
+        self.assertTrue(Char.hitpoints == 10 + modifier(Char.constitution))
 
     def test_each_ability_is_only_calculated_once(self):
         Char = Character()
-        self.assertEqual(Char.strength, Char.strength)
+        self.assertTrue(Char.strength == Char.strength)
 
 
 if __name__ == "__main__":
