42e4e6aa684bc750e6645538a0f284a0e4e40ab1
Michael Morehouse <640167+yawpitch@users.noreply.github.com>
2019-11-20 18:04:52 +0000

    gigasecond: generate with dateutil parse
    Adds a dependency to the generator, but shifts the work off the student.
    As an added benefit should parse a lot more than fromisoformat would.
    
    Must be merged _after_ #2134.

--- exercises/gigasecond/gigasecond_test.py
+++ exercises/gigasecond/gigasecond_test.py
@@ -9,32 +9,27 @@ from gigasecond import add
 class GigasecondTest(unittest.TestCase):
     def test_date_only_specification_of_time(self):
         self.assertEqual(
-            add(datetime.fromisoformat("2011-04-25")),
-            datetime.fromisoformat("2043-01-01T01:46:40"),
+            add(datetime(2011, 4, 25, 0, 0)), datetime(2043, 1, 1, 1, 46, 40)
         )
 
     def test_second_test_for_date_only_specification_of_time(self):
         self.assertEqual(
-            add(datetime.fromisoformat("1977-06-13")),
-            datetime.fromisoformat("2009-02-19T01:46:40"),
+            add(datetime(1977, 6, 13, 0, 0)), datetime(2009, 2, 19, 1, 46, 40)
         )
 
     def test_third_test_for_date_only_specification_of_time(self):
         self.assertEqual(
-            add(datetime.fromisoformat("1959-07-19")),
-            datetime.fromisoformat("1991-03-27T01:46:40"),
+            add(datetime(1959, 7, 19, 0, 0)), datetime(1991, 3, 27, 1, 46, 40)
         )
 
     def test_full_time_specified(self):
         self.assertEqual(
-            add(datetime.fromisoformat("2015-01-24T22:00:00")),
-            datetime.fromisoformat("2046-10-02T23:46:40"),
+            add(datetime(2015, 1, 24, 22, 0)), datetime(2046, 10, 2, 23, 46, 40)
         )
 
     def test_full_time_with_day_roll_over(self):
         self.assertEqual(
-            add(datetime.fromisoformat("2015-01-24T23:59:59")),
-            datetime.fromisoformat("2046-10-03T01:46:39"),
+            add(datetime(2015, 1, 24, 23, 59, 59)), datetime(2046, 10, 3, 1, 46, 39)
         )
 
 
