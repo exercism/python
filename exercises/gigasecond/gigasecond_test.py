b594e97652ca7edf766caddbe70f8c399d4baa00
Michael Morehouse <640167+yawpitch@users.noreply.github.com>
2019-11-20 16:28:16 +0000

    gigasecond: add test template
    Adds the gigasecond test template. Addresses most of the concerns on the
    defunct #2066 submitted by @simonbcodes, however the `strptime` fix
    suggested by @cmccandless was brittle, as some of the incoming data had
    time data.
    
    We'd _maybe_ want to add a `dateutil` dependency if the canonical data
    strays in any way from bog standard ISO 8601 dates, but hopefully if
    that happens the `datetime.fromisoformat` calls here will blow up and
    let us know.
    
    Closes #1950

--- exercises/gigasecond/gigasecond_test.py
+++ exercises/gigasecond/gigasecond_test.py
@@ -7,30 +7,35 @@ from gigasecond import add
 
 
 class GigasecondTest(unittest.TestCase):
-    def date_only_specification_of_time(self):
-        test_value = "2011-04-25"
-        expected = "2043-01-01T01:46:40"
-        self.assertEqual(add(test_value), expected)
+    def test_date_only_specification_of_time(self):
+        self.assertEqual(
+            add(datetime.fromisoformat("2011-04-25")),
+            datetime.fromisoformat("2043-01-01T01:46:40"),
+        )
 
-    def second_test_for_date_only_specification_of_time(self):
-        test_value = "1977-06-13"
-        expected = "2009-02-19T01:46:40"
-        self.assertEqual(add(test_value), expected)
+    def test_second_test_for_date_only_specification_of_time(self):
+        self.assertEqual(
+            add(datetime.fromisoformat("1977-06-13")),
+            datetime.fromisoformat("2009-02-19T01:46:40"),
+        )
 
-    def third_test_for_date_only_specification_of_time(self):
-        test_value = "1959-07-19"
-        expected = "1991-03-27T01:46:40"
-        self.assertEqual(add(test_value), expected)
+    def test_third_test_for_date_only_specification_of_time(self):
+        self.assertEqual(
+            add(datetime.fromisoformat("1959-07-19")),
+            datetime.fromisoformat("1991-03-27T01:46:40"),
+        )
 
-    def full_time_specified(self):
-        test_value = "2015-01-24T22:00:00"
-        expected = "2046-10-02T23:46:40"
-        self.assertEqual(add(test_value), expected)
+    def test_full_time_specified(self):
+        self.assertEqual(
+            add(datetime.fromisoformat("2015-01-24T22:00:00")),
+            datetime.fromisoformat("2046-10-02T23:46:40"),
+        )
 
-    def full_time_with_day_roll_over(self):
-        test_value = "2015-01-24T23:59:59"
-        expected = "2046-10-03T01:46:39"
-        self.assertEqual(add(test_value), expected)
+    def test_full_time_with_day_roll_over(self):
+        self.assertEqual(
+            add(datetime.fromisoformat("2015-01-24T23:59:59")),
+            datetime.fromisoformat("2046-10-03T01:46:39"),
+        )
 
 
 if __name__ == "__main__":
