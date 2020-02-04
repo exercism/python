997099dd07827facc44241662472c04de5c70193
c4llmeco4ch <cbb692@gmail.com>
2020-02-03 17:02:41 -0800

    Reverted prob-spec data file to master copy

--- exercises/word-count/word_count_test.py
+++ exercises/word-count/word_count_test.py
@@ -2,7 +2,7 @@ import unittest
 
 from word_count import count_words
 
-# Tests adapted from `problem-specifications//canonical-data.json` @ v1.5.0
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.4.0
 
 
 class WordCountTest(unittest.TestCase):
