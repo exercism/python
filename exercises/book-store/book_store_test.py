02d26aa6fc933ad8024e52c01db2bfc664a5597d
Michael Morehouse <640167+yawpitch@users.noreply.github.com>
2020-02-20 18:47:07 +0000

    [book-store] Add additional tests (#2174)
    * [book-store] Add additional tests
    
    Adds tests that requires use of booth groups of 4 and 5 volumes.
    
    Closes #2141, which had gone stale.
    
    * [book-store] Update from latest template.
    
    * Crank the handle on Travis

--- exercises/book-store/book_store_test.py
+++ exercises/book-store/book_store_test.py
@@ -68,6 +68,16 @@ class BookStoreTest(unittest.TestCase):
         basket = [1, 1, 2, 2, 3, 3, 4, 5, 1, 1, 2, 2, 3, 3, 4, 5]
         self.assertEqual(total(basket), 10240)
 
+    # Additional tests for this track
+
+    def test_two_groups_of_four_and_a_group_of_five(self):
+        basket = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5]
+        self.assertEqual(total(basket), 8120)
+
+    def test_shuffled_book_order(self):
+        basket = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3]
+        self.assertEqual(total(basket), 8120)
+
 
 if __name__ == "__main__":
     unittest.main()
