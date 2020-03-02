685c88a1433e8bb786906d18ede4bba35978eeb2
Michael Morehouse <640167+yawpitch@users.noreply.github.com>
2020-03-02 12:19:16 +0000

    [grep] Fix for 3.4

--- exercises/grep/grep_test.py
+++ exercises/grep/grep_test.py
@@ -43,7 +43,7 @@ def open_mock(fname, *args, **kwargs):
         )
 
 
-@mock.patch("grep.open", name="open", side_effect=open_mock)
+@mock.patch("grep.open", name="open", side_effect=open_mock, create=True)
 @mock.patch("io.StringIO", name="StringIO", wraps=io.StringIO)
 class GrepTest(unittest.TestCase):
     # Test grepping a single file
