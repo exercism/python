1b9ae9df147f9485b87b126767af22ef1c593a1b
AnAccountForReportingBugs <45908752+AnAccountForReportingBugs@users.noreply.github.com>
2020-04-23 01:12:39 -0500

    passio: Implement exercise

---
+++ exercises/paasio/example.py
@@ -0,0 +1,102 @@
+import io
+
+
+class MeteredFile(io.BufferedRandom):
+    """Implement using a subclassing model."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._read_bytes = 0
+        self._read_ops = 0
+        self._write_bytes = 0
+        self._write_ops = 0
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return super().__exit__(exc_type, exc_val, exc_tb)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self._read_ops += 1
+        data = super().readline()
+        self._read_bytes += len(data)
+        if data:
+            return data
+        raise StopIteration
+
+    def read(self, size=-1):
+        self._read_ops += 1
+        data = super().read(size)
+        self._read_bytes += len(data)
+        return data
+
+    @property
+    def read_bytes(self):
+        return self._read_bytes
+
+    @property
+    def read_ops(self):
+        return self._read_ops
+
+    def write(self, b):
+        self._write_ops += 1
+        length = super().write(b)
+        self._write_bytes += length
+        return length
+
+    @property
+    def write_bytes(self):
+        return self._write_bytes
+
+    @property
+    def write_ops(self):
+        return self._write_ops
+
+
+class MeteredSocket:
+    """Implement using a delegation model."""
+
+    def __init__(self, socket):
+        self._socket = socket
+        self._recv_bytes = 0
+        self._recv_ops = 0
+        self._send_bytes = 0
+        self._send_ops = 0
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return self._socket.__exit__(exc_type, exc_val, exc_tb)
+
+    def recv(self, bufsize, flags=0):
+        self._recv_ops += 1
+        data = self._socket.recv(bufsize, flags)
+        self._recv_bytes += len(data)
+        return data
+
+    @property
+    def recv_bytes(self):
+        return self._recv_bytes
+
+    @property
+    def recv_ops(self):
+        return self._recv_ops
+
+    def send(self, data, flags=0):
+        self._send_ops += 1
+        length = self._socket.send(data, flags)
+        self._send_bytes += length
+        return length
+
+    @property
+    def send_bytes(self):
+        return self._send_bytes
+
+    @property
+    def send_ops(self):
+        return self._send_ops
