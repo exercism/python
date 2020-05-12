27729f5388378627643a0afb1ce6a7cbcfe04978
Srdjan S <stankovic.srdjo@gmail.com>
2020-05-12 20:25:55 +0200

    Make input values in test cases more verbose
    Add comment to random private key test
    Replace for loop with list comphrenhension

--- exercises/diffie-hellman/diffie_hellman_test.py
+++ exercises/diffie-hellman/diffie_hellman_test.py
@@ -8,21 +8,27 @@ from diffie_hellman import private_key, public_key, secret
 class DiffieHellmanTest(unittest.TestCase):
     def test_private_key_is_in_range_1_p(self):
         primes = [5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
-        for i in primes:
-            self.assertTrue(1 < private_key(i) < i)
+        for p in primes:
+            self.assertTrue(p < private_key(p) < p)
 
     def test_private_key_is_random(self):
+        # Can fail due to randomness, but most likely will not,
+        # due to pseudo-randomness and the large number chosen
         p = 2147483647
-        private_keys = []
-        for i in range(5):
-            private_keys.append(private_key(p))
+        private_keys = [private_key(p) for _ in range(5)]
         self.assertEqual(len(set(private_keys)), len(private_keys))
 
     def test_can_calculate_public_key_using_private_key(self):
-        self.assertEqual(8, public_key(*[23, 5, 6]))
+        p = 23
+        g = 5
+        private_key = 6
+        self.assertEqual(8, public_key(p, g, private_key))
 
     def test_can_calculate_secret_using_other_party_s_public_key(self):
-        self.assertEqual(2, secret(*[23, 19, 6]))
+        p = 23
+        their_public_key = 19
+        my_private_key = 6
+        self.assertEqual(2, secret(p, their_public_key, my_private_key))
 
     def test_key_exchange(self):
         p = 23
