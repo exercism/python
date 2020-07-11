43165419544125abcda7f4e921a97f2576d2b20a
BethanyG <BethanyG@users.noreply.github.com>
2020-07-10 23:19:18 -0700

    Add tuples concept exercise
    * New tuples concept exercise design file.
    
    * New tuples concept exercise example implementation file.
    
    * New tuples concept exercise config  file.
    
    * New tuples concept exercist after file w TODO.
    
    * New tuples concept exercist hints file.
    
    * New tuples concept exercise introduction file.
    
    * New tuples concept exercise instructions file.
    
    * New tuples concept exercise boilerplate file.
    
    * New tuples concept exercise cleaned boilerplate file.
    
    * New tuples concept exercise tests.
    
    * Ran prettier to clean up file markdown formatting.  Edited language and cleaned up bloat.
    
    * Ran prettier to correct markdown formatting and dleted star operator as learning objective.
    
    * Removed tuple multiplication with star operator.
    
    * Edited to remove multiply function.
    
    * Edited to remove multiply function test case.
    
    * Fixed quoting on Scrimshaw Whale's tooth to match quoting style.
    
    * Ran prettier for config.json
    
    * Edits and adjustments per PR feedback and style guides.  Also corrected introduction.md file name spelling.
    
    * Edited for clarity and brevity per PR feedback.  Removed excess concepts and code examples.
    
    * Re-worked final task to be a returned report string rather than print()-ing of content.  Adjusted instructions accordingly.
    
    * Cleaned up per PR feedback.  Removed excess lines.
    
    * Changed final test to support altered task for report string.  Removed context handler decorator for re-routing stdout and extra imports.
    
    * Corrected terminal output code exxample for task 4.
    
    * Removed print as a prerequisite of the exercise.  Added absolute link for tuples.md refrence doc.
    
    * Removed excess PyTest config from test class.
    
    * Added tuples concept exercise information and uuid.
    
    * Ran prettier on config file, since I forgot to.

---
+++ exercises/concept/tuples/.docs/instructions.md
@@ -0,0 +1,107 @@
+Aazra and Rui are teammates competing in a pirate-themed treasure hunt. One has a list of treasures with map coordinates, the other a list of location names with map coordinates. They've also been given blank maps with a starting place marked YOU ARE HERE.
+
+<br>
+<table>
+<tr><th>Azara's List</th><th></th><th>Rui's List</th></tr>
+<tr><td>
+
+| Treasure                    | Coordinates |
+| --------------------------- | ----------- |
+| Amethyst Octopus            | 1F          |
+| Angry Monkey Figurine       | 5B          |
+| Antique Glass Fishnet Float | 3D          |
+| Brass Spyglass              | 4B          |
+| Carved Wooden Elephant      | 8C          |
+| Crystal Crab                | 6A          |
+| Glass Starfish              | 6D          |
+| Model Ship in Large Bottle  | 8A          |
+| Pirate Flag                 | 7F          |
+| Robot Parrot                | 1C          |
+| Scrimshaw Whale's Tooth     | 2A          |
+| Silver Seahorse             | 4E          |
+| Vintage Pirate Hat          | 7E          |
+
+</td><td></td><td>
+
+| Location Name                         | Coordinates | Quandrant |
+| ------------------------------------- | ----------- | --------- |
+| Seaside Cottages                      | ("1", "C")  | Blue      |
+| Aqua Lagoon (Island of Mystery)       | ("1", "F")  | Yellow    |
+| Deserted Docks                        | ("2", "A")  | Blue      |
+| Spiky Rocks                           | ("3", "D")  | Yellow    |
+| Abandoned Lighthouse                  | ("4", "B")  | Blue      |
+| Hidden Spring (Island of Mystery)     | ("4", "E")  | Yellow    |
+| Stormy Breakwater                     | ("5", "B")  | Purple    |
+| Old Schooner                          | ("6", "A")  | Purple    |
+| Tangled Seaweed Patch                 | ("6", "D")  | Orange    |
+| Quiet Inlet (Island of Mystery)       | ("7", "E")  | Orange    |
+| Windswept Hilltop (Island of Mystery) | ("7", "F")  | Orange    |
+| Harbor Managers Office                | ("8", "A")  | Purple    |
+| Foggy Seacave                         | ("8", "C")  | Purple    |
+
+</td></tr>
+</table>
+
+<br>
+
+But things are a bit disorganized: Azara's coordinates appear to be formatted and sorted differently from Rui's, and they have to keep looking from one list to the other to figure out which treasures go with which locations. Being budding pythonistas, they've come to you for help in writing a small program (a set of functions, really) to better organize their hunt information.
+
+## 1. Extract coordinates
+
+Implement the `get_cooordinate()` function that takes a `(treasure, coordinate)` pair from Azaras list and returns only the extracted map coordinate.
+​
+
+```python
+>>> get_coordinate(("Scrimshaw Whale's Tooth", "2A"))
+"2A"
+```
+
+## 2. Format coordinates
+
+Implement the `convert_coordinate()` function that takes a coordinate in the format "2A" and returns a tuple in the format `("2", "A")`.
+
+​
+
+```python
+>>> convert_coordinate("2A")
+("2", "A")
+```
+
+## 3. Match coordinates
+
+Implement the `compare_records()` function that takes a `(treasure, coordinate)` pair and a `(location, coordinate, quadrant)` record and compares coordinates from each. Return **`True`** if the coordinates "match", and return **`False`** if they do not. Re-format coordinates as needed for accurate comparison.
+
+```python
+>>> compare_records(('Brass Spyglass', '4B'),('Seaside Cottages', ("1", "C"), 'blue'))
+False
+
+>>> compare_records(('Model Ship in Large Bottle', '8A'), ('Harbor Managers Office', ("8", "A"), 'purple'))
+True
+```
+
+## 4. Combine matched records
+
+Implement the `create_record()` function that takes a `(treasure, coordinate)` pair from Azaras list and a `(location, coordinate, quadrant)` record from Ruis list and returns `(treasure, coordinate, location, coordinate, quadrant)` **if the coordinates match**. If the coordinats _do not_ match, return the string "not a match.". Re-format the coordinate as needed for accurate comparison.
+
+```python
+>>> create_record(('Brass Spyglass', '4B'),('Abandoned Lighthouse', ("4", "B"), 'Blue'))
+('Brass Spyglass', '4B', 'Abandoned Lighthouse', ("4", "B"), 'Blue')
+
+>>> create_record(('Brass Spyglass', '4B'),(("1", "C"), 'Seaside Cottages', 'blue'))
+"not a match."
+```
+
+## 5. "Clean up" & make a report of all records
+
+Clean up the combined records from Azara and Rui so that there's only one set of coordinates per record. Make a report so they can see one list of everything they need to put on their maps.
+Implement the `clean_up()` function that takes a tuple of tuples (_everything from both lists_), looping through the _outer_ tuple, dropping the unwanted coordinates from each _inner_ tuple and adding each to a 'report'. Format and return the 'report' so that there is one cleaned record on each line.
+
+```python
+>>> clean_up((('Brass Spyglass', '4B', 'Abandoned Lighthouse', '("4", "B")', 'Blue'), ('Vintage Pirate Hat', '7E', 'Quiet Inlet (Island of Mystery)', '("7", "E")', 'Orange'), ('Crystal Crab', '6A', 'Old Schooner', '("6", "A")', 'Purple')))
+
+"""
+('Brass Spyglass', 'Abandoned Lighthouse', '("4", "B")', 'Blue')\n
+('Vintage Pirate Hat', 'Quiet Inlet (Island of Mystery)', '("7", "E")', 'Orange')\n
+('Crystal Crab', 'Old Schooner', '("6", "A")','Purple')\n
+"""
+```
---
+++ exercises/concept/tuples/tuples_test.py
@@ -0,0 +1,128 @@
+import unittest
+from tuples import get_coordinate, convert_coordinate, compare_records, create_record, clean_up
+
+
+class TuplesTest(unittest.TestCase):
+
+    def test_get_coordinate(self):
+        input_data = [("Scrimshaw Whale's Tooth", '2A'),
+                                ('Brass Spyglass', '4B'),
+                                ('Robot Parrot', '1C'),
+                                ('Glass Starfish', '6D'),
+                                ('Vintage Pirate Hat', '7E'),
+                                ('Pirate Flag', '7F'),
+                                ('Crystal Crab', '6A'),
+                                ('Model Ship in Large Bottle', '8A'),
+                                ('Angry Monkey Figurine', '5B'),
+                                ('Carved Wooden Elephant', '8C'),
+                                ('Amethyst  Octopus', '1F'),
+                                ('Antique Glass Fishnet Float', '3D'),
+                                ('Silver Seahorse', '4E')]
+
+        result_data = ['2A', '4B', '1C', '6D', '7E', '7F', '6A', '8A', '5B', '8C', '1F', '3D', '4E']
+
+        for item, result  in zip(input_data, result_data):
+            with self.subTest("tuple/coordinate variants", item=item, result=result):
+                self.assertEqual(get_coordinate(item), result)
+
+    def test_convert_coordinate(self):
+        input_data = ['2A', '4B', '1C', '6D', '7E', '7F', '6A', '8A', '5B', '8C', '1F', '3D', '4E']
+        result_data = [('2', 'A'),
+                                ('4', 'B'),
+                                ('1', 'C'),
+                                ('6', 'D'),
+                                ('7', 'E'),
+                                ('7', 'F'),
+                                ('6', 'A'),
+                                ('8', 'A'),
+                                ('5', 'B'),
+                                ('8', 'C'),
+                                ('1', 'F'),
+                                ('3', 'D'),
+                                ('4', 'E')]
+
+        for item, result in zip(input_data, result_data):
+            with self.subTest("coordinate variants for conversion", item=item, result=result):
+                self.assertEqual(convert_coordinate(item), result)
+
+    def test_compare_records(self):
+        input_data = [
+                                (("Scrimshaw Whale's Tooth", '2A'), ('Deserted Docks', ('2', 'A') ,'Blue')),
+                                (('Brass Spyglass', '4B'), ('Abandoned Lighthouse', ('4', 'B') ,'Blue')),
+                                (('Robot Parrot', '1C'), ('Seaside Cottages', ('1', 'C') ,'Blue')),
+                                (('Glass Starfish', '6D'), ('Tangled Seaweed Patch', ('6', 'D'),'Orange')),
+                                (('Vintage Pirate Hat', '7E'), ('Quiet Inlet (Island of Mystery)', ('7', 'E') ,'Orange')),
+                                (('Amethyst  Octopus', '1F'), ('Seaside Cottages', ('1', 'C') ,'Blue')),
+                                (('Angry Monkey Figurine', '5B'), ('Aqua Lagoon (Island of Mystery)', ('1', 'F') ,'Yellow')),
+                                (('Antique Glass Fishnet Float', '3D'), ('Deserted Docks', ('2', 'A') ,'Blue')),
+                                (('Brass Spyglass', '4B'), ('Spiky Rocks', ('3', 'D') ,'Yellow')),
+                                (('Carved Wooden Elephant', '8C'), ('Abandoned Lighthouse', ('4', 'B') ,'Blue'))
+                            ]
+        result_data = [True, True, True, True, True, False, False, False, False, False]
+
+        for item, result in zip(input_data, result_data):
+            with self.subTest("do the coordinates match once reformatted?", item=item, result=result):
+                self.assertEqual(compare_records(item[0], item[1]), result)
+
+    def test_create_record(self):
+        input_data=[
+                            (('Angry Monkey Figurine', '5B'), ('Stormy Breakwater', ('5', 'B') ,'Purple')),
+                            (('Carved Wooden Elephant', '8C'), ('Foggy Seacave', ('8', 'C'), 'Purple')),
+                            (('Amethyst  Octopus', '1F'), ('Aqua Lagoon (Island of Mystery)', ('1', 'F') ,'Yellow')),
+                            (('Antique Glass Fishnet Float', '3D'), ('Spiky Rocks', ('3', 'D'),'Yellow')),
+                            (('Silver Seahorse', '4E'), ('Hidden Spring (Island of Mystery)', ('4', 'E') ,'Yellow')),
+                            (('Amethyst  Octopus', '1F'), ('Seaside Cottages', ('1', 'C') ,'Blue')),
+                            (('Angry Monkey Figurine', '5B'), ('Aqua Lagoon (Island of Mystery)', ('1', 'F') ,'Yellow')),
+                            (('Antique Glass Fishnet Float', '3D'), ('Deserted Docks', ('2', 'A') ,'Blue')),
+                            (('Brass Spyglass', '4B'), ('Spiky Rocks', ('3', 'D'),'Yellow')),
+                            (('Carved Wooden Elephant', '8C'), ('Abandoned Lighthouse', ('4', 'B') ,'Blue'))
+                            ]
+        result_data = [
+                                ('Angry Monkey Figurine', '5B', 'Stormy Breakwater', ('5', 'B'), 'Purple'),
+                                ('Carved Wooden Elephant', '8C', 'Foggy Seacave', ('8', 'C'), 'Purple'),
+                                ('Amethyst  Octopus', '1F', 'Aqua Lagoon (Island of Mystery)', ('1', 'F'), 'Yellow'),
+                                ('Antique Glass Fishnet Float', '3D', 'Spiky Rocks', ('3', 'D'), 'Yellow'),
+                                ('Silver Seahorse', '4E', 'Hidden Spring (Island of Mystery)', ('4', 'E'), 'Yellow'),
+                                'not a match',
+                                'not a match',
+                                'not a match',
+                                'not a match',
+                                'not a match'
+                            ]
+
+        for item, result in zip(input_data, result_data):
+            with self.subTest("make record if coordinates match", item=item, result=result):
+                self.assertEqual(create_record(item[0], item[1]), result)
+
+    def test_clean_up(self):
+        input_data = (
+                                ("Scrimshaw Whale's Tooth", '2A', 'Deserted Docks', ('2', 'A'), 'Blue'),
+                                ('Brass Spyglass', '4B', 'Abandoned Lighthouse', ('4', 'B'), 'Blue'),
+                                ('Robot Parrot', '1C', 'Seaside Cottages', ('1', 'C'), 'Blue'),
+                                ('Glass Starfish', '6D', 'Tangled Seaweed Patch', ('6', 'D'), 'Orange'),
+                                ('Vintage Pirate Hat', '7E', 'Quiet Inlet (Island of Mystery)', ('7', 'E'), 'Orange'),
+                                ('Pirate Flag', '7F', 'Windswept Hilltop (Island of Mystery)', ('7', 'F'), 'Orange'),
+                                 ('Crystal Crab', '6A', 'Old Schooner', ('6', 'A'), 'Purple'),
+                                ('Model Ship in Large Bottle', '8A', 'Harbor Managers Office', ('8', 'A'), 'Purple'),
+                                ('Angry Monkey Figurine', '5B', 'Stormy Breakwater', ('5', 'B'), 'Purple'),
+                                ('Carved Wooden Elephant', '8C', 'Foggy Seacave', ('8', 'C'), 'Purple'),
+                                ('Amethyst  Octopus', '1F', 'Aqua Lagoon (Island of Mystery)', ('1', 'F'), 'Yellow'),
+                                ('Antique Glass Fishnet Float', '3D', 'Spiky Rocks', ('3', 'D'), 'Yellow'),
+                                ('Silver Seahorse', '4E', 'Hidden Spring (Island of Mystery)', ('4', 'E'), 'Yellow')
+                            )
+
+        result_data = """(\"Scrimshaw Whale's Tooth\", 'Deserted Docks', ('2', 'A'), 'Blue')\n\
+('Brass Spyglass', 'Abandoned Lighthouse', ('4', 'B'), 'Blue')\n\
+('Robot Parrot', 'Seaside Cottages', ('1', 'C'), 'Blue')\n\
+('Glass Starfish', 'Tangled Seaweed Patch', ('6', 'D'), 'Orange')\n\
+('Vintage Pirate Hat', 'Quiet Inlet (Island of Mystery)', ('7', 'E'), 'Orange')\n\
+('Pirate Flag', 'Windswept Hilltop (Island of Mystery)', ('7', 'F'), 'Orange')\n\
+('Crystal Crab', 'Old Schooner', ('6', 'A'), 'Purple')\n\
+('Model Ship in Large Bottle', 'Harbor Managers Office', ('8', 'A'), 'Purple')\n\
+('Angry Monkey Figurine', 'Stormy Breakwater', ('5', 'B'), 'Purple')\n\
+('Carved Wooden Elephant', 'Foggy Seacave', ('8', 'C'), 'Purple')\n\
+('Amethyst  Octopus', 'Aqua Lagoon (Island of Mystery)', ('1', 'F'), 'Yellow')\n\
+('Antique Glass Fishnet Float', 'Spiky Rocks', ('3', 'D'), 'Yellow')\n\
+('Silver Seahorse', 'Hidden Spring (Island of Mystery)', ('4', 'E'), 'Yellow')\n"""
+
+        self.assertEqual(clean_up(input_data), result_data)
