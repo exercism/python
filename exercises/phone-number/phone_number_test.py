1b6fbdefa39d5f10499bbf5c528fb3be8e77289f
Corey McCandless <cmccandless@users.noreply.github.com>
2020-08-27 12:16:31 -0400

    phone-number: standardize pretty format (#2217)

--- exercises/phone-number/phone_number_test.py
+++ exercises/phone-number/phone_number_test.py
@@ -85,11 +85,11 @@ class PhoneNumberTest(unittest.TestCase):
 
     def test_pretty_print(self):
         number = PhoneNumber("2234567890")
-        self.assertEqual(number.pretty(), "(223) 456-7890")
+        self.assertEqual(number.pretty(), "(223)-456-7890")
 
     def test_pretty_print_with_full_us_phone_number(self):
         number = PhoneNumber("12234567890")
-        self.assertEqual(number.pretty(), "(223) 456-7890")
+        self.assertEqual(number.pretty(), "(223)-456-7890")
 
     # Utility functions
     def assertRaisesWithMessage(self, exception):
