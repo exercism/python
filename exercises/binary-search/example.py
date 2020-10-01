6c531818b1022f1dfd92fe58a4248381a801ee35
dipu007 <72186394+dipu007@users.noreply.github.com>
2020-10-01 15:57:44 +0530

    Update example.py

--- exercises/binary-search/example.py
+++ exercises/binary-search/example.py
@@ -1,12 +1,35 @@
-def find(search_list, value):
-    low = 0
-    high = len(search_list) - 1
-    while low <= high:
-        middle = (low + high) // 2
-        if search_list[middle] > value:
-            high = middle - 1
-        elif search_list[middle] < value:
-            low = middle + 1
-        else:
-            return middle
-    raise ValueError("Value not found.")
+def binary_search(arr, low, high, x): 
+  
+    # Check base case 
+    if high >= low: 
+  
+        mid = (high + low) // 2
+  
+        # If element is present at the middle itself 
+        if arr[mid] == x: 
+            return mid 
+  
+        # If element is smaller than mid, then it can only 
+        # be present in left subarray 
+        elif arr[mid] > x: 
+            return binary_search(arr, low, mid - 1, x) 
+  
+        # Else the element can only be present in right subarray 
+        else: 
+            return binary_search(arr, mid + 1, high, x) 
+  
+    else: 
+        # Element is not present in the array 
+        return -1
+  
+# Test array 
+arr = [ 2, 3, 4, 10, 40 ] 
+x = 10
+  
+# Function call 
+result = binary_search(arr, 0, len(arr)-1, x) 
+  
+if result != -1: 
+    print("Element is present at index", str(result)) 
+else: 
+    print("Element is not present in array") 
