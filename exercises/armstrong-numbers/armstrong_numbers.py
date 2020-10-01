40bde02db8f21df74b77ba8a275a52719b6f881f
shubhambhawsar-5782 <51023390+shubhambhawsar-5782@users.noreply.github.com>
2020-10-01 18:26:42 +0530

    Update armstrong_numbers.py

--- exercises/armstrong-numbers/armstrong_numbers.py
+++ exercises/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,21 @@
-def is_armstrong_number(number):
-    pass
+def is_armstrong_number(num):
+    # Changed num variable to string, 
+    # and calculated the length (number of digits)
+    order = len(str(num))
+
+    # initialize sum
+    sum = 0
+
+    # find the sum of the cube of each digit
+    temp = num
+    while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+    # display the result
+    if num == sum:
+       return (num,"is an Armstrong number")
+    else:
+       return (num,"is not an Armstrong number")
+print(is_armstrong_number(int(input())))
