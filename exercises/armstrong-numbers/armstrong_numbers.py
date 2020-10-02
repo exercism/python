d1177f064a819e5ae83a764bd4190f063c098648
Your Name <gadgetsboy147@gmail.com>
2020-10-02 23:39:53 +0530

    Armstrong question solved

--- exercises/armstrong-numbers/armstrong_numbers.py
+++ exercises/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,20 @@
 def is_armstrong_number(number):
-    pass
+    org_num = number
+    sum = 0
+    try:
+        while (number!=0):
+            digit = number%10
+            sum = sum+ digit**len(str(org_num))
+            number = number//10
+        if sum == org_num:
+            return True
+        else:
+            return False
+
+    except Exception as e:
+        print("Input is not a number. Please Enter a number", e)
+
+
+
+
+
