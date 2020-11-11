f8cdd322d38723103c4b68e4bd14d90e2e8c0b57
Zeaan <59172904+Zeaan@users.noreply.github.com>
2020-11-11 14:37:36 +0530

    Update reverse_string.py
    Created a project which reverses a String

--- exercises/reverse-string/reverse_string.py
+++ exercises/reverse-string/reverse_string.py
@@ -1,2 +1,6 @@
-def reverse(text):
-    pass
+word = input(print("Enter word"))
+reverse_word = ""
+for i in range(len(word)-1,-1,-1):
+  reverse_word = reverse_word + word[i]
+
+print(reverse_word)  
