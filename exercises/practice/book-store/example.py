f1728573819e30ed6425dec4141ace6d54c9fc0b
Erik Schierboom <erik_schierboom@hotmail.com>
2021-01-29 15:50:01 +0100

    [v3] Move existing exercises to exercises/practice

---
+++ exercises/practice/book-store/example.py
@@ -0,0 +1,26 @@
+from collections import Counter
+
+PER_BOOK = 800.00
+PER_GROUP = {
+    1: 1 * PER_BOOK * 1.00,
+    2: 2 * PER_BOOK * 0.95,
+    3: 3 * PER_BOOK * 0.90,
+    4: 4 * PER_BOOK * 0.80,
+    5: 5 * PER_BOOK * 0.75,
+}
+
+
+def _total(books):
+    volumes = Counter(books)
+    price = len(books) * PER_BOOK
+    for size in range(len(volumes), 1, -1):
+        group = volumes - Counter(k for k, _ in volumes.most_common(size))
+        group_books = sorted(group.elements())
+        price = min(price, PER_GROUP[size] + _total(group_books))
+    return price
+
+
+def total(books):
+    if not books:
+        return 0
+    return _total(sorted(books))
