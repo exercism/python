9fa612a4641d6ba763f3a06cc71204a5e423fc84
BethanyG <bethanymgarcia@gmail.com>
2021-02-05 01:45:41 -0800

    Added user-facing test error messages and subtests.

--- exercises/concept/guidos-gorgeous-lasagna/lasagna_test.py
+++ exercises/concept/guidos-gorgeous-lasagna/lasagna_test.py
@@ -5,34 +5,53 @@ from lasagna import EXPECTED_BAKE_TIME, bake_time_remaining, preparation_time_in
 class LasagnaTest(unittest.TestCase):
 
     def test_EXPECTED_BAKE_TIME(self):
-        self.assertEqual(EXPECTED_BAKE_TIME, 40)
+        self.assertEqual(EXPECTED_BAKE_TIME, 40, msg="Expected a constant of EXPECTED_BAKE_TIME with a value of 40.")
 
 
-    def bake_time_remaining(self):
-        input_data = [1, 2, 5, 10, 15, 23, 33, 39 ]
+    def test_bake_time_remaining(self):
+        input_data = [1, 2, 5, 10, 15, 23, 33, 39]
         result_data = [40 - item for item in input_data]
+        number_of_variants = range(1, len(input_data) + 1)
 
-        for time, result in zip(input_data, result_data):
-            with self.subTest("bake time remaining variants", time=time, result=result):
-                self.assertEqual(bake_time_remaining(time), result)
+        for variant, time, result in zip(number_of_variants, input_data, result_data):
+            with self.subTest(f"variation #{variant}", time=time, result=result):
+                self.assertEqual(bake_time_remaining(time), result,
+                                 msg=f'Expected: {result} but the bake time remaining was calculated incorrectly.')
+
 
     def test_preparation_time_in_minutes(self):
         input_data = [1, 2, 5, 8, 11, 15]
         result_data = [item * 2 for item in input_data]
+        number_of_variants = range(1, len(input_data) + 1)
 
-        for layers, time in zip(input_data, result_data):
-            with self.subTest("preparation time calculation variants", layers=layers, time=time):
-                self.assertEqual(preparation_time_in_minutes(layers), time)
+        for variant, layers, time in zip(number_of_variants, input_data, result_data):
+            with self.subTest(f"variation #{variant}", layers=layers, time=time):
+                self.assertEqual(preparation_time_in_minutes(layers), time,
+                                 msg=f'Expected: {time} minutes, but preparation time'
+                                     f' was calculated incorrectly.')
+
 
     def test_elapsed_time_in_minutes(self):
-        input_data = [1, 2, 5, 8, 11, 15]
-        result_data = [item * 2 for item in input_data]
+        layer_data = (1, 2, 5, 8, 11, 15)
+        time_data = (3, 7, 8, 4, 15, 20)
+        result_data = [prep * 2 + elapsed for prep, elapsed in zip(layer_data, time_data)]
+        number_of_variants = range(1, len(time_data) + 1)
 
-        for layers, time in zip(input_data, result_data):
-            with self.subTest("bake time remaining variants", layers=layers, time=time):
-                self.assertEqual(elapsed_time_in_minutes(8, 10), 26)
+        for variant, layers, time, total_time in zip(number_of_variants, layer_data, time_data, result_data):
+            with self.subTest(f"variation #{variant}", layers=layers, time=time, total_time=total_time):
 
-    def test_docstrings(self):
-        self.assertIsNotNone(elapsed_time_in_minutes.__doc__)
-        self.assertIsNotNone(preparation_time_in_minutes.__doc__)
-        self.assertIsNotNone(elapsed_time_in_minutes.__doc__)
+                failure_msg = f'Expected {time} minutes elapsed, but the timing was calculated incorrectly.'
+                self.assertEqual(elapsed_time_in_minutes(layers, time), total_time,msg=failure_msg)
+
+
+    def test_docstrings_were_written(self):
+        self.assertIsNotNone(elapsed_time_in_minutes.__doc__,
+                             msg="Expected a docstring for elapsed_time_in_minutes, but recieved None instead.")
+        self.assertIsNotNone(preparation_time_in_minutes.__doc__,
+                             msg="Expected a docstring for preparation_time_in_minutes, but recieved None instead.")
+        self.assertIsNotNone(bake_time_remaining.__doc__,
+                             msg="Expected a docstring for bake_time_remaining, but no docstring was found.")
+
+
+if __name__ == "__main__":
+    unittest.main()
