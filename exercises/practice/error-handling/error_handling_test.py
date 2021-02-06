a2c3752df587831286f5dbbd027e1ccfc5c243af
BethanyG <bethanymgarcia@gmail.com>
2021-02-06 07:20:40 -0800

    Moved Helper class in test file to test_util.py file.

--- exercises/practice/error-handling/error_handling_test.py
+++ exercises/practice/error-handling/error_handling_test.py
@@ -1,34 +1,7 @@
 import unittest
 
 import error_handling as er
-
-
-class FileLike:
-    def __init__(self, fail_something=True):
-        self.is_open = False
-        self.was_open = False
-        self.did_something = False
-        self.fail_something = fail_something
-
-    def open(self):
-        self.was_open = False
-        self.is_open = True
-
-    def close(self):
-        self.is_open = False
-        self.was_open = True
-
-    def __enter__(self):
-        self.open()
-        return self
-
-    def __exit__(self, *args):
-        self.close()
-
-    def do_something(self):
-        self.did_something = True
-        if self.fail_something:
-            raise Exception("Failed while doing something")
+from test_helpers import FileLike
 
 
 class ErrorHandlingTest(unittest.TestCase):
