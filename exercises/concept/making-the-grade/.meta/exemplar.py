81b83621d0c5b4c9a262306e1ebe2d00bc0740c9
Corey McCandless <corey.mccandless@atlasied.com>
2021-02-06 02:23:02 -0500

    making-the-grade: fix infinite loop in exemplar.py

--- exercises/concept/making-the-grade/.meta/exemplar.py
+++ exercises/concept/making-the-grade/.meta/exemplar.py
@@ -2,15 +2,14 @@ def count_failed_students(student_marks):
     failed_marks = 0
     for mark in student_marks:
         if mark <=40:
-            failed_marks +=1
+            failed_marks += 1
     return failed_marks
 
-def above_threshold(student_marks, x):
+def above_threshold(student_marks, threshold):
     above_marks = []
     for mark in student_marks:
-        if mark < x:
-            continue
-        above_marks.append(mark)
+        if mark >= threshold:
+            above_marks.append(mark)
     return above_marks
 
 def first_k_student_marks(student_marks, k):
@@ -18,6 +17,7 @@ def first_k_student_marks(student_marks, k):
     marks = []
     while i <= k:
         marks.append(student_marks[i])
+        i += 1
     return marks
 
 def perfect_score(student_info):
