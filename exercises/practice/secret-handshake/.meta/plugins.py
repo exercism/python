389d007716b1588385c9ade9a0a7909ede56af25
Corey McCandless <corey.mccandless@atlasied.com>
2021-03-04 21:16:30 -0500

    secret-handshake: make input a binary string instead of integer

---
+++ exercises/practice/secret-handshake/.meta/plugins.py
@@ -0,0 +1,2 @@
+def to_binary(num, digits=5):
+    return bin(num)[2:].zfill(digits)
--- exercises/practice/secret-handshake/secret_handshake_test.py
+++ exercises/practice/secret-handshake/secret_handshake_test.py
@@ -9,41 +9,41 @@ from secret_handshake import (
 
 class SecretHandshakeTest(unittest.TestCase):
     def test_wink_for_1(self):
-        self.assertEqual(commands(1), ["wink"])
+        self.assertEqual(commands("00001"), ["wink"])
 
     def test_double_blink_for_10(self):
-        self.assertEqual(commands(2), ["double blink"])
+        self.assertEqual(commands("00010"), ["double blink"])
 
     def test_close_your_eyes_for_100(self):
-        self.assertEqual(commands(4), ["close your eyes"])
+        self.assertEqual(commands("00100"), ["close your eyes"])
 
     def test_jump_for_1000(self):
-        self.assertEqual(commands(8), ["jump"])
+        self.assertEqual(commands("01000"), ["jump"])
 
     def test_combine_two_actions(self):
-        self.assertEqual(commands(3), ["wink", "double blink"])
+        self.assertEqual(commands("00011"), ["wink", "double blink"])
 
     def test_reverse_two_actions(self):
-        self.assertEqual(commands(19), ["double blink", "wink"])
+        self.assertEqual(commands("10011"), ["double blink", "wink"])
 
     def test_reversing_one_action_gives_the_same_action(self):
-        self.assertEqual(commands(24), ["jump"])
+        self.assertEqual(commands("11000"), ["jump"])
 
     def test_reversing_no_actions_still_gives_no_actions(self):
-        self.assertEqual(commands(16), [])
+        self.assertEqual(commands("10000"), [])
 
     def test_all_possible_actions(self):
         self.assertEqual(
-            commands(15), ["wink", "double blink", "close your eyes", "jump"]
+            commands("01111"), ["wink", "double blink", "close your eyes", "jump"]
         )
 
     def test_reverse_all_possible_actions(self):
         self.assertEqual(
-            commands(31), ["jump", "close your eyes", "double blink", "wink"]
+            commands("11111"), ["jump", "close your eyes", "double blink", "wink"]
         )
 
     def test_do_nothing_for_zero(self):
-        self.assertEqual(commands(0), [])
+        self.assertEqual(commands("00000"), [])
 
 
 if __name__ == "__main__":
