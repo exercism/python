970cdea06df4556a34086b0bccda37aa70f7506b
BethanyG <bethanymgarcia@gmail.com>
2021-05-09 02:07:03 -0700

    Fixed Jinja template and regenerated tests for prime-factors.

--- exercises/practice/prime-factors/prime_factors_test.py
+++ exercises/practice/prime-factors/prime_factors_test.py
@@ -8,26 +8,10 @@ from prime_factors import (
 
 
 class PrimeFactorsTest(unittest.TestCase):
-    def test_no_factors(self):
-        self.assertEqual(factors(1), [])
 
-    def test_prime_number(self):
-        self.assertEqual(factors(2), [2])
-
-    def test_square_of_a_prime(self):
-        self.assertEqual(factors(9), [3, 3])
-
-    def test_cube_of_a_prime(self):
-        self.assertEqual(factors(8), [2, 2, 2])
-
-    def test_product_of_primes_and_non_primes(self):
-        self.assertEqual(factors(12), [2, 2, 3])
-
-    def test_product_of_primes(self):
-        self.assertEqual(factors(901255), [5, 17, 23, 461])
-
-    def test_factors_include_a_large_prime(self):
-        self.assertEqual(factors(93819012551), [11, 9539, 894119])
+    # Utility functions
+    def assertRaisesWithMessage(self, exception):
+        return self.assertRaisesRegex(exception, r".+")
 
 
 if __name__ == "__main__":
