805fcb53a4ef32a956da77a2235c7fd7af872bf2
BethanyG <bethanymgarcia@gmail.com>
2021-05-09 20:10:41 -0700

    Removed nesting from Jinja templates and re-generated test files.

--- exercises/practice/etl/etl_test.py
+++ exercises/practice/etl/etl_test.py
@@ -8,10 +8,60 @@ from etl import (
 
 
 class EtlTest(unittest.TestCase):
+    def test_single_letter(self):
+        legacy_data = {1: ["A"]}
+        data = {"a": 1}
+        self.assertEqual(transform(legacy_data), data)
 
-    # Utility functions
-    def assertRaisesWithMessage(self, exception):
-        return self.assertRaisesRegex(exception, r".+")
+    def test_single_score_with_multiple_letters(self):
+        legacy_data = {1: ["A", "E", "I", "O", "U"]}
+        data = {"a": 1, "e": 1, "i": 1, "o": 1, "u": 1}
+        self.assertEqual(transform(legacy_data), data)
+
+    def test_multiple_scores_with_multiple_letters(self):
+        legacy_data = {1: ["A", "E"], 2: ["D", "G"]}
+        data = {"a": 1, "d": 2, "e": 1, "g": 2}
+        self.assertEqual(transform(legacy_data), data)
+
+    def test_multiple_scores_with_differing_numbers_of_letters(self):
+        legacy_data = {
+            1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+            2: ["D", "G"],
+            3: ["B", "C", "M", "P"],
+            4: ["F", "H", "V", "W", "Y"],
+            5: ["K"],
+            8: ["J", "X"],
+            10: ["Q", "Z"],
+        }
+        data = {
+            "a": 1,
+            "b": 3,
+            "c": 3,
+            "d": 2,
+            "e": 1,
+            "f": 4,
+            "g": 2,
+            "h": 4,
+            "i": 1,
+            "j": 8,
+            "k": 5,
+            "l": 1,
+            "m": 3,
+            "n": 1,
+            "o": 1,
+            "p": 3,
+            "q": 10,
+            "r": 1,
+            "s": 1,
+            "t": 1,
+            "u": 1,
+            "v": 4,
+            "w": 4,
+            "x": 8,
+            "y": 4,
+            "z": 10,
+        }
+        self.assertEqual(transform(legacy_data), data)
 
 
 if __name__ == "__main__":
--- exercises/practice/food-chain/food_chain_test.py
+++ exercises/practice/food-chain/food_chain_test.py
@@ -8,10 +8,178 @@ from food_chain import (
 
 
 class FoodChainTest(unittest.TestCase):
+    def test_fly(self):
+        self.assertEqual(
+            recite(1, 1),
+            [
+                "I know an old lady who swallowed a fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+            ],
+        )
 
-    # Utility functions
-    def assertRaisesWithMessage(self, exception):
-        return self.assertRaisesRegex(exception, r".+")
+    def test_spider(self):
+        self.assertEqual(
+            recite(2, 2),
+            [
+                "I know an old lady who swallowed a spider.",
+                "It wriggled and jiggled and tickled inside her.",
+                "She swallowed the spider to catch the fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+            ],
+        )
+
+    def test_bird(self):
+        self.assertEqual(
+            recite(3, 3),
+            [
+                "I know an old lady who swallowed a bird.",
+                "How absurd to swallow a bird!",
+                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.",
+                "She swallowed the spider to catch the fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+            ],
+        )
+
+    def test_cat(self):
+        self.assertEqual(
+            recite(4, 4),
+            [
+                "I know an old lady who swallowed a cat.",
+                "Imagine that, to swallow a cat!",
+                "She swallowed the cat to catch the bird.",
+                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.",
+                "She swallowed the spider to catch the fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+            ],
+        )
+
+    def test_dog(self):
+        self.assertEqual(
+            recite(5, 5),
+            [
+                "I know an old lady who swallowed a dog.",
+                "What a hog, to swallow a dog!",
+                "She swallowed the dog to catch the cat.",
+                "She swallowed the cat to catch the bird.",
+                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.",
+                "She swallowed the spider to catch the fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+            ],
+        )
+
+    def test_goat(self):
+        self.assertEqual(
+            recite(6, 6),
+            [
+                "I know an old lady who swallowed a goat.",
+                "Just opened her throat and swallowed a goat!",
+                "She swallowed the goat to catch the dog.",
+                "She swallowed the dog to catch the cat.",
+                "She swallowed the cat to catch the bird.",
+                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.",
+                "She swallowed the spider to catch the fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+            ],
+        )
+
+    def test_cow(self):
+        self.assertEqual(
+            recite(7, 7),
+            [
+                "I know an old lady who swallowed a cow.",
+                "I don't know how she swallowed a cow!",
+                "She swallowed the cow to catch the goat.",
+                "She swallowed the goat to catch the dog.",
+                "She swallowed the dog to catch the cat.",
+                "She swallowed the cat to catch the bird.",
+                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.",
+                "She swallowed the spider to catch the fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+            ],
+        )
+
+    def test_horse(self):
+        self.assertEqual(
+            recite(8, 8),
+            ["I know an old lady who swallowed a horse.", "She's dead, of course!"],
+        )
+
+    def test_multiple_verses(self):
+        self.assertEqual(
+            recite(1, 3),
+            [
+                "I know an old lady who swallowed a fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+                "",
+                "I know an old lady who swallowed a spider.",
+                "It wriggled and jiggled and tickled inside her.",
+                "She swallowed the spider to catch the fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+                "",
+                "I know an old lady who swallowed a bird.",
+                "How absurd to swallow a bird!",
+                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.",
+                "She swallowed the spider to catch the fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+            ],
+        )
+
+    def test_full_song(self):
+        self.assertEqual(
+            recite(1, 8),
+            [
+                "I know an old lady who swallowed a fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+                "",
+                "I know an old lady who swallowed a spider.",
+                "It wriggled and jiggled and tickled inside her.",
+                "She swallowed the spider to catch the fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+                "",
+                "I know an old lady who swallowed a bird.",
+                "How absurd to swallow a bird!",
+                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.",
+                "She swallowed the spider to catch the fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+                "",
+                "I know an old lady who swallowed a cat.",
+                "Imagine that, to swallow a cat!",
+                "She swallowed the cat to catch the bird.",
+                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.",
+                "She swallowed the spider to catch the fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+                "",
+                "I know an old lady who swallowed a dog.",
+                "What a hog, to swallow a dog!",
+                "She swallowed the dog to catch the cat.",
+                "She swallowed the cat to catch the bird.",
+                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.",
+                "She swallowed the spider to catch the fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+                "",
+                "I know an old lady who swallowed a goat.",
+                "Just opened her throat and swallowed a goat!",
+                "She swallowed the goat to catch the dog.",
+                "She swallowed the dog to catch the cat.",
+                "She swallowed the cat to catch the bird.",
+                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.",
+                "She swallowed the spider to catch the fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+                "",
+                "I know an old lady who swallowed a cow.",
+                "I don't know how she swallowed a cow!",
+                "She swallowed the cow to catch the goat.",
+                "She swallowed the goat to catch the dog.",
+                "She swallowed the dog to catch the cat.",
+                "She swallowed the cat to catch the bird.",
+                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.",
+                "She swallowed the spider to catch the fly.",
+                "I don't know why she swallowed the fly. Perhaps she'll die.",
+                "",
+                "I know an old lady who swallowed a horse.",
+                "She's dead, of course!",
+            ],
+        )
 
 
 if __name__ == "__main__":
