4120d11cf6bc66a3866b17fa5812553eb497fc80
BethanyG <bethanymgarcia@gmail.com>
2021-05-18 19:46:57 -0700

    Rewrote config.json, design.md, and exemplar.py.

---
+++ exercises/concept/little-sisters-vocab/.meta/design.md
@@ -0,0 +1,33 @@
+# Design
+
+## Goal
+
+The goal of this exercise is to teach the student about Python strings, and familiarize them with string manipulation in Python.
+
+## Things to teach
+
+- Know that Python has a `str` type.
+- How to use `<str>.join()`
+- How to use `<str>.split()`
+- Know how to manipulate strings to create new strings.
+
+## Things not to teach
+
+- Regex: `regex`.  That is for a different exercise.
+- Iteration: Although strings are iterable, this is not the focus of this exercise.
+
+
+## Prerequisites
+
+- `basics`: The student should be familiar with the basics exercise.
+
+## Representer
+
+This exercise does not require any logic to be added to the [representer][representer]
+
+## Analyzer
+
+This exercise does not require any logic to be added to the [analyzer][analyzer].
+
+[analyzer]: https://github.com/exercism/python-analyzer
+[representer]: https://github.com/exercism/python-representer
---
+++ exercises/concept/little-sisters-vocab/.meta/exemplar.py
@@ -0,0 +1,61 @@
+def add_prefix_un(word):
+    '''
+
+    :param word: str of a root word
+    :return:  str of root word with un prefix
+
+    This function takes `word` as a parameter and
+    returns a new word with an 'un' prefix.
+    '''
+    return 'un' + word
+
+def make_word_groups(vocab_words):
+    '''
+
+    :param vocab_words: list of vocabulary words with a prefix.
+    :return: str of prefix followed by vocabulary words with
+             prefix applied, separated by ' :: '.
+
+    This function takes a `vocab_words` list and returns a string
+    with the prefix  and the words with prefix applied, separated
+     by ' :: '.
+    '''
+    prefix = vocab_words[0]
+    joiner = ' :: ' + prefix
+
+    return joiner.join(vocab_words)
+
+def remove_suffix_ness(word):
+    '''
+
+    :param word: str of word to remvoe suffix from.
+    :return: str of word with suffix removed & spelling adjusted.
+
+    This function takes in a word and returns the base word with `ness` removed.
+    '''
+    word = word[:-4]
+    if word[-1] == 'i':
+        word = word[:-1] + 'y'
+
+    return word
+
+def noun_to_verb(sentence, index):
+    '''
+
+    :param sentence: str that uses the word in sentance
+    :param index:  index of the word to remove and transform
+    :return:  str word that changes the extracted adjective to a verb.
+
+    A function takes a `sentence` using the
+    vocabulary word, and the `index` of the word once that sentence
+    is split apart.  The function should return the extracted
+    adjective as a verb.
+    '''
+    word = sentence.split()[index]
+
+    if word[-1] == '.':
+        word = word[:-1] + 'en'
+    else:
+        word = word + 'en'
+
+    return word
