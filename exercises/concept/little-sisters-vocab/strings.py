88b7c2d0641b5f35aa8efeabfc47969935bfb8c5
BethanyG <bethanymgarcia@gmail.com>
2021-05-18 19:48:33 -0700

    Rewrote stub file and strings_test.py

---
+++ exercises/concept/little-sisters-vocab/strings.py
@@ -0,0 +1,47 @@
+def add_prefix_un(word):
+    '''
+
+    :param word: str of a root word
+    :return:  str of root word with un prefix
+
+    This function takes `word` as a parameter and
+    returns a new word with an 'un' prefix.
+    '''
+    pass
+
+def make_word_groups(vocab_words):
+    '''
+
+    :param vocab_words: list of vocabulary words with a prefix.
+    :return: str of prefix followed by vocabulary words with
+             prefix applied, separated by ' :: '.
+
+    This function takes a `vocab_words` list and returns a string
+    with the prefix  and the words with prefix applied, separated
+     by ' :: '.
+    '''
+    pass
+
+def remove_suffix_ness(word):
+    '''
+
+    :param word: str of word to remvoe suffix from.
+    :return: str of word with suffix removed & spelling adjusted.
+
+    This function takes in a word and returns the base word with `ness` removed.
+    '''
+    pass
+
+def noun_to_verb(sentence, index):
+    '''
+
+    :param sentence: str that uses the word in sentance
+    :param index:  index of the word to remove and transform
+    :return:  str word that changes the extracted adjective to a verb.
+
+    A function takes a `sentence` using the
+    vocabulary word, and the `index` of the word once that sentence
+    is split apart.  The function should return the extracted
+    adjective as a verb.
+    '''
+    pass
