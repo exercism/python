9de477222ed8e2d70222483c4feedac69d069c0a
BethanyG <bethanymgarcia@gmail.com>
2021-08-21 11:50:46 -0700

    Added stub, test, and test_data files.

---
+++ exercises/concept/cater-waiter/sets.py
@@ -0,0 +1,113 @@
+from sets_categories import (VEGAN,
+                            VEGETARIAN,
+                            KETO, PALEO,
+                            OMNIVORE,
+                            ALCOHOLS,
+                            SPECIAL_INGREDIENTS,
+                            VEGAN_INTERSECTIONS,
+                            VEGETARIAN_INTERSECTIONS,
+                            PALEO_INTERSECTIONS,
+                            KETO_INTERSECTIONS,
+                            OMNIVORE_INTERSECTIONS)
+
+
+def clean_ingredients(dish_name, dish_ingredients):
+    '''
+
+    :param dish_name: str
+    :param dish_ingredients: list
+    :return: tuple of (dish_name, ingredient set)
+
+    This function should return a `tuple` with the name of the dish as the first item,
+    followed by the de-duped `set` of ingredients as the second item.
+    '''
+
+    pass
+
+
+
+def check_drinks(drink_name, drink_ingredients):
+    '''
+
+    :param drink_name: str
+    :param drink_ingredients: list
+    :return: str drink name + ("Mocktail" or "Cocktail")
+
+    The function should return the name of the drink followed by "Mocktail" if the drink has
+    no alcoholic ingredients, and drink name followed by "Cocktail" if the drink includes alcohol.
+    '''
+
+    pass
+
+def categorize_dish(dish_name, dish_ingredients):
+    '''
+
+    :param dish_name: str
+    :param dish_ingredients: list
+    :return: str "dish name: CATEGORY"
+
+    This function should return a string with the `dish name: <CATEGORY>` (which meal category the dish belongs to).
+    All dishes will "fit" into one of the categories imported from `categories.py`
+    (VEGAN, VEGETARIAN, PALEO, KETO, or OMNIVORE).
+    '''
+
+    pass
+
+
+def tag_special_ingredients(dish):
+    '''
+
+    :param dish: tuple of (str of dish name, list of dish ingredients)
+    :return: tuple of (str of dish name, set of dish special ingredients)
+
+    Return the dish name followed by the `set` of ingredients that require a special note on the dish description.
+    For the purposes of this exercise, all allergens or special ingredients that need to be tracked are in the
+    SPECIAL_INGREDIENTS constant imported from `categories.py`.
+    '''
+
+    pass
+
+
+
+def compile_ingredients(dishes):
+    '''
+
+    :param dishes: list of dish ingredient sets
+    :return: set
+
+    This function should return a `set` of all ingredients from all listed dishes.
+    '''
+
+    pass
+
+
+
+def separate_appetizers(dishes, appetizers):
+    '''
+
+    :param dishes: list of dish names
+    :param appetizers: list of appetizer names
+    :return: list of dish names
+
+    The function should return the list of dish names with appetizer names removed.
+    Either list could contain duplicates and may require de-duping.
+    '''
+
+    pass
+
+
+
+def singleton_ingredients(intersection, dishes):
+    '''
+
+    :param intersection: constant - one of (VEGAN_INTERSECTION,VEGETARIAN_INTERSECTION,PALEO_INTERSECTION,
+                                            KETO_INTERSECTION,OMNIVORE_INTERSECTION)
+    :param dishes:  list of ingredient sets
+    :return: set of singleton ingredients
+
+    Each dish is represented by a `set` of its ingredients.
+    Each `<CATEGORY>_INTERSECTION` is an `intersection` of all dishes in the category.
+    The function should return a `set` of ingredients that only appear in a single dish.
+    '''
+
+    pass
---
+++ exercises/concept/cater-waiter/sets_test.py
@@ -0,0 +1,109 @@
+import unittest
+import pytest
+from sets_categories import (VEGAN,
+                             VEGETARIAN,
+                             KETO,
+                             PALEO,
+                             OMNIVORE,
+                             ALCOHOLS,
+                             SPECIAL_INGREDIENTS,
+                             VEGAN_INTERSECTIONS,
+                             VEGETARIAN_INTERSECTIONS,
+                             PALEO_INTERSECTIONS,
+                             KETO_INTERSECTIONS,
+                             OMNIVORE_INTERSECTIONS)
+
+from sets_test_data import (recipes_with_duplicates,
+                            recipes_without_duplicates,
+                            all_drinks,
+                            drink_names,
+                            dishes_categorized,
+                            dishes_to_special_label,
+                            dishes_labeled,
+                            ingredients_only,
+                            dishes_and_appetizers,
+                            dishes_cleaned,
+                            dishes_and_overlap,
+                            singletons)
+
+
+from exemplar import (clean_ingredients,
+                      check_drinks,
+                      categorize_dish,
+                      tag_special_ingredients,
+                      compile_ingredients,
+                      separate_appetizers,
+                      singleton_ingredients,)
+
+
+class SetsTest(unittest.TestCase):
+
+    @pytest.mark.task(taskno=1)
+    def test_clean_ingredients(self):
+        input_data =  recipes_with_duplicates
+        result_data = recipes_without_duplicates
+        number_of_variants = range(1, len(input_data) + 1)
+
+        for variant, item, result  in zip(number_of_variants, input_data, result_data):
+            with self.subTest(f"variation #{variant}", item=item, result=result):
+                self.assertEqual(clean_ingredients(item[0], item[1]), (result[1], result[2]))
+
+    @pytest.mark.task(taskno=2)
+    def test_check_drinks(self):
+        input_data = all_drinks
+        result_data = drink_names
+        number_of_variants = range(1, len(input_data) + 1)
+
+        for variant, item, result  in zip(number_of_variants, input_data, result_data):
+            with self.subTest(f"variation #{variant}", item=item, result=result):
+                self.assertEqual(check_drinks(item[0], item[1]), (result))
+
+    @pytest.mark.task(taskno=3)
+    def test_categorize_dish(self):
+        input_data = sorted(recipes_without_duplicates, reverse=True)
+        result_data = dishes_categorized
+        number_of_variants = range(1, len(input_data) + 1)
+
+        for variant, item, result  in zip(number_of_variants, input_data, result_data):
+            with self.subTest(f"variation #{variant}", item=item, result=result):
+                self.assertEqual(categorize_dish(item[1], item[2]), (result))
+
+    @pytest.mark.task(taskno=4)
+    def test_tag_special_ingredients(self):
+        input_data = dishes_to_special_label
+        result_data = dishes_labeled
+        number_of_variants = range(1, len(input_data) + 1)
+
+        for variant, item, result  in zip(number_of_variants, input_data, result_data):
+            with self.subTest(f"variation #{variant}", item=item, result=result):
+                self.assertEqual(tag_special_ingredients(item), (result))
+
+    @pytest.mark.task(taskno=5)
+    def test_compile_ingredients(self):
+        input_data = ingredients_only
+        result_data = [VEGAN, VEGETARIAN, PALEO, KETO, OMNIVORE]
+        number_of_variants = number_of_variants = range(1, len(input_data) + 1)
+
+        for variant, item, result  in zip(number_of_variants, input_data, result_data):
+            with self.subTest(f"variation #{variant}", item=item, result=result):
+                self.assertEqual(compile_ingredients(item), (result))
+
+    @pytest.mark.task(taskno=6)
+    def test_separate_appetizers(self):
+        input_data = dishes_and_appetizers
+        result_data = dishes_cleaned
+        number_of_variants = number_of_variants = range(1, len(input_data) + 1)
+
+        for variant, item, result  in zip(number_of_variants, input_data, result_data):
+            with self.subTest(f"variation #{variant}", item=item, result=result):
+                self.assertEqual(sorted(separate_appetizers(item[0], item[1])), (sorted(result)))
+
+    @pytest.mark.task(taskno=7)
+    def test_singleton_ingredients(self):
+        input_data = dishes_and_overlap
+        result_data = singletons
+        number_of_variants = number_of_variants = range(1, len(input_data) + 1)
+
+        for variant, item, result  in zip(number_of_variants, input_data, result_data):
+            with self.subTest(f"variation #{variant}", item=item, result=result):
+                self.assertEqual(singleton_ingredients(item[0], item[1]), (result))
