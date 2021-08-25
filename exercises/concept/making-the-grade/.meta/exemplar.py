9a6594f755292bfefdbb2ccda0a4c112c788f3dc
BethanyG <bethanymgarcia@gmail.com>
2021-08-25 14:06:26 -0700

    Cleaned up formatting and spelling.

--- exercises/concept/making-the-grade/.meta/exemplar.py
+++ exercises/concept/making-the-grade/.meta/exemplar.py
@@ -4,12 +4,14 @@ def round_scores(student_scores):
         rounded.append(round(student_scores.pop()))
     return rounded
 
+
 def count_failed_students(student_scores):
     non_passing = 0
     for score in student_scores:
         if score <= 40:
             non_passing += 1
     return non_passing
+
 
 def above_threshold(student_scores, threshold):
     above_threshold = []
@@ -18,12 +20,14 @@ def above_threshold(student_scores, threshold):
             above_threshold.append(score)
     return above_threshold
 
+
 def letter_grades(highest):
     increment = round((highest - 40)/4)
     scores = []
     for score in range(41, highest, increment):
         scores.append(score)
     return scores
+
 
 def student_ranking(student_scores, student_names):
     results = []
@@ -32,6 +36,7 @@ def student_ranking(student_scores, student_names):
         results.append(rank_string)
         #results.append(f"{index + 1}. {name}: {student_scores[index]}")
     return results
+
 
 def perfect_score(student_info):
     result = "No perfect score."
