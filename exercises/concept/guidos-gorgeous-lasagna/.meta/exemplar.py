72aaa9b784967a9ff8a5a24555dfe7125b671e9c
BethanyG <bethanymgarcia@gmail.com>
2021-08-26 08:43:54 -0700

    Added more formal and verbose docstrings to exemplar for this exercise.

--- exercises/concept/guidos-gorgeous-lasagna/.meta/exemplar.py
+++ exercises/concept/guidos-gorgeous-lasagna/.meta/exemplar.py
@@ -4,29 +4,39 @@ PREPARATION_TIME = 2
 
 
 def bake_time_remaining(elapsed_bake_time):
-    '''Return remaining bake time in minutes.
-
-    This function takes an integer representing the elapsed bake time and
-    returns the bake time remaining for the lasagna based on the EXPECTED_BAKE_TIME.
     '''
+   :param elapsed_bake_time: int baking time already elapsed
+   :return: int remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'
+
+   Function that takes the actual minutes the lasagna has been in the oven as
+   an argument and returns how many minutes the lasagna still needs to bake
+   based on the `EXPECTED_BAKE_TIME`.
+   '''
 
     return EXPECTED_BAKE_TIME - elapsed_bake_time
 
 
 def preparation_time_in_minutes(number_of_layers):
-    '''Return preparation time calculation in minutes.
+    '''
+
+    :param number_of_layers: int the number of lasagna layers made
+    :return: int amount of prep time (in minutes), based on 2 minutes per layer added
 
     This function takes an integer representing the number of layers added to the dish,
-    calculating the preparation time using a time of 2 minutes per layer added.
+    calculating preparation time using a time of 2 minutes per layer added.
     '''
 
     return number_of_layers * PREPARATION_TIME
 
 
 def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
-    '''Return elapsed cooking time.
+    '''
 
-    This function takes two integers representing te number of layers and the time already spent baking
+    :param number_of_layers: int the number of layers in the lasagna
+    :param elapsed_bake_time: int elapsed cooking time
+    :return:  int total time elapsed (in in minutes) preparing and cooking
+
+    This function takes two integers representing the number of lasagna layers and the time already spent baking
      and calculates the total elapsed minutes spent cooking the lasagna.
     '''
 
