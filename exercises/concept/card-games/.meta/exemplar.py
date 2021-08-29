d85076496a2b985187e40950ab88879dff2cc58d
BethanyG <bethanymgarcia@gmail.com>
2021-08-29 10:33:42 -0700

    Added formal docstrings to exemplar file and corrected code errors.

--- exercises/concept/card-games/.meta/exemplar.py
+++ exercises/concept/card-games/.meta/exemplar.py
@@ -1,20 +1,42 @@
 def get_rounds(number):
-    """Return a list of rounds that includes `number` and the next two."""
+    '''
+
+     :param number: int - current round number.
+     :return: list - current round and the two that follow.
+    '''
+
     return [number, number + 1, number + 2]
 
 
-def concatenate_rounds(rounds1, rounds2):
-    """Return a list of rounds that combines `rounds1` and `rounds2`."""
+def concatenate_rounds(rounds_1, rounds_2):
+    '''
+
+    :param rounds_1: list - first rounds played.
+    :param rounds_2: list - second set of rounds played.
+    :return: list - all rounds played.
+    '''
+
     return rounds1 + rounds2
 
 
 def list_contains_round(rounds, number):
-    """Return whether `rounds` includes the `number` round."""
+    '''
+
+    :param rounds: list - rounds played.
+    :param number: int - round number.
+    :return:  bool - was the round played?
+    '''
+
     return number in rounds
 
 
 def card_average(hand):
-    """Return the average of the hand."""
+    '''
+
+    :param hand: list - cards in hand.
+    :return:  int - average value of the cards in the hand.
+    '''
+
     total = 0
     count = 0
     for card in hand:
@@ -24,16 +46,33 @@ def card_average(hand):
 
 
 def approx_average_is_average(hand):
-    """Return whether the average of the first and last card is the average."""
+    '''
+
+    :param hand: list - cards in hand.
+    :return: int - approximate average value of the cards in the hand.
+    '''
+
     return card_average([hand[0], hand[-1]]) == card_average(hand)
 
 
 def average_even_is_average_odd(hand):
-    """Return whether the average of the even cards is the same as that odd cards."""
+    '''
+
+    :param hand: list - cards in hand.
+    :return: int - approximate average value of the cards in the hand.
+    '''
+
     return card_average(hand[::2]) == card_average(hand[1::2])
 
 
 def maybe_double_last(hand):
-    """Double the value of the last card if it is a Jack (11)."""
+    '''
+
+    :param hand: list - cards in hand.
+    :return: list - hand with Jacks (if present) value doubled.
+    '''
+
     if hand[-1] == 11:
       hand[-1] *= 2
+    return hand
+
