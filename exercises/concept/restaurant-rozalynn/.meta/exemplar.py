fd9b7bf3367777d9bb76851028fcb7b4e4088d96
BethanyG <bethanymgarcia@gmail.com>
2021-09-01 02:11:57 -0700

    Added docstrings to exemplars and stubs.  Changed some exemplar code to conform to Pylint nags.

--- exercises/concept/restaurant-rozalynn/.meta/exemplar.py
+++ exercises/concept/restaurant-rozalynn/.meta/exemplar.py
@@ -1,43 +1,87 @@
+
 def new_seating_chart(size=22):
-    seats = dict()
-    for number in range(1, size+1):
-        seats[number] = None
-    return seats
+    '''
+
+    :param size: int - number if seats in the seating chart.
+    :return: dict - with number of seats specified, and placeholder values.
+    '''
+
+    return {number: None for number in range(1, size+1) }
+
 
 def arrange_reservations(guests=None):
-    if guests is None:
-        return new_seating_chart()
-    else:
-        seats = new_seating_chart()
+    '''
+
+    :param guest_list: list - list of guest names for reservations.
+    :return: dict - Default sized dictionary with guests assigned seats,
+             and placeholders for empty seats.
+    '''
+
+    seats = new_seating_chart()
+
+    if guests:
         for seat_number in range(1, len(guests)):
             seats[seat_number] = guests[seat_number]
     return seats
 
+
 def find_all_available_seats(seats):
+    '''
+
+    :param seats: dict - seating chart.
+    :return: list - list of seat numbers available for reserving..
+    '''
+
     available = []
     for seat_num, value in seats.items():
         if value is None:
             available.append(seat_num)
     return available
 
+
 def curr_empty_seat_capacity(seats):
+    '''
+
+    :param seats: dict - dictionary of reserved seats.
+    :return: int - number of seats empty.
+    '''
+
     count = 0
     for value in seats.values():
         if value is None:
             count += 1
     return count
 
+
 def accommodate_waiting_guests(seats, guests):
+    '''
+
+    :param seats: dict - seating chart dictionary.
+    :param guests: list - walk-in guests
+    :return: dict - updated seating chart with available spaces filled.
+    '''
+
     curr_empty_seats = curr_empty_seat_capacity(seats)
     empty_seat_list = find_all_available_seats(seats)
+
     if len(guests) > curr_empty_seats:
         return False
     else:
-        for index in range(len(guests)):
+        for index, item in enumerate(guests):
             seats[empty_seat_list[index]] = guests[index]
+
     return seats
 
+
 def empty_seats(seats, seat_numbers):
-   for seat in seat_numbers:
-      seats[seat] = None
-   return seats
+    '''
+
+    :param seats: dict - seating chart dictionary.
+    :param seat_numbers: list - list of seat numbers to free up or empty.
+    :return: updated seating chart dictionary.
+    '''
+
+    for seat in seat_numbers:
+        seats[seat] = None
+
+    return seats
--- exercises/concept/tisbury-treasure-hunt/.meta/exemplar.py
+++ exercises/concept/tisbury-treasure-hunt/.meta/exemplar.py
@@ -1,21 +1,58 @@
 def get_coordinate(record):
+    '''
+
+    :param record: tuple - a (treasure, coordinate) pair.
+    :return: str - the extracted map coordinate.
+    '''
+
     return record[1]
 
+
 def convert_coordinate(coordinate):
+    '''
+
+    :param coordinate: str - a string map coordinate
+    :return:  tuple - the string coordinate seperated into its individual components.
+    '''
+
     return tuple(coordinate)
 
+
 def compare_records(azara_record, rui_record):
+    '''
+
+    :param azara_record: tuple - a (treasure, coordinate) pair.
+    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
+    :return: bool - True if coordinates match, False otherwise.
+    '''
+
     return convert_coordinate(azara_record[1]) in rui_record
 
+
 def create_record(azara_record, rui_record):
+    '''
+
+    :param azara_record: tuple - a (treasure, coordinate) pair.
+    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
+    :return:  tuple - combined record, or "not a match" if the records are incompatible.
+    '''
+
     if compare_records(azara_record, rui_record):
         return azara_record + rui_record
     else:
         return "not a match"
 
+
 def clean_up(combined_record_group):
+    '''
+
+    :param combined_record_group: tuple of tuples - everything from both participants.
+    :return: tuple of tuples - everything "cleaned", with excess coordinates and information removed.
+    '''
 
     report = ""
+
     for item in combined_record_group:
-       report += f"{(item[0], item[2], item[3], item[4])}\n"
+        report += f"{(item[0], item[2], item[3], item[4])}\n"
+
     return report
