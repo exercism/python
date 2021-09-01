c3c5aceb1533b3d492d6c3d14ca1540a4c08aa7d
BethanyG <bethanymgarcia@gmail.com>
2021-08-31 23:11:15 -0700

    Removed solution code from stub file.  Added lines below docstrings.

--- exercises/concept/inventory-management/.meta/exemplar.py
+++ exercises/concept/inventory-management/.meta/exemplar.py
@@ -4,6 +4,7 @@ def create_inventory(items):
     :param items: list - list of items to create an inventory from.
     :return:  dict - the inventory dictionary.
     '''
+
     inventory = dict()
     add_items(inventory, items)
     return inventory
@@ -16,6 +17,7 @@ def add_items(inventory, items):
     :param items: list - list of items to update the inventory with.
     :return:  dict - the inventory dictionary update with the new items.
     '''
+
     for item in items:
         inventory.setdefault(item, 0)
         inventory[item] += 1
@@ -29,6 +31,7 @@ def delete_items(inventory, items):
     :param items: list - list of items to remove from the inventory.
     :return:  dict - updated inventory dictionary with items removed.
     '''
+
     for item in items:
         if item in inventory:
             inventory[item] = max(inventory[item] - 1, 0)
@@ -41,6 +44,7 @@ def list_inventory(inventory):
     :param inventory: dict - an inventory dictionary.
     :return: list of tuples - list of key, value pairs from the inventory dictionary.
     '''
+
     output = list()
     for item in sorted(inventory.items()):
         if item[1] > 0:
