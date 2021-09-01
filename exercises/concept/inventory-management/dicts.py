7bd21aef055fd83f9078812a407f22cc20c81721
BethanyG <bethanymgarcia@gmail.com>
2021-08-31 23:11:15 -0700

    Removed solution code from stub file.  Added lines below docstrings.

--- exercises/concept/inventory-management/dicts.py
+++ exercises/concept/inventory-management/dicts.py
@@ -1,52 +1,11 @@
-# def create_inventory(items):
-#     '''
-#
-#     :param items: list - list of items to create an inventory from.
-#     :return:  dict - the inventory dictionary.
-#     '''
-#
-#     pass
-#
-#
-# def add_items(inventory, items):
-#     '''
-#
-#     :param inventory: dict - dictionary of existing inventory.
-#     :param items: list - list of items to update the inventory with.
-#     :return:  dict - the inventory dictionary update with the new items.
-#     '''
-#
-#     pass
-#
-#
-# def delete_items(inventory, items):
-#     '''
-#
-#     :param inventory: dict - inventory dictionary.
-#     :param items: list - list of items to remove from the inventory.
-#     :return:  dict - updated inventory dictionary with items removed.
-#     '''
-#
-#     pass
-#
-#
-# def list_inventory(inventory):
-#     '''
-#
-#     :param inventory: dict - an inventory dictionary.
-#     :return: list of tuples - list of key, value pairs from the inventory dictionary.
-#     '''
-#
-#     pass
 def create_inventory(items):
     '''
 
     :param items: list - list of items to create an inventory from.
     :return:  dict - the inventory dictionary.
     '''
-    inventory = dict()
-    add_items(inventory, items)
-    return inventory
+
+    pass
 
 
 def add_items(inventory, items):
@@ -56,10 +15,8 @@ def add_items(inventory, items):
     :param items: list - list of items to update the inventory with.
     :return:  dict - the inventory dictionary update with the new items.
     '''
-    for item in items:
-        inventory.setdefault(item, 0)
-        inventory[item] += 1
-    return inventory
+
+    pass
 
 
 def delete_items(inventory, items):
@@ -69,10 +26,8 @@ def delete_items(inventory, items):
     :param items: list - list of items to remove from the inventory.
     :return:  dict - updated inventory dictionary with items removed.
     '''
-    for item in items:
-        if item in inventory:
-            inventory[item] = max(inventory[item] - 1, 0)
-    return inventory
+
+    pass
 
 
 def list_inventory(inventory):
@@ -81,8 +36,5 @@ def list_inventory(inventory):
     :param inventory: dict - an inventory dictionary.
     :return: list of tuples - list of key, value pairs from the inventory dictionary.
     '''
-    output = list()
-    for item in sorted(inventory.items()):
-        if item[1] > 0:
-            output.append(item)
-    return output
+
+    pass
