b4ed2fcb44289f0a10f24660e2a6e5eb56bc24c0
BethanyG <bethanymgarcia@gmail.com>
2021-08-31 17:40:34 -0700

    Updated exemplar code git status

--- exercises/concept/cater-waiter/sets.py
+++ exercises/concept/cater-waiter/sets.py
@@ -1,3 +1,120 @@
+# from sets_categories_data import (VEGAN,
+#                                   VEGETARIAN,
+#                                   KETO,
+#                                   PALEO,
+#                                   OMNIVORE,
+#                                   ALCOHOLS,
+#                                   SPECIAL_INGREDIENTS,
+#                                   VEGAN_INTERSECTIONS,
+#                                   VEGETARIAN_INTERSECTIONS,
+#                                   PALEO_INTERSECTIONS,
+#                                   KETO_INTERSECTIONS,
+#                                   OMNIVORE_INTERSECTIONS)
+#
+#
+# def clean_ingredients(dish_name, dish_ingredients):
+#     '''
+#
+#     :param dish_name: str
+#     :param dish_ingredients: list
+#     :return: tuple of (dish_name, ingredient set)
+#
+#     This function should return a `tuple` with the name of the dish as the first item,
+#     followed by the de-duped `set` of ingredients as the second item.
+#     '''
+#
+#     pass
+#
+#
+#
+# def check_drinks(drink_name, drink_ingredients):
+#     '''
+#
+#     :param drink_name: str
+#     :param drink_ingredients: list
+#     :return: str drink name + ("Mocktail" or "Cocktail")
+#
+#     The function should return the name of the drink followed by "Mocktail" if the drink has
+#     no alcoholic ingredients, and drink name followed by "Cocktail" if the drink includes alcohol.
+#     '''
+#
+#     pass
+#
+# def categorize_dish(dish_name, dish_ingredients):
+#     '''
+#
+#     :param dish_name: str
+#     :param dish_ingredients: list
+#     :return: str "dish name: CATEGORY"
+#
+#     This function should return a string with the `dish name: <CATEGORY>` (which meal category the dish belongs to).
+#     All dishes will "fit" into one of the categories imported from `categories.py`
+#     (VEGAN, VEGETARIAN, PALEO, KETO, or OMNIVORE).
+#     '''
+#
+#     pass
+#
+#
+# def tag_special_ingredients(dish):
+#     '''
+#
+#     :param dish: tuple of (str of dish name, list of dish ingredients)
+#     :return: tuple of (str of dish name, set of dish special ingredients)
+#
+#     Return the dish name followed by the `set` of ingredients that require a special note on the dish description.
+#     For the purposes of this exercise, all allergens or special ingredients that need to be tracked are in the
+#     SPECIAL_INGREDIENTS constant imported from `categories.py`.
+#     '''
+#
+#     pass
+#
+#
+#
+# def compile_ingredients(dishes):
+#     '''
+#
+#     :param dishes: list of dish ingredient sets
+#     :return: set
+#
+#     This function should return a `set` of all ingredients from all listed dishes.
+#     '''
+#
+#     pass
+#
+#
+#
+# def separate_appetizers(dishes, appetizers):
+#     '''
+#
+#     :param dishes: list of dish names
+#     :param appetizers: list of appetizer names
+#     :return: list of dish names
+#
+#     The function should return the list of dish names with appetizer names removed.
+#     Either list could contain duplicates and may require de-duping.
+#     '''
+#
+#     pass
+#
+#
+#
+# def singleton_ingredients(intersection, dishes):
+#     '''
+#
+#     :param intersection: constant - one of (VEGAN_INTERSECTION,VEGETARIAN_INTERSECTION,PALEO_INTERSECTION,
+#                                             KETO_INTERSECTION,OMNIVORE_INTERSECTION)
+#     :param dishes:  list of ingredient sets
+#     :return: set of singleton ingredients
+#
+#     Each dish is represented by a `set` of its ingredients.
+#     Each `<CATEGORY>_INTERSECTION` is an `intersection` of all dishes in the category.
+#     The function should return a `set` of ingredients that only appear in a single dish.
+#     '''
+#
+#     pass
+"""
+docstring to make pylint happier
+"""
 from sets_categories_data import (VEGAN,
                                   VEGETARIAN,
                                   KETO,
@@ -23,7 +140,7 @@ def clean_ingredients(dish_name, dish_ingredients):
     followed by the de-duped `set` of ingredients as the second item.
     '''
 
-    pass
+    return dish_name, set(dish_ingredients)
 
 
 
@@ -38,7 +155,11 @@ def check_drinks(drink_name, drink_ingredients):
     no alcoholic ingredients, and drink name followed by "Cocktail" if the drink includes alcohol.
     '''
 
-    pass
+    if not ALCOHOLS.isdisjoint(drink_ingredients):
+        return drink_name + ' Cocktail'
+
+    return drink_name + ' Mocktail'
+
 
 def categorize_dish(dish_name, dish_ingredients):
     '''
@@ -52,7 +173,16 @@ def categorize_dish(dish_name, dish_ingredients):
     (VEGAN, VEGETARIAN, PALEO, KETO, or OMNIVORE).
     '''
 
-    pass
+    categories = ((VEGAN,'VEGAN'),
+                  (VEGETARIAN,'VEGETARIAN'),
+                  (KETO,'KETO'),
+                  (PALEO,'PALEO'),
+                  (OMNIVORE,'OMNIVORE'))
+
+    for category in categories:
+        if set(dish_ingredients) <= category[0]:
+            return dish_name + ': ' + category[1]
+    return None
 
 
 def tag_special_ingredients(dish):
@@ -66,7 +196,7 @@ def tag_special_ingredients(dish):
     SPECIAL_INGREDIENTS constant imported from `categories.py`.
     '''
 
-    pass
+    return dish[0], (SPECIAL_INGREDIENTS & set(dish[1]))
 
 
 
@@ -79,7 +209,12 @@ def compile_ingredients(dishes):
     This function should return a `set` of all ingredients from all listed dishes.
     '''
 
-    pass
+    combined_ingredients = set()
+
+    for ingredients in dishes:
+        combined_ingredients = combined_ingredients.union(ingredients)
+
+    return combined_ingredients
 
 
 
@@ -94,8 +229,7 @@ def separate_appetizers(dishes, appetizers):
     Either list could contain duplicates and may require de-duping.
     '''
 
-    pass
-
+    return list(set(dishes) - set(appetizers))
 
 
 def singleton_ingredients(intersection, dishes):
@@ -111,4 +245,9 @@ def singleton_ingredients(intersection, dishes):
     The function should return a `set` of ingredients that only appear in a single dish.
     '''
 
-    pass
+    all_ingredients = set()
+
+    for ingredients in dishes:
+        all_ingredients = all_ingredients ^ ingredients
+
+    return all_ingredients - intersection
