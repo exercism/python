fbe1c906ac8c58ca35aacb745a6f582ac6f79d1f
BethanyG <bethanymgarcia@gmail.com>
2021-08-31 23:03:37 -0700

    Added docstrings to exemplar and stub files.

--- exercises/concept/inventory-management/.meta/exemplar.py
+++ exercises/concept/inventory-management/.meta/exemplar.py
@@ -1,10 +1,21 @@
 def create_inventory(items):
+    '''
+
+    :param items: list - list of items to create an inventory from.
+    :return:  dict - the inventory dictionary.
+    '''
     inventory = dict()
     add_items(inventory, items)
     return inventory
 
 
 def add_items(inventory, items):
+    '''
+
+    :param inventory: dict - dictionary of existing inventory.
+    :param items: list - list of items to update the inventory with.
+    :return:  dict - the inventory dictionary update with the new items.
+    '''
     for item in items:
         inventory.setdefault(item, 0)
         inventory[item] += 1
@@ -12,6 +23,12 @@ def add_items(inventory, items):
 
 
 def delete_items(inventory, items):
+    '''
+
+    :param inventory: dict - inventory dictionary.
+    :param items: list - list of items to remove from the inventory.
+    :return:  dict - updated inventory dictionary with items removed.
+    '''
     for item in items:
         if item in inventory:
             inventory[item] = max(inventory[item] - 1, 0)
@@ -19,6 +36,11 @@ def delete_items(inventory, items):
 
 
 def list_inventory(inventory):
+    '''
+
+    :param inventory: dict - an inventory dictionary.
+    :return: list of tuples - list of key, value pairs from the inventory dictionary.
+    '''
     output = list()
     for item in sorted(inventory.items()):
         if item[1] > 0:
