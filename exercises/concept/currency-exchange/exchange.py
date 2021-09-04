6bcef9cdb3e690bd76125798918cc0a9450a972e
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-09-04 11:41:21 +0500

    Updated all <concept_name>.py docstrings
    Updated all exercises/concept/<exercise-name>/<concept-name>.py docstrings from single to double triple quotes

--- exercises/concept/currency-exchange/exchange.py
+++ exercises/concept/currency-exchange/exchange.py
@@ -1,54 +1,54 @@
 def estimate_value(budget, exchange_rate):
-    '''
+    """
 
     :param budget: float - amount of money you are planning to exchange.
     :param exchange_rate: float - unit value of the foreign currency.
     :return:
-    '''
+    """
 
     pass
 
 def get_change(budget, exchanging_value):
-    '''
+    """
 
     :param budget: float - amount of money you own.
     :param exchanging_value: int - amount of your money you want to exchange now.
     :return:
-    '''
+    """
 
     pass
 
 
 def get_value(denomination, number_of_bills):
-    '''
+    """
 
     :param denomination: int - the value of a bill.
     :param number_of_bills: int amount of bills you received.
     :return:
-    '''
+    """
 
     pass
 
 
 def get_number_of_bills(budget, denomination):
-    '''
+    """
 
     :param budget: float - the amount of money you are planning to exchange.
     :param denomination: int - the value of a single bill.
     :return:
-    '''
+    """
 
     pass
 
 
 def exchangeable_value(budget, exchange_rate, spread, denomination):
-    '''
+    """
 
     :param budget: float - the amount of your money you are planning to exchange.
     :param exchange_rate: float - the unit value of the foreign currency.
     :param spread: int - percentage that is taken as an exchange fee.
     :param denomination: int - the value of a single bill.
     :return:
-    '''
+    """
 
     pass
--- exercises/concept/inventory-management/dicts.py
+++ exercises/concept/inventory-management/dicts.py
@@ -1,40 +1,40 @@
 def create_inventory(items):
-    '''
+    """
 
     :param items: list - list of items to create an inventory from.
     :return:  dict - the inventory dictionary.
-    '''
+    """
 
     pass
 
 
 def add_items(inventory, items):
-    '''
+    """
 
     :param inventory: dict - dictionary of existing inventory.
     :param items: list - list of items to update the inventory with.
     :return:  dict - the inventory dictionary update with the new items.
-    '''
+    """
 
     pass
 
 
 def delete_items(inventory, items):
-    '''
+    """
 
     :param inventory: dict - inventory dictionary.
     :param items: list - list of items to remove from the inventory.
     :return:  dict - updated inventory dictionary with items removed.
-    '''
+    """
 
     pass
 
 
 def list_inventory(inventory):
-    '''
+    """
 
     :param inventory: dict - an inventory dictionary.
     :return: list of tuples - list of key, value pairs from the inventory dictionary.
-    '''
+    """
 
     pass
