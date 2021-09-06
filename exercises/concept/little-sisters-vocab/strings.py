c1fb1599db0e1f33d88114fcfd8c24b81e2ce1e4
BethanyG <bethanymgarcia@gmail.com>
2021-09-05 19:31:44 -0700

    Changed docstrings to use double quotes.  Used double lines between functions.  Other msc. edits for linting.

--- exercises/concept/little-sisters-vocab/strings.py
+++ exercises/concept/little-sisters-vocab/strings.py
@@ -1,17 +1,18 @@
 def add_prefix_un(word):
-    '''
+    """
 
     :param word: str of a root word
     :return:  str of root word with un prefix
 
     This function takes `word` as a parameter and
     returns a new word with an 'un' prefix.
-    '''
+    """
 
     pass
 
+
 def make_word_groups(vocab_words):
-    '''
+    """
 
     :param vocab_words: list of vocabulary words with a prefix.
     :return: str of prefix followed by vocabulary words with
@@ -20,23 +21,25 @@ def make_word_groups(vocab_words):
     This function takes a `vocab_words` list and returns a string
     with the prefix  and the words with prefix applied, separated
      by ' :: '.
-    '''
+    """
 
     pass
 
+
 def remove_suffix_ness(word):
-    '''
+    """
 
     :param word: str of word to remove suffix from.
     :return: str of word with suffix removed & spelling adjusted.
 
     This function takes in a word and returns the base word with `ness` removed.
-    '''
+    """
 
     pass
 
+
 def noun_to_verb(sentence, index):
-    '''
+    """
 
     :param sentence: str that uses the word in sentence
     :param index:  index of the word to remove and transform
@@ -46,6 +49,6 @@ def noun_to_verb(sentence, index):
     vocabulary word, and the `index` of the word once that sentence
     is split apart.  The function should return the extracted
     adjective as a verb.
-    '''
+    """
 
     pass
