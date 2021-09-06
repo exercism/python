4d1eb939f0303c71e1444bda41a5735c0edadd37
BethanyG <bethanymgarcia@gmail.com>
2021-09-05 19:32:03 -0700

    Changed docstrings to use double quotes.  Used double lines between functions.  Other msc. edits for linting.

--- exercises/concept/little-sisters-essay/string_methods_test.py
+++ exercises/concept/little-sisters-essay/string_methods_test.py
@@ -28,19 +28,19 @@ class TestStringMethods(unittest.TestCase):
     @pytest.mark.task(taskno=3)
     def test_remove_extra_spaces_only_start(self):
         self.assertEqual(clean_up_spacing("  A rolling stone gathers no moss"),
-                        "A rolling stone gathers no moss")
+                         "A rolling stone gathers no moss")
 
     @pytest.mark.task(taskno=3)
     def test_remove_extra_spaces(self):
         self.assertEqual(clean_up_spacing("  Elephants can't jump.  "),
-                        "Elephants can't jump.")
+                         "Elephants can't jump.")
 
     @pytest.mark.task(taskno=4)
     def test_replace_word_choice(self):
         self.assertEqual(replace_word_choice("Animals are cool.", "cool", "awesome"),
-                        "Animals are awesome.")
+                         "Animals are awesome.")
 
     @pytest.mark.task(taskno=4)
     def test_replace_word_not_exist(self):
         self.assertEqual(replace_word_choice("Animals are cool.", "small", "tiny"),
-                        "Animals are cool.")
+                         "Animals are cool.")
