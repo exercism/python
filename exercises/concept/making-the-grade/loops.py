8f22608e16ba37afe7ee75d4af163d8ef64636e3
BethanyG <bethanymgarcia@gmail.com>
2021-09-05 19:31:21 -0700

    Changed docstrings to use double quotes.  Used double lines between functions.  Other msc. edits for linting.

--- exercises/concept/making-the-grade/loops.py
+++ exercises/concept/making-the-grade/loops.py
@@ -1,48 +1,53 @@
 def round_scores(student_scores):
-    '''
+    """
     :param student_scores: list of student exam scores as float or int.
     :return: list of student scores *rounded* to nearest integer value.
-    '''
+    """
 
     pass
+
 
 def count_failed_students(student_scores):
-    '''
+    """
     :param student_scores: list of integer student scores.
     :return: integer count of student scores at or below 40.
-    '''
+    """
 
     pass
 
+
 def above_threshold(student_scores, threshold):
-    '''
+    """
     :param student_scores: list of integer scores
     :param threshold :  integer
     :return: list of integer scores that are at or above the "best" threshold.
-    '''
+    """
 
     pass
+
 
 def letter_grades(highest):
-    '''
+    """
     :param highest: integer of highest exam score.
     :return: list of integer score thresholds for each F-A letter grades.
-    '''
+    """
 
     pass
 
+
 def student_ranking(student_scores, student_names):
-    '''
+    """
      :param student_scores: list of scores in descending order.
      :param student_names: list of names in descending order by exam score.
      :return: list of strings in format ["<rank>. <student name>: <score>"].
-     '''
+     """
 
     pass
 
+
 def perfect_score(student_info):
-    '''
+    """
     :param student_info: list of [<student name>, <score>] lists
     :return: First [<student name>, 100] found OR "No perfect score."
-    '''
+    """
     pass
