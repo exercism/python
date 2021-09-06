12ba1a8d449c9870b28273b7d337a80baf776450
BethanyG <bethanymgarcia@gmail.com>
2021-09-05 19:30:58 -0700

    Changed docstrings to use double quotes.  Used double lines between functions.  Other msc. edits for linting.

--- exercises/concept/tisbury-treasure-hunt/.meta/exemplar.py
+++ exercises/concept/tisbury-treasure-hunt/.meta/exemplar.py
@@ -1,54 +1,55 @@
 def get_coordinate(record):
-    '''
+    """
 
     :param record: tuple - a (treasure, coordinate) pair.
     :return: str - the extracted map coordinate.
-    '''
+    """
 
     return record[1]
 
 
 def convert_coordinate(coordinate):
-    '''
+    """
 
     :param coordinate: str - a string map coordinate
     :return:  tuple - the string coordinate seperated into its individual components.
-    '''
+    """
 
     return tuple(coordinate)
 
 
 def compare_records(azara_record, rui_record):
-    '''
+    """
 
     :param azara_record: tuple - a (treasure, coordinate) pair.
     :param rui_record: tuple - a (location, coordinate, quadrant) trio.
     :return: bool - True if coordinates match, False otherwise.
-    '''
+    """
 
     return convert_coordinate(azara_record[1]) in rui_record
 
 
 def create_record(azara_record, rui_record):
-    '''
+    """
 
     :param azara_record: tuple - a (treasure, coordinate) pair.
     :param rui_record: tuple - a (location, coordinate, quadrant) trio.
     :return:  tuple - combined record, or "not a match" if the records are incompatible.
-    '''
+    """
+    result = "not a match"
 
     if compare_records(azara_record, rui_record):
-        return azara_record + rui_record
-    else:
-        return "not a match"
+        result = azara_record + rui_record
+
+    return result
 
 
 def clean_up(combined_record_group):
-    '''
+    """
 
     :param combined_record_group: tuple of tuples - everything from both participants.
-    :return: tuple of tuples - everything "cleaned", with excess coordinates and information removed.
-    '''
+    :return: tuple of tuples - everything "cleaned". Excess coordinates and information removed.
+    """
 
     report = ""
 
