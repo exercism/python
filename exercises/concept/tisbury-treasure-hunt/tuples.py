14ffda9ff8fd4e8cc5e4700ec3600d9631c54129
BethanyG <bethanymgarcia@gmail.com>
2021-09-05 19:30:58 -0700

    Changed docstrings to use double quotes.  Used double lines between functions.  Other msc. edits for linting.

--- exercises/concept/tisbury-treasure-hunt/tuples.py
+++ exercises/concept/tisbury-treasure-hunt/tuples.py
@@ -1,46 +1,50 @@
 def get_coordinate(record):
-    '''
+    """
 
     :param record: tuple - a (treasure, coordinate) pair.
     :return: str - the extracted map coordinate.
-    '''
+    """
 
     pass
 
+
 def convert_coordinate(coordinate):
-    '''
+    """
 
     :param coordinate: str - a string map coordinate
     :return:  tuple - the string coordinate seperated into its individual components.
-    '''
+    """
 
     pass
 
+
 def compare_records(azara_record, rui_record):
-    '''
+    """
 
     :param azara_record: tuple - a (treasure, coordinate) pair.
     :param rui_record: tuple - a (location, coordinate, quadrant) trio.
     :return: bool - True if coordinates match, False otherwise.
-    '''
+    """
 
     pass
 
+
 def create_record(azara_record, rui_record):
-    '''
+    """
 
     :param azara_record: tuple - a (treasure, coordinate) pair.
     :param rui_record: tuple - a (location, coordinate, quadrant) trio.
     :return:  tuple - combined record, or "not a match" if the records are incompatible.
-    '''
+    """
 
     pass
 
+
 def clean_up(combined_record_group):
-    '''
+    """
 
     :param combined_record_group: tuple of tuples - everything from both participants.
-    :return: tuple of tuples - everything "cleaned", with excess coordinates and information removed.
-    '''
+    :return: tuple of tuples - everything "cleaned". Excess coordinates and information removed.
+    """
 
     pass
--- exercises/concept/tisbury-treasure-hunt/tuples_test.py
+++ exercises/concept/tisbury-treasure-hunt/tuples_test.py
@@ -8,23 +8,23 @@ class TuplesTest(unittest.TestCase):
     @pytest.mark.task(taskno=1)
     def test_get_coordinate(self):
         input_data = [("Scrimshaw Whale's Tooth", '2A'),
-                                ('Brass Spyglass', '4B'),
-                                ('Robot Parrot', '1C'),
-                                ('Glass Starfish', '6D'),
-                                ('Vintage Pirate Hat', '7E'),
-                                ('Pirate Flag', '7F'),
-                                ('Crystal Crab', '6A'),
-                                ('Model Ship in Large Bottle', '8A'),
-                                ('Angry Monkey Figurine', '5B'),
-                                ('Carved Wooden Elephant', '8C'),
-                                ('Amethyst  Octopus', '1F'),
-                                ('Antique Glass Fishnet Float', '3D'),
-                                ('Silver Seahorse', '4E')]
+                      ('Brass Spyglass', '4B'),
+                      ('Robot Parrot', '1C'),
+                      ('Glass Starfish', '6D'),
+                      ('Vintage Pirate Hat', '7E'),
+                      ('Pirate Flag', '7F'),
+                      ('Crystal Crab', '6A'),
+                      ('Model Ship in Large Bottle', '8A'),
+                      ('Angry Monkey Figurine', '5B'),
+                      ('Carved Wooden Elephant', '8C'),
+                      ('Amethyst  Octopus', '1F'),
+                      ('Antique Glass Fishnet Float', '3D'),
+                      ('Silver Seahorse', '4E')]
 
         result_data = ['2A', '4B', '1C', '6D', '7E', '7F', '6A', '8A', '5B', '8C', '1F', '3D', '4E']
         number_of_variants = range(1, len(input_data) + 1)
 
-        for variant, item, result  in zip(number_of_variants, input_data, result_data):
+        for variant, item, result in zip(number_of_variants, input_data, result_data):
             with self.subTest(f"variation #{variant}", item=item, result=result):
                 self.assertEqual(get_coordinate(item), result)
 
@@ -32,18 +32,18 @@ class TuplesTest(unittest.TestCase):
     def test_convert_coordinate(self):
         input_data = ['2A', '4B', '1C', '6D', '7E', '7F', '6A', '8A', '5B', '8C', '1F', '3D', '4E']
         result_data = [('2', 'A'),
-                                ('4', 'B'),
-                                ('1', 'C'),
-                                ('6', 'D'),
-                                ('7', 'E'),
-                                ('7', 'F'),
-                                ('6', 'A'),
-                                ('8', 'A'),
-                                ('5', 'B'),
-                                ('8', 'C'),
-                                ('1', 'F'),
-                                ('3', 'D'),
-                                ('4', 'E')]
+                       ('4', 'B'),
+                       ('1', 'C'),
+                       ('6', 'D'),
+                       ('7', 'E'),
+                       ('7', 'F'),
+                       ('6', 'A'),
+                       ('8', 'A'),
+                       ('5', 'B'),
+                       ('8', 'C'),
+                       ('1', 'F'),
+                       ('3', 'D'),
+                       ('4', 'E')]
 
         number_of_variants = range(1, len(input_data) + 1)
 
@@ -54,17 +54,17 @@ class TuplesTest(unittest.TestCase):
     @pytest.mark.task(taskno=3)
     def test_compare_records(self):
         input_data = [
-                                (("Scrimshaw Whale's Tooth", '2A'), ('Deserted Docks', ('2', 'A') ,'Blue')),
-                                (('Brass Spyglass', '4B'), ('Abandoned Lighthouse', ('4', 'B') ,'Blue')),
-                                (('Robot Parrot', '1C'), ('Seaside Cottages', ('1', 'C') ,'Blue')),
-                                (('Glass Starfish', '6D'), ('Tangled Seaweed Patch', ('6', 'D'),'Orange')),
-                                (('Vintage Pirate Hat', '7E'), ('Quiet Inlet (Island of Mystery)', ('7', 'E') ,'Orange')),
-                                (('Amethyst  Octopus', '1F'), ('Seaside Cottages', ('1', 'C') ,'Blue')),
-                                (('Angry Monkey Figurine', '5B'), ('Aqua Lagoon (Island of Mystery)', ('1', 'F') ,'Yellow')),
-                                (('Antique Glass Fishnet Float', '3D'), ('Deserted Docks', ('2', 'A') ,'Blue')),
-                                (('Brass Spyglass', '4B'), ('Spiky Rocks', ('3', 'D') ,'Yellow')),
-                                (('Carved Wooden Elephant', '8C'), ('Abandoned Lighthouse', ('4', 'B') ,'Blue'))
-                            ]
+            (("Scrimshaw Whale's Tooth", '2A'), ('Deserted Docks', ('2', 'A'), 'Blue')),
+            (('Brass Spyglass', '4B'), ('Abandoned Lighthouse', ('4', 'B'), 'Blue')),
+            (('Robot Parrot', '1C'), ('Seaside Cottages', ('1', 'C'), 'Blue')),
+            (('Glass Starfish', '6D'), ('Tangled Seaweed Patch', ('6', 'D'), 'Orange')),
+            (('Vintage Pirate Hat', '7E'), ('Quiet Inlet (Island of Mystery)', ('7', 'E'), 'Orange')),
+            (('Amethyst  Octopus', '1F'), ('Seaside Cottages', ('1', 'C'), 'Blue')),
+            (('Angry Monkey Figurine', '5B'), ('Aqua Lagoon (Island of Mystery)', ('1', 'F'), 'Yellow')),
+            (('Antique Glass Fishnet Float', '3D'), ('Deserted Docks', ('2', 'A'), 'Blue')),
+            (('Brass Spyglass', '4B'), ('Spiky Rocks', ('3', 'D'), 'Yellow')),
+            (('Carved Wooden Elephant', '8C'), ('Abandoned Lighthouse', ('4', 'B'), 'Blue'))
+        ]
         result_data = [True, True, True, True, True, False, False, False, False, False]
         number_of_variants = range(1, len(input_data) + 1)
 
@@ -74,30 +74,30 @@ class TuplesTest(unittest.TestCase):
 
     @pytest.mark.task(taskno=4)
     def test_create_record(self):
-        input_data=[
-                            (('Angry Monkey Figurine', '5B'), ('Stormy Breakwater', ('5', 'B') ,'Purple')),
-                            (('Carved Wooden Elephant', '8C'), ('Foggy Seacave', ('8', 'C'), 'Purple')),
-                            (('Amethyst  Octopus', '1F'), ('Aqua Lagoon (Island of Mystery)', ('1', 'F') ,'Yellow')),
-                            (('Antique Glass Fishnet Float', '3D'), ('Spiky Rocks', ('3', 'D'),'Yellow')),
-                            (('Silver Seahorse', '4E'), ('Hidden Spring (Island of Mystery)', ('4', 'E') ,'Yellow')),
-                            (('Amethyst  Octopus', '1F'), ('Seaside Cottages', ('1', 'C') ,'Blue')),
-                            (('Angry Monkey Figurine', '5B'), ('Aqua Lagoon (Island of Mystery)', ('1', 'F') ,'Yellow')),
-                            (('Antique Glass Fishnet Float', '3D'), ('Deserted Docks', ('2', 'A') ,'Blue')),
-                            (('Brass Spyglass', '4B'), ('Spiky Rocks', ('3', 'D'),'Yellow')),
-                            (('Carved Wooden Elephant', '8C'), ('Abandoned Lighthouse', ('4', 'B') ,'Blue'))
-                            ]
+        input_data = [
+            (('Angry Monkey Figurine', '5B'), ('Stormy Breakwater', ('5', 'B'), 'Purple')),
+            (('Carved Wooden Elephant', '8C'), ('Foggy Seacave', ('8', 'C'), 'Purple')),
+            (('Amethyst  Octopus', '1F'), ('Aqua Lagoon (Island of Mystery)', ('1', 'F'), 'Yellow')),
+            (('Antique Glass Fishnet Float', '3D'), ('Spiky Rocks', ('3', 'D'), 'Yellow')),
+            (('Silver Seahorse', '4E'), ('Hidden Spring (Island of Mystery)', ('4', 'E'), 'Yellow')),
+            (('Amethyst  Octopus', '1F'), ('Seaside Cottages', ('1', 'C'), 'Blue')),
+            (('Angry Monkey Figurine', '5B'), ('Aqua Lagoon (Island of Mystery)', ('1', 'F'), 'Yellow')),
+            (('Antique Glass Fishnet Float', '3D'), ('Deserted Docks', ('2', 'A'), 'Blue')),
+            (('Brass Spyglass', '4B'), ('Spiky Rocks', ('3', 'D'), 'Yellow')),
+            (('Carved Wooden Elephant', '8C'), ('Abandoned Lighthouse', ('4', 'B'), 'Blue'))
+        ]
         result_data = [
-                                ('Angry Monkey Figurine', '5B', 'Stormy Breakwater', ('5', 'B'), 'Purple'),
-                                ('Carved Wooden Elephant', '8C', 'Foggy Seacave', ('8', 'C'), 'Purple'),
-                                ('Amethyst  Octopus', '1F', 'Aqua Lagoon (Island of Mystery)', ('1', 'F'), 'Yellow'),
-                                ('Antique Glass Fishnet Float', '3D', 'Spiky Rocks', ('3', 'D'), 'Yellow'),
-                                ('Silver Seahorse', '4E', 'Hidden Spring (Island of Mystery)', ('4', 'E'), 'Yellow'),
-                                'not a match',
-                                'not a match',
-                                'not a match',
-                                'not a match',
-                                'not a match'
-                            ]
+            ('Angry Monkey Figurine', '5B', 'Stormy Breakwater', ('5', 'B'), 'Purple'),
+            ('Carved Wooden Elephant', '8C', 'Foggy Seacave', ('8', 'C'), 'Purple'),
+            ('Amethyst  Octopus', '1F', 'Aqua Lagoon (Island of Mystery)', ('1', 'F'), 'Yellow'),
+            ('Antique Glass Fishnet Float', '3D', 'Spiky Rocks', ('3', 'D'), 'Yellow'),
+            ('Silver Seahorse', '4E', 'Hidden Spring (Island of Mystery)', ('4', 'E'), 'Yellow'),
+            'not a match',
+            'not a match',
+            'not a match',
+            'not a match',
+            'not a match'
+        ]
 
         number_of_variants = range(1, len(input_data) + 1)
 
@@ -108,20 +108,20 @@ class TuplesTest(unittest.TestCase):
     @pytest.mark.task(taskno=5)
     def test_clean_up(self):
         input_data = (
-                                ("Scrimshaw Whale's Tooth", '2A', 'Deserted Docks', ('2', 'A'), 'Blue'),
-                                ('Brass Spyglass', '4B', 'Abandoned Lighthouse', ('4', 'B'), 'Blue'),
-                                ('Robot Parrot', '1C', 'Seaside Cottages', ('1', 'C'), 'Blue'),
-                                ('Glass Starfish', '6D', 'Tangled Seaweed Patch', ('6', 'D'), 'Orange'),
-                                ('Vintage Pirate Hat', '7E', 'Quiet Inlet (Island of Mystery)', ('7', 'E'), 'Orange'),
-                                ('Pirate Flag', '7F', 'Windswept Hilltop (Island of Mystery)', ('7', 'F'), 'Orange'),
-                                 ('Crystal Crab', '6A', 'Old Schooner', ('6', 'A'), 'Purple'),
-                                ('Model Ship in Large Bottle', '8A', 'Harbor Managers Office', ('8', 'A'), 'Purple'),
-                                ('Angry Monkey Figurine', '5B', 'Stormy Breakwater', ('5', 'B'), 'Purple'),
-                                ('Carved Wooden Elephant', '8C', 'Foggy Seacave', ('8', 'C'), 'Purple'),
-                                ('Amethyst  Octopus', '1F', 'Aqua Lagoon (Island of Mystery)', ('1', 'F'), 'Yellow'),
-                                ('Antique Glass Fishnet Float', '3D', 'Spiky Rocks', ('3', 'D'), 'Yellow'),
-                                ('Silver Seahorse', '4E', 'Hidden Spring (Island of Mystery)', ('4', 'E'), 'Yellow')
-                            )
+            ("Scrimshaw Whale's Tooth", '2A', 'Deserted Docks', ('2', 'A'), 'Blue'),
+            ('Brass Spyglass', '4B', 'Abandoned Lighthouse', ('4', 'B'), 'Blue'),
+            ('Robot Parrot', '1C', 'Seaside Cottages', ('1', 'C'), 'Blue'),
+            ('Glass Starfish', '6D', 'Tangled Seaweed Patch', ('6', 'D'), 'Orange'),
+            ('Vintage Pirate Hat', '7E', 'Quiet Inlet (Island of Mystery)', ('7', 'E'), 'Orange'),
+            ('Pirate Flag', '7F', 'Windswept Hilltop (Island of Mystery)', ('7', 'F'), 'Orange'),
+            ('Crystal Crab', '6A', 'Old Schooner', ('6', 'A'), 'Purple'),
+            ('Model Ship in Large Bottle', '8A', 'Harbor Managers Office', ('8', 'A'), 'Purple'),
+            ('Angry Monkey Figurine', '5B', 'Stormy Breakwater', ('5', 'B'), 'Purple'),
+            ('Carved Wooden Elephant', '8C', 'Foggy Seacave', ('8', 'C'), 'Purple'),
+            ('Amethyst  Octopus', '1F', 'Aqua Lagoon (Island of Mystery)', ('1', 'F'), 'Yellow'),
+            ('Antique Glass Fishnet Float', '3D', 'Spiky Rocks', ('3', 'D'), 'Yellow'),
+            ('Silver Seahorse', '4E', 'Hidden Spring (Island of Mystery)', ('4', 'E'), 'Yellow')
+        )
 
         result_data = """(\"Scrimshaw Whale's Tooth\", 'Deserted Docks', ('2', 'A'), 'Blue')\n\
 ('Brass Spyglass', 'Abandoned Lighthouse', ('4', 'B'), 'Blue')\n\
