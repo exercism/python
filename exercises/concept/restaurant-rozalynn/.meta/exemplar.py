bd9eeff50b732be7afe2ca8e4da7e2d6eb3b059d
BethanyG <bethanymgarcia@gmail.com>
2021-09-06 09:22:50 -0700

    Docstring, document, and code changes to conform to pylinting.

--- exercises/concept/restaurant-rozalynn/.meta/exemplar.py
+++ exercises/concept/restaurant-rozalynn/.meta/exemplar.py
@@ -1,21 +1,20 @@
-
 def new_seating_chart(size=22):
-    '''
+    """Create a new seating chart.
 
     :param size: int - number if seats in the seating chart.
     :return: dict - with number of seats specified, and placeholder values.
-    '''
+    """
 
-    return {number: None for number in range(1, size+1) }
+    return {number: None for number in range(1, size + 1)}
 
 
 def arrange_reservations(guests=None):
-    '''
+    """Assign guests to seats.
 
     :param guest_list: list - list of guest names for reservations.
     :return: dict - Default sized dictionary with guests assigned seats,
              and placeholders for empty seats.
-    '''
+    """
 
     seats = new_seating_chart()
 
@@ -26,11 +25,11 @@ def arrange_reservations(guests=None):
 
 
 def find_all_available_seats(seats):
-    '''
+    """Find and return seat numbers that are unassigned.
 
     :param seats: dict - seating chart.
     :return: list - list of seat numbers available for reserving..
-    '''
+    """
 
     available = []
     for seat_num, value in seats.items():
@@ -39,12 +38,12 @@ def find_all_available_seats(seats):
     return available
 
 
-def curr_empty_seat_capacity(seats):
-    '''
+def current_empty_seat_capacity(seats):
+    """Find the number of seats that are currently empty.
 
     :param seats: dict - dictionary of reserved seats.
     :return: int - number of seats empty.
-    '''
+    """
 
     count = 0
     for value in seats.values():
@@ -54,32 +53,30 @@ def curr_empty_seat_capacity(seats):
 
 
 def accommodate_waiting_guests(seats, guests):
-    '''
+    """Asses if guest can be accommodated.  Update seating if they can be.
 
     :param seats: dict - seating chart dictionary.
     :param guests: list - walk-in guests
     :return: dict - updated seating chart with available spaces filled.
-    '''
+    """
 
-    curr_empty_seats = curr_empty_seat_capacity(seats)
+    curr_empty_seats = current_empty_seat_capacity(seats)
     empty_seat_list = find_all_available_seats(seats)
 
-    if len(guests) > curr_empty_seats:
-        return False
-    else:
-        for index, item in enumerate(guests):
+    if len(guests) <= curr_empty_seats:
+        for index, _ in enumerate(guests):
             seats[empty_seat_list[index]] = guests[index]
 
     return seats
 
 
 def empty_seats(seats, seat_numbers):
-    '''
+    """Empty listed seats of their previous reservations.
 
     :param seats: dict - seating chart dictionary.
     :param seat_numbers: list - list of seat numbers to free up or empty.
     :return: updated seating chart dictionary.
-    '''
+    """
 
     for seat in seat_numbers:
         seats[seat] = None
--- exercises/concept/restaurant-rozalynn/none.py
+++ exercises/concept/restaurant-rozalynn/none.py
@@ -1,57 +1,61 @@
 def new_seating_chart(size=22):
-    '''
+    """Create a new seating chart.
 
     :param size: int - number if seats in the seating chart. Default is 22.
     :return: dict - with number of seats specified, and placeholder values.
-    '''
+    """
 
     pass
 
+
 def arrange_reservations(guest_list):
-    '''
+    """Assign guests to seats.
 
     :param guest_list: list - list of guest names for reservations.
     :return: dict - Default sized dictionary with guests assigned seats,
              and placeholders for empty seats.
-    '''
+    """
 
     pass
 
+
 def find_all_available_seats(seats):
-    '''
+    """Find and return seat numbers that are unassigned.
 
     :param seats: dict - seating chart.
     :return: list - list of seat numbers available for reserving..
-    '''
+    """
 
     pass
 
-def current_seating_capacity(seats):
-    '''
+
+def current_empty_seat_capacity(seats):
+    """Find the number of seats that are currently empty.
 
     :param seats: dict - dictionary of reserved seats.
     :return: int - number of seats empty.
-    '''
+    """
 
     pass
 
+
 def accommodate_waiting_guests(seats, guests):
-    '''
+    """Asses if guest can be accommodated.  Update seating if they can be.
 
     :param seats: dict - seating chart dictionary.
     :param guests: list - walk-in guests
     :return: dict - updated seating chart with available spaces filled.
-    '''
+    """
 
     pass
 
 
 def empty_seats(seats, seat_numbers):
-    '''
+    """Empty listed seats of their previous reservations.
 
     :param seats: dict - seating chart dictionary.
     :param seat_numbers: list - list of seat numbers to free up or empty.
     :return: updated seating chart dictionary.
-    '''
+    """
 
     pass
