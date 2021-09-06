1bce563edae1dd2c7756221d9dcdb5f06d958400
BethanyG <BethanyG@users.noreply.github.com>
2021-09-06 05:03:12 -0700

    Update exercises/concept/currency-exchange/.meta/exemplar.py

--- exercises/concept/currency-exchange/.meta/exemplar.py
+++ exercises/concept/currency-exchange/.meta/exemplar.py
@@ -45,8 +45,8 @@ def get_number_of_bills(budget, denomination):
 def exchangeable_value(budget, exchange_rate, spread, denomination):
     """
 
-	:param budget: float - the amount of your money you are planning to exchange.
-	:param exchange_rate: float - the unit value of the foreign currency.
+    :param budget: float - the amount of your money you are planning to exchange.
+    :param exchange_rate: float - the unit value of the foreign currency.
 	:param spread: int - percentage that is taken as an exchange fee.
 	:param denomination: int - the value of a single bill.
 	:return: int - maximum value you can get
