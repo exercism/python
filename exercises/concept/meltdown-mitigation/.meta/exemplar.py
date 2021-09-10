f42f9907d847f975069cae64fe9f7aabab954081
kbuc <28578761+kbuc@users.noreply.github.com>
2021-09-10 11:34:23 +0200

    Meltdown exercise change according to issue #2565
    Changes:
    - Change Task 2 intervals to cover non-integer comparison.
    - Rewrite tests to test more points, especially the border cases.
    - Reduce number of tests of only one per function, but test many points
      (asserts in loop) in each test.
    - Remove hints of integer input parameters (they can be any numeric type).

--- exercises/concept/meltdown-mitigation/.meta/exemplar.py
+++ exercises/concept/meltdown-mitigation/.meta/exemplar.py
@@ -1,14 +1,14 @@
 def is_criticality_balanced(temperature, neutrons_emitted):
     """Verify criticality is balanced.
 
-    :param temperature: int
-    :param neutrons_emitted: int
+    :param temperature: temperature
+    :param neutrons_emitted: number of neutrons emitted per second
     :return:  boolean True if conditions met, False if not
 
     A reactor is said to be critical if it satisfies the following conditions:
-    - The temperature less than 800.
-    - The number of neutrons emitted per second greater than 500.
-    - The product of temperature and neutrons emitted per second less than 500000.
+    - The temperature is less than 800.
+    - The number of neutrons emitted per second is greater than 500.
+    - The product of temperature and neutrons emitted per second is less than 500000.
     """
     output = temperature * neutrons_emitted
     balanced = False
@@ -22,19 +22,19 @@ def is_criticality_balanced(temperature, neutrons_emitted):
 def reactor_efficiency(voltage, current, theoretical_max_power):
     """Assess reactor efficiency zone.
 
-    :param voltage: int
-    :param current: int
-    :param theoretical_max_power: int
+    :param voltage: voltage
+    :param current: current
+    :param theoretical_max_power: power that correspods to a 100% efficiency
     :return: str one of 'green', 'orange', 'red', or 'black'
 
     Efficiency can be grouped into 4 bands:
 
-    1. green  ->   80-100% efficiency
-    2. orange ->   60-79% efficiency
-    3. red    ->   30-59% efficiency
-    4. black  ->   <30% efficient
+    1. green -> efficiency of 80% or more,
+    2. orange -> efficiency of less than 80% but at least 60%,
+    3. red -> efficiency below 60%, but still 30% or more,
+    4. black ->  less than 30% efficient.
 
-    These percentage ranges are calculated as
+    The percentage value is calculated as
     (generated power/ theoretical max power)*100
     where generated power = voltage * current
     """
@@ -44,9 +44,9 @@ def reactor_efficiency(voltage, current, theoretical_max_power):
 
     if 80 <= percentage_range <= 100:
         efficiency_level = 'green'
-    elif 60 <= percentage_range <= 79:
+    elif 60 <= percentage_range < 80:
         efficiency_level = 'orange'
-    elif 30 <= percentage_range <= 59:
+    elif 30 <= percentage_range < 60:
         efficiency_level = 'red'
     else:
         efficiency_level = 'black'
@@ -67,8 +67,9 @@ def fail_safe(temperature, neutrons_produced_per_second, threshold):
     - `temperature * neutron per second` is not in the above-stated ranges ==  'DANGER'
     """
     output = temperature * neutrons_produced_per_second
-    operational_percentage = int((output / threshold) * 100)
+    operational_percentage = (output / threshold) * 100
     safety_range = 'UNKNOWN'
+    print(operational_percentage)
 
     if operational_percentage < 40:
         safety_range = 'LOW'
