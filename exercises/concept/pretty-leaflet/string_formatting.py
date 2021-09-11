92d75b8859a3838557653d70a357d3363da5b802
tigerBoss101 <tigerpython101@gmail.com>
2021-09-11 20:51:27 +0700

    Updated design.md, the stub file, and instructions.md

--- exercises/concept/pretty-leaflet/string_formatting.py
+++ exercises/concept/pretty-leaflet/string_formatting.py
@@ -10,3 +10,15 @@ class Leaflet:
 
     def print_leaflet(self):
         pass
+
+def capitalize_header(event_name):
+    pass
+
+def format_date(event_date):
+    pass
+
+def display_icons(icons):
+    pass
+
+def print_leaflet(event_name, icons, event_date=None):
+    pass
