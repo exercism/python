167bf270975deae5053adf1e7e2746d99a1f39e9
BethanyG <bethanymgarcia@gmail.com>
2021-10-09 07:57:55 -0700

    Removed solution code from stub file.

--- exercises/concept/log-levels/enums.py
+++ exercises/concept/log-levels/enums.py
@@ -1,67 +1,21 @@
-# from enum import Enum
-#
-#
-# class LogLevel(Enum):
-#     pass
-#
-#
-# def parse_log_level(message):
-#     pass
-#
-#
-# def convert_to_short_log(log_level, message):
-#     pass
-#
-#
-# def get_warn_alias():
-#     pass
-#
-#
-# def get_members():
-#     pass
 from enum import Enum
 
 
 class LogLevel(Enum):
-    TRACE = 'TRC'
-    DEBUG = 'DBG'
-    INFO = 'INF'
-    WARNING = 'WRN'
-    WARN = 'WRN'
-    ERROR = 'ERR'
-    FATAL = 'FTL'
-    UNKNOWN = 'UKN'
-
-
-class LogLevelInt(Enum):
-    TRACE = 0
-    DEBUG = 1
-    INFO = 4
-    WARNING = 5
-    WARN = 5
-    ERROR = 6
-    FATAL = 7
-    UNKNOWN = 42
+    pass
 
 
 def parse_log_level(message):
-    str_split = message.split(':')
-    lvl = str_split[0][1:-1]
-    if lvl in [level.value for level in LogLevel]:
-        return LogLevel(lvl)
-    return LogLevel('UKN')
+    pass
 
 
 def convert_to_short_log(log_level, message):
-    return '{}:{}'.format(LogLevelInt[log_level.name].value, message)
+    pass
 
 
 def get_warn_alias():
-    return LogLevel('WRN')
+    pass
 
 
 def get_members():
-    out_list = list()
-    for member in LogLevel:
-        out_list.append((member.name, member.value))
-    return out_list
+    pass
