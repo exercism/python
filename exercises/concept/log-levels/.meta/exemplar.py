c70acb5ad551bfb87e8cce59d4a9e69beefe7759
BethanyG <bethanymgarcia@gmail.com>
2021-10-09 07:57:06 -0700

    Fixed constant naming and quote mis-matches.

--- exercises/concept/log-levels/.meta/exemplar.py
+++ exercises/concept/log-levels/.meta/exemplar.py
@@ -2,41 +2,41 @@ from enum import Enum
 
 
 class LogLevel(Enum):
-    Trace = 'TRC'
-    Debug = 'DBG'
-    Info = 'INF'
-    Warning = 'WRN'
-    Warn = 'WRN'
-    Error = 'ERR'
-    Fatal = 'FTL'
-    Unknown = 'UKN'
+    TRACE = 'TRC'
+    DEBUG = 'DBG'
+    INFO = 'INF'
+    WARNING = 'WRN'
+    WARN = 'WRN'
+    ERROR = 'ERR'
+    FATAL = 'FTL'
+    UNKNOWN = 'UKN'
 
 
 class LogLevelInt(Enum):
-    Trace = 0
-    Debug = 1
-    Info = 4
-    Warning = 5
-    Warn = 5
-    Error = 6
-    Fatal = 7
-    Unknown = 42
+    TRACE = 0
+    DEBUG = 1
+    INFO = 4
+    WARNING = 5
+    WARN = 5
+    ERROR = 6
+    FATAL = 7
+    UNKNOWN = 42
 
 
 def parse_log_level(message):
-    str_split = message.split(":")
+    str_split = message.split(':')
     lvl = str_split[0][1:-1]
     if lvl in [level.value for level in LogLevel]:
         return LogLevel(lvl)
-    return LogLevel("UKN")
+    return LogLevel('UKN')
 
 
 def convert_to_short_log(log_level, message):
-    return "{}:{}".format(LogLevelInt[log_level.name].value, message)
+    return '{}:{}'.format(LogLevelInt[log_level.name].value, message)
 
 
 def get_warn_alias():
-    return LogLevel("WRN")
+    return LogLevel('WRN')
 
 
 def get_members():
--- exercises/concept/log-levels/enums.py
+++ exercises/concept/log-levels/enums.py
@@ -1,21 +1,67 @@
+# from enum import Enum
+#
+#
+# class LogLevel(Enum):
+#     pass
+#
+#
+# def parse_log_level(message):
+#     pass
+#
+#
+# def convert_to_short_log(log_level, message):
+#     pass
+#
+#
+# def get_warn_alias():
+#     pass
+#
+#
+# def get_members():
+#     pass
 from enum import Enum
 
 
 class LogLevel(Enum):
-    pass
+    TRACE = 'TRC'
+    DEBUG = 'DBG'
+    INFO = 'INF'
+    WARNING = 'WRN'
+    WARN = 'WRN'
+    ERROR = 'ERR'
+    FATAL = 'FTL'
+    UNKNOWN = 'UKN'
+
+
+class LogLevelInt(Enum):
+    TRACE = 0
+    DEBUG = 1
+    INFO = 4
+    WARNING = 5
+    WARN = 5
+    ERROR = 6
+    FATAL = 7
+    UNKNOWN = 42
 
 
 def parse_log_level(message):
-    pass
+    str_split = message.split(':')
+    lvl = str_split[0][1:-1]
+    if lvl in [level.value for level in LogLevel]:
+        return LogLevel(lvl)
+    return LogLevel('UKN')
 
 
 def convert_to_short_log(log_level, message):
-    pass
+    return '{}:{}'.format(LogLevelInt[log_level.name].value, message)
 
 
 def get_warn_alias():
-    pass
+    return LogLevel('WRN')
 
 
 def get_members():
-    pass
+    out_list = list()
+    for member in LogLevel:
+        out_list.append((member.name, member.value))
+    return out_list
