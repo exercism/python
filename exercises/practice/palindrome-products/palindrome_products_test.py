8be45cc05c5a1975f300d9c3ee194902c67df749
Leonardo Festa <4375330+leonardofesta@users.noreply.github.com>
2021-10-11 20:44:50 +0200

    palindrome-products: Reducing test length to avoid timeouts
    Reducing the input of the function to avoid test not needed cases

--- exercises/practice/palindrome-products/palindrome_products_test.py
+++ exercises/practice/palindrome-products/palindrome_products_test.py
@@ -39,13 +39,15 @@ class PalindromeProductsTest(unittest.TestCase):
         self.assertEqual(value, 906609)
         self.assertFactorsEqual(factors, [[913, 993]])
 
+    #testing only the first thousand numbers, to improve test performances
     def test_find_smallest_palindrome_from_four_digit_factors(self):
-        value, factors = smallest(min_factor=1000, max_factor=9999)
+        value, factors = smallest(min_factor=1000, max_factor=1999)
         self.assertEqual(value, 1002001)
         self.assertFactorsEqual(factors, [[1001, 1001]])
 
+    #testing only the last thousand numbers, to improve test performances
     def test_find_the_largest_palindrome_from_four_digit_factors(self):
-        value, factors = largest(min_factor=1000, max_factor=9999)
+        value, factors = largest(min_factor=9000, max_factor=9999)
         self.assertEqual(value, 99000099)
         self.assertFactorsEqual(factors, [[9901, 9999]])
 
