6f841fb74dcb33ee4b5b6665be6d3fdf8df73dec
Pranas Ziaukas <pranas@balena.io>
2021-10-22 11:26:39 +0300

    Change formatting of closing parentheses and brackets

--- exercises/concept/making-the-grade/loops_test.py
+++ exercises/concept/making-the-grade/loops_test.py
@@ -6,8 +6,7 @@ from loops import (
     above_threshold,
     letter_grades,
     student_ranking,
-    perfect_score,
-)
+    perfect_score)
 
 
 class MakingTheGradeTest(unittest.TestCase):
@@ -20,13 +19,10 @@ class MakingTheGradeTest(unittest.TestCase):
             ([1.5], [2]),
             (
                 [90.33, 40.5, 55.44, 70.05, 30.55, 25.45, 80.45, 95.3, 38.7, 40.3],
-                [90, 40, 55, 70, 31, 25, 80, 95, 39, 40]
-            ),
+                [90, 40, 55, 70, 31, 25, 80, 95, 39, 40]),
             (
                 [50, 36.03, 76.92, 40.7, 43, 78.29, 63.58, 91, 28.6, 88.0],
-                [50, 36, 77, 41, 43, 78, 64, 91, 29, 88],
-            ),
-        ]
+                [50, 36, 77, 41, 43, 78, 64, 91, 29, 88])]
 
         for variant, (student_scores, result) in enumerate(data, start=1):
             error_message = f'Expected: {result} but one or more {student_scores} were rounded incorrectly.'
@@ -37,8 +33,7 @@ class MakingTheGradeTest(unittest.TestCase):
     def test_count_failed_students(self):
         data = [
             ([89, 85, 42, 57, 90, 100, 95, 48, 70, 96], 0),
-            ([40, 40, 35, 70, 30, 41, 90], 4),
-        ]
+            ([40, 40, 35, 70, 30, 41, 90], 4)]
 
         for variant, (student_scores, result) in enumerate(data, start=1):
             error_message = f'Expected the count to be {result}, but the count was not calculated correctly.'
@@ -52,8 +47,7 @@ class MakingTheGradeTest(unittest.TestCase):
             (([88, 29, 91, 64, 78, 43, 41, 77, 36, 50], 80), [88, 91]),
             (([100, 89], 100), [100]),
             (([88, 29, 91, 64, 78, 43, 41, 77, 36, 50], 78), [88, 91, 78]),
-            (([], 80), []),
-        ]
+            (([], 80), [])]
 
         for variant, (params, result) in enumerate(data, start=1):
             error_message = f'Expected: {result} but the number of scores above the threshold is incorrect.'
@@ -67,8 +61,7 @@ class MakingTheGradeTest(unittest.TestCase):
             (97, [41, 55, 69, 83]),
             (85, [41, 52, 63, 74]),
             (92, [41, 54, 67, 80]),
-            (81, [41, 51, 61, 71]),
-        ]
+            (81, [41, 51, 61, 71])]
 
         for variant, (highest, result) in enumerate(data, start=1):
             error_message = f'Expected: {result} but the grade thresholds for a high score of {highest} are incorrect.'
@@ -81,11 +74,9 @@ class MakingTheGradeTest(unittest.TestCase):
             (([82], ['Betty']), ['1. Betty: 82']),
             (([88, 73], ['Paul', 'Ernest']), ['1. Paul: 88', '2. Ernest: 73']),
             (
-                ([100, 98, 92, 86, 70, 68, 67, 60], ['Rui', 'Betty', 'Joci', 'Yoshi', 'Kora', 'Bern', 'Jan', 'Rose'],),
-                ['1. Rui: 100', '2. Betty: 98', '3. Joci: 92', '4. Yoshi: 86', '5. Kora: 70', '6. Bern: 68',
-                 '7. Jan: 67', '8. Rose: 60'],
-            ),
-        ]
+                ([100, 98, 92, 86, 70, 68, 67, 60], ['Rui', 'Betty', 'Joci', 'Yoshi', 'Kora', 'Bern', 'Jan', 'Rose']),
+                ['1. Rui: 100', '2. Betty: 98', '3. Joci: 92', '4. Yoshi: 86',
+                 '5. Kora: 70', '6. Bern: 68', '7. Jan: 67', '8. Rose: 60'])]
 
         for variant, (params, result) in enumerate(data, start=1):
             error_message = f'Expected: {result} but the rankings were compiled incorrectly.'
@@ -99,16 +90,11 @@ class MakingTheGradeTest(unittest.TestCase):
             ([['Jill', 30], ['Paul', 73], ], 'No perfect score.'),
             ([], 'No perfect score.'),
             (
-                [['Rui', 60], ['Joci', 58], ['Sara', 91], ['Kora', 93], ['Alex', 42], ['Jan', 81], ['Lilliana', 40],
-                 ['John', 60], ['Bern', 28], ['Vlad', 55]],
-                'No perfect score.'
-            ),
+                [['Rui', 60], ['Joci', 58], ['Sara', 91], ['Kora', 93], ['Alex', 42],
+                 ['Jan', 81], ['Lilliana', 40], ['John', 60], ['Bern', 28], ['Vlad', 55]], 'No perfect score.'),
             (
-                [['Yoshi', 52], ['Jan', 86], ['Raiana', 100], ['Betty', 60], ['Joci', 100], ['Kora', 81], ['Bern', 41],
-                 ['Rose', 94]],
-                ['Raiana', 100]
-            ),
-        ]
+                [['Yoshi', 52], ['Jan', 86], ['Raiana', 100], ['Betty', 60],
+                 ['Joci', 100], ['Kora', 81], ['Bern', 41], ['Rose', 94]], ['Raiana', 100])]
 
         for variant, (student_info, result) in enumerate(data, start=1):
             error_message = f'Expected: {result} but got something different for perfect scores.'
