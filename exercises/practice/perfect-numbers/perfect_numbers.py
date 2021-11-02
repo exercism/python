31f84912c8f763f742141fdd791634eb4f02260d
BethanyG <bethanymgarcia@gmail.com>
2021-11-02 03:46:05 -0700

    Added error handling instruction append, added additional test cases, corrected JinJa2 template, and regenerated test cases.

--- exercises/practice/perfect-numbers/perfect_numbers.py
+++ exercises/practice/perfect-numbers/perfect_numbers.py
@@ -1,2 +1,7 @@
 def classify(number):
+    """ A perfect number equals the sum of its positive divisors.
+
+    :param number: int a positive integer
+    :return: str the classification of the input integer
+    """
     pass
