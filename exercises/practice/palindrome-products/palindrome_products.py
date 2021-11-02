2d9af26e75c03ed7c2e32e5d090fc0e35e4d2068
BethanyG <bethanymgarcia@gmail.com>
2021-11-02 03:16:29 -0700

    Added error handling instruction append, added additional test cases, corrected JinJa2 template, and regenerated test cases.

--- exercises/practice/palindrome-products/palindrome_products.py
+++ exercises/practice/palindrome-products/palindrome_products.py
@@ -1,6 +1,24 @@
 def largest(min_factor, max_factor):
+    """Given a range of numbers, find the largest palindromes which
+       are products of two numbers within that range.
+
+    :param min_factor: int with a default value of 0
+    :param max_factor: int
+    :return: tuple of (palindrome, iterable).
+             Iterable should contain both factors of the palindrome in an arbitrary order.
+    """
+
     pass
 
 
 def smallest(min_factor, max_factor):
+    """Given a range of numbers, find the smallest palindromes which
+    are products of two numbers within that range.
+
+    :param min_factor: int with a default value of 0
+    :param max_factor: int
+    :return: tuple of (palindrome, iterable).
+    Iterable should contain both factors of the palindrome in an arbitrary order.
+    """
+
     pass
