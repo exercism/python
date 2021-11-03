33db1d0331286c0a448a15a967ee6b97d219a51e
BethanyG <bethanymgarcia@gmail.com>
2021-11-03 15:32:19 -0700

    Added instructions append for error handling and updated JinJa2 template and regenerated test file.

--- exercises/practice/satellite/satellite_test.py
+++ exercises/practice/satellite/satellite_test.py
@@ -41,27 +41,27 @@ class SatelliteTest(unittest.TestCase):
         preorder = ["a", "b"]
         inorder = ["b", "a", "r"]
 
-        with self.assertRaisesWithMessage(ValueError):
+        with self.assertRaises(ValueError) as err:
             tree_from_traversals(preorder, inorder)
+        self.assertEqual(type(err.exception), ValueError)
+        self.assertEqual(err.exception.args[0], "traversals must have the same length")
 
     def test_reject_inconsistent_traversals_of_same_length(self):
         preorder = ["x", "y", "z"]
         inorder = ["a", "b", "c"]
 
-        with self.assertRaisesWithMessage(ValueError):
+        with self.assertRaises(ValueError) as err:
             tree_from_traversals(preorder, inorder)
+        self.assertEqual(type(err.exception), ValueError)
+        self.assertEqual(
+            err.exception.args[0], "traversals must have the same elements"
+        )
 
     def test_reject_traversals_with_repeated_items(self):
         preorder = ["a", "b", "a"]
         inorder = ["b", "a", "a"]
 
-        with self.assertRaisesWithMessage(ValueError):
+        with self.assertRaises(ValueError) as err:
             tree_from_traversals(preorder, inorder)
-
-    # Utility functions
-    def assertRaisesWithMessage(self, exception):
-        return self.assertRaisesRegex(exception, r".+")
-
-
-if __name__ == "__main__":
-    unittest.main()
+        self.assertEqual(type(err.exception), ValueError)
+        self.assertEqual(err.exception.args[0], "traversals must contain unique items")
