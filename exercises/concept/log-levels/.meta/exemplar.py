3752806c951263789efd5ca11bddef767f469ced
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-11-06 20:40:50 +0530

    Add doc-strings to stub file and exemplar.py

--- exercises/concept/log-levels/.meta/exemplar.py
+++ exercises/concept/log-levels/.meta/exemplar.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 
 class LogLevel(Enum):
+    """Represents different logging levels by their verbose codes"""
+
     TRACE = 'TRC'
     DEBUG = 'DBG'
     INFO = 'INF'
@@ -13,6 +15,8 @@ class LogLevel(Enum):
 
 
 class LogLevelInt(Enum):
+    """Represents different logging levels by their short codes"""
+
     TRACE = 0
     DEBUG = 1
     INFO = 4
@@ -24,6 +28,17 @@ class LogLevelInt(Enum):
 
 
 def parse_log_level(message):
+    """Takes a log message and returns the enum member of its level
+    Returns a LogLevel.Unknown incase an unknown severity is found
+
+    :param message: log message (string)
+    :return: <enum 'LogLevel'>
+
+    Ex:
+    - parse_log_level("[INF]: File deleted") #=> LogLevel.Info
+    - parse_log_level("[XYZ]: Out of context message") #=> LogLevel.Unknown
+    """
+
     str_split = message.split(':')
     lvl = str_split[0][1:-1]
     if lvl in [level.value for level in LogLevel]:
@@ -32,14 +47,35 @@ def parse_log_level(message):
 
 
 def convert_to_short_log(log_level, message):
+    """Converts a log message to a shorter format optimized for storage.
+
+    :param log_level: The Log level of the log sent. ex: LogLevel.Error.
+    :param message: log message (string)
+    :return: <enum 'LogLevelInt'>
+
+    Ex:
+    - convert_to_short_log(LogLevel.Error, "Stack overflow") #=> "6:Stack overflow"
+    """
+
     return f'{LogLevelInt[log_level.name].value}:{message}'
 
 
 def get_warn_alias():
+    """Returns the enum for LogLevel Warning
+
+    :return: <enum 'LogLevel'>
+    """
+
     return LogLevel('WRN')
 
 
 def get_members():
+    """Returns a list of tuples (name, value) containing all the members
+    of the enum LogLevel.
+
+    :return: List of tuples [(name1, value1), (name2, value2)]
+    """
+
     out_list = []
     for member in LogLevel:
         out_list.append((member.name, member.value))
--- exercises/concept/log-levels/enums.py
+++ exercises/concept/log-levels/enums.py
@@ -2,20 +2,54 @@ from enum import Enum
 
 
 class LogLevel(Enum):
+    """Represents different logging levels by their verbose codes"""
+
     pass
 
 
 def parse_log_level(message):
+    """Takes a log message and returns the enum member of its level
+    Returns a LogLevel.Unknown incase an unknown severity is found
+
+    :param message: log message (string)
+    :return: <enum 'LogLevel'>
+
+    Ex:
+    - parse_log_level("[INF]: File deleted") #=> LogLevel.Info
+    - parse_log_level("[XYZ]: Out of context message") #=> LogLevel.Unknown
+    """
+
     pass
 
 
 def convert_to_short_log(log_level, message):
+    """Converts a log message to a shorter format optimized for storage.
+
+    :param log_level: The Log level of the log sent. ex: LogLevel.Error.
+    :param message: log message (string)
+    :return: <enum 'LogLevelInt'>
+
+    Ex:
+    - convert_to_short_log(LogLevel.Error, "Stack overflow") #=> "6:Stack overflow"
+    """
+
     pass
 
 
 def get_warn_alias():
+    """Returns the enum for LogLevel Warning
+
+    :return: <enum 'LogLevel'>
+    """
+
     pass
 
 
 def get_members():
+    """Returns a list of tuples (name, value) containing all the members
+    of the enum LogLevel.
+
+    :return: List of tuples [(name1, value1), (name2, value2)]
+    """
+
     pass
