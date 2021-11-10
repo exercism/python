c1e029f54a591e2835a7cc8f149a9798051111ca
BethanyG <bethanymgarcia@gmail.com>
2021-11-10 02:25:59 -0800

    Pulled down updares from problem-specs.  Updated JinJa2 teamplate and instructions and regenerated test file.

--- exercises/practice/resistor-color-duo/resistor_color_duo_test.py
+++ exercises/practice/resistor-color-duo/resistor_color_duo_test.py
@@ -17,12 +17,14 @@ class ResistorColorDuoTest(unittest.TestCase):
     def test_yellow_and_violet(self):
         self.assertEqual(value(["yellow", "violet"]), 47)
 
+    def test_white_and_red(self):
+        self.assertEqual(value(["white", "red"]), 92)
+
     def test_orange_and_orange(self):
         self.assertEqual(value(["orange", "orange"]), 33)
 
     def test_ignore_additional_colors(self):
         self.assertEqual(value(["green", "brown", "orange"]), 51)
 
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_black_and_brown_one_digit(self):
+        self.assertEqual(value(["black", "brown"]), 1)
