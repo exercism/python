3b551cb0e15c4287891237f1daa7feab2faac4a6
BethanyG <bethanymgarcia@gmail.com>
2021-11-10 02:15:35 -0800

    Pulled down updares from problem-specs.  Updated JinJa2 teamplate and instructions and regenerated test file.

--- exercises/practice/prime-factors/prime_factors.py
+++ exercises/practice/prime-factors/prime_factors.py
@@ -1,2 +1,13 @@
+# def factors(value):
+#     pass
 def factors(value):
-    pass
+    factors = []
+    divisor = 2
+    while value > 1:
+        while value % divisor == 0:
+            factors.append(divisor)
+            value /= divisor
+
+        divisor += 1
+
+    return factors
