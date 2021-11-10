dbf56e6e3be50953fdac5da2bb8b2bcf6fb981fb
BethanyG <bethanymgarcia@gmail.com>
2021-11-10 00:59:28 -0800

    Forgot to clean the code from the stub file.  Eeek.

--- exercises/practice/luhn/luhn.py
+++ exercises/practice/luhn/luhn.py
@@ -1,25 +1,6 @@
-# class Luhn:
-#     def __init__(self, card_num):
-#         pass
-#
-#     def valid(self):
-#         pass
 class Luhn:
     def __init__(self, card_num):
-        self.card_num = card_num
-        self.checksum = -1
-        digits = card_num.replace(" ", "")
-        length = len(digits)
-        if digits.isdigit() and length > 1:
-            self.checksum = 0
-            cadence = length % 2
-            for idx, digit in enumerate(digits):
-                num = int(digit)
-                if idx % 2 == cadence:
-                    num *= 2
-                    if num > 9:
-                        num -= 9
-                self.checksum += num
+        pass
 
     def valid(self):
-        return self.checksum % 10 == 0
+        pass
