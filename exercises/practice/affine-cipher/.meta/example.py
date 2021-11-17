9ba54db531c6cf8e1247c612c2a819764389f7c6
Job van der Wal <48634934+J08K@users.noreply.github.com>
2021-11-18 00:29:56 +0100

    affine cipher

--- exercises/practice/affine-cipher/.meta/example.py
+++ exercises/practice/affine-cipher/.meta/example.py
@@ -2,42 +2,40 @@ BLOCK_SIZE = 5
 ALPHABET = 26
 
 
-def mod_inverse(a, alphabet):
-    a = a % alphabet
+def mod_inverse(a_key, alphabet):
+    a_key = a_key % alphabet
     for idx in range(1, alphabet):
-        if (a * idx) % alphabet == 1:
+        if (a_key * idx) % alphabet == 1:
             return idx
     return 1
 
 
-def translate(text, a, b, mode):
-    inv = mod_inverse(a, ALPHABET)
-    if inv == 1:
-        raise ValueError("a and m must be coprime.")
+def translate(text, a_key, b_key, mode):
+    inverse = mod_inverse(a_key, ALPHABET)
+    if inverse == 1:
+        raise ValueError('a and m must be coprime.')
 
     chars = []
-    for c in text:
-        if c.isalnum():
-            orig = ord(c.lower()) - 97
-            if orig < 0:
-                chars.append(c)
+    for character in text:
+        if character.isalnum():
+            origin = ord(character.lower()) - 97
+            if origin < 0:
+                chars.append(character)
                 continue
             if mode == 0:
-                new = (a * orig + b) % ALPHABET
+                new = (a_key * origin + b_key) % ALPHABET
             elif mode == 1:
-                new = (inv * (orig - b)) % ALPHABET
+                new = (inverse * (origin - b_key)) % ALPHABET
             chars.append(chr(new + 97))
 
-    return "".join(chars)
+    return ''.join(chars)
 
 
 def encode(plain, a, b):
     cipher = translate(plain, a, b, 0)
-    return " ".join([cipher[idx:idx + BLOCK_SIZE]
+    return ' '.join([cipher[idx:idx + BLOCK_SIZE]
                      for idx in range(0, len(cipher), BLOCK_SIZE)])
 
 
 def decode(ciphered, a, b):
     return translate(ciphered, a, b, 1)
-
-print(mod_inverse(5, ALPHABET))
