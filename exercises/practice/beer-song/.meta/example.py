37d4a6ec654f23807117fc4262901321f903e4a9
Job van der Wal <48634934+J08K@users.noreply.github.com>
2021-11-18 00:16:16 +0100

    Fixed some stuff

--- exercises/practice/beer-song/.meta/example.py
+++ exercises/practice/beer-song/.meta/example.py
@@ -3,17 +3,17 @@ def recite(start, take=1):
     for idx in range(start, start - take, -1):
         results.extend(verse(idx))
         if idx > start - take + 1:
-            results.append("")
+            results.append('')
     return results
 
 
 def verse(number):
     return [
-        "".join([
-            "{} of beer on the wall, ".format(_bottles(number).capitalize()),
-            "{} of beer.".format(_bottles(number))
+        ''.join([
+            f'{_bottles(number).capitalize()} of beer on the wall, ',
+            f'{_bottles(number)} of beer.'
         ]),
-        "".join([
+        ''.join([
             _action(number),
             _next_bottle(number)
         ])
@@ -22,24 +22,20 @@ def verse(number):
 
 def _action(current_verse):
     if current_verse == 0:
-        return "Go to the store and buy some more, "
+        return 'Go to the store and buy some more, '
     else:
-        return "Take {} down and pass it around, ".format(
-            "one" if current_verse > 1 else "it",
-        )
+        return f'Take {"one" if current_verse > 1 else "it"} down and pass it around, '
 
 
 def _next_bottle(current_verse):
-    return "{} of beer on the wall.".format(
-        _bottles(_next_verse(current_verse)),
-    )
+    return f'{_bottles(_next_verse(current_verse))} of beer on the wall.'
 
 
 def _bottles(number):
     if number == 0:
-        return "no more bottles"
+        return 'no more bottles'
     if number == 1:
-        return "1 bottle"
+        return '1 bottle'
     else:
         return f'{number} bottles'
 
