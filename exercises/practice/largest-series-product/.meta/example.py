839941134c48dd5ed6fc5cb92667f522f969a7e0
Job van der Wal <J08K0L@gmail.com>
2021-11-18 15:14:15 +0100

    first 5

--- exercises/practice/largest-series-product/.meta/example.py
+++ exercises/practice/largest-series-product/.meta/example.py
@@ -2,22 +2,22 @@ from functools import reduce
 from operator import mul
 
 
-def slices(series, length):
+def slices(series, size):
 
-    if not length <= len(series):
-        raise ValueError("span must be smaller than string length")
-    elif not 0 < length:
-        raise ValueError("span must be greater than zero")
+    if not size <= len(series):
+        raise ValueError('span must be smaller than string length')
+    elif not 0 < size:
+        raise ValueError('span must be greater than zero')
     elif not all(item.isdigit() for item in series):
-        raise ValueError("digits input must only contain digits")
+        raise ValueError('digits input must only contain digits')
 
     numbers = [int(digit) for digit in series]
 
-    return [numbers[i:i + length]
-            for i in range(len(numbers) - length + 1)]
+    return [numbers[idx:idx + size]
+            for idx in range(len(numbers) - size + 1)]
 
 
-def largest_product(series, length):
-    if length == 0:
+def largest_product(series, size):
+    if size == 0:
         return 1
-    return max(reduce(mul, slc) for slc in slices(series, length))
+    return max(reduce(mul, slice) for slice in slices(series, size))
