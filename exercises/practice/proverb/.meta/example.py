e155a1c1c9633e8a55c98cdee68423eee51e5afe
Job van der Wal <48634934+J08K@users.noreply.github.com>
2021-11-18 22:03:24 +0100

    Finished all the Ps

--- exercises/practice/proverb/.meta/example.py
+++ exercises/practice/proverb/.meta/example.py
@@ -1,7 +1,7 @@
 def proverb(rhyme_items):
     if not rhyme_items:
-        return ""
-    phrases = ['For want of a {0} the {1} was lost.'.format(el1, el2)
-               for el1, el2 in zip(rhyme_items, rhyme_items[1:])]
-    phrases.append('And all for the want of a {0}.'.format(rhyme_items[0]))
+        return ''
+    phrases = [f'For want of a {element_1} the {element_2} was lost.'
+               for element_1, element_2 in zip(rhyme_items, rhyme_items[1:])]
+    phrases.append(f'And all for the want of a {rhyme_items[0]}.')
     return '\n'.join(phrases)
