e5425abe1975d71a096810418c6dce55482e1992
Job van der Wal <J08K0L@gmail.com>
2021-11-18 20:36:34 +0100

    First files

--- exercises/practice/ocr-numbers/.meta/example.py
+++ exercises/practice/ocr-numbers/.meta/example.py
@@ -3,8 +3,8 @@ NUM_COLS = 3
 
 
 def split_ocr(ocr):
-    return [[ocr[i][NUM_COLS * j:NUM_COLS * (j + 1)] for i in range(NUM_ROWS)]
-            for j in range(len(ocr[0]) // NUM_COLS)]
+    return [[ocr[idx][NUM_COLS * jam:NUM_COLS * (jam + 1)] for idx in range(NUM_ROWS)]
+            for jam in range(len(ocr[0]) // NUM_COLS)]
 
 
 ALL = ['    _  _     _  _  _  _  _  _ ',
@@ -23,21 +23,21 @@ def convert(input_grid):
     lines = [input_grid[start:end]
              for start, end in zip(split_indices[:-1], split_indices[1:])]
 
-    return ",".join(convert_one_line(line) for line in lines)
+    return ','.join(convert_one_line(line) for line in lines)
 
 
 def convert_one_line(input_grid):
     if len(input_grid) != NUM_ROWS:
-        raise ValueError("Number of input lines is not a multiple of four")
+        raise ValueError('Number of input lines is not a multiple of four')
 
     if len(input_grid[0]) % NUM_COLS:
-        raise ValueError("Number of input columns is not a multiple of three")
+        raise ValueError('Number of input columns is not a multiple of three')
 
     numbers = split_ocr(input_grid)
     digits = ''
-    for n in numbers:
+    for num in numbers:
         try:
-            digits += str(OCR_LIST.index(n))
+            digits += str(OCR_LIST.index(num))
         except ValueError:
             digits += '?'
     return digits
--- exercises/practice/octal/.meta/example.py
+++ exercises/practice/octal/.meta/example.py
@@ -1,11 +1,11 @@
 def parse_octal(digits):
     digits = _validate_octal(digits)
-    return sum(int(digit) * 8 ** i
-               for (i, digit) in enumerate(reversed(digits)))
+    return sum(int(digit) * 8 ** idx
+               for (idx, digit) in enumerate(reversed(digits)))
 
 
 def _validate_octal(digits):
-    for d in digits:
-        if not '0' <= d < '8':
-            raise ValueError("Invalid octal digit: " + d)
+    for digit in digits:
+        if not '0' <= digit < '8':
+            raise ValueError("Invalid octal digit: " + digit)
     return digits
