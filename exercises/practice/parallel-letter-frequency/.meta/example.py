36293227bf7bbf25baf9d5ccb425e4305e11d597
Job van der Wal <48634934+J08K@users.noreply.github.com>
2021-11-19 00:12:05 +0100

    MAJOR CHANGES

--- exercises/practice/parallel-letter-frequency/.meta/example.py
+++ exercises/practice/parallel-letter-frequency/.meta/example.py
@@ -4,8 +4,8 @@ from threading import Lock, Thread
 from time import sleep
 from queue import Queue
 
-total_workers = 3  # Maximum number of threads chosen arbitrarily
 
+TOTAL_WORKERS = 3  # Maximum number of threads chosen arbitrarily
 
 class LetterCounter:
 
@@ -26,8 +26,7 @@ def count_letters(queue_of_texts, letter_to_frequency, worker_id):
         sleep(worker_id + 1)
         line_input = queue_of_texts.get()
         if line_input is not None:
-            letters_in_line = Counter([idx for idx in line_input.lower() if
-                                       idx.isalpha()])
+            letters_in_line = Counter(idx for idx in line_input.lower() if idx.isalpha())
             letter_to_frequency.add_counter(letters_in_line)
         queue_of_texts.task_done()
         if line_input is None:
@@ -40,13 +39,12 @@ def calculate(list_of_texts):
         queue_of_texts.put(line)
     letter_to_frequency = LetterCounter()
     threads = []
-    for idx in range(total_workers):
-        worker = Thread(target=count_letters, args=(queue_of_texts,
-                                                    letter_to_frequency, idx))
+    for idx in range(TOTAL_WORKERS):
+        worker = Thread(target=count_letters, args=(queue_of_texts, letter_to_frequency, idx))
         worker.start()
         threads.append(worker)
     queue_of_texts.join()
-    for _ in range(total_workers):
+    for _ in range(TOTAL_WORKERS):
         queue_of_texts.put(None)
     for thread in threads:
         thread.join()
