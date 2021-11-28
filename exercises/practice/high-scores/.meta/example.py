b5c30d35ee7e8bb3e0ad42a6e6881c2175fc3272
BethanyG <bethanymgarcia@gmail.com>
2021-11-28 12:16:23 -0800

    Skipped scores after personal top cases.  Regenerated test file.  Edited example.py to pass tests.

--- exercises/practice/high-scores/.meta/example.py
+++ exercises/practice/high-scores/.meta/example.py
@@ -1,3 +1,4 @@
+
 def latest(scores):
     return scores[-1]
 
@@ -8,11 +9,3 @@ def personal_best(scores):
 
 def personal_top_three(scores):
     return sorted(scores, reverse=True)[:3]
-
-
-def latest_after_top_three(scores):
-    return latest(scores)
-
-
-def scores_after_top_three(scores):
-    return scores
