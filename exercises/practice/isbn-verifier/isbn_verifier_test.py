fe57146690eb70466b6fc008efd83b61e79b588f
BethanyG <BethanyG@users.noreply.github.com>
2022-01-04 15:40:35 -0800

    Second attempt to update isbn-verifier
    Re-synced and re-generated tests.

--- exercises/practice/isbn-verifier/isbn_verifier_test.py
+++ exercises/practice/isbn-verifier/isbn_verifier_test.py
@@ -20,7 +20,7 @@ class IsbnVerifierTest(unittest.TestCase):
     def test_check_digit_is_a_character_other_than_x(self):
         self.assertIs(is_valid("3-598-21507-A"), False)
 
-    def test_invalid_character_in_isbn(self):
+    def test_invalid_character_in_isbn_is_not_treated_as_zero(self):
         self.assertIs(is_valid("3-598-P1581-X"), False)
 
     def test_x_is_only_valid_as_a_check_digit(self):
@@ -53,8 +53,11 @@ class IsbnVerifierTest(unittest.TestCase):
     def test_input_is_9_characters(self):
         self.assertIs(is_valid("134456729"), False)
 
-    def test_invalid_characters_are_not_ignored(self):
+    def test_invalid_characters_are_not_ignored_after_checking_length(self):
         self.assertIs(is_valid("3132P34035"), False)
+
+    def test_invalid_characters_are_not_ignored_before_checking_length(self):
+        self.assertIs(is_valid("3598P215088"), False)
 
     def test_input_is_too_long_but_contains_a_valid_isbn(self):
         self.assertIs(is_valid("98245726788"), False)
