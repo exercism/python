92309c6f81174796fb21811a65ff8c55b1b757ee
Isaac Good <nuc-git@isaacgood.com>
2022-01-22 12:31:55 -0800

    Copy docstrings from stub to exemplar

--- exercises/concept/black-jack/.meta/exemplar.py
+++ exercises/concept/black-jack/.meta/exemplar.py
@@ -9,7 +9,7 @@ def value_of_card(card):
     """Determine the scoring value of a card.
 
     :param card: str - given card.
-    :return: int - value of a given card (J, Q, K = 10, 'A' = 1) numerical value otherwise.
+    :return: int - value of a given card. 'J', 'Q', 'K' = 10; 'A' = 1; numerical value otherwise.
     """
 
     if card in('JQK'):
@@ -27,7 +27,7 @@ def value_of_card(card):
 def higher_card(card_one, card_two):
     """Determine which card has a higher value in the hand.
 
-    :param card_one, card_two: str - cards dealt.  J, Q, K = 10, 'A' = 1, all others are numerical value.
+    :param card_one, card_two: str - cards dealt. 'J', 'Q', 'K' = 10; 'A' = 1; numerical value otherwise.
     :return: higher value card - str. Tuple of both cards if they are of equal value.
     """
 
@@ -49,7 +49,7 @@ def higher_card(card_one, card_two):
 def value_of_ace(card_one, card_two):
     """Calculate the most advantageous value for the ace card.
 
-    :param card_one, card_two: str - card (J, Q, K == 10, numerical value otherwise)
+    :param card_one, card_two: str - card dealt. 'J', 'Q', 'K' = 10; 'A' = 11 (if already in hand); numerical value otherwise.
     :return: int - value of the upcoming ace card (either 1 or 11).
     """
 
@@ -65,7 +65,7 @@ def value_of_ace(card_one, card_two):
 def is_blackjack(card_one, card_two):
     """Determine if the hand is a 'natural' or 'blackjack'.
 
-    :param card_one, card_two: str - cards dealt.  J, Q, K = 10, 'A' = 11, all others are numerical value.
+    :param card_one, card_two: str - cards dealt. 'J', 'Q', 'K' = 10; 'A' = 11; numerical value otherwise.
     :return: bool - if the hand is a blackjack (two cards worth 21).
     """
 
@@ -75,7 +75,7 @@ def is_blackjack(card_one, card_two):
 def can_split_pairs(card_one, card_two):
     """Determine if a player can split their hand into two hands.
 
-    :param card_one, card_two: str - cards in hand.
+    :param card_one, card_two: str - cards dealt.
     :return: bool - if the hand can be split into two pairs (i.e. cards are of the same value).
     """
 
@@ -85,7 +85,7 @@ def can_split_pairs(card_one, card_two):
 def can_double_down(card_one, card_two):
     """Determine if a blackjack player can place a double down bet.
 
-    :param card_one, card_two: str - first and second cards in hand.
+    :param card_one, card_two: str - first and second cards dealt.
     :return: bool - if the hand can be doubled down (i.e. totals 9, 10 or 11 points).
     """
 
