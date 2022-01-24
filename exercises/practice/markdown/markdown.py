efc434c787dfa1376f077be38d8870afeb33108f
Aashish Vivekanand <aashishvanand@gmail.com>
2022-01-24 09:53:26 +0530

    Updated module docstring

--- exercises/practice/markdown/markdown.py
+++ exercises/practice/markdown/markdown.py
@@ -1,2 +1,9 @@
 def parse(markdown):
+    """ Markup
+    Input: would be a markdown phrase for an example
+    Output: expected to pass a HTML equivalent for the same
+    Sample: "#exercism.org is a useful practice ground"
+    you syntax should return <h1>exercism.org is a useful practice ground </h1>
+    Hint: Try exporing regular expressions. Makes life simple
+    """
     pass
