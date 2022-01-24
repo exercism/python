c9b0d9d35ebb687bc56fa955a2a192e796fd5e0a
Aashish Vivekanand <aashishvanand@gmail.com>
2022-01-24 14:57:19 +0530

    Reverted back to orignial file with extra markup

--- exercises/practice/markdown/markdown.py
+++ exercises/practice/markdown/markdown.py
@@ -1,12 +1,13 @@
 import re
+
 def parse(markdown):
+
     """ Markup
-Input: would be a markdown phrase for an example
-Output: expected to pass a HTML equivalent for the same
-Sample: "#exercism.org is a useful practice ground"
-you syntax should return <h1>exercism.org is a useful practice ground </h1>
-Hint: Try exporing regular expressions. Makes life simple
-"""
+    Input: would be a markdown phrase for an example
+    Output: expected to pass a HTML equivalent for the same
+    Sample: "#exercism.org is a useful practice ground"
+    you syntax should return <h1>exercism.org is a useful practice ground </h1>
+    """
     lines = markdown.split('\n')
     res = ''
     in_list = False
@@ -14,9 +15,16 @@ Hint: Try exporing regular expressions. Makes life simple
     for i in lines:
         if re.match('###### (.*)', i) is not None:
             i = '<h6>' + i[7:] + '</h6>'
-        '''
-        handle other cases here similar to <h6>
-        '''
+        elif re.match('##### (.*)', i) is not None:
+            i = '<h5>' + i[6:] + '</h5>'
+        elif re.match('#### (.*)', i) is not None:
+            i = '<h4>' + i[5:] + '</h4>'
+        elif re.match('### (.*)', i) is not None:
+            i = '<h3>' + i[4:] + '</h3>'
+        elif re.match('## (.*)', i) is not None:
+            i = '<h2>' + i[3:] + '</h2>'
+        elif re.match('# (.*)', i) is not None:
+            i = '<h1>' + i[2:] + '</h1>'
         m = re.match(r'\* (.*)', i)
         if m:
             if not in_list:
@@ -25,20 +33,51 @@ Hint: Try exporing regular expressions. Makes life simple
                 is_italic = False
                 curr = m.group(1)
                 m1 = re.match('(.*)__(.*)__(.*)', curr)
-                '''
-                handle the cases here too
-                '''
-            '''
-            handle other cases here also
-            '''
+                if m1:
+                    curr = m1.group(1) + '<strong>' + \
+                        m1.group(2) + '</strong>' + m1.group(3)
+                    is_bold = True
+                m1 = re.match('(.*)_(.*)_(.*)', curr)
+                if m1:
+                    curr = m1.group(1) + '<em>' + m1.group(2) + \
+                        '</em>' + m1.group(3)
+                    is_italic = True
+                i = '<ul><li>' + curr + '</li>'
+            else:
+                is_bold = False
+                is_italic = False
+                curr = m.group(1)
+                m1 = re.match('(.*)__(.*)__(.*)', curr)
+                if m1:
+                    is_bold = True
+                m1 = re.match('(.*)_(.*)_(.*)', curr)
+                if m1:
+                    is_italic = True
+                if is_bold:
+                    curr = m1.group(1) + '<strong>' + \
+                        m1.group(2) + '</strong>' + m1.group(3)
+                if is_italic:
+                    curr = m1.group(1) + '<em>' + m1.group(2) + \
+                        '</em>' + m1.group(3)
+                i = '<li>' + curr + '</li>'
         else:
             if in_list:
                 in_list_append = True
                 in_list = False
+
         m = re.match('<h|<ul|<p|<li', i)
-        '''
-        handle more cases here too
-        '''
+        if not m:
+            i = '<p>' + i + '</p>'
+        m = re.match('(.*)__(.*)__(.*)', i)
+        if m:
+            i = m.group(1) + '<strong>' + m.group(2) + '</strong>' + m.group(3)
+        m = re.match('(.*)_(.*)_(.*)', i)
+        if m:
+            i = m.group(1) + '<em>' + m.group(2) + '</em>' + m.group(3)
+        if in_list_append:
+            i = '</ul>' + i
+            in_list_append = False
+        res += i
     if in_list:
         res += '</ul>'
     return res
