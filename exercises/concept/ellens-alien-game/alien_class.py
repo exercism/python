0820252c7960f7640c4388284040d2998d75866f
PaulT89 <paul.d.turner.1989@gmail.com>
2022-01-26 04:57:05 -0500

    New concept exercise: ellens-alien-game
    Introduces the concept of classes.
    Message-Id: <20220126095726.46310-2-keeperotphones+patch@gmail.com>

---
+++ exercises/concept/ellens-alien-game/alien_class.py
@@ -0,0 +1,14 @@
+## Create the Alien class here
+
+
+def new_alien_list(positions):
+    """
+
+    :param positions: list - a list of tuples of (x, y) coordinates
+    :return: list - a list of alien objects
+    
+    Function that takes a list of positions and creates one alien
+    instance per position
+    """
+    
+    pass
