d0be1c2485ef40eb8d6fea9aa10f3ae4153651d9
BethanyG <BethanyG@users.noreply.github.com>
2022-02-09 17:39:19 -0800

    Updated Roman Numerals from problem-specifications. Synced tests.toml and regenerated test file.

--- exercises/practice/roman-numerals/roman_numerals.py
+++ exercises/practice/roman-numerals/roman_numerals.py
@@ -1,2 +1,20 @@
+# def roman(number):
+#     pass
+NUMERAL_MAPPINGS = (
+        (1000, 'M'), (900, 'CM'),
+        (500, 'D'), (400, 'CD'),
+        (100, 'C'), (90, 'XC'),
+        (50, 'L'), (40, 'XL'),
+        (10, 'X'), (9, 'IX'),
+        (5, 'V'), (4, 'IV'),
+        (1, 'I')
+)
+
+
 def roman(number):
-    pass
+    result = ''
+    for arabic_num, roman_num in NUMERAL_MAPPINGS:
+        while number >= arabic_num:
+            result += roman_num
+            number -= arabic_num
+    return result
