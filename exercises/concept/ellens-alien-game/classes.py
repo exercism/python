4d49df686de90ef84de4d101bf1612a3f5cf5c33
Matthias Rahlf <matthias@webding.de>
2022-02-19 09:07:28 +0100

    Fix: update old name new_alien_list

--- exercises/concept/ellens-alien-game/classes.py
+++ exercises/concept/ellens-alien-game/classes.py
@@ -1,7 +1,7 @@
 # TODO: Create a class called "Alien" here
 
 
-def new_alien_list(positions):
+def new_aliens_collection(positions):
     """Function taking a list of position tuples, creating one Alien instance per position.
 
     :param positions: list - A list of tuples of (x, y) coordinates.
