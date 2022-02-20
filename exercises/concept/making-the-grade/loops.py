0e52a974524ca768c65a3d4ef36a2f031948e971
Metallifax <mathew_forgione@msn.com>
2022-02-20 09:54:22 -0400

    Making the Grade: Added newlines in code file docstrings
    Consistent with other code files in concepts that include a docstring

--- exercises/concept/making-the-grade/loops.py
+++ exercises/concept/making-the-grade/loops.py
@@ -1,5 +1,6 @@
 def round_scores(student_scores):
     """
+
     :param student_scores: list of student exam scores as float or int.
     :return: list of student scores *rounded* to nearest integer value.
     """
@@ -9,6 +10,7 @@ def round_scores(student_scores):
 
 def count_failed_students(student_scores):
     """
+
     :param student_scores: list of integer student scores.
     :return: integer count of student scores at or below 40.
     """
@@ -18,6 +20,7 @@ def count_failed_students(student_scores):
 
 def above_threshold(student_scores, threshold):
     """
+
     :param student_scores: list of integer scores
     :param threshold :  integer
     :return: list of integer scores that are at or above the "best" threshold.
@@ -28,6 +31,7 @@ def above_threshold(student_scores, threshold):
 
 def letter_grades(highest):
     """
+
     :param highest: integer of highest exam score.
     :return: list of integer lower threshold scores for each D-A letter grade interval.
              For example, where the highest score is 100, and failing is <= 40,
@@ -44,6 +48,7 @@ def letter_grades(highest):
 
 def student_ranking(student_scores, student_names):
     """
+
      :param student_scores: list of scores in descending order.
      :param student_names: list of names in descending order by exam score.
      :return: list of strings in format ["<rank>. <student name>: <score>"].
@@ -54,6 +59,7 @@ def student_ranking(student_scores, student_names):
 
 def perfect_score(student_info):
     """
+
     :param student_info: list of [<student name>, <score>] lists
     :return: first `[<student name>, 100]` or `[]` if no student score of 100 is found.
     """
