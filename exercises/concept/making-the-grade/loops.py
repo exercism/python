f2ce06ed9748f58f13cb748dcd3694fc9d497f51
Metallifax <mathew_forgione@msn.com>
2022-02-21 16:45:24 -0400

    Making the Grade: Added summary sentences to each docstring in both code files

--- exercises/concept/making-the-grade/loops.py
+++ exercises/concept/making-the-grade/loops.py
@@ -1,5 +1,5 @@
 def round_scores(student_scores):
-    """
+    """Round all student scores in the provided list.
 
     :param student_scores: list of student exam scores as float or int.
     :return: list of student scores *rounded* to nearest integer value.
@@ -9,7 +9,7 @@ def round_scores(student_scores):
 
 
 def count_failed_students(student_scores):
-    """
+    """Count the number of failing students in the provided list.
 
     :param student_scores: list of integer student scores.
     :return: integer count of student scores at or below 40.
@@ -19,7 +19,7 @@ def count_failed_students(student_scores):
 
 
 def above_threshold(student_scores, threshold):
-    """
+    """Count the number of student scores and determine how many performed the best based on the provided threshold.
 
     :param student_scores: list of integer scores
     :param threshold :  integer
@@ -30,7 +30,7 @@ def above_threshold(student_scores, threshold):
 
 
 def letter_grades(highest):
-    """
+    """Create a list of grade thresholds based on the provided highest grade.
 
     :param highest: integer of highest exam score.
     :return: list of integer lower threshold scores for each D-A letter grade interval.
@@ -47,7 +47,7 @@ def letter_grades(highest):
 
 
 def student_ranking(student_scores, student_names):
-    """
+    """Create a list of strings that contain the student's rank, name, and grade in ascending order.
 
      :param student_scores: list of scores in descending order.
      :param student_names: list of names in descending order by exam score.
@@ -58,7 +58,7 @@ def student_ranking(student_scores, student_names):
 
 
 def perfect_score(student_info):
-    """
+    """Create a list that contains the name and grade of the first student to make a perfect score on the exam.
 
     :param student_info: list of [<student name>, <score>] lists
     :return: first `[<student name>, 100]` or `[]` if no student score of 100 is found.
