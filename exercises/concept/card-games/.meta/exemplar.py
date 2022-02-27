49c4c5e4f82030d0d98895e2913330f6c7885b10
Metallifax <mathew_forgione@msn.com>
2022-02-27 19:32:56 -0400

    [Card Games]: Added module public module docstring
    As per title. Also fixed an over-indent in exemplar.py

--- exercises/concept/card-games/.meta/exemplar.py
+++ exercises/concept/card-games/.meta/exemplar.py
@@ -1,8 +1,14 @@
+"""Functions for tracking poker hands and assorted card tasks.
+
+Python list documentation: https://docs.python.org/3/tutorial/datastructures.html
+"""
+
+
 def get_rounds(number):
     """Create a list containing the current and next two round numbers.
 
-     :param number: int - current round number.
-     :return: list - current round and the two that follow.
+    :param number: int - current round number.
+    :return: list - current round and the two that follow.
     """
 
     return [number, number + 1, number + 2]
