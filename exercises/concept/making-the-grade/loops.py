4bf3c813ece174eb616df9fe7ecb46b9cbee0cd0
Metallifax <mathew_forgione@msn.com>
2022-02-26 20:00:19 -0400

    [Making the Grade]: Made summary first lines sentences in each docstring use active voicing
    - For example, create -> creates, or round -> rounds, etc.
    - Removed an extra quotation mark at the start of round_scores docstring

--- exercises/concept/making-the-grade/loops.py
+++ exercises/concept/making-the-grade/loops.py
@@ -1,5 +1,5 @@
 def round_scores(student_scores):
-    """"Round all provided student scores.
+    """Rounds all provided student scores.
 
     :param student_scores: list of student exam scores as float or int.
     :return: list of student scores *rounded* to nearest integer value.
@@ -9,7 +9,7 @@ def round_scores(student_scores):
 
 
 def count_failed_students(student_scores):
-    """Count the number of failing students out of the group provided.
+    """Counts the number of failing students out of the group provided.
 
     :param student_scores: list of integer student scores.
     :return: integer count of student scores at or below 40.
@@ -19,7 +19,7 @@ def count_failed_students(student_scores):
 
 
 def above_threshold(student_scores, threshold):
-    """Determine how many of the provided student scores were 'the best' based on the provided threshold.
+    """Determines how many of the provided student scores were 'the best' based on the provided threshold.
 
     :param student_scores: list of integer scores
     :param threshold :  integer
@@ -30,7 +30,7 @@ def above_threshold(student_scores, threshold):
 
 
 def letter_grades(highest):
-    """Create a list of grade thresholds based on the provided highest grade.
+    """Creates a list of grade thresholds based on the provided highest grade.
 
     :param highest: integer of highest exam score.
     :return: list of integer lower threshold scores for each D-A letter grade interval.
@@ -47,7 +47,7 @@ def letter_grades(highest):
 
 
 def student_ranking(student_scores, student_names):
-    """Organize the student's rank, name, and grade information in ascending order.
+    """Organizes the student's rank, name, and grade information in ascending order.
 
      :param student_scores: list of scores in descending order.
      :param student_names: list of names in descending order by exam score.
@@ -58,7 +58,7 @@ def student_ranking(student_scores, student_names):
 
 
 def perfect_score(student_info):
-    """Create a list that contains the name and grade of the first student to make a perfect score on the exam.
+    """Creates a list that contains the name and grade of the first student to make a perfect score on the exam.
 
     :param student_info: list of [<student name>, <score>] lists
     :return: first `[<student name>, 100]` or `[]` if no student score of 100 is found.
