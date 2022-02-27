0aa2de03443990ac7db67b75f1447d2cb057dff6
Metallifax <mathew_forgione@msn.com>
2022-02-27 13:17:53 -0400

    [Card Games]: Imperative mood for docstring start

--- exercises/concept/card-games/lists.py
+++ exercises/concept/card-games/lists.py
@@ -1,15 +1,15 @@
 def get_rounds(number):
-    """Creates a list containing the current and next two round numbers.
+    """Create a list containing the current and next two round numbers.
 
-     :param number: int - current round number.
-     :return: list - current round and the two that follow.
+    :param number: int - current round number.
+    :return: list - current round and the two that follow.
     """
 
     pass
 
 
 def concatenate_rounds(rounds_1, rounds_2):
-    """Concatenates two lists of round numbers.
+    """Concatenate two lists of round numbers.
 
     :param rounds_1: list - first rounds played.
     :param rounds_2: list - second set of rounds played.
@@ -20,7 +20,7 @@ def concatenate_rounds(rounds_1, rounds_2):
 
 
 def list_contains_round(rounds, number):
-    """Checks if the list of rounds contains the specified number.
+    """Check if the list of rounds contains the specified number.
 
     :param rounds: list - rounds played.
     :param number: int - round number.
@@ -31,7 +31,7 @@ def list_contains_round(rounds, number):
 
 
 def card_average(hand):
-    """Calculates and returns the average card value from the list.
+    """Calculate and returns the average card value from the list.
 
     :param hand: list - cards in hand.
     :return:  float - average value of the cards in the hand.
@@ -41,7 +41,7 @@ def card_average(hand):
 
 
 def approx_average_is_average(hand):
-    """Returns if an average using (first + last index values ) OR ('middle' card) == calculated average.
+    """Return if an average using (first + last index values ) OR ('middle' card) == calculated average.
 
     :param hand: list - cards in hand.
     :return: bool - if approximate average equals to the `true average`.
@@ -51,7 +51,7 @@ def approx_average_is_average(hand):
 
 
 def average_even_is_average_odd(hand):
-    """Returns if the (average of even indexed card values) == (average of odd indexed card values).
+    """Return if the (average of even indexed card values) == (average of odd indexed card values).
 
     :param hand: list - cards in hand.
     :return: bool - are even and odd averages equal?
@@ -61,7 +61,7 @@ def average_even_is_average_odd(hand):
 
 
 def maybe_double_last(hand):
-    """Multiplies a Jack card value in the last index position by 2.
+    """Multiply a Jack card value in the last index position by 2.
 
     :param hand: list - cards in hand.
     :return: list - hand with Jacks (if present) value doubled.
