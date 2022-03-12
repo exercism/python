20fdd2418255ca65caa2dbc1637ee003f83a61f1
Metallifax <mathew_forgione@msn.com>
2022-03-12 13:57:21 -0400

    [Lasagna]: corrected under-indentation in exemplar

--- exercises/concept/guidos-gorgeous-lasagna/.meta/exemplar.py
+++ exercises/concept/guidos-gorgeous-lasagna/.meta/exemplar.py
@@ -11,13 +11,13 @@ PREPARATION_TIME = 2
 def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
 
-   :param elapsed_bake_time: int baking time already elapsed
-   :return: int remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'
+    :param elapsed_bake_time: int baking time already elapsed
+    :return: int remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'
 
-   Function that takes the actual minutes the lasagna has been in the oven as
-   an argument and returns how many minutes the lasagna still needs to bake
-   based on the `EXPECTED_BAKE_TIME`.
-   """
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
 
     return EXPECTED_BAKE_TIME - elapsed_bake_time
 
