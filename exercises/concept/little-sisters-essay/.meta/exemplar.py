f5e4cabd5018e7b168f67380950580af0b53f02e
Metallifax <mathew_forgione@msn.com>
2022-04-02 22:38:04 -0300

    [Sis's Essay]: Added Module and summary docstrings
    Also added dashes as per discussions on style
    
    Began adding periods to the end of return and param descriptions,
    as per discussion in issue this branch is based on brought up by Isaac.
    Will go back and add this to others and make note of other exercises
    that have this inconsistency
    
    removed extra whitespace on lines 5, 15, & 37
    
    added the word 'return' on line 18 so that there is a lowercase letter
    at the starting word of the description

--- exercises/concept/little-sisters-essay/.meta/exemplar.py
+++ exercises/concept/little-sisters-essay/.meta/exemplar.py
@@ -1,28 +1,31 @@
-def capitalize_title(title):
-    """
+"""Functions to help you edit little sister's essays."""
 
-    :param title: str title string that needs title casing
-    :return:  str title string in title case (first letters capitalized)
+
+def capitalize_title(title):
+    """Convert the first letter of each word in the title to uppercase if needed.
+
+    :param title: str - title string that needs title casing.
+    :return: str - title string in title case (first letters capitalized).
     """
 
     return title.title()
 
 
 def check_sentence_ending(sentence):
-    """
+    """Check the ending of the sentence to verify that a period is present.
 
-    :param sentence: str a sentence to check.
-    :return:  bool True if punctuated correctly with period, False otherwise.
+    :param sentence: - str a sentence to check.
+    :return: bool - return True if punctuated correctly with period, False otherwise.
     """
 
     return sentence.endswith(".")
 
 
 def clean_up_spacing(sentence):
-    """
+    """Verify that there isn't any whitespace at the start and end of the sentence.
 
-    :param sentence: str a sentence to clean of leading and trailing space characters.
-    :return: str a sentence that has been cleaned of leading and trailing space characters.
+    :param sentence: str - a sentence to clean of leading and trailing space characters.
+    :return: str - a sentence that has been cleaned of leading and trailing space characters.
     """
 
     clean_sentence = sentence.strip()
@@ -30,12 +33,12 @@ def clean_up_spacing(sentence):
 
 
 def replace_word_choice(sentence, old_word, new_word):
-    """
+    """Replace a word in the provided sentence with a new one.
 
-    :param sentence: str a sentence to replace words in.
-    :param old_word: str word to replace
-    :param new_word: str replacement word
-    :return:  str input sentence with new words in place of old words
+    :param sentence: str - a sentence to replace words in.
+    :param old_word: str - word to replace.
+    :param new_word: str - replacement word.
+    :return: str - input sentence with new words in place of old words.
     """
 
     better_sentence = sentence.replace(old_word, new_word)
