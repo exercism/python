f4e5b74148c0f3f594eb72294056f4104f56e20d
Metallifax <mathew_forgione@msn.com>
2022-04-09 13:38:44 -0300

    [Meltdown]: docstring cleanup
    added periods to param and return lines in all docstrings
    
    line 9 changed to begin description /w lowercase letter (stub & exemp)

--- exercises/concept/meltdown-mitigation/.meta/exemplar.py
+++ exercises/concept/meltdown-mitigation/.meta/exemplar.py
@@ -4,9 +4,9 @@
 def is_criticality_balanced(temperature, neutrons_emitted):
     """Verify criticality is balanced.
 
-    :param temperature: Union[int, float] - temperature value in kelvin (integer or float)
-    :param neutrons_emitted: Union[int, float] - number of neutrons emitted per second (integer or float)
-    :return: bool - True if conditions met, False if not
+    :param temperature: Union[int, float] - temperature value in kelvin (integer or float).
+    :param neutrons_emitted: Union[int, float] - number of neutrons emitted per second (integer or float).
+    :return: bool - return True if conditions met, False if not.
 
     A reactor is said to be critical if it satisfies the following conditions:
     - The temperature is less than 800 K.
@@ -26,10 +26,10 @@ def is_criticality_balanced(temperature, neutrons_emitted):
 def reactor_efficiency(voltage, current, theoretical_max_power):
     """Assess reactor efficiency zone.
 
-    :param voltage: Union[int, float] - voltage value (integer or float)
-    :param current: Union[int, float] - current value (integer or float)
-    :param theoretical_max_power: Union[int, float] - power that corresponds to a 100% efficiency (integer or float)
-    :return: str - one of 'green', 'orange', 'red', or 'black'
+    :param voltage: Union[int, float] - voltage value (integer or float).
+    :param current: Union[int, float] - current value (integer or float).
+    :param theoretical_max_power: Union[int, float] - power that corresponds to a 100% efficiency (integer or float).
+    :return: str - one of 'green', 'orange', 'red', or 'black'.
 
     Efficiency can be grouped into 4 bands:
 
@@ -62,10 +62,10 @@ def reactor_efficiency(voltage, current, theoretical_max_power):
 def fail_safe(temperature, neutrons_produced_per_second, threshold):
     """Assess and return status code for the reactor.
 
-    :param temperature: Union[int, float] - value of the temperature in kelvin (integer or float)
-    :param neutrons_produced_per_second: Union[int, float] - neutron flux (integer or float)
-    :param threshold: Union[int, float] - threshold (integer or float)
-    :return: str - one of: 'LOW', 'NORMAL', 'DANGER'
+    :param temperature: Union[int, float] - value of the temperature in kelvin (integer or float).
+    :param neutrons_produced_per_second: Union[int, float] - neutron flux (integer or float).
+    :param threshold: Union[int, float] - threshold (integer or float).
+    :return: str - one of: 'LOW', 'NORMAL', 'DANGER'.
 
     - `temperature * neutrons per second` < 90% of `threshold` == 'LOW'
     - `temperature * neutrons per second` +/- 10% of `threshold` == 'NORMAL'
