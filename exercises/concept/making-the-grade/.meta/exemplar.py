a72ee6a711d5e9cff951b9d3fd112ee9bcd89b2e
Metallifax <mathew_forgione@msn.com>
2022-04-10 19:46:09 -0300

    [Making the Grade]: docstring cleanup/housekeeping
    added module level docstring in stub and exemplar (both files).
    
    various whitespace and tabbing issues cleaned up (both).
    
    added types to all docstring return and param lines (both).
    
    added some periods to docstring descriptions.
    
    brought the example portion of letter_grades docstring to exemplar

--- exercises/concept/making-the-grade/.meta/exemplar.py
+++ exercises/concept/making-the-grade/.meta/exemplar.py
@@ -1,8 +1,11 @@
+"""Functions for organizing and calculating student exam scores."""
+
+
 def round_scores(student_scores):
     """Round all provided student scores.
 
-    :param student_scores: list of student exam scores as float or int.
-    :return: list of student scores *rounded* to nearest integer value.
+    :param student_scores: [int or float] - list of student exam scores as float or int.
+    :return: [int] - list of student scores *rounded* to nearest integer value.
     """
 
     rounded = []
@@ -14,8 +17,8 @@ def round_scores(student_scores):
 def count_failed_students(student_scores):
     """Count the number of failing students out of the group provided.
 
-    :param student_scores: list of integer student scores.
-    :return: integer count of student scores at or below 40.
+    :param student_scores: [int] - list of integer student scores.
+    :return: int - integer count of student scores at or below 40.
     """
 
     non_passing = 0
@@ -28,9 +31,9 @@ def count_failed_students(student_scores):
 def above_threshold(student_scores, threshold):
     """Determine how many of the provided student scores were 'the best' based on the provided threshold.
 
-    :param student_scores: list of integer scores
-    :param threshold :  integer
-    :return: list of integer scores that are at or above the "best" threshold.
+    :param student_scores: [int] - list of integer scores.
+    :param threshold: int - threshold to cross to be the "best" score.
+    :return: [int] - list of integer scores that are at or above the "best" threshold.
     """
 
     above = []
@@ -44,11 +47,18 @@ def above_threshold(student_scores, threshold):
 def letter_grades(highest):
     """Create a list of grade thresholds based on the provided highest grade.
 
-    :param highest: integer of highest exam score.
-    :return: list of integer score thresholds for each F-A letter grades.
+    :param highest: int - value of highest exam score.
+    :return: [int] - list of lower threshold scores for each D-A letter grade interval.
+            For example, where the highest score is 100, and failing is <= 40,
+            The result would be [41, 56, 71, 86]:
+
+            41 <= "D" <= 55
+            56 <= "C" <= 70
+            71 <= "B" <= 85
+            86 <= "A" <= 100
     """
 
-    increment = round((highest - 40)/4)
+    increment = round((highest - 40) / 4)
     scores = []
     for score in range(41, highest, increment):
         scores.append(score)
@@ -58,10 +68,10 @@ def letter_grades(highest):
 def student_ranking(student_scores, student_names):
     """Organize the student's rank, name, and grade information in ascending order.
 
-     :param student_scores: list of scores in descending order.
-     :param student_names: list of names in descending order by exam score.
-     :return: list of strings in format ["<rank>. <student name>: <score>"].
-     """
+    :param student_scores: [int] - list of scores in descending order.
+    :param student_names: [str] - list of names in descending order by exam score.
+    :return: [str] - list of strings in format ["<rank>. <student name>: <score>"].
+    """
 
     results = []
     for index, name in enumerate(student_names):
@@ -74,7 +84,7 @@ def student_ranking(student_scores, student_names):
 def perfect_score(student_info):
     """Create a list that contains the name and grade of the first student to make a perfect score on the exam.
 
-    :param student_info: list of [<student name>, <score>] lists
+    :param student_info: list of [<student name>, <score>] lists.
     :return: first `[<student name>, 100]` or `[]` if no student score of 100 is found.
     """
 
