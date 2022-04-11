eeaee9f54c64322fe3a0c338912507f0b7b80a99
Metallifax <mathew_forgione@msn.com>
2022-04-10 21:17:21 -0300

    [Treasure]: docstring cleanup & housekeeping
    added module level docstrings to both stub and exemplar (both files).
    
    added summary first line docstrings (both).
    
    converted the 'tuple' types to tuple syntax with nested
    types in param and return lines in docstrings
    (not attached,  its consistent with other concepts were altered though)
    
    added periods at the end of some descriptions (both).
    
    removed some extra whitespaces (both)
    
    added a newline on line 42 in exemplar
    
    grammar fix on line 50 in stub, line 55 in exemplar
    
    changed the return line of 'compare_records' docstring to start
    with lowercase letter (both)

--- exercises/concept/tisbury-treasure-hunt/tuples.py
+++ exercises/concept/tisbury-treasure-hunt/tuples.py
@@ -1,7 +1,10 @@
-def get_coordinate(record):
-    """
+"""Functions to help Azara and Rui locate pirate treasure."""
 
-    :param record: tuple - a (treasure, coordinate) pair.
+
+def get_coordinate(record):
+    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
+
+    :param record: (str, str) - a tuple with a (treasure, coordinate) pair.
     :return: str - the extracted map coordinate.
     """
 
@@ -9,42 +12,42 @@ def get_coordinate(record):
 
 
 def convert_coordinate(coordinate):
-    """
+    """Create and return a tuple containing two characters from the treasure coordinate.
 
     :param coordinate: str - a string map coordinate
-    :return:  tuple - the string coordinate seperated into its individual components.
+    :return: (str, str) - the string coordinate seperated into its individual components.
     """
 
     pass
 
 
 def compare_records(azara_record, rui_record):
-    """
+    """Compare two record types and determine if the coordinates match.
 
-    :param azara_record: tuple - a (treasure, coordinate) pair.
-    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
-    :return: bool - True if coordinates match, False otherwise.
+    :param azara_record: (str, str) - a (treasure, coordinate) pair.
+    :param rui_record: (str, (str, str), str) - a (location, coordinate, quadrant) trio.
+    :return: bool - returns True if coordinates match, False otherwise.
     """
 
     pass
 
 
 def create_record(azara_record, rui_record):
-    """
+    """Combine the two record types (if possible) and create a combined record group, else return "not a match".
 
-    :param azara_record: tuple - a (treasure, coordinate) pair.
-    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
-    :return:  tuple - combined record, or "not a match" if the records are incompatible.
+    :param azara_record: (str, str) - a (treasure, coordinate) pair.
+    :param rui_record: (str, (str, str), str) - a (location, coordinate, quadrant) trio.
+    :return: (str, str, str, (str, str), str) or str - combined record, or "not a match" if the records are incompatible.
     """
 
     pass
 
 
 def clean_up(combined_record_group):
-    """
+    """Clean up a combined record group into a multi-line string of single records.
 
-    :param combined_record_group: tuple of tuples - everything from both participants.
-    :return: string of tuples separated by newlines - everything "cleaned". Excess coordinates and information removed.
+    :param combined_record_group: ((str, str, str, (str, str), str), ...) - everything from both participants.
+    :return: str - everything "cleaned", excess coordinates and information are removed.
     """
 
     pass
