273f5f991ed37ea22f0bbc9cf75bfc074378d5f5
Metallifax <mathew_forgione@msn.com>
2022-04-11 00:29:50 -0300

    [Cater Waiter]: docstring cleanup & housekeeping
    added a module level docstring to both stub and exemplar (both files)
    
    added summary first line docstrings to all (both).
    
    added dashes and periods to return/param line descriptions. (both)
    
    added missing param and return line descriptions to docstrings (both)
    
    cleaned up the 'intersection' param docstrings -- the long list
    of constant var names was moved to the additional notes section (both)
    
    removed various extra whitespaces (both)
    
    'singleton_ingredients' dishes & intersection params swapped places
    (both)
    
    newline on line 104 in stub
    
    newline on line 124 in exemplar

--- exercises/concept/cater-waiter/.meta/exemplar.py
+++ exercises/concept/cater-waiter/.meta/exemplar.py
@@ -1,3 +1,6 @@
+"""Functions for compiling dishes and ingredients for a catering company."""
+
+
 from sets_categories_data import (VEGAN,
                                   VEGETARIAN,
                                   KETO,
@@ -8,11 +11,11 @@ from sets_categories_data import (VEGAN,
 
 
 def clean_ingredients(dish_name, dish_ingredients):
-    """
+    """Remove duplicates from `dish_ingredients` and return a tuple containing `dish_name` and the cleaned list.
 
-    :param dish_name: str
-    :param dish_ingredients: list
-    :return: tuple of (dish_name, ingredient set)
+    :param dish_name: str - containing the dish name.
+    :param dish_ingredients: list - dish ingredients.
+    :return: tuple - containing (dish_name, ingredient set).
 
     This function should return a `tuple` with the name of the dish as the first item,
     followed by the de-duped `set` of ingredients as the second item.
@@ -22,11 +25,11 @@ def clean_ingredients(dish_name, dish_ingredients):
 
 
 def check_drinks(drink_name, drink_ingredients):
-    """
+    """Append "Mocktail" or "Cocktail" to `dish_name` if alcoholic ingredients are present in `drink_ingredients`.
 
-    :param drink_name: str
-    :param drink_ingredients: list
-    :return: str drink name + ("Mocktail" or "Cocktail")
+    :param drink_name: str - name of the drink.
+    :param drink_ingredients: list - ingredients in the drink.
+    :return: str - drink name appended with "Mocktail" or "Cocktail".
 
     The function should return the name of the drink followed by "Mocktail" if the drink has
     no alcoholic ingredients, and drink name followed by "Cocktail" if the drink includes alcohol.
@@ -39,14 +42,14 @@ def check_drinks(drink_name, drink_ingredients):
 
 
 def categorize_dish(dish_name, dish_ingredients):
-    """
+    """Categorize `dish_name` by appending its classification based on the `dish_ingredients` list.
 
-    :param dish_name: str
-    :param dish_ingredients: list
-    :return: str "dish name: CATEGORY"
+    :param dish_name: str - dish to be categorized.
+    :param dish_ingredients: list - of ingredients for the dish.
+    :return: str - the dish name appended with ": <CATEGORY>".
 
     This function should return a string with the `dish name: <CATEGORY>` (which meal category the dish belongs to).
-    All dishes will "fit" into one of the categories imported from `sets_categories_data`
+    All dishes will "fit" into one of the categories imported from `sets_categories_data.py`
     (VEGAN, VEGETARIAN, PALEO, KETO, or OMNIVORE).
     """
 
@@ -63,24 +66,24 @@ def categorize_dish(dish_name, dish_ingredients):
 
 
 def tag_special_ingredients(dish):
-    """
+    """Compare `dish` ingredients to `SPECIAL_INGREDIENTS`, return a tuple with dish name and matching items in a set.
 
-    :param dish: tuple of (str of dish name, list of dish ingredients)
-    :return: tuple of (str of dish name, set of dish special ingredients)
+    :param dish: tuple - of (str of dish name, list of dish ingredients).
+    :return: tuple - of (str of dish name, set of dish special ingredients).
 
     Return the dish name followed by the `set` of ingredients that require a special note on the dish description.
     For the purposes of this exercise, all allergens or special ingredients that need to be tracked are in the
-    SPECIAL_INGREDIENTS constant imported from `sets_categories_data`.
+    SPECIAL_INGREDIENTS constant imported from `sets_categories_data.py`.
     """
 
     return dish[0], (SPECIAL_INGREDIENTS & set(dish[1]))
 
 
 def compile_ingredients(dishes):
-    """
+    """Create a master list of ingredients using the list of sets of ingredients from `dishes`.
 
-    :param dishes: list of dish ingredient sets
-    :return: set
+    :param dishes: list - of dish ingredient sets.
+    :return: set - of dishes compiled from ingredients sets.
 
     This function should return a `set` of all ingredients from all listed dishes.
     """
@@ -94,11 +97,11 @@ def compile_ingredients(dishes):
 
 
 def separate_appetizers(dishes, appetizers):
-    """
+    """From the list of `dishes`, determine which are `appetizers` and return the result as a list.
 
-    :param dishes: list of dish names
-    :param appetizers: list of appetizer names
-    :return: list of dish names
+    :param dishes: list - of dish names.
+    :param appetizers: list - of appetizer names.
+    :return: list - of dish names.
 
     The function should return the list of dish names with appetizer names removed.
     Either list could contain duplicates and may require de-duping.
@@ -108,15 +111,17 @@ def separate_appetizers(dishes, appetizers):
 
 
 def singleton_ingredients(dishes, intersection):
-    """
+    """Use `example_dishes` and `EXAMPLE_INTERSECTION` to determine which dishes have a singleton ingredient.
 
-    :param intersection: constant - one of (VEGAN_INTERSECTION,VEGETARIAN_INTERSECTION,PALEO_INTERSECTION,
-                                            KETO_INTERSECTION,OMNIVORE_INTERSECTION)
-    :param dishes:  list of ingredient sets
-    :return: set of singleton ingredients
+    :param dishes: list - of ingredient sets.
+    :param intersection: constant - can be one of `EXAMPLE_INTERSECTION` constants.
+    :return: set - containing singleton ingredients.
 
     Each dish is represented by a `set` of its ingredients.
-    Each `<CATEGORY>_INTERSECTION` is an `intersection` of all dishes in the category.
+
+    Each `<CATEGORY>_INTERSECTION` is an `intersection` of all dishes in the category and can be either:
+        VEGAN_INTERSECTION, VEGETARIAN_INTERSECTION, PALEO_INTERSECTION, KETO_INTERSECTION, or OMNIVORE_INTERSECTION.
+
     The function should return a `set` of ingredients that only appear in a single dish.
     """
 
