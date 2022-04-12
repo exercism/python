475af106f780ba6a89ff445b133615dd5c3e2d1f
Metallifax <mathew_forgione@msn.com>
2022-04-12 00:09:52 -0300

    [Grade]: docstring list type change and grammar
    per title, reverting the list type changes so
    docstrings are easier to read in both stub and exemplar (both)
    
    missing types and dashes in `perfect_score` (both)

--- exercises/concept/making-the-grade/.meta/exemplar.py
+++ exercises/concept/making-the-grade/.meta/exemplar.py
@@ -4,8 +4,8 @@
 def round_scores(student_scores):
     """Round all provided student scores.
 
-    :param student_scores: [int or float] - list of student exam scores as float or int.
-    :return: [int] - list of student scores *rounded* to nearest integer value.
+    :param student_scores: list - student exam scores as float or int.
+    :return: list - student scores *rounded* to nearest integer value.
     """
 
     rounded = []
@@ -17,8 +17,8 @@ def round_scores(student_scores):
 def count_failed_students(student_scores):
     """Count the number of failing students out of the group provided.
 
-    :param student_scores: [int] - list of integer student scores.
-    :return: int - integer count of student scores at or below 40.
+    :param student_scores: list - list of integer student scores.
+    :return: int - count of student scores at or below 40.
     """
 
     non_passing = 0
@@ -31,9 +31,9 @@ def count_failed_students(student_scores):
 def above_threshold(student_scores, threshold):
     """Determine how many of the provided student scores were 'the best' based on the provided threshold.
 
-    :param student_scores: [int] - list of integer scores.
+    :param student_scores: list - of integer scores.
     :param threshold: int - threshold to cross to be the "best" score.
-    :return: [int] - list of integer scores that are at or above the "best" threshold.
+    :return: list - of integer scores that are at or above the "best" threshold.
     """
 
     above = []
@@ -48,7 +48,7 @@ def letter_grades(highest):
     """Create a list of grade thresholds based on the provided highest grade.
 
     :param highest: int - value of highest exam score.
-    :return: [int] - list of lower threshold scores for each D-A letter grade interval.
+    :return: list - of lower threshold scores for each D-A letter grade interval.
             For example, where the highest score is 100, and failing is <= 40,
             The result would be [41, 56, 71, 86]:
 
@@ -68,9 +68,9 @@ def letter_grades(highest):
 def student_ranking(student_scores, student_names):
     """Organize the student's rank, name, and grade information in ascending order.
 
-    :param student_scores: [int] - list of scores in descending order.
-    :param student_names: [str] - list of names in descending order by exam score.
-    :return: [str] - list of strings in format ["<rank>. <student name>: <score>"].
+    :param student_scores: list - of scores in descending order.
+    :param student_names: list - of string names by exam score in descending order.
+    :return: list - of strings in format ["<rank>. <student name>: <score>"].
     """
 
     results = []
@@ -84,8 +84,8 @@ def student_ranking(student_scores, student_names):
 def perfect_score(student_info):
     """Create a list that contains the name and grade of the first student to make a perfect score on the exam.
 
-    :param student_info: list of [<student name>, <score>] lists.
-    :return: first `[<student name>, 100]` or `[]` if no student score of 100 is found.
+    :param student_info: list - of [<student name>, <score>] lists.
+    :return: list - first `[<student name>, 100]` or `[]` if no student score of 100 is found.
     """
 
     result = []
