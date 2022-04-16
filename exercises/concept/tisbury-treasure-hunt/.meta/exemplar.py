2c10b2409ce2d0b39d888111024a0effb9b622b3
BethanyG <BethanyG@users.noreply.github.com>
2022-04-16 16:51:49 -0700

    [Tisbury], [Making the Grade], [Inventory Management] Apply suggestions from code review

--- exercises/concept/tisbury-treasure-hunt/.meta/exemplar.py
+++ exercises/concept/tisbury-treasure-hunt/.meta/exemplar.py
@@ -12,20 +12,20 @@ def get_coordinate(record):
 
 
 def convert_coordinate(coordinate):
-    """Create and return a tuple containing two characters from the treasure coordinate.
+    """Split the given coordinate into tuple containing its individual components.
 
     :param coordinate: str - a string map coordinate
-    :return: tuple - the string coordinate seperated into its individual components.
+    :return: tuple - the string coordinate split into its individual components.
     """
 
     return tuple(coordinate)
 
 
 def compare_records(azara_record, rui_record):
-    """Compare two record types and determine if the coordinates match.
+    """Compare two record types and determine if their coordinates match.
 
     :param azara_record: tuple - a (treasure, coordinate) pair.
-    :param rui_record: tuple - a (location, coordinate, quadrant) trio with a nested tuple.
+    :param rui_record: tuple - a (location, tuple(coordinate_1, coordinate_2), quadrant) trio.
     :return: bool - returns True if coordinates match, False otherwise.
     """
 
@@ -33,11 +33,11 @@ def compare_records(azara_record, rui_record):
 
 
 def create_record(azara_record, rui_record):
-    """Combine the two record types (if possible) and create a combined record group, else return "not a match".
+    """Combine the two record types (if possible) and create a combined record group.
 
     :param azara_record: tuple - a (treasure, coordinate) pair.
     :param rui_record: tuple - a (location, coordinate, quadrant) trio.
-    :return: tuple or str - combined record, or "not a match" if the records are incompatible.
+    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
     """
 
     result = "not a match"
