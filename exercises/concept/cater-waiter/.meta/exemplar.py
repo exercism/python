466e5e1ab751b3531d68cf5280ba94bc3f74c208
BethanyG <BethanyG@users.noreply.github.com>
2022-04-16 15:36:48 -0700

    [Cater Waiter] & [Lil Sisters Essay] Code Review Changes
    More docstring edits

--- exercises/concept/cater-waiter/.meta/exemplar.py
+++ exercises/concept/cater-waiter/.meta/exemplar.py
@@ -25,14 +25,14 @@ def clean_ingredients(dish_name, dish_ingredients):
 
 
 def check_drinks(drink_name, drink_ingredients):
-    """Append "Cocktail" (alcoholic)  or "Mocktail" (no alcohol) to `drink_name`, based on the content of `drink_ingredients`.
+    """Append "Cocktail" (alcohol)  or "Mocktail" (no alcohol) to `drink_name`, based on `drink_ingredients`.
 
     :param drink_name: str - name of the drink.
     :param drink_ingredients: list - ingredients in the drink.
     :return: str - drink_name appended with "Mocktail" or "Cocktail".
 
-    The function should return the name of the drink followed by "Mocktail" if the drink has
-    no alcoholic ingredients, and drink name followed by "Cocktail" if the drink includes alcohol.
+    The function should return the name of the drink followed by "Mocktail" (non-alcoholic) and drink
+    name followed by "Cocktail" (includes alcohol).
     """
 
     if not ALCOHOLS.isdisjoint(drink_ingredients):
@@ -42,15 +42,16 @@ def check_drinks(drink_name, drink_ingredients):
 
 
 def categorize_dish(dish_name, dish_ingredients):
-    """Categorize `dish_name` and append its classification based on `dish_ingredients`.
+    """Categorize `dish_name` based on `dish_ingredients`.
 
     :param dish_name: str - dish to be categorized.
-    :param dish_ingredients: list - of ingredients for the dish.
+    :param dish_ingredients: list - ingredients for the dish.
     :return: str - the dish name appended with ": <CATEGORY>".
 
     This function should return a string with the `dish name: <CATEGORY>` (which meal category the dish belongs to).
+    `<CATEGORY>` can be any one of  (VEGAN, VEGETARIAN, PALEO, KETO, or OMNIVORE).
     All dishes will "fit" into one of the categories imported from `sets_categories_data.py`
-    (VEGAN, VEGETARIAN, PALEO, KETO, or OMNIVORE).
+
     """
 
     categories = ((VEGAN, 'VEGAN'),
@@ -68,7 +69,7 @@ def categorize_dish(dish_name, dish_ingredients):
 def tag_special_ingredients(dish):
     """Compare `dish` ingredients to `SPECIAL_INGREDIENTS`.
 
-    :param dish: tuple - of (str of dish name, list of dish ingredients).
+    :param dish: tuple - of (dish name, list of dish ingredients).
     :return: tuple - containing (dish name, dish special ingredients).
 
     Return the dish name followed by the `set` of ingredients that require a special note on the dish description.
@@ -120,7 +121,7 @@ def singleton_ingredients(dishes, intersection):
     Each dish is represented by a `set` of its ingredients.
 
     Each `<CATEGORY>_INTERSECTION` is an `intersection` of all dishes in the category. `<CATEGORY>` can be any one of:
-        VEGAN, VEGETARIAN, PALEO, KETO, or OMNIVORE.
+        (VEGAN, VEGETARIAN, PALEO, KETO, or OMNIVORE).
 
     The function should return a `set` of ingredients that only appear in a single dish.
     """
