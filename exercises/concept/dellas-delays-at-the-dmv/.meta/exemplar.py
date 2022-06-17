2d15c60f8eb835f2db419efc9a2f695d4d2551f8
Bob Hoeppner <32035397+bobahop@users.noreply.github.com>
2022-06-17 11:31:57 -0500

    Update exemplar.py

--- exercises/concept/dellas-delays-at-the-dmv/.meta/exemplar.py
+++ exercises/concept/dellas-delays-at-the-dmv/.meta/exemplar.py
@@ -1,1 +1,13 @@
-# just a stub right now
+# really just a stub right now
+
+def line_length(oclock):
+    if oclock == 9 or oclock == 4:
+        return 10
+    if oclock == 12:
+        return 20
+    return 5
+
+
+def wait_minutes(oclock):
+    return line_length(oclock) * 5
+  
