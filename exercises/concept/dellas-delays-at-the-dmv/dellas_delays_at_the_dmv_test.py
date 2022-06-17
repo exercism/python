7c9642fc47cb737571716de1f469e4bb2c5273ae
Bob Hoeppner <32035397+bobahop@users.noreply.github.com>
2022-06-17 11:15:28 -0500

    Update dellas_delays_at_the_dmv_test.py

--- exercises/concept/dellas-delays-at-the-dmv/dellas_delays_at_the_dmv_test.py
+++ exercises/concept/dellas-delays-at-the-dmv/dellas_delays_at_the_dmv_test.py
@@ -1,12 +1,10 @@
-# this is really just a stub
-
 import unittest
 import pytest
 
 from dellas_delays_at_the_dmv import (
-                                      line_length,
-                                      wait_minutes
-                                      )
+    line_length,
+    wait_minutes
+)
 
 
 class DellasDelaysTest(unittest.TestCase):
@@ -14,25 +12,25 @@ class DellasDelaysTest(unittest.TestCase):
     @pytest.mark.task(taskno=1)
     def test_line_length(self):
         data = [
-                (9, 10), (10, 5), (11, 5),
-                (12, 20), (1, 5), (2, 5),
-                (3, 5), (4, 10)]
+            (9, 10), (10, 5), (11, 5),
+            (12, 20), (1, 5), (2, 5),
+            (3, 5), (4, 10)]
 
         for variant, (hour, length) in enumerate(data, 1):
             with self.subTest(f'variation #{variant}', input=hour, output=length):
                 error_msg = f"Expected {length} for the length of the line at {hour} o'clock."
 
                 self.assertEqual(line_length(hour), length, msg=error_msg)
-                
+
     @pytest.mark.task(taskno=2)
-        def test_wait_minutes(self):
+    def test_wait_minutes(self):
         data = [
-                (9, 50), (10, 25), (11, 25),
-                (12, 100), (1, 25), (2, 25),
-                (3, 25), (4, 50)]
+            (9, 50), (10, 25), (11, 25),
+            (12, 100), (1, 25), (2, 25),
+            (3, 25), (4, 50)]
 
         for variant, (hour, minutes) in enumerate(data, 1):
             with self.subTest(f'variation #{variant}', input=hour, output=minutes):
                 error_msg = f"Expected {minutes} for the minutes to wait at {hour} o'clock."
 
-                    self.assertEqual(wait_minutes(hour), minutes, msg=error_msg)
+                self.assertEqual(wait_minutes(hour), minutes, msg=error_msg)
