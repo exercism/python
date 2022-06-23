d8bd67d054ea7425c624b81c0327055f5b3e32cf
Bob Hoeppner <32035397+bobahop@users.noreply.github.com>
2022-06-23 05:43:17 -0500

    Update dellas_delays_at_the_dmv.py

--- exercises/concept/dellas-delays-at-the-dmv/dellas_delays_at_the_dmv.py
+++ exercises/concept/dellas-delays-at-the-dmv/dellas_delays_at_the_dmv.py
@@ -8,3 +8,15 @@ def wait_minutes(oclock):
     pass
 
 wait_time = None
+
+
+def respond(greeting):
+    response = None
+    secret_thought = ""
+
+    def thinking():
+        nonlocal secret_thought
+        pass
+    thinking()
+    return f"{response} {secret_thought}"
+
