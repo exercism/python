d60932f0f94e0af20d7c53771c8a623ba024fe3a
ThomasWard <22226719+tward333@users.noreply.github.com>
2022-07-13 20:48:42 -0400

    Update sets.py
    Update helper text to maintain consistency of constant names between docs and sets_categories_data.py

--- exercises/concept/cater-waiter/sets.py
+++ exercises/concept/cater-waiter/sets.py
@@ -99,12 +99,12 @@ def singleton_ingredients(dishes, intersection):
     """Determine which `dishes` have a singleton ingredient (an ingredient that only appears once across dishes).
 
     :param dishes: list - of ingredient sets.
-    :param intersection: constant - can be one of `<CATEGORY>_INTERSECTION` constants imported from `sets_categories_data.py`.
+    :param intersection: constant - can be one of `<CATEGORY>_INTERSECTIONS` constants imported from `sets_categories_data.py`.
     :return: set - containing singleton ingredients.
 
     Each dish is represented by a `set` of its ingredients.
 
-    Each `<CATEGORY>_INTERSECTION` is an `intersection` of all dishes in the category. `<CATEGORY>` can be any one of:
+    Each `<CATEGORY>_INTERSECTIONS` is an `intersection` of all dishes in the category. `<CATEGORY>` can be any one of:
         (VEGAN, VEGETARIAN, PALEO, KETO, or OMNIVORE).
 
     The function should return a `set` of ingredients that only appear in a single dish.
