16ad9f1dd622030d8f02968faf45db700e80631f
Pranas Ziaukas <pranas@balena.io>
2022-08-25 14:22:09 +0300

    [Currency Exchange] Improve the `int`/`float` usage consistency

--- exercises/concept/currency-exchange/.meta/exemplar.py
+++ exercises/concept/currency-exchange/.meta/exemplar.py
@@ -13,7 +13,7 @@ def get_change(budget, exchanging_value):
     """
 
     :param budget: float - amount of money you own.
-    :param exchanging_value: int - amount of your money you want to exchange now.
+    :param exchanging_value: float - amount of your money you want to exchange now.
     :return: float - amount left of your starting currency after exchanging.
     """
 
@@ -24,11 +24,11 @@ def get_value_of_bills(denomination, number_of_bills):
     """
 
     :param denomination: int - the value of a bill.
-    :param number_of_bills: int amount of bills you received.
+    :param number_of_bills: int - amount of bills you received.
     :return: int - total value of bills you now have.
     """
 
-    return number_of_bills * denomination
+    return denomination * number_of_bills
 
 
 def get_number_of_bills(budget, denomination):
@@ -39,7 +39,18 @@ def get_number_of_bills(budget, denomination):
     :return: int - number of bills after exchanging all your money.
     """
 
-    return int(budget / denomination)
+    return int(budget) // denomination
+
+
+def get_leftover_of_bills(budget, denomination):
+    """
+
+    :param budget: float - the amount of money you are planning to exchange.
+    :param denomination: int - the value of a single bill.
+    :return: float - the leftover amount that cannot be exchanged given the current denomination.
+    """
+
+    return budget % denomination
 
 
 def exchangeable_value(budget, exchange_rate, spread, denomination):
@@ -53,22 +64,7 @@ def exchangeable_value(budget, exchange_rate, spread, denomination):
     """
 
     exchange_fee = (exchange_rate / 100) * spread
-    actual_rate = exchange_rate + exchange_fee
-    exchangeable_amount = int((budget / actual_rate) / denomination)
-    return exchangeable_amount * denomination
-
-
-def non_exchangeable_value(budget, exchange_rate, spread, denomination):
-    """
-
-    :param budget: float - amount of money you are planning to exchange.
-    :param exchange_rate: float - unit value of the foreign currency.
-    :param spread: int - the percentage taken as an exchange fee.
-    :param denomination:  int - the value of a single bill.
-    :return: int - the value that cannot be exchanged, due to the denomination.
-    """
-
-    exchange_fee = (exchange_rate / 100) * spread
-    actual_rate = exchange_rate + exchange_fee
-    non_exchangeable_amount = int((budget / actual_rate) % denomination)
-    return non_exchangeable_amount
+    exchange_value = exchange_money(budget, exchange_rate + exchange_fee)
+    number_of_bills = get_number_of_bills(exchange_value, denomination)
+    value_of_bills = get_value_of_bills(denomination, number_of_bills)
+    return value_of_bills
