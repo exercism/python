f709502ceb804c3a5efc9fedee17b2157d5817cb
Bob Hoeppner <32035397+bobahop@users.noreply.github.com>
2022-11-17 05:58:12 -0600

    Update Benchmark.py

--- exercises/practice/pangram/.articles/performance/code/Benchmark.py
+++ exercises/practice/pangram/.articles/performance/code/Benchmark.py
@@ -18,8 +18,8 @@ from string import ascii_lowercase
 
 ALPHABET = set(ascii_lowercase)
 
-def is_pangram(string):
-    return ALPHABET.issubset(string.lower())
+def is_pangram(sentence):
+    return ALPHABET.issubset(sentence.lower())
 
 """, number=loops) / loops
 
