5a4ec07c882b6bf1f33a0c47e898cd95127f5572
meatball <69751659+meatball133@users.noreply.github.com>
2022-11-20 15:00:15 +0100

    Added filles for exercise

---
+++ exercises/concept/locomotive-engineer/locomotive-engineer.py
@@ -0,0 +1,27 @@
+"""Functions used in preparing Guido's gorgeous lasagna.
+
+Learn about Guido, the creator of the Python language: https://en.wikipedia.org/wiki/Guido_van_Rossum
+"""
+
+# TODO: define the 'get_list_of_wagons' function
+
+# TODO: define the 'fixListOfWagons()' function
+def fix_list_of_wagons():
+    """Calculate the bake time remaining.
+
+    :param elapsed_bake_time: int - baking time already elapsed.
+    :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+
+    pass
+
+
+# TODO: define the 'preparation_time_in_minutes()' function
+#       and consider using 'PREPARATION_TIME' here
+
+
+# TODO: define the 'elapsed_time_in_minutes()' function
