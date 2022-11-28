34b1f849e687a94fd6cd8be4472707dc917394cc
Guillermo <106453304+Guillermo-N@users.noreply.github.com>
2022-11-28 06:36:08 +0100

    Finalazing tests.

--- exercises/concept/diamond-problem/diamond_inheritance_test.py
+++ exercises/concept/diamond-problem/diamond_inheritance_test.py
@@ -40,33 +40,47 @@ class InventoryTest(unittest.TestCase):
     def test_class_inheritance_order(self):
         e = diamond.ClassE()
         error = "The constructor calls seem to be in the wrong order."
-        self.assertEquals("ADBCE", e.sequence, msg = error)
+        self.assertEqual("ADBCE", e.sequence, msg = error)
 
     @pytest.mark.task(taskno=7)
     def test_ClassB_sequence_as_tuple_method(self):
         error = "Method <sequence_as_tuple> not working correctly for class B."
         b = diamond.ClassB()
-        self.assertTupleEqual(('A', 'B'), b.sequence_as_tuple())
+        self.assertTupleEqual(('A', 'B'), b.sequence_as_tuple(), msg = error)
     
     @pytest.mark.task(taskno=8)
     def test_ClassC_sequence_as_tuple_method(self):
         error = "Method <sequence_as_tuple> not working correctly for class C."
         c = diamond.ClassC()
-        self.assertTupleEqual(('A', 'C'), c.sequence_as_tuple())
+        self.assertTupleEqual(('A', 'C'), c.sequence_as_tuple(), msg = error)
     
     @pytest.mark.task(taskno=9)
     def test_ClassD_sequence_as_tuple_method(self):
         error = "Method <sequence_as_tuple> not working correctly for class D."
         d = diamond.ClassD()
-        self.assertTupleEqual(('A', 'D'), d.sequence_as_tuple())
+        self.assertTupleEqual(('A', 'D'), d.sequence_as_tuple(), msg = error)
     
     @pytest.mark.task(taskno=10)
     def test_ClassE_sequence_as_tuple_method(self):
         error = "Method <sequence_as_tuple> not working correctly for class E."
         e = diamond.ClassE()
-        self.assertTupleEqual(('A', 'D', 'B', 'C', 'E'), e.sequence_as_tuple())
+        self.assertTupleEqual(('A', 'D', 'B', 'C', 'E'), e.sequence_as_tuple(), msg = error)
+    
+    @pytest.mark.task(taskno=11)
+    def test_mixin_class_present(self):
+        error = "Try to implement a mix in class."
+        self.assertRegex(" ".join(dir(diamond)), ' .*Mixin ', msg = error)
 
-        
+    @pytest.mark.task(taskno=12)
+    def test_mixin_class_in_use(self):
+        error = "Double check your mix-in class."
+        b = diamond.ClassB()
+        c = diamond.ClassC()
+        d = diamond.ClassD()
+        mixin = lambda x: x.sequence_as_tuple.__repr__().split()[2]
+        self.assertEqual(mixin(b), mixin(c), msg = error)
+        self.assertEqual(mixin(c), mixin(d), msg = error)
+        self.assertEqual(mixin(b), mixin(d), msg = error)
 
 
 if __name__ == "__main__":
