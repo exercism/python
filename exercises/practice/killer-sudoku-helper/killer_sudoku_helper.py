2c102dcef1e0a1325d188c08b0f1db827e135075
Meatball <meatballsofmeatballs@gmail.com>
2022-12-05 21:26:06 +0100

    Fix

--- exercises/practice/killer-sudoku-helper/killer_sudoku_helper.py
+++ exercises/practice/killer-sudoku-helper/killer_sudoku_helper.py
@@ -1,2 +1,13 @@
+import itertools
+
 def combinations(target, size, exclude):
-    pass
+    result = []
+    if size == 1:
+        return [[target]]
+    else:
+        possible = [index for index in range(1, int((target ** 2 / size) ** 0.6)) if index not in exclude]
+        for index in range(len(possible), 0, -1):
+            for seq in itertools.combinations(possible, index):
+                if sum(seq) == target and len(seq) == size:
+                    result.append(list(seq))
+    return result
