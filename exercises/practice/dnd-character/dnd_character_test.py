0cbb955be680886ac9b51b63ed3e3f5d98d2fb71
Carl <meatballsofmeatballs@gmail.com>
2022-12-27 21:48:54 +0100

    minor improvments

--- exercises/practice/dnd-character/dnd_character_test.py
+++ exercises/practice/dnd-character/dnd_character_test.py
@@ -59,22 +59,18 @@ class DndCharacterTest(unittest.TestCase):
 
     def test_random_ability_is_within_range(self):
         score = Character().ability()
-        self.assertIs(score >= 3 and score <= 18, True)
+        self.assertTrue(score >= 3 and score <= 18)
 
     def test_random_character_is_valid(self):
         Char = Character()
-        self.assertIs(Char.strength >= 3 and Char.strength <= 18, True)
-        self.assertIs(Char.dexterity >= 3 and Char.dexterity <= 18, True)
-        self.assertIs(Char.constitution >= 3 and Char.constitution <= 18, True)
-        self.assertIs(Char.intelligence >= 3 and Char.intelligence <= 18, True)
-        self.assertIs(Char.wisdom >= 3 and Char.wisdom <= 18, True)
-        self.assertIs(Char.charisma >= 3 and Char.charisma <= 18, True)
-        self.assertIs(Char.hitpoints == 10 + modifier(Char.constitution), True)
+        self.assertTrue(Char.strength >= 3 and Char.strength <= 18)
+        self.assertTrue(Char.dexterity >= 3 and Char.dexterity <= 18)
+        self.assertTrue(Char.constitution >= 3 and Char.constitution <= 18)
+        self.assertTrue(Char.intelligence >= 3 and Char.intelligence <= 18)
+        self.assertTrue(Char.wisdom >= 3 and Char.wisdom <= 18)
+        self.assertTrue(Char.charisma >= 3 and Char.charisma <= 18)
+        self.assertTrue(Char.hitpoints == 10 + modifier(Char.constitution))
 
     def test_each_ability_is_only_calculated_once(self):
         Char = Character()
-        self.assertIs(Char.strength == Char.strength, True)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        self.assertTrue(Char.strength == Char.strength)
