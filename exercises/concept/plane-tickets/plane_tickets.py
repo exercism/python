1cc64ac300f3eac61ef5b07a26561709b3d36211
Carl <meatballsofmeatballs@gmail.com>
2022-12-29 14:30:16 +0100

    Updated doc-string

--- exercises/concept/plane-tickets/plane_tickets.py
+++ exercises/concept/plane-tickets/plane_tickets.py
@@ -3,8 +3,8 @@
 def generate_seat_letters(amount):
     """ Generate a series of seat letters for airline boarding.
 
-    :param amount: Amount of seat letters to be generated. (int)
-    :return: Generator that yields seat letters.
+    :param amount: int - amount of seat letters to be generated.
+    :return: generator - generator that yields seat letters.
 
     Seat letters are generated with each row having 4 seats.
     These should be sorted from low to high.
@@ -19,8 +19,8 @@ def generate_seat_letters(amount):
 def generate_seats(amount):
     """ Generate a series of seat numbers for airline boarding.
 
-    :param amount: Amount of seats to be generated. (int)
-    :return: Generator that yields seat numbers.
+    :param amount: int - Amount of seats to be generated.
+    :return: generator - generator that yields seat numbers.
 
     There should be no row 13
 
@@ -36,8 +36,8 @@ def generate_seats(amount):
 def assign_seats(passengers):
     """ Assign seats to passengers.
 
-    :param passengers: A list of strings containing names of passengers. (list[str])
-    :return: A dictionary type object containing the names of the passengers as keys and seat numbers as values.
+    :param passengers: list[str] - A list of strings containing names of passengers.
+    :return: dict - with the names of the passengers as keys and seat numbers as values.
 
     Example output: {"Foo": "1A", "Bar": "1B"}
 
@@ -48,8 +48,8 @@ def assign_seats(passengers):
 def generate_codes(seat_numbers, flight_id):
     """Generate codes for a ticket.
 
-    :param seat_numbers: A list of seat numbers. (list[str])
-    :param flight_id: A string containing the flight identification. (str)
-    :return: Generator that generates 12 character long strings. (generator[str])
+    :param seat_numbers: list[str] - list of seat numbers.
+    :param flight_id: str - string containing the flight identification.
+    :return: generator - generator that yields 12 character long strings.
 
     """
