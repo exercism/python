90efc4cdbaa2334e0bb3bea22c8d834825dbddf8
BethanyG <BethanyG@users.noreply.github.com>
2023-02-24 23:40:55 -0800

    Modified error messages for hello world to be more direct and clearer.

--- exercises/practice/hello-world/hello_world_test.py
+++ exercises/practice/hello-world/hello_world_test.py
@@ -1,16 +1,26 @@
 import unittest
 
-from hello_world import (
-    hello,
-)
+try:
+    from hello_world import (
+        hello,
+    )
 
-# Tests adapted from `problem-specifications//canonical-data.json`
+except ImportError as import_fail:
+    message = import_fail.args[0].split("(", maxsplit=1)
+    item_name = import_fail.args[0].split()[3]
+
+    item_name = item_name[:-1] + "()'"
+
+    # pylint: disable=raise-missing-from
+    raise ImportError(
+        "\n\nMISSING FUNCTION --> In your 'hello_world.py' file, we can not find or import the"
+        f" function named {item_name}. \nThe tests for this first exercise expect a function that"
+        f' returns the string "Hello, World!"'
+        f'\n\nDid you use print("Hello, World!") instead?'
+    ) from None
 
 
 class HelloWorldTest(unittest.TestCase):
     def test_say_hi(self):
-        self.assertEqual(hello(), "Hello, World!")
-
-
-if __name__ == "__main__":
-    unittest.main()
+        msg = "\n\nThis test expects a return of the string 'Hello, World!' \nDid you use print('Hello, World!') by mistake?"
+        self.assertEqual(hello(), "Hello, World!", msg=msg)
