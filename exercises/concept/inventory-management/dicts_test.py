7c23bdf784352e9468ee9af6027041071a830fca
BethanyG <BethanyG@users.noreply.github.com>
2023-11-06 22:02:57 -0800

    Updated tests error messages and reformatted hints. (#3539)
    [no important files changed]

--- exercises/concept/inventory-management/dicts_test.py
+++ exercises/concept/inventory-management/dicts_test.py
@@ -1,62 +1,107 @@
 import unittest
 import pytest
-from dicts import create_inventory, add_items, decrement_items, remove_item, list_inventory
+from dicts import (create_inventory,
+                   add_items,
+                   decrement_items,
+                   remove_item,
+                   list_inventory)
 
 
 class InventoryTest(unittest.TestCase):
 
     @pytest.mark.task(taskno=1)
     def test_create_inventory(self):
-        self.assertEqual(create_inventory(["wood", "iron", "iron", "diamond", "diamond"]),
-                         {"wood": 1, "iron": 2, "diamond": 2})
+
+        actual_result = create_inventory(["wood", "iron", "iron", "diamond", "diamond"])
+        expected = {"wood": 1, "iron": 2, "diamond": 2}
+        error_message = ('Called create_inventory(["wood", "iron", "iron", "diamond", "diamond"]). '
+                         f'The function returned {actual_result}, but the tests expected {expected}.')
+
+        self.assertEqual(actual_result, expected, msg=error_message)
 
     @pytest.mark.task(taskno=2)
     def test_add_one_item(self):
-        self.assertEqual(add_items({"wood": 4, "iron": 2}, ["iron", "iron"]),
-                         {"wood": 4, "iron": 4})
+        actual_result = add_items({"wood": 4, "iron": 2}, ["iron", "iron"])
+        expected = {"wood": 4, "iron": 4}
+        error_message = ('Called add_items({"wood": 4, "iron": 2}, ["iron", "iron"]). '
+                         f'The function returned {actual_result}, but the tests expected {expected}.')
+
+        self.assertEqual(actual_result, expected, msg=error_message)
 
     @pytest.mark.task(taskno=2)
     def test_add_multiple_items(self):
-        self.assertEqual(add_items({"wood": 2, "gold": 1, "diamond": 3}, ["wood", "gold", "gold"]),
-                         {"wood": 3, "gold": 3, "diamond": 3})
+        actual_result = add_items({"wood": 2, "gold": 1, "diamond": 3}, ["wood", "gold", "gold"])
+        expected =  {"wood": 3, "gold": 3, "diamond": 3}
+        error_message = ('Called add_items({"wood": 2, "gold": 1, "diamond": 3}, ["wood", "gold", "gold"]). '
+                         f'The function returned {actual_result}, but the tests expected {expected}.')
+
+        self.assertEqual(actual_result, expected, msg=error_message)
 
     @pytest.mark.task(taskno=2)
     def test_add_new_item(self):
-        self.assertEqual(add_items({"iron": 1, "diamond": 2}, ["iron", "wood", "wood"]),
-                         {"iron": 2, "diamond": 2, "wood": 2})
+        actual_result = add_items({"iron": 1, "diamond": 2}, ["iron", "wood", "wood"])
+        expected = {"iron": 2, "diamond": 2, "wood": 2}
+        error_message = ('Called add_items({"iron": 1, "diamond": 2}, ["iron", "wood", "wood"]). '
+                         f'The function returned {actual_result}, but the tests expected {expected}.')
+
+        self.assertEqual(actual_result, expected, msg=error_message)
 
     @pytest.mark.task(taskno=2)
     def test_add_from_empty_dict(self):
-        self.assertEqual(add_items({}, ["iron", "iron", "diamond"]),
-                         {"iron": 2, "diamond": 1})
+        actual_result = add_items({}, ["iron", "iron", "diamond"])
+        expected = {"iron": 2, "diamond": 1}
+        error_message = ('Called add_items({}, ["iron", "iron", "diamond"]). '
+                         f'The function returned {actual_result}, but the tests expected {expected}.')
+
+        self.assertEqual(actual_result, expected, msg=error_message)
 
     @pytest.mark.task(taskno=3)
     def test_decrement_items(self):
-        self.assertEqual(decrement_items({"iron": 3, "diamond": 4, "gold": 2},
-                                      ["iron", "iron", "diamond", "gold", "gold"]),
-                         {"iron": 1, "diamond": 3, "gold": 0})
+        actual_result = decrement_items({"iron": 3, "diamond": 4, "gold": 2},
+                                        ["iron", "iron", "diamond", "gold", "gold"])
+        expected = {"iron": 1, "diamond": 3, "gold": 0}
+        error_message = ('Called decrement_items({"iron": 3, "diamond": 4, "gold": 2},'
+                         '["iron", "iron", "diamond", "gold", "gold"]). The function '
+                         f'returned {actual_result}, but the tests expected {expected}.')
+
+        self.assertEqual(actual_result, expected, msg=error_message)
 
     @pytest.mark.task(taskno=3)
     def test_not_below_zero(self):
-        self.assertEqual(decrement_items({"wood": 2, "iron": 3, "diamond": 1},
-                                      ["wood", "wood", "wood", "iron", "diamond", "diamond"]),
-                         {"wood": 0, "iron": 2, "diamond": 0})
+        actual_result = decrement_items({"wood": 2, "iron": 3, "diamond": 1},
+                                      ["wood", "wood", "wood", "iron", "diamond", "diamond"])
+        expected = {"wood": 0, "iron": 2, "diamond": 0}
+        error_message = ('Called decrement_items({"wood": 2, "iron": 3, "diamond": 1}, '
+                         '["wood", "wood", "wood", "iron", "diamond", "diamond"]). The '
+                         f'function returned {actual_result}, but the tests expected {expected}.')
+
+        self.assertEqual(actual_result, expected, msg=error_message)
 
     @pytest.mark.task(taskno=4)
     def test_remove_item(self):
-        self.assertEqual(remove_item({"iron": 1, "diamond": 2, "gold": 1}, "diamond"),
-                         {"iron": 1, "gold": 1})
+        actual_result = remove_item({"iron": 1, "diamond": 2, "gold": 1}, "diamond")
+        expected =  {"iron": 1, "gold": 1}
+        error_message = ('Called remove_item({"iron": 1, "diamond": 2, "gold": 1}, "diamond"). '
+                         f'The function returned {actual_result}, but the tests expected {expected}.')
+
+        self.assertEqual(actual_result, expected, msg=error_message)
 
     @pytest.mark.task(taskno=4)
     def test_remove_item_not_in_inventory(self):
-        self.assertEqual(remove_item({"iron": 1, "diamond": 2, "gold": 1}, "wood"),
-                         {"iron": 1, "gold": 1, "diamond": 2})
+        actual_result = remove_item({"iron": 1, "diamond": 2, "gold": 1}, "wood")
+        expected = {"iron": 1, "gold": 1, "diamond": 2}
+        error_message = ('Called remove_item({"iron": 1, "diamond": 2, "gold": 1}, "wood"). '
+                         f'The function returned {actual_result}, '
+                         f'but the tests expected {expected}.')
+
+        self.assertEqual(actual_result, expected, msg=error_message)
 
     @pytest.mark.task(taskno=5)
     def test_list_inventory(self):
-        self.assertEqual(list_inventory({"coal": 15, "diamond": 3, "wood": 67, "silver": 0}),
-                         [("coal", 15), ("diamond", 3), ("wood", 67)])
+        actual_result = list_inventory({"coal": 15, "diamond": 3, "wood": 67, "silver": 0})
+        expected = [("coal", 15), ("diamond", 3), ("wood", 67)]
+        error_message = ('Called list_inventory({"coal": 15, "diamond": 3, "wood": 67, "silver": 0}). '
+                         f'The function returned {actual_result}, '
+                         f'but the tests expected {expected}.')
 
-
-if __name__ == "__main__":
-    unittest.main()
+        self.assertEqual(actual_result, expected, msg=error_message)
