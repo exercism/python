c9c67a864fa5fbc0dbc6e711d7ee7b0e41bdc5f8
BethanyG <BethanyG@users.noreply.github.com>
2023-12-27 05:07:37 -0800

    Synced exercise tests to problem specifications. (#3578)

--- exercises/practice/knapsack/knapsack_test.py
+++ exercises/practice/knapsack/knapsack_test.py
@@ -1,6 +1,6 @@
 # These tests are auto-generated with test data from:
 # https://github.com/exercism/problem-specifications/tree/main/exercises/knapsack/canonical-data.json
-# File last updated on 2023-07-19
+# File last updated on 2023-12-27
 
 import unittest
 
