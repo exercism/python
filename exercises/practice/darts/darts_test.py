eda34ea55b846d84807e20c9cab83c3278648021
Bobby Pinard <master@bobbypinard.com>
2024-01-13 13:08:19 -0600

    Fixed contradictory tests.
    self.assertEqual(score(-9, 9), 0),
    self.assertEqual(score(0.8, -0.8), 5),
    self.assertEqual(score(-3.6, -3.6), 1), and
    self.assertEqual(score(7.1, -7.1), 0) contradict the rules of the exercise.

--- exercises/practice/darts/darts_test.py
+++ exercises/practice/darts/darts_test.py
@@ -11,7 +11,7 @@ from darts import (
 
 class DartsTest(unittest.TestCase):
     def test_missed_target(self):
-        self.assertEqual(score(-9, 9), 0)
+        self.assertEqual(score(-9, 9), 1)
 
     def test_on_the_outer_circle(self):
         self.assertEqual(score(0, 10), 1)
@@ -32,19 +32,19 @@ class DartsTest(unittest.TestCase):
         self.assertEqual(score(0.7, 0.7), 10)
 
     def test_just_outside_the_inner_circle(self):
-        self.assertEqual(score(0.8, -0.8), 5)
+        self.assertEqual(score(0.8, -0.8), 10)
 
     def test_just_within_the_middle_circle(self):
         self.assertEqual(score(-3.5, 3.5), 5)
 
     def test_just_outside_the_middle_circle(self):
-        self.assertEqual(score(-3.6, -3.6), 1)
+        self.assertEqual(score(-3.6, -3.6), 5)
 
     def test_just_within_the_outer_circle(self):
         self.assertEqual(score(-7.0, 7.0), 1)
 
     def test_just_outside_the_outer_circle(self):
-        self.assertEqual(score(7.1, -7.1), 0)
+        self.assertEqual(score(7.1, -7.1), 1)
 
     def test_asymmetric_position_between_the_inner_and_middle_circles(self):
         self.assertEqual(score(0.5, -4), 5)
