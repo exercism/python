add19d94405cba83dac08cafbdae9b67d897d59a
BethanyG <BethanyG@users.noreply.github.com>
2024-02-28 07:49:29 -0800

    Removed accidental solution code from stub file.

--- exercises/practice/anagram/anagram.py
+++ exercises/practice/anagram/anagram.py
@@ -1,12 +1,2 @@
-# def find_anagrams(word, candidates):
-#     pass
-
 def find_anagrams(word, candidates):
-    return [candidate
-            for candidate in candidates
-            if _letters(candidate) == _letters(word)
-            if candidate.lower() != word.lower()]
-
-
-def _letters(word):
-    return sorted(word.lower())
+    pass
