5b550ec4258c91369a4953c388e3314979081fc5
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-01-25 22:06:59 -0800

    Fix test case formatting

--- exercises/practice/game-of-life/game_of_life_test.py
+++ exercises/practice/game-of-life/game_of_life_test.py
@@ -1,6 +1,6 @@
 # These tests are auto-generated with test data from:
 # https://github.com/exercism/problem-specifications/tree/main/exercises/game-of-life/canonical-data.json
-# File last updated on 2026-01-25
+# File last updated on 2026-01-26
 
 import unittest
 
@@ -11,126 +11,107 @@ from game_of_life import (
 
 class GameOfLifeTest(unittest.TestCase):
     def test_empty_matrix(self):
-        self.assertEqual(tick([]), [])
+        matrix = []
+        expected = []
+        self.assertEqual(tick(matrix), expected)
 
     def test_live_cells_with_zero_live_neighbors_die(self):
-        self.assertEqual(
-            tick(
-                [
-                    [0, 0, 0],
-                    [0, 1, 0],
-                    [0, 0, 0],
-                ]
-            ),
-            [
-                [0, 0, 0],
-                [0, 0, 0],
-                [0, 0, 0],
-            ],
-        )
+        matrix = [
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0],
+        ]
+        expected = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]
+        self.assertEqual(tick(matrix), expected)
 
     def test_live_cells_with_only_one_live_neighbor_die(self):
-        self.assertEqual(
-            tick(
-                [
-                    [0, 0, 0],
-                    [0, 1, 0],
-                    [0, 1, 0],
-                ]
-            ),
-            [
-                [0, 0, 0],
-                [0, 0, 0],
-                [0, 0, 0],
-            ],
-        )
+        matrix = [
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 1, 0],
+        ]
+        expected = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]
+        self.assertEqual(tick(matrix), expected)
 
     def test_live_cells_with_two_live_neighbors_stay_alive(self):
-        self.assertEqual(
-            tick(
-                [
-                    [1, 0, 1],
-                    [1, 0, 1],
-                    [1, 0, 1],
-                ]
-            ),
-            [
-                [0, 0, 0],
-                [1, 0, 1],
-                [0, 0, 0],
-            ],
-        )
+        matrix = [
+            [1, 0, 1],
+            [1, 0, 1],
+            [1, 0, 1],
+        ]
+        expected = [
+            [0, 0, 0],
+            [1, 0, 1],
+            [0, 0, 0],
+        ]
+        self.assertEqual(tick(matrix), expected)
 
     def test_live_cells_with_three_live_neighbors_stay_alive(self):
-        self.assertEqual(
-            tick(
-                [
-                    [0, 1, 0],
-                    [1, 0, 0],
-                    [1, 1, 0],
-                ]
-            ),
-            [
-                [0, 0, 0],
-                [1, 0, 0],
-                [1, 1, 0],
-            ],
-        )
+        matrix = [
+            [0, 1, 0],
+            [1, 0, 0],
+            [1, 1, 0],
+        ]
+        expected = [
+            [0, 0, 0],
+            [1, 0, 0],
+            [1, 1, 0],
+        ]
+        self.assertEqual(tick(matrix), expected)
 
     def test_dead_cells_with_three_live_neighbors_become_alive(self):
-        self.assertEqual(
-            tick(
-                [
-                    [1, 1, 0],
-                    [0, 0, 0],
-                    [1, 0, 0],
-                ]
-            ),
-            [
-                [0, 0, 0],
-                [1, 1, 0],
-                [0, 0, 0],
-            ],
-        )
+        matrix = [
+            [1, 1, 0],
+            [0, 0, 0],
+            [1, 0, 0],
+        ]
+        expected = [
+            [0, 0, 0],
+            [1, 1, 0],
+            [0, 0, 0],
+        ]
+        self.assertEqual(tick(matrix), expected)
 
     def test_live_cells_with_four_or_more_neighbors_die(self):
-        self.assertEqual(
-            tick(
-                [
-                    [1, 1, 1],
-                    [1, 1, 1],
-                    [1, 1, 1],
-                ]
-            ),
-            [
-                [1, 0, 1],
-                [0, 0, 0],
-                [1, 0, 1],
-            ],
-        )
+        matrix = [
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+        ]
+        expected = [
+            [1, 0, 1],
+            [0, 0, 0],
+            [1, 0, 1],
+        ]
+        self.assertEqual(tick(matrix), expected)
 
     def test_bigger_matrix(self):
-        self.assertEqual(
-            tick(
-                [
-                    [1, 1, 0, 1, 1, 0, 0, 0],
-                    [1, 0, 1, 1, 0, 0, 0, 0],
-                    [1, 1, 1, 0, 0, 1, 1, 1],
-                    [0, 0, 0, 0, 0, 1, 1, 0],
-                    [1, 0, 0, 0, 1, 1, 0, 0],
-                    [1, 1, 0, 0, 0, 1, 1, 1],
-                    [0, 0, 1, 0, 1, 0, 0, 1],
-                    [1, 0, 0, 0, 0, 0, 1, 1],
-                ]
-            ),
-            [
-                [1, 1, 0, 1, 1, 0, 0, 0],
-                [0, 0, 0, 0, 0, 1, 1, 0],
-                [1, 0, 1, 1, 1, 1, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 1],
-                [1, 1, 0, 0, 1, 0, 0, 1],
-                [1, 1, 0, 1, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 1, 1],
-            ],
-        )
+        matrix = [
+            [1, 1, 0, 1, 1, 0, 0, 0],
+            [1, 0, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 0, 0, 1, 1, 1],
+            [0, 0, 0, 0, 0, 1, 1, 0],
+            [1, 0, 0, 0, 1, 1, 0, 0],
+            [1, 1, 0, 0, 0, 1, 1, 1],
+            [0, 0, 1, 0, 1, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1, 1],
+        ]
+        expected = [
+            [1, 1, 0, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 1, 0],
+            [1, 0, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 0, 0, 1, 0, 0, 1],
+            [1, 1, 0, 1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 1],
+        ]
+        self.assertEqual(tick(matrix), expected)
