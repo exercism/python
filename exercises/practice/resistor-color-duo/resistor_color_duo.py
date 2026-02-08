8d5244ef5225b8314755a9023ef5913d6b404ed3
Guy Muhoza <127846730+Muhozgu@users.noreply.github.com>
2026-02-08 22:07:03 +0100

    Add functions to retrieve color indices and calculate value
    Implemented functions to get resistor color values and calculate total resistance.

--- exercises/practice/resistor-color-duo/resistor_color_duo.py
+++ exercises/practice/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,39 @@
+
+dict = {
+        "black": 0,
+        "brown": 1,
+        "red": 2,
+        "orange": 3,
+        "yellow": 4,
+        "green": 5,
+        "blue": 6,
+        "violet": 7,
+        "grey": 8,
+        "white": 9
+    }
+    
+
+def get_color1_index(color1): 
+    if color1 in dict:
+        for c in dict:
+            s = dict[color1]
+        return s
+    else:
+        return "The color doesn't exist."
+
+def get_color2_index(color2): 
+    if color2 in dict:
+        for c in dict:
+            s = dict[color2]
+        return s
+    else:
+        return "The color doesn't exist."
+        
 def value(colors):
-    pass
+    color1 = colors[0]
+    color2 = colors[1]
+    ind1 = str(get_color1_index(color1))
+    ind2 = str(get_color2_index(color2))
+    ind  = ind1 + ind2
+    value = int(ind)
+    return value
