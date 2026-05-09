b2f2172e7935804b19d77a89477169f149c979fe
BethanyG <BethanyG@users.noreply.github.com>
2026-05-09 13:19:14 -0700

    Updated docstrings in stub to follow Google Style. (#4147)

--- exercises/concept/ellens-alien-game/classes.py
+++ exercises/concept/ellens-alien-game/classes.py
@@ -4,22 +4,22 @@
 class Alien:
     """Create an Alien object with location x_coordinate and y_coordinate.
 
-    Attributes
-    ----------
-    (class)total_aliens_created: int
-    x_coordinate: int - Position on the x-axis.
-    y_coordinate: int - Position on the y-axis.
-    health: int - Number of health points.
+    Attributes:
+        (class) total_aliens_created (int): Total number of Alien instances.
+        x_coordinate (int):  Position on the x-axis.
+        y_coordinate (int): Position on the y-axis.
+        health (int):  Number of health points.
 
-    Methods
-    -------
-    hit(): Decrement Alien health by one point.
-    is_alive(): Return a boolean for if Alien is alive (if health is > 0).
-    teleport(new_x_coordinate, new_y_coordinate): Move Alien object to new coordinates.
-    collision_detection(other): Implementation TBD.
+    Methods:
+        hit(): Decrement Alien health by one point.
+        is_alive(): Return a boolean for if Alien is alive (if health is > 0).
+        teleport(new_x_coordinate, new_y_coordinate): Move Alien object to new coordinates.
+        collision_detection(other): Implementation TBD.
+
     """
 
     pass
 
 
-#TODO:  create the new_aliens_collection() function below to call your Alien class with a list of coordinates.
+#TODO (Student):  Create the new_aliens_collection() function below to call your Alien class with a list of coordinates
+
