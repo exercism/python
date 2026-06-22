5f5bdf33f47af68f7ff8d5d72f744b0dbace54d3
Sérgio Henrique <103338880+serghenr@users.noreply.github.com>
2026-06-22 19:29:48 -0300

    exercício anagram resolvido

--- exercises/practice/anagram/anagram.py
+++ exercises/practice/anagram/anagram.py
@@ -1,2 +1,7 @@
 def find_anagrams(word, candidates):
-    pass
+    resultado = []
+    
+    for anagrams in candidates:
+        if word.lower() != anagrams.lower() and sorted(anagrams.lower()) == sorted(word.lower()):
+            resultado.append(anagrams)
+    return resultado
